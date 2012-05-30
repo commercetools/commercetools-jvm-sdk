b0732f3302df7e0d356140e047ed466bd2fb5072
mkonicek <martin.konicek@gmail.com>
2012-05-30 20:20:33 +0200

    Categories are a tree.

--- sdk-java/app/sphere/model/products/Category.java
+++ sdk-java/app/sphere/model/products/Category.java
@@ -8,31 +8,71 @@ import sphere.util.ReadJson;
 
 import org.codehaus.jackson.type.TypeReference;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /** Category of product in the product catalog. */
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Category {
-    String id;
-    String version;
-    String name;
-    String description;
-    String parent;
+    private String id;
+    private String version;
+    private String name;
+    private String description;
+    private String parent;
+    private List<Category> children = new ArrayList<Category>();
+    
+    public String getReference() {
+        return "category:" + this.getID();
+    }
 
     // for JSON deserializer
     private Category() { }
 
     /** Queries for all categories. */
-    public static F.Promise<QueryResult<Category>> getAll() throws IOException {
+    public static F.Promise<QueryResult<Category>> getAll() {
         return WS.url("http://localhost:4242/bias/categories").get().map(
                 new ReadJson<QueryResult<Category>>(new TypeReference<QueryResult<Category>>() { })
         );
     }
 
     /** Gets a Category by id. */
-    public static F.Promise<Category> getByID(String id) {
-        return WS.url("http://localhost:4242/bias/categories/" + id).get().map(
-                new ReadJson<Category>(new TypeReference<Category>() { })
-        );
+    public static F.Promise<Category> getByID(final String id) {
+        return getSubtree(id).map(new F.Function<QueryResult<Category>, Category>() {
+            @Override
+            public Category apply(QueryResult<Category> qr) throws Throwable {
+                try {
+                    List<Category> all = qr.getResults();
+                    Category category = null;
+                    for(Category c: all) {
+                        if (c.getID().equals(id)) {
+                            category = c;
+                            break;
+                        }
+                    }
+                    return buildChildren(category, all);
+                } catch (Exception e) {
+                    // Workaround for exception swallowing bug in Play 2.0 - at least print the stack trace
+                    // (fixed in master, so this can be removed with the next release of Play)
+                    e.printStackTrace();
+                    throw e;
+                }
+            }
+        });
+    }
+
+    private static F.Promise<QueryResult<Category>> getSubtree(String id) {
+        return getAll();
+    } 
+    
+    private static Category buildChildren(Category root, List<Category> all) {
+        String rootRef = root.getReference();
+        for(Category c: all) {
+            if (c.getParent() != null && c.getParent().equals(rootRef)) {
+                root.children.add(c);
+                buildChildren(c, all);
+            }
+        }
+        return root;
     }
 
     /** Gets a Category by a reference. */
@@ -41,7 +81,7 @@ public class Category {
         return getByID(category.split(":")[1]);
     }
 
-    public String getId() { return id; }
+    public String getID() { return id; }
     public String getVersion() { return version; }
     /** Gets the name of this category. */
     public String getName() { return name; }
@@ -49,4 +89,6 @@ public class Category {
     public String getDescription() { return description; }
     /** Gets a reference to the parent category. */
     public String getParent() { return parent; }
+    /** Gets child categories of this category. */
+    public List<Category> getChildren() { return children; }
 }
