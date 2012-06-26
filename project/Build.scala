776aeb289fef0bf7a17da911ed7cda7adfb45998
mkonicek <martin.konicek@gmail.com>
2012-06-26 17:09:19 +0200

    [Build] Add XML test listener for Jenkins.

--- project/Build.scala
+++ project/Build.scala
@@ -1,6 +1,7 @@
 import sbt._
 import Keys._
 import PlayProject._
+import eu.henkelmann.sbt.JUnitXmlTestsListener
 
 object ApplicationBuild extends Build {
 
@@ -17,14 +18,156 @@ object ApplicationBuild extends Build {
   ).settings(
     organization := "de.commercetools",
     scalaVersion := "2.9.1",
-    libraryDependencies ++= Seq(Libs.commonsCodec, Libs.commonsIO)
-//  testOptions in Test := Seq(
-//    Tests.Argument(TestFrameworks.ScalaTest, "-l", "disabled integration"),
-//    Tests.Argument(TestFrameworks.ScalaTest, "-oD")) // show durations
+    libraryDependencies ++= Seq(Libs.commonsCodec, Libs.commonsIO, Libs.scalacheck, Libs.scalatest),
+    testListeners <<= target.map(t => Seq(new OriginalXmlTestsListener(t.getAbsolutePath))),
+    testOptions in Test := Seq(
+      Tests.Argument(TestFrameworks.ScalaTest, "-l", "disabled integration"),
+      Tests.Argument(TestFrameworks.ScalaTest, "-oD")) // show durations
   )
 }
 
 object Libs {
   lazy val commonsCodec = "commons-codec" % "commons-codec" % "1.5"
   lazy val commonsIO = "commons-io" % "commons-io" % "2.3"
+
+  lazy val scalatest = "org.scalatest" %% "scalatest" % "1.7.1" % "test"
+  lazy val scalacheck = "org.scala-tools.testing" %% "scalacheck" % "1.9" % "test"
+}
+
+// To get around Play having its (not-working) JUnitXmlTestListener in the original eu.henkelmann package: http://bit.ly/MXrEmY
+// https://github.com/hydrasi/junit_xml_listener/blob/master/src/main/scala/eu/henkelmann/sbt/JUnitXmlTestsListener.scala
+/**
+ * A tests listener that outputs the results it receives in junit xml
+ * report format.
+ * @param outputDir path to the dir in which a folder with results is generated
+ */
+class OriginalXmlTestsListener(val outputDir:String) extends TestsListener
+{
+  import java.io.{StringWriter, PrintWriter, File}
+  import java.net.InetAddress
+  import scala.collection.mutable.ListBuffer
+  import scala.xml.{Elem, Node, XML}
+  import org.scalatools.testing.{Event => TEvent, Result => TResult, Logger => TLogger}
+
+  /**Current hostname so we know which machine executed the tests*/
+  val hostname = InetAddress.getLocalHost.getHostName
+  /**The dir in which we put all result files. Is equal to the given dir + "/test-reports"*/
+  val targetDir = new File(outputDir + "/test-reports/")
+
+  /**all system properties as XML*/
+  val properties =
+    <properties> {
+      val iter = System.getProperties.entrySet.iterator
+      val props:ListBuffer[Node] = new ListBuffer()
+      while (iter.hasNext) {
+        val next = iter.next
+        props += <property name={next.getKey.toString} value={next.getValue.toString} />
+      }
+      props
+      }
+    </properties>
+
+  /** Gathers data for one Test Suite. We map test groups to TestSuites.
+   * Each TestSuite gets its own output file.
+   */
+  class TestSuite(val name:String) {
+    val events:ListBuffer[TEvent] = new ListBuffer()
+    val start                     = System.currentTimeMillis
+    var end                       = System.currentTimeMillis
+
+    /**Adds one test result to this suite.*/
+    def addEvent(e:TEvent) = events += e
+
+    /** Returns a triplet with the number of errors, failures and the
+     * total numbers of tests in this suite.
+     */
+    def count():(Int, Int, Int) = {
+      var errors, failures = 0
+      for (e <- events) {
+        e.result match {
+          case TResult.Error   => errors +=1
+          case TResult.Failure => failures +=1
+          case _               =>
+        }
+      }
+      (errors, failures, events.size)
+    }
+
+    /** Stops the time measuring and emits the XML for
+     * All tests collected so far.
+     */
+    def stop():Elem = {
+      end = System.currentTimeMillis
+      val duration  = end - start
+
+      val (errors, failures, tests) = count()
+
+      val result = <testsuite hostname={hostname} name={name}
+                              tests={tests + ""} errors={errors + ""} failures={failures + ""}
+                              time={(duration/1000.0).toString} >
+        {properties}
+        {
+        for (e <- events) yield
+          <testcase classname={name} name={e.testName} time={"0.0"}> {
+            var trace:String = if (e.error!=null) {
+              val stringWriter = new StringWriter()
+              val writer = new PrintWriter(stringWriter)
+              e.error.printStackTrace(writer)
+              writer.flush()
+              stringWriter.toString
+            }
+            else {
+              ""
+            }
+            e.result match {
+              case TResult.Error   if (e.error!=null) => <error message={e.error.getMessage} type={e.error.getClass.getName}>{trace}</error>
+              case TResult.Error                      => <error message={"No Exception or message provided"} />
+              case TResult.Failure if (e.error!=null) => <failure message={e.error.getMessage} type={e.error.getClass.getName}>{trace}</failure>
+              case TResult.Failure                    => <failure message={"No Exception or message provided"} />
+              case TResult.Skipped                    => <skipped />
+              case _               => {}
+            }
+            }
+          </testcase>
+
+        }
+        <system-out><![CDATA[]]></system-out>
+        <system-err><![CDATA[]]></system-err>
+      </testsuite>
+
+      result
+    }
+  }
+
+  /**The currently running test suite*/
+  var testSuite:TestSuite = null
+
+  /**Creates the output Dir*/
+  override def doInit() = {targetDir.mkdirs()}
+
+  /** Starts a new, initially empty Suite with the given name.
+   */
+  override def startGroup(name: String) {testSuite = new TestSuite(name)}
+
+  /** Adds all details for the given even to the current suite.
+   */
+  override def testEvent(event: TestEvent): Unit = for (e <- event.detail) {testSuite.addEvent(e)}
+
+  override def endGroup(name: String, t: Throwable) = {
+    System.err.println("Throwable escaped the test run of '" + name + "': " + t)
+    t.printStackTrace(System.err)
+  }
+
+  /** Ends the current suite, wraps up the result and writes it to an XML file
+   *  in the output folder that is named after the suite.
+   */
+  override def endGroup(name: String, result: TestResult.Value) = {
+    XML.save (new File(targetDir, testSuite.name + ".xml").getAbsolutePath, testSuite.stop(), "UTF-8", true, null)
+  }
+
+  /**Does nothing, as we write each file after a suite is done.*/
+  override def doComplete(finalResult: TestResult.Value): Unit = {}
+
+  /**Returns None*/
+  override def contentLogger(test: TestDefinition): Option[ContentLogger] = None
 }
--- sphere-sdk-java/test/ConfigSpec.scala
+++ sphere-sdk-java/test/ConfigSpec.scala
@@ -7,21 +7,23 @@ import sphere.Config
 
 class ConfigSpec extends Specification {
 
-  "Read config" in {
-    val config = Map(
-      "sphere.auth" -> "http://localhost:7777",
-      "sphere.core" -> "configDoesNotValidateURLs",
-      "sphere.clientID" -> "client1",
-      "sphere.project" -> "%7/",
-      "unused" -> "unused")
-    running(FakeApplication(additionalConfiguration = config)) {
-      Config.authEndpoint must be ("http://localhost:7777")
-      Config.coreEndpoint must be ("configDoesNotValidateURLs")
-      Config.projectID must throwAn[Exception].like { case e =>
-        e.getMessage must equalTo("Configuration error [Invalid project name '%7/'. Project name can only contain letters, numbers, dashes and underscores.]")}
-      Config.clientSecret must throwAn[Exception].like { case e =>
-        e.getMessage must equalTo("Configuration error [Path sphere.clientSecret not found in configuration.]")}
-      Config.clientID must be ("client1")
+  "ConfigTest" should {
+    "Read config Test" in {
+      val config = Map(
+        "sphere.auth" -> "http://localhost:7777",
+        "sphere.core" -> "configDoesNotValidateURLs",
+        "sphere.clientID" -> "client1",
+        "sphere.project" -> "%7/",
+        "unused" -> "unused")
+      running(FakeApplication(additionalConfiguration = config)) {
+        Config.authEndpoint must be ("http://localhost:7777")
+        Config.coreEndpoint must be ("configDoesNotValidateURLs")
+        Config.projectID must throwAn[Exception].like { case e =>
+          e.getMessage must equalTo("Configuration error [Invalid project name '%7/'. Project name can only contain letters, numbers, dashes and underscores.]")}
+        Config.clientSecret must throwAn[Exception].like { case e =>
+          e.getMessage must equalTo("Configuration error [Path sphere.clientSecret not found in configuration.]")}
+        Config.clientID must be ("client1")
+      }
     }
   }
 }
