97f81cc8f8e61299c6db8a07ec77e600caa1e1bc
mkonicek <martin.konicek@gmail.com>
2012-07-10 17:07:23 +0200

    [SDK ]Improve packaging script.

--- install/package-sdk-here.sh
+++ install/package-sdk-here.sh
@@ -4,22 +4,26 @@ echo "Pack SDK: Package sphere sdk into a zipfile (sphere-sdk.zip) into current 
 
 INSTALL_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ROOT_PATH=$INSTALL_PATH/..
-TEMP_SDK_DIR="_sphere-sdk"
+TEMP_WORK_DIR="_sphere-sdk"
 SAMPLE_STORE="sample-store-java"
 SAMPLE_NAME="sphere-quickstart"
-ZIP_NAME="sphere-sdk.zip"
+ZIP_NAME="sphere-sdk"
+
+# Work in a temp subdirectory
+mkdir $TEMP_WORK_DIR
+cd $TEMP_WORK_DIR
 
 # Copy to current directory
-echo "Pack SDK: Copying store template '$ROOT_PATH/$SAMPLE_STORE' to '`pwd`/$TEMP_SDK_DIR'"
-mkdir -p $TEMP_SDK_DIR/templates
-cp -r $ROOT_PATH/$SAMPLE_STORE $TEMP_SDK_DIR/templates/
-mv $TEMP_SDK_DIR/templates/$SAMPLE_STORE $TEMP_SDK_DIR/templates/$SAMPLE_NAME
+echo "Copying store template '$ROOT_PATH/$SAMPLE_STORE' to '`pwd`/$ZIP_NAME'"
+mkdir -p $ZIP_NAME/templates
+cp -r $ROOT_PATH/$SAMPLE_STORE $ZIP_NAME/templates/
+mv $ZIP_NAME/templates/$SAMPLE_STORE $ZIP_NAME/templates/$SAMPLE_NAME
 
-echo "Pack SDK: Enabling sample build file by renaming '_project' to 'project'."
-mv $TEMP_SDK_DIR/templates/$SAMPLE_NAME/_project $TEMP_SDK_DIR/templates/$SAMPLE_NAME/project
+echo "Enabling sample build file by renaming '_project' to 'project'."
+mv $ZIP_NAME/templates/$SAMPLE_NAME/_project $ZIP_NAME/templates/$SAMPLE_NAME/project
 
-echo "Pack SDK: Cleaning up the template a bit."
-pushd $TEMP_SDK_DIR/templates/$SAMPLE_NAME/ > /dev/null # quiet
+echo "Cleaning up the template a bit."
+pushd $ZIP_NAME/templates/$SAMPLE_NAME/ > /dev/null # quiet
 rm -r .idea
 rm -r .DS_Store
 rm -r logs
@@ -27,13 +31,15 @@ rm -r packaging
 rm -r target
 popd > /dev/null # quiet
 
-echo "Pack SDK: Copying sphere script '$ROOT_PATH/install/sphere' to '$TEMP_SDK_DIR'."
-cp $ROOT_PATH/install/sphere $TEMP_SDK_DIR/
+echo "Copying sphere script '$ROOT_PATH/install/sphere' to '$ZIP_NAME'."
+cp $ROOT_PATH/install/sphere $ZIP_NAME/
 
-echo "Pack SDK: Zipping everything in the temporary directory."
-zip -r --quiet $ZIP_NAME $TEMP_SDK_DIR/
+echo "Zipping everything in the temporary directory."
+zip -r --quiet $ZIP_NAME $ZIP_NAME/
+mv $ZIP_NAME.zip ../
 
-echo "Pack SDK: Cleaning up the temporary directory."
-rm -r $TEMP_SDK_DIR
+cd ..
+echo "Cleaning up the temporary directory."
+rm -r $TEMP_WORK_DIR
 
-echo "Pack SDK: Done: `pwd`/$ZIP_NAME"
+echo "Pack SDK: Done: `pwd`/$ZIP_NAME.zip. Have fun!"
