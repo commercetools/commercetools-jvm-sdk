28241b2c54e829adecf8f313382b04b92a965e0d
mkonicek <martin.konicek@gmail.com>
2012-07-11 15:04:33 +0200

    SDK's 'sphere quickstart' bootstraps a sample project on the backend.

--- install/package-sdk-here.sh
+++ install/package-sdk-here.sh
@@ -9,6 +9,8 @@ ROOT_PATH=$INSTALL_PATH/..
 TEMP_WORK_DIR="_sphere-sdk"
 SAMPLE_STORE="sample-store-java"
 SAMPLE_NAME="sphere-quickstart"
+PROJECT_TEMPLATE="_project"
+CONF_TEMPLATE="application.conf.template"
 
 # Work in a temp subdirectory
 mkdir $TEMP_WORK_DIR
@@ -20,8 +22,11 @@ mkdir -p $ZIP_NAME/templates
 cp -r $ROOT_PATH/$SAMPLE_STORE $ZIP_NAME/templates/
 mv $ZIP_NAME/templates/$SAMPLE_STORE $ZIP_NAME/templates/$SAMPLE_NAME
 
-echo "  - Enabling template's build file by renaming '_project' to 'project'."
-mv $ZIP_NAME/templates/$SAMPLE_NAME/_project $ZIP_NAME/templates/$SAMPLE_NAME/project
+echo "  - Enabling template's build file by renaming '$PROJECT_TEMPLATE' to 'project'."
+mv $ZIP_NAME/templates/$SAMPLE_NAME/$PROJECT_TEMPLATE $ZIP_NAME/templates/$SAMPLE_NAME/project
+
+echo "  - Enabling template's config file by renaming '$CONF_TEMPLATE' to 'application.conf'."
+mv $ZIP_NAME/templates/$SAMPLE_NAME/conf/$CONF_TEMPLATE $ZIP_NAME/templates/$SAMPLE_NAME/conf/application.conf
 
 echo "  - Cleaning up the template a bit."
 pushd $ZIP_NAME/templates/$SAMPLE_NAME/ > /dev/null # quiet
