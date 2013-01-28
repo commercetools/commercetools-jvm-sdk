e5396e3796650536f9d16a7d8ed73c170452cc5b
Laura Luiz <laura.luiz@commercetools.de>
2013-01-28 11:48:33 +0100

    Improve shellscript generating javadoc

--- build-javadoc.sh
+++ build-javadoc.sh
@@ -1,17 +1,57 @@
 #!/bin/bash
 
+usage() {
+    echo "$(basename $0) - Generates the SDK and Java Client Javadoc in the specified path."
+    echo ""
+    echo "Arguments:"
+    echo "-o <output path>: output path where generated Javadoc is placed - default 'target/doc'"
+    echo "-d: do something - otherwise nothing is done."
+}
+
+update_lib() {
+    sbt clean update
+    LIB_DIR=$(mktemp -d)
+    trap "rm -rf '${LIB_DIR}'" EXIT
+    find ~/.ivy2/cache -name '*.jar' -exec cp {} "${LIB_DIR}" \;
+}
+
+clean_output() {
+    rm -rf "${DOC_DIR}"
+    mkdir -p "${DOC_DIR}"
+}
+
+create_doc() {
+    javadoc -d "${DOC_DIR}" -classpath "$LIB_DIR/*" -use -sourcepath sphere-java-client/src/main/java:sphere-sdk-java/app -subpackages de.commercetools.sphere -subpackages sphere
+}
+
 set -e
 
-DOC_DIR=target/doc
-LIB_DIR=target/lib
+while getopts "c:d" OPT; do
+    case "${OPT}" in
+        c)
+            DOC_DIR="${OPTARG}"
+            ;;
+        d)
+            readonly DO_IT="true"
+            ;;
+        r)
+    esac
+done
 
-sbt clean update
-mkdir -p "$DOC_DIR" "$LIB_DIR"
-find ~/.ivy2/cache -name *.jar -exec cp {} "$LIB_DIR" \;
+if [ -z "${DOC_DIR}" ]; then
+    DOC_DIR="target/doc"
+fi
 
-javadoc -d "$DOC_DIR" -classpath "$LIB_DIR/*" -use -sourcepath sphere-java-client/src/main/java:sphere-sdk-java/app -subpackages de.commercetools.sphere -subpackages sphere
+if [ "${DO_IT}" != "true" ];then
+    usage
+    exit 1
+fi
+
+update_lib
+clean_output
+create_doc
 
 echo "===================================="
 echo "Done."
-echo "Find generated Javadoc in $DOC_DIR"
+echo "Find generated Javadoc in '${DOC_DIR}'"
 echo "===================================="
