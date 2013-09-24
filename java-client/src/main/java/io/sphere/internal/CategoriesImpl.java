5174fe1dfade16d49a5c0552e3bbfd21b1fb5062
Gregor Goldmann <gregor.goldmann@commercetools.de>
2013-09-24 10:35:01 +0200

    Always fetch categories sorted.

--- java-client/src/main/java/io/sphere/internal/CategoriesImpl.java
+++ java-client/src/main/java/io/sphere/internal/CategoriesImpl.java
@@ -1,12 +1,12 @@
 package io.sphere.internal;
 
-import com.google.common.base.Optional;
-import io.sphere.client.model.products.BackendCategory;
-import io.sphere.client.shop.ApiMode;
-import io.sphere.internal.request.RequestFactory;
 import io.sphere.client.ProjectEndpoints;
 import io.sphere.client.QueryRequest;
 import io.sphere.client.model.QueryResult;
+import io.sphere.client.model.products.BackendCategory;
+import io.sphere.client.shop.ApiMode;
+import io.sphere.internal.request.RequestFactory;
+import com.google.common.base.Optional;
 import net.jcip.annotations.Immutable;
 import org.codehaus.jackson.type.TypeReference;
 
@@ -22,7 +22,7 @@ public final class CategoriesImpl extends ProjectScopedAPI implements Categories
     /** Queries all categories. */
     public QueryRequest<BackendCategory> all() {
         return requestFactory.createQueryRequest(
-                endpoints.categories.root(),
+                endpoints.categories.allCategoriesSorted(),
                 Optional.<ApiMode>absent(),
                 new TypeReference<QueryResult<BackendCategory>>() {});
     }
