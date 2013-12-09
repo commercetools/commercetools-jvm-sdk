9c1149572cf22908489714e81f0d7e0c4f4177e6
schleichardt <michael.schleichardt@commercetools.de>
2013-12-09 13:06:38 +0100

    Expose read methods from inventory API to Play SDK

--- play-sdk/app/sphere/InventoryService.java
+++ play-sdk/app/sphere/InventoryService.java
@@ -1,18 +1,13 @@
 package sphere;
 
-import io.sphere.client.SphereResult;
-import io.sphere.client.model.VersionedId;
 import io.sphere.client.shop.model.InventoryEntry;
-
-import io.sphere.client.shop.model.InventoryEntryUpdate;
-import play.libs.F.Promise;
 
 /**
  * Sphere HTTP API for retrieving product inventory information.
  */
 public interface InventoryService {
     FetchRequest<InventoryEntry> bySku(String sku);
+    FetchRequest<InventoryEntry> bySku(String sku, String channelId);
+    QueryRequest<InventoryEntry> queryBySku(String sku);
     QueryRequest<InventoryEntry> query();
-    InventoryEntry updateInventoryEntry(VersionedId id, InventoryEntryUpdate update);
-    Promise<SphereResult<InventoryEntry>> updateInventoryEntryAsync(VersionedId id, InventoryEntryUpdate update);
 }
