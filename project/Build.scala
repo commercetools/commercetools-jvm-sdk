6354115b3b2f007d79f021580a318b5bfe4a9a09
schleichardt <michael.schleichardt@commercetools.de>
2014-04-24 16:56:05 +0200

    Update to Google Guava to 17.

--- project/Build.scala
+++ project/Build.scala
@@ -161,7 +161,7 @@ public final class Version {
 
   object Libs {
     lazy val asyncHttpClient = "com.ning" % "async-http-client" % "1.7.16"
-    lazy val guava           = "com.google.guava" % "guava" % "14.0.1"
+    lazy val guava           = "com.google.guava" % "guava" % "17.0"
     lazy val findBugs = "com.google.code.findbugs" % "jsr305" % "2.0.3" //needed by guava
     lazy val jodaTime        = "joda-time" % "joda-time" % "2.2"
     lazy val jodaConvert     = "org.joda" % "joda-convert" % "1.3.1"
