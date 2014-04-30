3e4f7bbbbc5a1c093c140eba60d117e62750a9a6
schleichardt <michael.schleichardt@commercetools.de>
2014-04-30 16:04:19 +0200

    Add package for SPHERE.IO Java client.

---
+++ sphere-java-client/src/main/java/io/sphere/sdk/client/SphereJavaClient.java
@@ -0,0 +1,4 @@
+package io.sphere.sdk.client;
+
+public class SphereJavaClient {
+}
---
+++ sphere-java-client/src/test/scala/io/sphere/sdk/client/SphereJavaClientSpec.scala
@@ -0,0 +1,9 @@
+package io.sphere.sdk.client
+
+import org.scalatest._
+
+class SphereJavaClientSpec extends WordSpec with ShouldMatchers {
+
+  "SPHERE.IO Java client" must {
+  }
+}
