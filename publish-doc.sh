c92a815f99071015166284128cfaa42d27ae425e
schleichardt <michael.schleichardt@commercetools.de>
2014-06-04 09:54:18 +0200

    Remove git author name and email setting in publish-doc script.

--- publish-doc.sh
+++ publish-doc.sh
@@ -8,8 +8,6 @@ version=$(cat target/version.txt) && \
 cd target && \
 docDir=gh-pages/javadoc/${version} && \
 rm -rf gh-pages && \
-git config user.name "TravisCI" && \
-git config user.email "build@commercetools.com" && \
 git clone --depth=1 $git_path -b gh-pages gh-pages && \
 mkdir -p $docDir && \
 cp -r javaunidoc/ $docDir && \
