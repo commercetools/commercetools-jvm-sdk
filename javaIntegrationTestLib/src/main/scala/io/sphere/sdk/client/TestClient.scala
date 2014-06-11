cffa4d7cc630707a2d15ec2652b8c9807e5ee5a0
schleichardt <michael.schleichardt@commercetools.de>
2014-06-11 11:29:28 +0200

    Create test client that does not return futures.

---
+++ javaIntegrationTestLib/src/main/scala/io/sphere/sdk/client/TestClient.scala
@@ -0,0 +1,13 @@
+package io.sphere.sdk.client
+
+import com.google.common.base.Optional
+
+class TestClient(underlying: JavaClient) {
+  def execute[I, R](fetch: Fetch[I, R]): Optional[I] = underlying.execute(fetch).get()
+
+  def execute[I, R](query: Query[I, R]): PagedQueryResult[I] = underlying.execute(query).get()
+
+  def execute[T, V](command: Command[T, V]): T = underlying.execute(command).get()
+
+  def close() = underlying.close()
+}
