5c2bf89c2a8a23680a8372ee178bda1ec1f848e1
schleichardt <michael.schleichardt@commercetools.de>
2014-06-11 17:43:54 +0200

    Update dokumentation.

--- queries/src/main/java/io/sphere/sdk/queries/IsInPredicate.java
+++ queries/src/main/java/io/sphere/sdk/queries/IsInPredicate.java
@@ -3,11 +3,14 @@ package io.sphere.sdk.queries;
 import com.google.common.base.Joiner;
 import com.google.common.collect.Iterables;
 
-import java.util.List;
-
 public class IsInPredicate<T, V> extends QueryModelPredicate<T> {
     private final Iterable<V> values;
 
+    /**
+     * Creates a predicate which is a shortcut for multiple or statements.
+     * @param queryModel the parent model
+     * @param values possible values to query for, if it is for Strings, the may need to be escaped concerning double quotes.
+     */
     public IsInPredicate(final QueryModel<T> queryModel, final Iterable<V> values) {
         super(queryModel);
         if (Iterables.isEmpty(values)) {
