b7cc4f78c718f24ef299014a026101cf88556fe9
schleichardt <michael.schleichardt@commercetools.de>
2014-06-11 11:29:52 +0200

    StringQueryModel supports not equals.

--- queries/src/main/java/io/sphere/sdk/queries/StringQueryModel.java
+++ queries/src/main/java/io/sphere/sdk/queries/StringQueryModel.java
@@ -3,11 +3,15 @@ package io.sphere.sdk.queries;
 import com.google.common.base.Optional;
 
 public class StringQueryModel<T> extends QueryModelImpl<T> {
-    protected StringQueryModel(final Optional<? extends QueryModel<T>> parent, final String pathSegment) {
+    public StringQueryModel(final Optional<? extends QueryModel<T>> parent, final String pathSegment) {
         super(parent, pathSegment);
     }
 
     public Predicate<T> is(String s) {
         return new EqPredicate<>(this, s);
+    }
+
+    public Predicate<T> isNot(String s) {
+        return new NotEqPredicate<>(this, s);
     }
 }
