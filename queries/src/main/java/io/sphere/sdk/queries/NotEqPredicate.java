69acd145069b490e62ad2bbc6b3779a033d35182
schleichardt <michael.schleichardt@commercetools.de>
2014-06-11 11:28:59 +0200

    Create NotEqPredicate.

---
+++ queries/src/main/java/io/sphere/sdk/queries/NotEqPredicate.java
@@ -0,0 +1,22 @@
+package io.sphere.sdk.queries;
+
+public class NotEqPredicate<T, V> extends QueryModelPredicate<T> {
+    private final V value;
+
+    NotEqPredicate(QueryModel<T> queryModel, V value) {
+        super(queryModel);
+        this.value = value;
+    }
+
+    @Override
+    protected String render() {
+        return "<>\"" + value + '"';
+    }
+
+    @Override
+    public String toString() {
+        return "NotEqPredicate{" +
+                "value=" + value +
+                '}';
+    }
+}
