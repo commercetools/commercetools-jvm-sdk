e96b88fd2a65855f8cd41dedb9bbc8848b99802c
schleichardt <michael.schleichardt@commercetools.de>
2014-06-25 11:21:09 +0200

    ProductTypeQuery extends DefaultModelQuery.

--- product-types/src/main/java/io/sphere/sdk/producttypes/ProductTypeQuery.java
+++ product-types/src/main/java/io/sphere/sdk/producttypes/ProductTypeQuery.java
@@ -1,17 +1,17 @@
 package io.sphere.sdk.producttypes;
 
 import com.fasterxml.jackson.core.type.TypeReference;
+import io.sphere.sdk.queries.DefaultModelQuery;
 import io.sphere.sdk.queries.PagedQueryResult;
-import io.sphere.sdk.queries.QueryDslImpl;
 
-public class ProductTypeQuery extends QueryDslImpl<ProductType, ProductTypeImpl, ProductTypeQueryModel<ProductType>> {
+public class ProductTypeQuery extends DefaultModelQuery<ProductType, ProductTypeImpl, ProductTypeQueryModel<ProductType>> {
 
     public ProductTypeQuery() {
-        super("/product-types", QueryDslImpl.<ProductType, ProductTypeImpl>resultMapperOf(new TypeReference<PagedQueryResult<ProductTypeImpl>>(){
+        super("/product-types", new TypeReference<PagedQueryResult<ProductTypeImpl>>(){
             @Override
             public String toString() {
                 return "TypeReference<PagedQueryResult<ProductTypeImpl>>";
             }
-        }));
+        });
     }
 }
