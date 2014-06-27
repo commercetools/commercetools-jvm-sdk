5f0f110aa6b4f7028ce061190b21983984af7ecc
schleichardt <michael.schleichardt@commercetools.de>
2014-06-27 16:28:02 +0200

    Create Base class which has implementation for equals, hashCode and toString using reflection.

---
+++ common/src/main/java/io/sphere/sdk/models/Base.java
@@ -0,0 +1,22 @@
+package io.sphere.sdk.models;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
+public abstract class Base {
+    @Override
+    public boolean equals(Object o) {
+        return EqualsBuilder.reflectionEquals(this, o);
+    }
+
+    @Override
+    public int hashCode() {
+        return HashCodeBuilder.reflectionHashCode(this);
+    }
+
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this);
+    }
+}
