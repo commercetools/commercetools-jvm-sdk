8c26378001d292ef91860e45bcfdb95ce510d0ca
schleichardt <michael.schleichardt@commercetools.de>
2014-07-03 18:38:42 +0200

    Refactor test.

--- product-types/src/it/java/io/sphere/sdk/producttypes/ProductTypeIntegrationTest.java
+++ product-types/src/it/java/io/sphere/sdk/producttypes/ProductTypeIntegrationTest.java
@@ -12,13 +12,14 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.function.Consumer;
 
-import static io.sphere.sdk.test.SphereTestUtils.*;
+import static io.sphere.sdk.test.SphereTestUtils.en;
 import static org.fest.assertions.Assertions.assertThat;
 
 public final class ProductTypeIntegrationTest extends QueryIntegrationTest<ProductType> {
     public static final List<LocalizedEnumValue> LOCALIZED_ENUM_VALUES = Arrays.asList(LocalizedEnumValue.of("key1", en("value1")), LocalizedEnumValue.of("key2", en("value2")));
     public static final TextInputHint TEXT_INPUT_HINT = TextInputHint.MultiLine;
     public static final LocalizedString LABEL = en("label");
+    public static final List<PlainEnumValue> PLAIN_ENUM_VALUES = Arrays.asList(PlainEnumValue.of("key1", "value1"), PlainEnumValue.of("key2", "value2"));
 
     @Override
     protected ClientRequest<ProductType> deleteCommand(Versioned item) {
@@ -50,237 +51,140 @@ public final class ProductTypeIntegrationTest extends QueryIntegrationTest<Produ
         return ProductTypes.query().withPredicate(ProductTypeQueryModel.get().name().isOneOf(names));
     }
 
-    static abstract class AttributeTestCase {
-        private final String attributeName;
-        private final Class<? extends AttributeType> attributeTypeClass;
-
-        protected AttributeTestCase(final String attributeName, final Class<? extends AttributeType> attributeTypeClass) {
-            this.attributeName = attributeName;
-            this.attributeTypeClass = attributeTypeClass;
-        }
-
-        public final String attributeName() {
-            return attributeName;
-        }
-
-        abstract AttributeDefinition getAttributeDefinition();
-
-        public final Class<? extends AttributeType> getExpectedAttributeTypeClass() {
-            return attributeTypeClass;
-        }
-
-        public void furtherAttributeDefinitionAssertions(final AttributeDefinition attributeDefinition){
-
-        }
+    @Test
+    public void createTextAttribute() throws Exception {
+        executeTest(TextType.class, AttributeDefinitionBuilder.
+                ofText("text-attribute", LABEL, TEXT_INPUT_HINT).
+                attributeConstraint(AttributeConstraint.CombinationUnique).
+                searchable(false).
+                required(true).
+                build(), attributeDefinitionFromServer -> {
+            assertThat(attributeDefinitionFromServer.getIsRequired()).isTrue();
+            assertThat(attributeDefinitionFromServer.getAttributeConstraint()).isEqualTo(AttributeConstraint.CombinationUnique);
+            assertThat(attributeDefinitionFromServer.getIsSearchable()).isFalse();
+            assertThat(((TextAttributeDefinition) attributeDefinitionFromServer).getTextInputHint()).isEqualTo(TEXT_INPUT_HINT);
+        });
     }
 
-    Consumer<AttributeTestCase> attributeTestFunction = conf -> {
+    @Test
+    public void createLocalizedTextAttribute() throws Exception {
+        executeTest(LocalizedTextType.class, AttributeDefinitionBuilder.ofLocalizedText("localized-text-attribute", LABEL, TEXT_INPUT_HINT).
+                attributeConstraint(AttributeConstraint.CombinationUnique).
+                searchable(false).
+                required(true).
+                build(), attributeDefinition -> {
+            assertThat(attributeDefinition.getIsRequired()).isTrue();
+            assertThat(attributeDefinition.getAttributeConstraint()).isEqualTo(AttributeConstraint.CombinationUnique);
+            assertThat(attributeDefinition.getIsSearchable()).isFalse();
+            assertThat(((LocalizedTextAttributeDefinition) attributeDefinition).getTextInputHint()).isEqualTo(TEXT_INPUT_HINT);
+        });
+    }
+
+    @Test
+    public void createEnumAttribute() throws Exception {
+        executeTest(EnumType.class, AttributeDefinitionBuilder.
+                ofEnum("enum-attribute", LABEL, PLAIN_ENUM_VALUES).
+                build(), attributeDefinition -> {
+            final EnumType attributeType = (EnumType) attributeDefinition.getAttributeType();
+            assertThat(attributeType.getValues()).isEqualTo(PLAIN_ENUM_VALUES);
+        });
+    }
+
+    @Test
+    public void createLocalizedEnumAttribute() throws Exception {
+        executeTest(LocalizedEnumType.class, AttributeDefinitionBuilder.
+                ofLocalizedEnum("lenum-attribute", LABEL, LOCALIZED_ENUM_VALUES).
+                build());
+    }
+
+    @Test
+    public void createNumberAttribute() throws Exception {
+        executeTest(NumberType.class, AttributeDefinitionBuilder.
+                ofNumber("number-attribute", LABEL).
+                build());
+    }
+
+    @Test
+    public void createMoneyAttribute() throws Exception {
+        executeTest(MoneyType.class, AttributeDefinitionBuilder.
+                ofMoney("money-attribute", LABEL).
+                build());
+    }
+
+    @Test
+    public void createDateAttribute() throws Exception {
+        executeTest(DateType.class, AttributeDefinitionBuilder.
+                ofDate("date-attribute", LABEL).
+                build());
+    }
+
+    @Test
+    public void createTimeAttribute() throws Exception {
+        executeTest(TimeType.class, AttributeDefinitionBuilder.
+                ofTime("time-attribute", LABEL).
+                build());
+    }
+
+    @Test
+    public void createDateTimeAttribute() throws Exception {
+        executeTest(DateTimeType.class, AttributeDefinitionBuilder.
+                ofDateTime("datetime-attribute", LABEL).
+                build());
+    }
+
+    @Test
+    public void createBooleanAttribute() throws Exception {
+        executeTest(BooleanType.class, AttributeDefinitionBuilder.
+                ofBoolean("boolean-attribute", LABEL).
+                build());
+    }
+
+    @Test
+    public void createSetOfTextAttribute() throws Exception {
+        executeTest(SetType.class, AttributeDefinitionBuilder.
+                ofSet("set-of-text-attribute", LABEL, new TextType()).
+                build());
+    }
+
+    @Test
+    public void createSetOfLocalizedEnumAttribute() throws Exception {
+        executeTest(SetType.class, AttributeDefinitionBuilder.
+                ofSet("set-of-localized-enum-attribute", LABEL, new LocalizedEnumType(LOCALIZED_ENUM_VALUES)).
+                build(), attributeDefinitionFromServer -> {
+            final SetAttributeDefinition setAttributeDefinition = (SetAttributeDefinition) attributeDefinitionFromServer;
+            final SetType setType = setAttributeDefinition.getAttributeType();
+            final LocalizedEnumType elementType = (LocalizedEnumType) setType.getElementType();
+            assertThat(elementType.getValues()).isEqualTo(LOCALIZED_ENUM_VALUES);
+        });
+    }
+
+    private void executeTest(final Class<? extends AttributeType> attributeTypeClass, final AttributeDefinition attributeDefinition, final Consumer<AttributeDefinition> furtherAttributeDefinitionAssertions) {
         final String productTypeName = "product type name";
         final String productTypeDescription = "product type description";
-        final String attributeName = conf.attributeName();
+        final String attributeName = attributeDefinition.getName();
 
         cleanUpByName(productTypeName);
 
-
-        final AttributeDefinition textAttribute = conf.getAttributeDefinition();
-        final List<AttributeDefinition> attributes = Arrays.asList(textAttribute);
+        final List<AttributeDefinition> attributes = Arrays.asList(attributeDefinition);
 
         final ProductTypeCreateCommand command = new ProductTypeCreateCommand(NewProductType.of(productTypeName, productTypeDescription, attributes));
         final ProductType productType = client.execute(command);
         assertThat(productType.getName()).isEqualTo(productTypeName);
         assertThat(productType.getDescription()).isEqualTo(productTypeDescription);
         assertThat(productType.getAttributes()).hasSize(1);
-        final AttributeDefinition attributeDefinition = productType.getAttributes().get(0);
+        final AttributeDefinition fetchedAttributeDefinition = productType.getAttributes().get(0);
 
-        assertThat(attributeDefinition.getName()).isEqualTo(attributeName);
-        assertThat(attributeDefinition.getLabel()).isEqualTo(en("label"));
-        assertThat(attributeDefinition.getAttributeType()).isInstanceOf(conf.getExpectedAttributeTypeClass());
+        assertThat(fetchedAttributeDefinition.getName()).isEqualTo(attributeName);
+        assertThat(fetchedAttributeDefinition.getLabel()).isEqualTo(en("label"));
+        assertThat(fetchedAttributeDefinition.getAttributeType()).isInstanceOf(attributeTypeClass);
 
-        conf.furtherAttributeDefinitionAssertions(attributeDefinition);
+        furtherAttributeDefinitionAssertions.accept(fetchedAttributeDefinition);
 
         cleanUpByName(productTypeName);
-    };
-
-    @Test
-    public void createTextAttribute() throws Exception {
-        attributeTestFunction.accept(new AttributeTestCase("text-attribute", TextType.class) {
-            @Override
-            public AttributeDefinition getAttributeDefinition() {
-                return AttributeDefinitionBuilder.
-                        ofText(attributeName(), LABEL, TEXT_INPUT_HINT).
-                        attributeConstraint(AttributeConstraint.CombinationUnique).
-                        searchable(false).
-                        required(true).
-                        build();
-            }
-
-            @Override
-            public void furtherAttributeDefinitionAssertions(final AttributeDefinition attributeDefinition) {
-                assertThat(attributeDefinition.getIsRequired()).isTrue();
-                assertThat(attributeDefinition.getAttributeConstraint()).isEqualTo(AttributeConstraint.CombinationUnique);
-                assertThat(attributeDefinition.getIsSearchable()).isFalse();
-                assertThat(((TextAttributeDefinition) attributeDefinition).getTextInputHint()).isEqualTo(TEXT_INPUT_HINT);
-            }
-
-        });
     }
 
-    @Test
-    public void createLocalizedTextAttribute() throws Exception {
-        attributeTestFunction.accept(new AttributeTestCase("localized-text-attribute", LocalizedTextType.class) {
-
-            @Override
-            public AttributeDefinition getAttributeDefinition() {
-                return AttributeDefinitionBuilder.ofLocalizedText(attributeName(), LABEL, TEXT_INPUT_HINT).
-                        attributeConstraint(AttributeConstraint.CombinationUnique).
-                        searchable(false).
-                        required(true).
-                        build();
-            }
-
-            @Override
-            public void furtherAttributeDefinitionAssertions(final AttributeDefinition attributeDefinition) {
-                assertThat(attributeDefinition.getIsRequired()).isTrue();
-                assertThat(attributeDefinition.getAttributeConstraint()).isEqualTo(AttributeConstraint.CombinationUnique);
-                assertThat(attributeDefinition.getIsSearchable()).isFalse();
-                assertThat(((LocalizedTextAttributeDefinition) attributeDefinition).getTextInputHint()).isEqualTo(TEXT_INPUT_HINT);
-            }
-        });
-    }
-
-    @Test
-    public void createEnumAttribute() throws Exception {
-        final List<PlainEnumValue> values = Arrays.asList(PlainEnumValue.of("key1", "value1"), PlainEnumValue.of("key2", "value2"));
-        attributeTestFunction.accept(new AttributeTestCase("enum-attribute", EnumType.class) {
-
-            @Override
-            public AttributeDefinition getAttributeDefinition() {
-                return AttributeDefinitionBuilder.
-                        ofEnum(attributeName(), LABEL, values).
-                        build();
-            }
-
-            @Override
-            public void furtherAttributeDefinitionAssertions(final AttributeDefinition attributeDefinition) {
-                final EnumType attributeType = (EnumType) attributeDefinition.getAttributeType();
-                assertThat(attributeType.getValues()).isEqualTo(values);
-            }
-        });
-    }
-
-    @Test
-    public void createLocalizedEnumAttribute() throws Exception {
-        attributeTestFunction.accept(new AttributeTestCase("lenum-attribute", LocalizedEnumType.class) {
-            @Override
-            public AttributeDefinition getAttributeDefinition() {
-                return AttributeDefinitionBuilder.
-                        ofLocalizedEnum(attributeName(), LABEL, LOCALIZED_ENUM_VALUES).
-                        build();
-            }
-        });
-    }
-
-    @Test
-    public void createNumberAttribute() throws Exception {
-        attributeTestFunction.accept(new AttributeTestCase("number-attribute", NumberType.class) {
-            @Override
-            public AttributeDefinition getAttributeDefinition() {
-                return AttributeDefinitionBuilder.
-                        ofNumber(attributeName(), LABEL).
-                        build();
-            }
-        });
-    }
-
-    @Test
-    public void createMoneyAttribute() throws Exception {
-        attributeTestFunction.accept(new AttributeTestCase("money-attribute", MoneyType.class) {
-            @Override
-            public AttributeDefinition getAttributeDefinition() {
-                return AttributeDefinitionBuilder.
-                        ofMoney(attributeName(), LABEL).
-                        build();
-            }
-        });
-    }
-
-    @Test
-    public void createDateAttribute() throws Exception {
-        attributeTestFunction.accept(new AttributeTestCase("date-attribute", DateType.class) {
-            @Override
-            public AttributeDefinition getAttributeDefinition() {
-                return AttributeDefinitionBuilder.
-                        ofDate(attributeName(), LABEL).
-                        build();
-            }
-        });
-    }
-
-    @Test
-    public void createTimeAttribute() throws Exception {
-        attributeTestFunction.accept(new AttributeTestCase("time-attribute", TimeType.class) {
-            @Override
-            public AttributeDefinition getAttributeDefinition() {
-                return AttributeDefinitionBuilder.
-                        ofTime(attributeName(), LABEL).
-                        build();
-            }
-        });
-    }
-
-    @Test
-    public void createDateTimeAttribute() throws Exception {
-        attributeTestFunction.accept(new AttributeTestCase("datetime-attribute", DateTimeType.class) {
-            @Override
-            public AttributeDefinition getAttributeDefinition() {
-                return AttributeDefinitionBuilder.
-                        ofDateTime(attributeName(), LABEL).
-                        build();
-            }
-        });
-    }
-
-    @Test
-    public void createBooleanAttribute() throws Exception {
-        attributeTestFunction.accept(new AttributeTestCase("boolean-attribute", BooleanType.class) {
-            @Override
-            public AttributeDefinition getAttributeDefinition() {
-                return AttributeDefinitionBuilder.
-                        ofBoolean(attributeName(), LABEL).
-                        build();
-            }
-        });
-    }
-
-    @Test
-    public void createSetOfTextAttribute() throws Exception {
-        attributeTestFunction.accept(new AttributeTestCase("set-of-text-attribute", SetType.class) {
-            @Override
-            public AttributeDefinition getAttributeDefinition() {
-                return AttributeDefinitionBuilder.
-                        ofSet(attributeName(), LABEL, new TextType()).
-                        build();
-            }
-        });
-    }
-
-    @Test
-    public void createSetOfLocalizedEnumAttribute() throws Exception {
-        attributeTestFunction.accept(new AttributeTestCase("set-of-localized-enum-attribute", SetType.class) {
-            @Override
-            public AttributeDefinition getAttributeDefinition() {
-                return AttributeDefinitionBuilder.
-                        ofSet(attributeName(), LABEL, new LocalizedEnumType(LOCALIZED_ENUM_VALUES)).
-                        build();
-            }
-
-            @Override
-            public void furtherAttributeDefinitionAssertions(final AttributeDefinition attributeDefinition) {
-                final SetAttributeDefinition setAttributeDefinition = (SetAttributeDefinition) attributeDefinition;
-                final SetType setType = setAttributeDefinition.getAttributeType();
-                final LocalizedEnumType elementType = (LocalizedEnumType) setType.getElementType();
-                assertThat(elementType.getValues()).isEqualTo(LOCALIZED_ENUM_VALUES);
-            }
+    private void executeTest(final Class<? extends AttributeType> attributeTypeClass, final AttributeDefinition attributeDefinition) {
+        executeTest(attributeTypeClass, attributeDefinition, x -> {
         });
     }
 }
