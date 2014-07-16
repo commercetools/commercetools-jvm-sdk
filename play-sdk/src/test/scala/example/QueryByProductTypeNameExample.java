19aa0be4020b13deb0b0356a28721cd767b1b107
schleichardt <michael.schleichardt@commercetools.de>
2014-07-16 11:50:18 +0200

    Improve query example for product types #30, throw exceptions since missing product type or attribute is an error.

--- play-sdk/src/test/scala/example/QueryByProductTypeNameExample.java
+++ play-sdk/src/test/scala/example/QueryByProductTypeNameExample.java
@@ -1,15 +1,12 @@
 package example;
 
-import java.util.Optional;
 import io.sphere.sdk.producttypes.ProductType;
-import io.sphere.sdk.producttypes.attributes.AttributeDefinition;
 import io.sphere.sdk.producttypes.attributes.EnumAttributeDefinition;
 import io.sphere.sdk.producttypes.attributes.EnumType;
 import io.sphere.sdk.producttypes.attributes.PlainEnumValue;
 import io.sphere.sdk.queries.PagedQueryResult;
 import play.libs.F;
 
-import java.util.Collections;
 import java.util.List;
 
 public class QueryByProductTypeNameExample extends ExampleWithClient {
@@ -20,20 +17,16 @@ public class QueryByProductTypeNameExample extends ExampleWithClient {
     }
 
     private static List<PlainEnumValue> extractPossibleEnumValuesForSize(PagedQueryResult<ProductType> pagedQueryResult) {
-        Optional<ProductType> productTypeOption = pagedQueryResult.head();
-        final Optional<List<PlainEnumValue>> valuesOption = productTypeOption.map(productType -> extractSizeEnumValues(productType));
-        return valuesOption.orElse(Collections.<PlainEnumValue>emptyList());
+        ProductType productType = pagedQueryResult.
+                head().
+                orElseThrow(() -> new MissingProductTypeException());
+        EnumAttributeDefinition sizeAttribute = productType.
+                getAttribute("size", EnumAttributeDefinition.class).
+                orElseThrow(() -> new MissingAttributeException());
+        EnumType attributeType = sizeAttribute.getAttributeType();
+        return attributeType.getValues();
     }
 
-    private static List<PlainEnumValue> extractSizeEnumValues(ProductType productType) {
-        List<AttributeDefinition> attributes = productType.getAttributes();
-        java.util.Optional<EnumAttributeDefinition> sizeAttribute =
-                productType.getAttribute("size", EnumAttributeDefinition.class);
-        java.util.Optional<List<PlainEnumValue>> valuesOptional = sizeAttribute.map(enumAttribute -> {
-            EnumType attributeType = enumAttribute.getAttributeType();
-            List<PlainEnumValue> values = attributeType.getValues();
-            return values;
-        });
-        return valuesOptional.orElse(Collections.<PlainEnumValue>emptyList());
-    }
+    public static class MissingProductTypeException extends RuntimeException { }
+    public static class MissingAttributeException extends RuntimeException { }
 }
