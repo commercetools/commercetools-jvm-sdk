e27eaf291733864644982c96cc9ea8302b0ebe83
schleichardt <michael.schleichardt@commercetools.de>
2014-08-01 19:01:36 +0200

    Remove unused method where.

--- queries/src/main/java/io/sphere/sdk/queries/EmbeddedQueryModel.java
+++ queries/src/main/java/io/sphere/sdk/queries/EmbeddedQueryModel.java
@@ -25,10 +25,6 @@ public abstract class EmbeddedQueryModel<T, C> extends QueryModelImpl<T> {
         super(parent, pathSegment);
     }
 
-    public Predicate<T> where(Predicate<C> embeddedPredicate) {
-        return new EmbeddedPredicate<>(this, embeddedPredicate);
-    }
-
     protected LocalizedStringQuerySortingModel<T> localizedStringQueryModel(final String pathSegment) {
         return new LocalizedStringQuerySortingModel<T>(Optional.of(this), Optional.of(pathSegment));
     }
