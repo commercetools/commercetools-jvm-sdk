61debb3e862822857140170e572a3756329730b5
schleichardt <michael.schleichardt@commercetools.de>
2014-08-05 17:43:11 +0200

    Add improvement of assertion messages to release notes, close #69.

---
+++ play-sdk/src/main/java/io/sphere/sdk/meta/ReleaseNotes.java
@@ -0,0 +1,10 @@
+package io.sphere.sdk.meta;
+
+/**
+ * <h3>1.0.0-M2</h3>
+ * <ul>
+ *     <li>It is possible to overwrite the error messages of DefaultModelAssert, LocalizedStringAssert and ReferenceAssert.</li>
+ * </ul>
+ */
+public class ReleaseNotes {
+}
