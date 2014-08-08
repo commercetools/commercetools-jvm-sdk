aa98be8dcad80c87ccece6c9e096e4cc6882129e
schleichardt <michael.schleichardt@commercetools.de>
2014-08-08 13:56:58 +0200

    Update release notes for highlighting feature, close #74.

--- play-sdk/src/main/java/io/sphere/sdk/meta/ReleaseNotes.java
+++ play-sdk/src/main/java/io/sphere/sdk/meta/ReleaseNotes.java
@@ -5,6 +5,7 @@ package io.sphere.sdk.meta;
  * <ul>
  *     <li>It is possible to overwrite the error messages of DefaultModelAssert, LocalizedStringAssert and ReferenceAssert.</li>
  *     <li>Versioned contains a type parameter the class which inherits it to enable the compile to find copy and past errors.</li>
+ *     <li>Sorting query model methods have better support in the IDE (using interfaces instead of base classes highlights important methods in bold).</li>
  * </ul>
  */
 public class ReleaseNotes {
