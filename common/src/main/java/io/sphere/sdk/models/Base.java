24cfc00763415ed0fd06f6bd6eb8531e72318a5f
schleichardt <michael.schleichardt@commercetools.de>
2014-08-11 14:55:27 +0200

    Document base class.

--- common/src/main/java/io/sphere/sdk/models/Base.java
+++ common/src/main/java/io/sphere/sdk/models/Base.java
@@ -1,9 +1,14 @@
 package io.sphere.sdk.models;
 
+import io.sphere.sdk.annotations.Internal;
 import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 
+/**
+ * A base class which implements {@link Object#toString()}, {@link Object#hashCode()} and {@link Object#equals(Object)} by reflection.
+ */
+@Internal
 public abstract class Base {
     @Override
     public boolean equals(Object o) {
