ad9639bff5f81fc0423639ad5bb00ef39e36eceb
schleichardt <michael.schleichardt@commercetools.de>
2014-08-13 10:16:47 +0200

    Add getting started page.

---
+++ src/main/java/io/sphere/sdk/meta/GettingStarted.java
@@ -0,0 +1,19 @@
+package io.sphere.sdk.meta;
+
+import io.sphere.sdk.models.Base;
+
+/**
+
+
+
+
+
+
+
+
+
+ */
+public final class GettingStarted extends Base {
+    private GettingStarted() {
+    }
+}
