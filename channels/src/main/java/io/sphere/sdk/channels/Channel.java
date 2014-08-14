7662eb848ce3238b450fa1611dd3fec03ccaea84
schleichardt <michael.schleichardt@commercetools.de>
2014-08-14 20:23:57 +0200

    Document channels.

--- channels/src/main/java/io/sphere/sdk/channels/Channel.java
+++ channels/src/main/java/io/sphere/sdk/channels/Channel.java
@@ -2,6 +2,9 @@ package io.sphere.sdk.channels;
 
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 
+/**
+ * Channels represent a source or destination of different entities.
+ */
 @JsonDeserialize(as = ChannelImpl.class)
 public interface Channel {
 }
