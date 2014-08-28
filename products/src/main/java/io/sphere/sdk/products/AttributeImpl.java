2b2932b8005c4472f26f070bd871650ee9a439f5
schleichardt <michael.schleichardt@commercetools.de>
2014-08-28 17:04:34 +0200

    Fix attribute deserialization.

--- products/src/main/java/io/sphere/sdk/products/AttributeImpl.java
+++ products/src/main/java/io/sphere/sdk/products/AttributeImpl.java
@@ -2,6 +2,7 @@ package io.sphere.sdk.products;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import io.sphere.sdk.models.AttributeMapper;
 import io.sphere.sdk.models.Base;
 import io.sphere.sdk.models.exceptions.JsonParseException;
@@ -10,6 +11,8 @@ import static java.lang.String.format;
 
 class AttributeImpl extends Base implements Attribute {
     private final String name;
+
+    @JsonSerialize
     private final JsonNode value;
 
     @JsonCreator
