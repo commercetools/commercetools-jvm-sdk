b4671833beb4b690dc710532d84343ccffe91f94
schleichardt <michael.schleichardt@commercetools.de>
2014-09-26 13:24:51 +0200

    Moved attributes package into products SBT module and add missing simple attribute types.

--- products/src/main/java/io/sphere/sdk/attributes/TypeSafeAttributeAccess.java
+++ products/src/main/java/io/sphere/sdk/attributes/TypeSafeAttributeAccess.java
@@ -3,6 +3,9 @@ package io.sphere.sdk.attributes;
 import com.fasterxml.jackson.core.type.TypeReference;
 import io.sphere.sdk.models.*;
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.function.Predicate;
 
 import static io.sphere.sdk.models.TypeReferences.*;
@@ -17,24 +20,20 @@ public final class TypeSafeAttributeAccess<T> extends Base {
         this.canHandle = canHandle;
     }
 
-    private static <T> TypeSafeAttributeAccess<T> ofPrimitive(final TypeReference<T> typeReference, final Class<? extends AttributeDefinition> attributeDefinitionClass) {
-        return new TypeSafeAttributeAccess<>(AttributeMapper.of(typeReference), attributeDefinition -> attributeDefinitionClass.isAssignableFrom(attributeDefinition.getClass()));
-    }
-
-    public static TypeSafeAttributeAccess<LocalizedString> ofLocalizedString() {
-        return ofPrimitive(LocalizedString.typeReference(), LocalizedTextAttributeDefinition.class);
-    }
-
-    public static TypeSafeAttributeAccess<Money> ofMoney() {
-        return ofPrimitive(Money.typeReference(), MoneyAttributeDefinition.class);
+    public static TypeSafeAttributeAccess<Boolean> ofBoolean() {
+        return ofPrimitive(booleanTypeReference(), BooleanAttributeDefinition.class);
     }
 
     public static TypeSafeAttributeAccess<String> ofString() {
         return ofPrimitive(stringTypeReference(), TextAttributeDefinition.class);
     }
 
-    public static TypeSafeAttributeAccess<Double> ofDouble() {
-        return ofPrimitive(doubleTypeReference(), NumberAttributeDefinition.class);
+    public static TypeSafeAttributeAccess<String> ofText() {
+        return ofString();
+    }
+
+    public static TypeSafeAttributeAccess<LocalizedString> ofLocalizedString() {
+        return ofPrimitive(LocalizedString.typeReference(), LocalizedTextAttributeDefinition.class);
     }
 
     public static TypeSafeAttributeAccess<PlainEnumValue> ofPlainEnumValue() {
@@ -43,6 +42,26 @@ public final class TypeSafeAttributeAccess<T> extends Base {
 
     public static TypeSafeAttributeAccess<LocalizedEnumValue> ofLocalizedEnumValue() {
         return ofEnumLike(LocalizedEnumValue.typeReference(), LocalizedEnumAttributeDefinition.class);
+    }
+
+    public static TypeSafeAttributeAccess<Double> ofDouble() {
+        return ofPrimitive(doubleTypeReference(), NumberAttributeDefinition.class);
+    }
+
+    public static TypeSafeAttributeAccess<Money> ofMoney() {
+        return ofPrimitive(Money.typeReference(), MoneyAttributeDefinition.class);
+    }
+
+    public static TypeSafeAttributeAccess<LocalDate> ofDate() {
+        return ofPrimitive(localDateTypeReference(), DateAttributeDefinition.class);
+    }
+
+    public static TypeSafeAttributeAccess<LocalTime> ofTime() {
+        return ofPrimitive(localTimeTypeReference(), TimeAttributeDefinition.class);
+    }
+
+    public static TypeSafeAttributeAccess<Instant> ofDateTime() {
+        return ofPrimitive(instantTypeReference(), DateTimeAttributeDefinition.class);
     }
 
     public <M> AttributeGetterSetter<M, T> getterSetter(final String name) {
@@ -70,5 +89,9 @@ public final class TypeSafeAttributeAccess<T> extends Base {
         return new TypeSafeAttributeAccess<>(mapper, attributeDefinition ->
                 attributeDefinitionClass.isAssignableFrom(attributeDefinition.getClass())
         );
+    }
+
+    private static <T> TypeSafeAttributeAccess<T> ofPrimitive(final TypeReference<T> typeReference, final Class<? extends AttributeDefinition> attributeDefinitionClass) {
+        return new TypeSafeAttributeAccess<>(AttributeMapper.of(typeReference), attributeDefinition -> attributeDefinitionClass.isAssignableFrom(attributeDefinition.getClass()));
     }
 }
