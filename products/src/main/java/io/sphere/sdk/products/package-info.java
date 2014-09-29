479628dec0f2e6d07e30c3c873aa5ec355125493
schleichardt <michael.schleichardt@commercetools.de>
2014-09-29 14:22:14 +0200

    Document product deletion.

--- products/src/main/java/io/sphere/sdk/products/package-info.java
+++ products/src/main/java/io/sphere/sdk/products/package-info.java
@@ -1,19 +1,4 @@
 /**
   Products are the sellable goods in an e-commerce project on SPHERE.IO.
-
-
-
-  <h3 id="query-product">Query Products</h3>
-
-  Use {@link io.sphere.sdk.products.queries.ProductQueryModel} to query for products:
-
-  {@include.example io.sphere.sdk.suppliers.ByEnglishNameProductQuerySupplier}
-
-  <h3 id="update-product">Update a Product</h3>
-  <h3 id="delete-product">Delete a Product</h3>
-
-  Use {@link io.sphere.sdk.products.commands.ProductDeleteByIdCommand} to delete a product:
-
-  {@include.example io.sphere.sdk.products.ProductDeleteExample#delete()}
  */
 package io.sphere.sdk.products;
