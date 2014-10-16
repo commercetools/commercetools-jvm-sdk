b6cb9d4aed70b4fc6e9471395dd931a729996793
schleichardt <michael.schleichardt@commercetools.de>
2014-10-16 14:57:06 +0200

    Improve query API documentation.

--- src/main/java/io/sphere/sdk/meta/QueryDocumentation.java
+++ src/main/java/io/sphere/sdk/meta/QueryDocumentation.java
@@ -10,9 +10,9 @@ import io.sphere.sdk.models.Base;
  While the {@link io.sphere.sdk.queries.Query} interface just contains information to execute a query,
  the interface {@link io.sphere.sdk.queries.QueryDsl} provides also a domain specific language to tune a query.</p>
 
- <p>For most of the SPHERE.IO resources classes (in a package queries) exist to support you formulating valid queries.</p>
+ <p>For most of the SPHERE.IO resources you can find classes to support you formulating valid queries (in a sub package queries).</p>
 
- <p>The following snipped creates a query which selects all products not meaningful ordered.</p>
+ <p>The following snipped creates a query which selects all products without a specific order.</p>
 
  {@include.example io.sphere.sdk.meta.QueryDocumentationTest#queryForAllDemo()}
 
@@ -25,7 +25,7 @@ import io.sphere.sdk.models.Base;
  {@include.example io.sphere.sdk.meta.QueryDocumentationTest#queryBySlug()}
 
  You may have noticed that the type of the query is not {@link io.sphere.sdk.products.queries.ProductQuery} anymore but {@link io.sphere.sdk.queries.QueryDsl} which does not contain the method {@link io.sphere.sdk.products.queries.ProductQuery#bySlug(io.sphere.sdk.products.ProductProjectionType, java.util.Locale, String)}.
- That is due to the implementation of the domain specific language and still enables you to configure pagination and sorting.
+ That is due to the implementation of the domain specific language but still enables you to configure pagination and sorting.
 
  <p>Important to know is that the {@link io.sphere.sdk.queries.QueryDsl} uses immutable objects, so the call of {@link io.sphere.sdk.products.queries.ProductQuery#bySlug(io.sphere.sdk.products.ProductProjectionType, java.util.Locale, String)} does not change the internal state of the {@link io.sphere.sdk.products.queries.ProductQuery} but creates a new {@link io.sphere.sdk.queries.QueryDsl} object with the selected predicate.</p>
 
@@ -35,13 +35,13 @@ import io.sphere.sdk.models.Base;
 
  {@include.example io.sphere.sdk.meta.QueryDocumentationTest#queryByNames()}
 
- <p>The Predicate API looks verbose, but it is it's goal to prevent typos and make it via the IDE discoverable for which attributes can be queried.</p>
+ <p>The Predicate API looks verbose, but it is its goal to prevent typos and make it discoverable via the IDE for which attributes can be queried.</p>
 
  You can still create predicates from strings, but you need to escape characters.
 
  {@include.example io.sphere.sdk.meta.QueryDocumentationTest#testX()}
 
- <p>For creating predicates for resource &lt;RESOURCE&gt; there is a method {@code model()} in a class &lt;RESOURCE&gt;Query.</p>
+ <p>For creating predicates for resource &lt;RESOURCE&gt; there is a method {@code model()} in a class &lt;RESOURCE&gt;Query (e.g., Product has ProductQuery).</p>
 
  <h4>Connecting predicates</h4>
 
