57de8ad042203abf1f034a754043d5435a834a30
schleichardt <michael.schleichardt@commercetools.de>
2014-10-27 15:54:26 +0100

    Fix doc generation.

--- products/src/main/java/io/sphere/sdk/carts/queries/FetchCartById.java
+++ products/src/main/java/io/sphere/sdk/carts/queries/FetchCartById.java
@@ -7,7 +7,7 @@ import io.sphere.sdk.queries.FetchImpl;
 /**
  Gets a cart by ID.
 
- {@include.example io.sphere.sdk.carts.commands.CartIntegrationTest#fetchById()}
+ {@include.example io.sphere.sdk.carts.CartIntegrationTest#fetchById()}
  */
 public class FetchCartById extends FetchImpl<Cart> {
     public FetchCartById(final Identifiable<Cart> identifiable) {
