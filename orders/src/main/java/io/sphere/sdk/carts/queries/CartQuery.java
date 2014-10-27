8cd343097a2ad8b674816b3da8eb1169512b8ffe
schleichardt <michael.schleichardt@commercetools.de>
2014-10-27 13:56:08 +0100

    Create CartQuery class. #125

---
+++ orders/src/main/java/io/sphere/sdk/carts/queries/CartQuery.java
@@ -0,0 +1,30 @@
+package io.sphere.sdk.carts.queries;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import io.sphere.sdk.carts.Cart;
+import io.sphere.sdk.queries.DefaultModelQuery;
+import io.sphere.sdk.queries.PagedQueryResult;
+
+/**
+
+ {@doc.gen summary carts}
+
+ */
+public class CartQuery extends DefaultModelQuery<Cart> {
+    public CartQuery(){
+        super(CartsEndpoint.ENDPOINT.endpoint(), resultTypeReference());
+    }
+
+    public static TypeReference<PagedQueryResult<Cart>> resultTypeReference() {
+        return new TypeReference<PagedQueryResult<Cart>>(){
+            @Override
+            public String toString() {
+                return "TypeReference<PagedQueryResult<Cart>>";
+            }
+        };
+    }
+
+    public static CartQuery of() {
+        return new CartQuery();
+    }
+}
