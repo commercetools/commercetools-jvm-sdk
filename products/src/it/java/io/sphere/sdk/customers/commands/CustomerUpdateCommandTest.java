417d8581b18de6db848a06443da3afd2a16a6ea9
Michael Schleichardt <michael@schleichardt.net>
2014-11-06 14:19:09 +0100

    Add ChangeEmail action. #149

--- products/src/it/java/io/sphere/sdk/customers/commands/CustomerUpdateCommandTest.java
+++ products/src/it/java/io/sphere/sdk/customers/commands/CustomerUpdateCommandTest.java
@@ -2,12 +2,14 @@ package io.sphere.sdk.customers.commands;
 
 import io.sphere.sdk.customers.Customer;
 import io.sphere.sdk.customers.CustomerName;
+import io.sphere.sdk.customers.commands.updateactions.ChangeEmail;
 import io.sphere.sdk.customers.commands.updateactions.ChangeName;
 import io.sphere.sdk.test.IntegrationTest;
 import org.junit.Test;
 
 import static io.sphere.sdk.customers.CustomerFixtures.withCustomer;
 import static org.fest.assertions.Assertions.assertThat;
+import static io.sphere.sdk.test.SphereTestUtils.*;
 
 public class CustomerUpdateCommandTest extends IntegrationTest {
     @Test
@@ -17,6 +19,16 @@ public class CustomerUpdateCommandTest extends IntegrationTest {
             assertThat(customer.getName()).isNotEqualTo(newName);
             final Customer updatedCustomer = execute(new CustomerUpdateCommand(customer, ChangeName.of(newName)));
             assertThat(updatedCustomer.getName()).isEqualTo(newName);
+        });
+    }
+
+    @Test
+    public void changeEmail() throws Exception {
+        withCustomer(client(), customer -> {
+            final String newEmail = randomEmail(CustomerUpdateCommandTest.class);
+            assertThat(customer.getEmail()).isNotEqualTo(newEmail);
+            final Customer updatedCustomer = execute(new CustomerUpdateCommand(customer, ChangeEmail.of(newEmail)));
+            assertThat(updatedCustomer.getEmail()).isEqualTo(newEmail);
         });
     }
 }
---
+++ products/src/main/java/io/sphere/sdk/customers/commands/updateactions/ChangeEmail.java
@@ -0,0 +1,25 @@
+package io.sphere.sdk.customers.commands.updateactions;
+
+import io.sphere.sdk.commands.UpdateAction;
+import io.sphere.sdk.customers.Customer;
+
+/**
+ *
+ * {@include.example io.sphere.sdk.customers.commands.CustomerUpdateCommandTest#changeEmail()}
+ */
+public class ChangeEmail extends UpdateAction<Customer> {
+    private final String email;
+
+    private ChangeEmail(final String email) {
+        super("changeEmail");
+        this.email = email;
+    }
+
+    public static ChangeEmail of(final String email) {
+        return new ChangeEmail(email);
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
