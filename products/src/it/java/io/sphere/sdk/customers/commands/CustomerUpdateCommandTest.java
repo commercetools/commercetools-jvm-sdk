5bab5a6b5994b3d9d72b52b93ff8c5671e118996
Michael Schleichardt <michael@schleichardt.net>
2014-11-06 14:29:34 +0100

    Add AddAddress action. #149

--- products/src/it/java/io/sphere/sdk/customers/commands/CustomerUpdateCommandTest.java
+++ products/src/it/java/io/sphere/sdk/customers/commands/CustomerUpdateCommandTest.java
@@ -2,10 +2,16 @@ package io.sphere.sdk.customers.commands;
 
 import io.sphere.sdk.customers.Customer;
 import io.sphere.sdk.customers.CustomerName;
+import io.sphere.sdk.customers.commands.updateactions.AddAddress;
 import io.sphere.sdk.customers.commands.updateactions.ChangeEmail;
 import io.sphere.sdk.customers.commands.updateactions.ChangeName;
+import io.sphere.sdk.models.Address;
+import io.sphere.sdk.models.AddressBuilder;
 import io.sphere.sdk.test.IntegrationTest;
 import org.junit.Test;
+
+import java.util.Optional;
+import java.util.function.Predicate;
 
 import static io.sphere.sdk.customers.CustomerFixtures.withCustomer;
 import static org.fest.assertions.Assertions.assertThat;
@@ -30,5 +36,24 @@ public class CustomerUpdateCommandTest extends IntegrationTest {
             final Customer updatedCustomer = execute(new CustomerUpdateCommand(customer, ChangeEmail.of(newEmail)));
             assertThat(updatedCustomer.getEmail()).isEqualTo(newEmail);
         });
+    }
+
+    @Test
+    public void addAddress() throws Exception {
+        withCustomer(client(), customer -> {
+            final String city = "addAddress";
+            final Address newAddress = AddressBuilder.of(DE).city(city).build();
+            final Predicate<Address> containsNewAddressPredicate = a -> a.getCity().equals(Optional.of(city));
+            assertThat(customer.getAddresses().stream()
+                    .anyMatch(containsNewAddressPredicate))
+                    .overridingErrorMessage("address is not present, yet")
+                    .isFalse();
+            final Customer updatedCustomer = execute(new CustomerUpdateCommand(customer, AddAddress.of(newAddress)));
+            System.out.println(updatedCustomer.getAddresses().stream()
+                    .anyMatch(containsNewAddressPredicate));
+            assertThat(updatedCustomer.getAddresses().stream()
+            .anyMatch(containsNewAddressPredicate)).isTrue();
+        });
+
     }
 }
