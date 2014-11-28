c851cb8885a94a6e71dc1b2f7f3570331ed007cc
Laura Luiz <laura.luiz@commercetools.de>
2014-11-28 19:26:45 +0100

    Receive a more generic class Throwable.

--- common/src/main/java/io/sphere/sdk/client/JsonParseException.java
+++ common/src/main/java/io/sphere/sdk/client/JsonParseException.java
@@ -3,7 +3,7 @@ package io.sphere.sdk.client;
 public class JsonParseException extends RuntimeException {
     private static final long serialVersionUID = 4954925590077093841L;
 
-    public JsonParseException(final Exception cause) {
+    public JsonParseException(final Throwable cause) {
         super(cause);
     }
 
