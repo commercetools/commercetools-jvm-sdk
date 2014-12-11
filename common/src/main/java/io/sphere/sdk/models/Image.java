8e3cd48b923b5c15a1f42563d5c17fe2d43713ea
schleichardt <michael.schleichardt@commercetools.de>
2014-12-11 09:04:56 +0100

    Create factory methods for Image. Close #180

--- common/src/main/java/io/sphere/sdk/models/Image.java
+++ common/src/main/java/io/sphere/sdk/models/Image.java
@@ -12,4 +12,19 @@ public interface Image {
 
     public Optional<String> getLabel();
 
+    static Image of(final String url, final ImageDimensions dimensions, final Optional<String> label) {
+        return new ImageImpl(url, dimensions, label);
+    }
+
+    static Image of(final String url, final ImageDimensions dimensions, final String label) {
+        return of(url, dimensions, Optional.of(label));
+    }
+
+    static Image ofWidthAndHeight(final String url, final int width, final int height, final String label) {
+        return of(url, ImageDimensions.of(width, height), Optional.of(label));
+    }
+
+    static Image of(final String url, final ImageDimensions dimensions) {
+        return of(url, dimensions, Optional.empty());
+    }
 }
