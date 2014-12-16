6b97b7431b4cb94cdb6d09f9089a9364621ac229
schleichardt <michael.schleichardt@commercetools.de>
2014-12-16 19:22:46 +0100

    Improve ProductData IDE usage by overriding methods.

--- models/src/main/java/io/sphere/sdk/products/ProductData.java
+++ models/src/main/java/io/sphere/sdk/products/ProductData.java
@@ -1,11 +1,43 @@
 package io.sphere.sdk.products;
 
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import io.sphere.sdk.categories.Category;
+import io.sphere.sdk.models.LocalizedStrings;
+import io.sphere.sdk.models.Reference;
+
+import java.util.List;
+import java.util.Optional;
 
 /**
  * For construction in unit tests use {@link io.sphere.sdk.products.ProductDataBuilder}.
  */
 @JsonDeserialize(as=ProductDataImpl.class)
 public interface ProductData extends ProductDataLike {
+    @Override
+    public LocalizedStrings getName();
+
+    @Override
+    public List<Reference<Category>> getCategories();
+
+    @Override
+    public Optional<LocalizedStrings> getDescription();
+
+    @Override
+    public LocalizedStrings getSlug();
+
+    @Override
+    public Optional<LocalizedStrings> getMetaTitle();
+
+    @Override
+    public Optional<LocalizedStrings> getMetaDescription();
+
+    @Override
+    public Optional<LocalizedStrings> getMetaKeywords();
+
+    @Override
+    public ProductVariant getMasterVariant();
+
+    @Override
+    public List<ProductVariant> getVariants();
 
 }
