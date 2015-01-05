3b0a63c5f842c30674c55bd96886f7635f8a0840
schleichardt <michael.schleichardt@commercetools.de>
2015-01-05 14:03:40 +0100

    Improve usability of API created PagedQueryResults by removing dsl methods.

--- common/src/main/java/io/sphere/sdk/queries/PagedQueryResult.java
+++ common/src/main/java/io/sphere/sdk/queries/PagedQueryResult.java
@@ -11,7 +11,7 @@ import java.util.List;
  * A container for query responses which contains a subset of the matching values.
  * @param <T> the type of the underlying model, like category or product.
  */
-public final class PagedQueryResult<T> extends PagedResult<T> {
+public class PagedQueryResult<T> extends PagedResult<T> {
 
     @JsonCreator
     PagedQueryResult(final int offset, final int total, final List<T> results) {
@@ -22,42 +22,24 @@ public final class PagedQueryResult<T> extends PagedResult<T> {
     }
 
     /**
-     * Creates a copy of this item with the given offset.
-     * @param offset the offset of the new copy
-     * @return the copy
-     */
-    public PagedQueryResult<T> withOffset(final int offset) {
-        return PagedQueryResult.of(offset, getTotal(), getResults());
-    }
-
-    /**
-     * Creates a copy of this with the given total items count.
-     * @param total the number of total items in the backend.
-     * @return a copy with total as new total.
-     */
-    public PagedQueryResult<T> withTotal(final int total) {
-        return PagedQueryResult.of(getOffset(), total, getResults());
-    }
-
-    /**
      * Creates a {@code PagedQueryResult} for queries with no matching values.
      * @param <T> the type of the underlying model
      * @return an empty {@code PagedQueryResult}
      */
-    public static <T> PagedQueryResult<T> empty() {
-        return new PagedQueryResult<>(0, 0, Collections.<T>emptyList());
+    public static <T> PagedQueryResultDsl<T> empty() {
+        return new PagedQueryResultDsl<>(0, 0, Collections.<T>emptyList());
     }
 
-    public static <T> PagedQueryResult<T> of(final int offset, final int total, final List<T> results) {
-        return new PagedQueryResult<>(offset, total, results);
+    public static <T> PagedQueryResultDsl<T> of(final int offset, final int total, final List<T> results) {
+        return new PagedQueryResultDsl<>(offset, total, results);
     }
 
-    public static <T> PagedQueryResult<T> of(final List<T> results) {
+    public static <T> PagedQueryResultDsl<T> of(final List<T> results) {
         return of(0, results.size(), results);
     }
 
     @JsonIgnore
-    public static <T> PagedQueryResult<T> of(final T singleResult) {
+    public static <T> PagedQueryResultDsl<T> of(final T singleResult) {
         return of(Arrays.asList(singleResult));
     }
 }
