ee1694aab877af18efe30085fe5d71d12bf6d4cb
schleichardt <michael.schleichardt@commercetools.de>
2015-01-09 16:12:12 +0100

    Write documentation for custom objects. #176

--- models/src/main/java/io/sphere/sdk/customobjects/CustomObject.java
+++ models/src/main/java/io/sphere/sdk/customobjects/CustomObject.java
@@ -7,12 +7,32 @@ import io.sphere.sdk.models.Referenceable;
 
 import java.util.Optional;
 
+/**
+ * Custom objects are a way to store arbitrary JSON-formatted data on the SPHERE.IO platform. It allows you to persist data that does not fit our standard data model.
+ *
+ * The storage can be seen as key value store but with an additional prefix called container to improve querying custom objects and provide an additional namespace.
+ *
+ * @param <T> The type of the value of this custom object.
+ */
 @JsonDeserialize(as = CustomObjectImpl.class)
 public interface CustomObject<T> extends DefaultModelView<CustomObject<T>>, Referenceable<CustomObject<Void>> {
+
+    /**
+     * The container is part of the custom object namespace to find it
+     * @return container
+     */
     String getContainer();
 
+    /**
+     * The key is part of the custom object namespace to find it
+     * @return container
+     */
     String getKey();
 
+    /**
+     * The value stored in the custom object.
+     * @return
+     */
     T getValue();
 
     @SuppressWarnings("unchecked")
