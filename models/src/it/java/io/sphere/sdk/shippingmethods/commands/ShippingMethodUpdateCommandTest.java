766ffc29df98c5853c45bf68c3b9da5d7ba321c6
Michael Schleichardt <michael@schleichardt.net>
2015-01-18 19:52:46 +0100

    Implement shipping method changeTaxCategory update action. #94

--- models/src/it/java/io/sphere/sdk/shippingmethods/commands/ShippingMethodUpdateCommandTest.java
+++ models/src/it/java/io/sphere/sdk/shippingmethods/commands/ShippingMethodUpdateCommandTest.java
@@ -2,7 +2,10 @@ package io.sphere.sdk.shippingmethods.commands;
 
 import io.sphere.sdk.shippingmethods.ShippingMethod;
 import io.sphere.sdk.shippingmethods.commands.updateactions.ChangeName;
+import io.sphere.sdk.shippingmethods.commands.updateactions.ChangeTaxCategory;
 import io.sphere.sdk.shippingmethods.commands.updateactions.SetDescription;
+import io.sphere.sdk.taxcategories.TaxCategory;
+import io.sphere.sdk.taxcategories.TaxCategoryFixtures;
 import io.sphere.sdk.test.IntegrationTest;
 import org.junit.Test;
 
@@ -33,6 +36,18 @@ public class ShippingMethodUpdateCommandTest extends IntegrationTest {
             final ShippingMethodUpdateCommand cmd = ShippingMethodUpdateCommand.of(shippingMethod, ChangeName.of(newName));
             final ShippingMethod updatedShippingMethod = execute(cmd);
             assertThat(updatedShippingMethod.getName()).isEqualTo(newName);
+            return updatedShippingMethod;
+        });
+    }
+
+    @Test
+    public void changeTaxCategory() throws Exception {
+        final TaxCategory newTaxCategory = TaxCategoryFixtures.defaultTaxCategory(client());
+        withUpdateableShippingMethod(client(), shippingMethod -> {
+            assertThat(shippingMethod.getTaxCategory().getId()).isNotEqualTo(newTaxCategory.getId());
+            final ShippingMethodUpdateCommand cmd = ShippingMethodUpdateCommand.of(shippingMethod, ChangeTaxCategory.of(newTaxCategory));
+            final ShippingMethod updatedShippingMethod = execute(cmd);
+            assertThat(updatedShippingMethod.getTaxCategory().getId()).isEqualTo(newTaxCategory.getId());
             return updatedShippingMethod;
         });
     }
