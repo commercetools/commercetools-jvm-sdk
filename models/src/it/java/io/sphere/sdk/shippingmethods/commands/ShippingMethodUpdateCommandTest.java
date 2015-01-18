f77f284201a8820384166031ee9eaa84dd5d3767
Michael Schleichardt <michael@schleichardt.net>
2015-01-18 20:11:08 +0100

    Implement shipping method ChangeIsDefault update action. #94

--- models/src/it/java/io/sphere/sdk/shippingmethods/commands/ShippingMethodUpdateCommandTest.java
+++ models/src/it/java/io/sphere/sdk/shippingmethods/commands/ShippingMethodUpdateCommandTest.java
@@ -1,9 +1,14 @@
 package io.sphere.sdk.shippingmethods.commands;
 
+import io.sphere.sdk.queries.Predicate;
+import io.sphere.sdk.queries.Query;
+import io.sphere.sdk.queries.QueryDsl;
 import io.sphere.sdk.shippingmethods.ShippingMethod;
+import io.sphere.sdk.shippingmethods.commands.updateactions.ChangeIsDefault;
 import io.sphere.sdk.shippingmethods.commands.updateactions.ChangeName;
 import io.sphere.sdk.shippingmethods.commands.updateactions.ChangeTaxCategory;
 import io.sphere.sdk.shippingmethods.commands.updateactions.SetDescription;
+import io.sphere.sdk.shippingmethods.queries.ShippingMethodQuery;
 import io.sphere.sdk.taxcategories.TaxCategory;
 import io.sphere.sdk.taxcategories.TaxCategoryFixtures;
 import io.sphere.sdk.test.IntegrationTest;
@@ -49,6 +54,22 @@ public class ShippingMethodUpdateCommandTest extends IntegrationTest {
             final ShippingMethod updatedShippingMethod = execute(cmd);
             assertThat(updatedShippingMethod.getTaxCategory().getId()).isEqualTo(newTaxCategory.getId());
             return updatedShippingMethod;
+        });
+    }
+
+    @Test
+    public void changeIsDefault() throws Exception {
+        //only one can be default one, so clean up if there is any
+        final Query<ShippingMethod> query = ShippingMethodQuery.of().withPredicate(Predicate.of("isDefault = true"));
+        final Optional<ShippingMethod> defaultShippingMethodOption = execute(query).head();
+        defaultShippingMethodOption.ifPresent(sm -> execute(ShippingMethodUpdateCommand.of(sm, ChangeIsDefault.toFalse())));
+
+        withUpdateableShippingMethod(client(), shippingMethod -> {
+            assertThat(shippingMethod.isDefault()).isFalse();
+            final ShippingMethodUpdateCommand cmd = ShippingMethodUpdateCommand.of(shippingMethod, ChangeIsDefault.toTrue());
+            final ShippingMethod updatedShippingMethod = execute(cmd);
+            assertThat(updatedShippingMethod.isDefault()).isTrue();
+            return execute(ShippingMethodUpdateCommand.of(updatedShippingMethod, ChangeIsDefault.toFalse()));
         });
     }
 }
---
+++ models/src/main/java/io/sphere/sdk/shippingmethods/queries/ShippingMethodQuery.java
@@ -0,0 +1,25 @@
+package io.sphere.sdk.shippingmethods.queries;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import io.sphere.sdk.queries.DefaultModelQuery;
+import io.sphere.sdk.queries.PagedQueryResult;
+import io.sphere.sdk.shippingmethods.ShippingMethod;
+
+public class ShippingMethodQuery extends DefaultModelQuery<ShippingMethod> {
+    private ShippingMethodQuery() {
+        super(ShippingMethodEndpoint.ENDPOINT.endpoint(), resultTypeReference());
+    }
+
+    public static TypeReference<PagedQueryResult<ShippingMethod>> resultTypeReference() {
+        return new TypeReference<PagedQueryResult<ShippingMethod>>(){
+            @Override
+            public String toString() {
+                return "TypeReference<PagedQueryResult<ShippingMethod>>";
+            }
+        };
+    }
+
+    public static ShippingMethodQuery of() {
+        return new ShippingMethodQuery();
+    }
+}
