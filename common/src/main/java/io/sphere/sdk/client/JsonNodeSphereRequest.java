63c183d0099239b68e95cd5d16f9aaab2f272405
schleichardt <michael.schleichardt@commercetools.de>
2015-02-03 15:03:10 +0100

    Add pure JSON request.

---
+++ common/src/main/java/io/sphere/sdk/client/JsonNodeSphereRequest.java
@@ -0,0 +1,48 @@
+package io.sphere.sdk.client;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonNode;
+import io.sphere.sdk.http.HttpMethod;
+import io.sphere.sdk.http.HttpRequest;
+import io.sphere.sdk.http.HttpResponse;
+import io.sphere.sdk.utils.JsonUtils;
+
+import java.util.Optional;
+import java.util.function.Function;
+
+public class JsonNodeSphereRequest implements SphereRequest<Optional<JsonNode>> {
+    private final HttpMethod httpMethod;
+    private final String path;
+    private final Optional<JsonNode> body;
+
+    protected JsonNodeSphereRequest(final HttpMethod httpMethod, final String path, final Optional<JsonNode> body) {
+        this.body = body;
+        this.httpMethod = httpMethod;
+        this.path = path;
+    }
+
+    @Override
+    public Function<HttpResponse, Optional<JsonNode>> resultMapper() {
+        return httpResponse -> httpResponse.getResponseBody().map(body -> JsonUtils.readObject(new TypeReference<JsonNode>() {
+        }, body));
+    }
+
+    @Override
+    public HttpRequest httpRequest() {
+        return body
+                .map(b -> HttpRequest.of(httpMethod, path, JsonUtils.toJson(b)).httpRequest())
+                .orElseGet(() -> HttpRequest.of(httpMethod, path));
+    }
+
+    public static JsonNodeSphereRequest of(final HttpMethod httpMethod, final String path, final JsonNode body) {
+        return of(httpMethod, path, Optional.of(body));
+    }
+
+    public static JsonNodeSphereRequest of(final HttpMethod httpMethod, final String path) {
+        return of(httpMethod, path, Optional.empty());
+    }
+
+    public static JsonNodeSphereRequest of(final HttpMethod httpMethod, final String path, final Optional<JsonNode> body) {
+        return new JsonNodeSphereRequest(httpMethod, path, body);
+    }
+}
---
+++ models/src/it/java/io/sphere/sdk/client/JsonNodeSphereRequestTest.java
@@ -0,0 +1,33 @@
+package io.sphere.sdk.client;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import io.sphere.sdk.test.IntegrationTest;
+import io.sphere.sdk.utils.JsonUtils;
+import org.junit.Test;
+
+import java.util.Optional;
+
+import static io.sphere.sdk.categories.CategoryFixtures.withCategory;
+import static io.sphere.sdk.http.HttpMethod.*;
+import static org.fest.assertions.Assertions.assertThat;
+
+public class JsonNodeSphereRequestTest extends IntegrationTest {
+    @Test
+    public void execution() throws Exception {
+        withCategory(client(), category -> {
+            final ObjectMapper objectMapper = JsonUtils.newObjectMapper();
+            final ObjectNode body = objectMapper.createObjectNode();
+            body.put("version", category.getVersion());
+            final ArrayNode actions = body.putArray("actions");
+            final ObjectNode changeNameUpdateAction = objectMapper.createObjectNode();
+            final String newName = "New Name";
+            changeNameUpdateAction.put("action", "changeName").set("name", objectMapper.createObjectNode().put("en", newName));
+            actions.add(changeNameUpdateAction);
+            final Optional<JsonNode> jsonNode = execute(JsonNodeSphereRequest.of(POST, "/categories/" + category.getId(), body));
+            assertThat(jsonNode.get().get("name").get("en").asText()).isEqualTo(newName);
+        });
+    }
+}
