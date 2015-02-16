6563fa2cf020abc1bd6feb263245c04298d0a0e8
Michael Schleichardt <michael@schleichardt.net>
2015-02-16 16:30:45 +0100

    Set PATH for Jenkins.

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,6 +5,7 @@ if [ -n "$JENKINS_URL" ]
 then
         export JAVA_HOME="/usr/lib/jvm/java-8-oracle/"
         export JDK_HOME="/usr/lib/jvm/java-8-oracle/"
+        export PATH=$JAVA_HOME/bin:$PATH
 	sbt_command="./sbt -java-home $JAVA_HOME"
 	
 fi
