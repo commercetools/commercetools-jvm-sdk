91c119b1334de5413d4eff60c53e1ee4b65ba889
schleichardt <michael.schleichardt@commercetools.de>
2015-03-02 11:10:42 +0100

    Improve ProductProjectionQueryModel usage, bold IDE support for meta model. #279

--- models/src/main/java/io/sphere/sdk/products/queries/ProductDataQueryModel.java
+++ models/src/main/java/io/sphere/sdk/products/queries/ProductDataQueryModel.java
@@ -3,6 +3,7 @@ package io.sphere.sdk.products.queries;
 import java.util.Optional;
 import java.util.function.Function;
 
+import io.sphere.sdk.categories.Category;
 import io.sphere.sdk.queries.*;
 
 public class ProductDataQueryModel<T> extends ProductDataQueryModelBase<T> {
@@ -21,6 +22,51 @@ public class ProductDataQueryModel<T> extends ProductDataQueryModelBase<T> {
 
     public Predicate<T> where(final Function<PartialProductDataQueryModel, Predicate<PartialProductDataQueryModel>> embeddedPredicate) {
         return where(embeddedPredicate.apply(ProductDataQueryModel.get()));
+    }
+
+    @Override
+    public ReferenceListQueryModel<T, Category> categories() {
+        return super.categories();
+    }
+
+    @Override
+    public LocalizedStringsQueryModel<T> description() {
+        return super.description();
+    }
+
+    @Override
+    public ProductVariantQueryModel<T> masterVariant() {
+        return super.masterVariant();
+    }
+
+    @Override
+    public LocalizedStringsQuerySortingModel<T> metaDescription() {
+        return super.metaDescription();
+    }
+
+    @Override
+    public LocalizedStringsQuerySortingModel<T> metaKeywords() {
+        return super.metaKeywords();
+    }
+
+    @Override
+    public LocalizedStringsQuerySortingModel<T> metaTitle() {
+        return super.metaTitle();
+    }
+
+    @Override
+    public LocalizedStringsQuerySortingModel<T> name() {
+        return super.name();
+    }
+
+    @Override
+    public LocalizedStringsQuerySortingModel<T> slug() {
+        return super.slug();
+    }
+
+    @Override
+    public ProductVariantQueryModel<T> variants() {
+        return super.variants();
     }
 }
 
