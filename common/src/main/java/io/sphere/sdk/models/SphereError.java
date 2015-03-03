a4ba9d8d3dc2508890553af7ff50f58d8523fb33
schleichardt <michael.schleichardt@commercetools.de>
2015-03-03 11:24:27 +0100

    Fix error on error casting.

--- common/src/main/java/io/sphere/sdk/models/SphereError.java
+++ common/src/main/java/io/sphere/sdk/models/SphereError.java
@@ -7,11 +7,15 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import io.sphere.sdk.json.JsonException;
 import io.sphere.sdk.json.JsonUtils;
 
+import java.lang.reflect.Field;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
+import java.util.function.Function;
 
 public class SphereError extends Base {
+    @JsonIgnore//forbid direct access to make error casting possible
     private final String code;
     private final String message;
     @JsonIgnore
@@ -35,19 +39,38 @@ public class SphereError extends Base {
     }
 
     public <T extends SphereError> Optional<T> as(final Class<T> errorClass) {
-        final ObjectMapper objectMapper = JsonUtils.newObjectMapper();
-        final JsonNode jsonNode = objectMapper.createObjectNode()
-                .put("code", code)
-                .put("message", message)
-                .setAll(furtherFields);
+        final Optional<String> classErrorCodeOption = codeValueOf(errorClass);
         Optional<T> result = Optional.empty();
-        try {
-            final T object = JsonUtils.readObject(errorClass, jsonNode);
-            result = Optional.of(object);
-        } catch (final JsonException e) {
-            result = Optional.empty();
+        if (classErrorCodeOption.map(classErrCode -> classErrCode.equals(code)).orElse(true)) {
+            final ObjectMapper objectMapper = JsonUtils.newObjectMapper();
+            final JsonNode jsonNode = objectMapper.createObjectNode()
+                    .put("code", code)
+                    .put("message", message)
+                    .setAll(furtherFields);
+            try {
+                final T object = JsonUtils.readObject(errorClass, jsonNode);
+                result = code.equals(object.getCode()) ? Optional.of(object) : Optional.<T>empty();
+            } catch (final JsonException e) {
+                result = Optional.empty();
+            }
         }
         return result;
+    }
+
+    private Optional<String> codeValueOf(final Class<?> errorClass) {
+        final String fieldName = "CODE";
+        final Field[] fields = errorClass.getFields();
+        final Optional<Field> codeField = Arrays.stream(fields)
+                .filter(field -> fieldName.equals(field.getName()) && field.getType().equals(String.class))
+                .findAny();
+        final Function<Field, Optional<String>> fieldUFunction = field -> {
+            try {
+                return Optional.ofNullable((String) field.get(errorClass));
+            } catch (IllegalAccessException e) {
+                return Optional.empty();
+            }
+        };
+        return codeField.flatMap(fieldUFunction);
     }
 
     @JsonAnySetter
