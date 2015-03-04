d42de82a930dfcf458aa90a50341d58ff12ca15c
Oleg Ilyenko <oleg.ilyenko@gmail.com>
2015-03-04 12:32:58 +0100

    Added nested attribute type

---
+++ models/src/main/java/io/sphere/sdk/attributes/NestedType.java
@@ -0,0 +1,26 @@
+package io.sphere.sdk.attributes;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.core.type.TypeReference;
+import io.sphere.sdk.categories.Category;
+import io.sphere.sdk.channels.Channel;
+import io.sphere.sdk.models.Reference;
+import io.sphere.sdk.products.Product;
+import io.sphere.sdk.producttypes.ProductType;
+
+public class NestedType extends AttributeTypeBase {
+    private final Reference<ProductType> typeReference;
+
+    protected NestedType(final Reference<ProductType> typeReference) {
+        this.typeReference = typeReference;
+    }
+
+    public Reference<ProductType> getTypeReference() {
+        return typeReference;
+    }
+
+    @JsonIgnore
+    private static NestedType of(final Reference<ProductType> typeReference) {
+        return new NestedType(typeReference);
+    }
+}
