f53c3e71f1fd17f21c06cd3b7821fb1c954b8a50
schleichardt <michael.schleichardt@commercetools.de>
2015-03-12 10:48:59 +0100

    Add setRestockableInDays update action for InventoryEntrys. #193

--- models/src/it/java/io/sphere/sdk/inventories/commands/InventoryEntryUpdateCommandTest.java
+++ models/src/it/java/io/sphere/sdk/inventories/commands/InventoryEntryUpdateCommandTest.java
@@ -4,11 +4,13 @@ import io.sphere.sdk.commands.UpdateAction;
 import io.sphere.sdk.inventories.InventoryEntry;
 import io.sphere.sdk.inventories.commands.updateactions.AddQuantity;
 import io.sphere.sdk.inventories.commands.updateactions.RemoveQuantity;
+import io.sphere.sdk.inventories.commands.updateactions.SetRestockableInDays;
 import io.sphere.sdk.test.IntegrationTest;
 import org.junit.Test;
 
 import static io.sphere.sdk.inventories.InventoryEntryFixtures.withUpdateableInventoryEntry;
 import static org.fest.assertions.Assertions.assertThat;
+import static io.sphere.sdk.test.OptionalAssert.assertThat;
 
 public class InventoryEntryUpdateCommandTest extends IntegrationTest {
     @Test
@@ -29,6 +31,17 @@ public class InventoryEntryUpdateCommandTest extends IntegrationTest {
             final UpdateAction<InventoryEntry> action = RemoveQuantity.of(removingQuantity);
             final InventoryEntry updatedEntry = execute(InventoryEntryUpdateCommand.of(entry, action));
             assertThat(updatedEntry.getQuantityOnStock()).isEqualTo(entry.getQuantityOnStock() - removingQuantity);
+            return updatedEntry;
+        });
+    }
+
+    @Test
+    public void setRestockableInDays() throws Exception {
+        withUpdateableInventoryEntry(client(), entry -> {
+            final int restockableInDays = entry.getRestockableInDays().map(i -> i + 4).orElse(4);
+            final UpdateAction<InventoryEntry> action = SetRestockableInDays.of(restockableInDays);
+            final InventoryEntry updatedEntry = execute(InventoryEntryUpdateCommand.of(entry, action));
+            assertThat(updatedEntry.getRestockableInDays()).isPresentAs(restockableInDays);
             return updatedEntry;
         });
     }
