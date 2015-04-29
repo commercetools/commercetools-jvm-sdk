d2198be90ff18b0779a5bda46550c1e432f3cdf3
Laura Luiz <laura.luiz@commercetools.de>
2015-04-29 19:44:21 +0200

    Remove unused import.

--- sphere-models/src/it/java/io/sphere/sdk/products/ProductProjectionSearchIntegrationTest.java
+++ sphere-models/src/it/java/io/sphere/sdk/products/ProductProjectionSearchIntegrationTest.java
@@ -20,7 +20,6 @@ import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
-import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.math.BigDecimal;
