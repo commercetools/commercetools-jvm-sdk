f633b45351d6daef14edd0e073d25cbc58bd0dc0
Stefan Meissner <stefan.meissner@commercetools.de>
2015-05-04 16:45:34 +0200

    Update GettingStarted.java
    - fixed typos and phrasing
    - Merchant Center now opens in a new browser tab

--- src/main/java/io/sphere/sdk/meta/GettingStarted.java
+++ src/main/java/io/sphere/sdk/meta/GettingStarted.java
@@ -4,14 +4,14 @@ import io.sphere.sdk.models.Base;
 
 /**
  <h3 id=about-clients>About the clients</h3>
- <p>The SPHERE.IO client communicates asynchronously with the SPHERE.IO API via HTTPS and takes care about authentication.</p>
- <p>The client uses Java objects to formulate a HTTP request, performs the request and maps the response JSON into a Java object.
- The resulting Java object is not directly accessible as object, it is embedded in a Future/Promise for asynchronous programming.
+ <p>The SPHERE.IO client communicates asynchronously with the SPHERE.IO API via HTTPS and it takes care about authentication.</p>
+ <p>The client uses Java objects to formulate an HTTP request, performs the request and maps the JSON response into a Java object.
+ The resulting Java object is not directly accessible as object, but it is embedded in a Future/Promise for asynchronous programming.
  Since the client is thread-safe you need only one client to perform multiple requests in parallel.</p>
 
  <p>There are different SPHERE.IO client flavors for different future implementations:</p>
 
- <table>
+ <table border="1">
  <caption>Clients and future implementations</caption>
  <tr><th>Client</th><th>Future implementation</th></tr>
  <tr><td>{@link io.sphere.sdk.client.SphereClient} (default)</td><td>{@code java.util.concurrent.CompletionStage}</td></tr>
@@ -21,9 +21,9 @@ import io.sphere.sdk.models.Base;
 
  <h3 id=preparation>Preparation</h3>
 
- <p>You need to <a href="https://admin.sphere.io">create a project in SPHERE.IO</a>.
- Then you need to go to the "Developers" section and open the tab "API clients".
- There you find the credentials to access the project with the client.</p>
+ <p>You need to <a href="https://admin.sphere.io" target="_blank">create a project in SPHERE.IO first</a>.
+ After that you need to go to the "Developers" section and open the tab "API clients".
+ There you'll find the credentials to access the project with the client.</p>
 
  <p>This is an example for the client credentials page (client secret has been modified):</p>
 
@@ -43,9 +43,9 @@ import io.sphere.sdk.models.Base;
 
  <p>A client works on the abstraction level of one HTTP request for one project.
  With one client you can start multiple requests in parallel, it is thread-safe.</p>
- <p>The clients have a method {@link io.sphere.sdk.client.SphereClient#execute(io.sphere.sdk.client.SphereRequest)}, which takes a {@link io.sphere.sdk.client.SphereRequest} as parameter.</p>
+ <p>The client method {@link io.sphere.sdk.client.SphereClient#execute(io.sphere.sdk.client.SphereRequest)} takes a {@link io.sphere.sdk.client.SphereRequest} as parameter.</p>
 
- <p>You can create {@link io.sphere.sdk.client.SphereRequest} yourself or use the given ones which are listed on {@link io.sphere.sdk.meta.SphereResources}.</p>
+ <p>You can create a {@link io.sphere.sdk.client.SphereRequest} yourself or use the predifined ones listed on {@link io.sphere.sdk.meta.SphereResources}.</p>
  <p>Example:</p>
 
  {@include.example example.TaxCategoryQueryExample#exampleQuery()}
