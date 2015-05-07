b84cbcaf7f2573a1b7162b664001bad6103a3bfe
Stefan Meissner <stefan.meissner@commercetools.de>
2015-05-07 15:03:45 +0200

    rephrased explanations
    Sometimes it was hard for me to understand the intention of the phrases, please review whether the meaning of the sentences is still aligned with the statement you wanted to make.

--- src/main/java/io/sphere/sdk/meta/JvmSdkFeatures.java
+++ src/main/java/io/sphere/sdk/meta/JvmSdkFeatures.java
@@ -14,27 +14,25 @@ import io.sphere.sdk.models.Base;
  </ul>
 
 <h3>Good defaults for toString(), equals() and hashCode()</h3>
-<p>SDK implementation classes extends {@link Base} which provides default implementations for the methods by using
+<p>The SDK's implementation classes extend {@link Base} which provides default implementations for the methods by using
  reflection following the suggestions of
  <a href="http://www.oracle.com/technetwork/java/effectivejava-136174.html">Effective Java</a>.</p>
 
 <h3>Domain models are immutable</h3>
-<p>Domain models are no plain old Java objects, since the client does not poses control over them but needs to send
- update commands to SPHERE.IO, so setters would pretend a functionality which is not possible.</p>
-<p>Clients for <em>other</em> cloud services provide setters and synchronize the model in the background. This approach still
- blocks the caller thread and makes it hard to impossible to tune error handling, timeouts and recover strategies.
- Our approach makes it explicit, that an operation can be performed in the background, takes time and might fail.</p>
+<p>Domain models are no plain old Java objects since the client does not pose control over them, but needs to send
+ update commands to SPHERE.IO. Thus setters, as provided by <em>other</em> cloud services are not applicable in SPHERE.IO.</p> 
+ <p>The approach to synchronize the model in the background blocks the caller thread and makes it hard to impossible to tune error handling, timeouts and recover strategies.
+ Our approach makes it explicit, that an operation can be performed in the background, that the operation takes time and that the operation might fail.</p>
 
  <h3>Domain models are interfaces</h3>
-<p>Since domain models are interfaces, you can use them in design patterns or add convenience methods.</p>
+<p>Since domain models are interfaces you can use them in design patterns of your choice or to add convenience methods.</p>
 
  <h3>Testability</h3>
- <p>The clients and the models are interfaces, so they can be replaced with test doubles.
+ <p>Since the clients and the models are interfaces they can be replaced with test doubles.
  In addition the SDK provides builders and JSON converters to create models for unit tests.</p>
 
  <h3>Domain specific languages to create requests</h3>
- <p>For example {@link io.sphere.sdk.queries.QueryDsl} helps to formulate valid queries and discover
- for which attributes can be in which way queried and sorted.</p>
+ <p>For example, {@link io.sphere.sdk.queries.QueryDsl} assists in formulating valid queries and to find out which attributes can be used in which way for querying and sorting.</p>
 
  */
 public final class JvmSdkFeatures extends Base {
