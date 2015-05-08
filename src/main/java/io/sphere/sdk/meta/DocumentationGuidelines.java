34ab92ccfbb25b7d463c51ddb4e4451c1728b88d
Stefan Meissner <stefan.meissner@commercetools.de>
2015-05-08 09:37:36 +0200

    rephrasings

--- src/main/java/io/sphere/sdk/meta/DocumentationGuidelines.java
+++ src/main/java/io/sphere/sdk/meta/DocumentationGuidelines.java
@@ -3,11 +3,11 @@ package io.sphere.sdk.meta;
 import io.sphere.sdk.models.Base;
 
 /**
- * This page describes for developers the necessary steps to document the API.
+ * This page describes the steps to follow for developers when documenting the API.
  *
  * <ul>
- *     <li>Use code comments not to explain the code, code should be self explaining. But it can be necessary to explain the reasons for the solutions.</li>
- *     <li>Models should link their commands and builders.</li>
+ *     <li>Do not use code comments for explaining the code; code should be self-explaining. But it can be necessary to explain the rationale for the solutions.</li>
+ *     <li>Models should link to their commands and builders.</li>
  *     <li>Every h3 tag should have an id to be referenced and in the toc.</li>
  * </ul>
  */
