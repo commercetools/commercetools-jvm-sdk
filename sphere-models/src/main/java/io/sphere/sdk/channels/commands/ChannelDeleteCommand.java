55a95813f77fecb33dd3a6e179a0e0ac4f209891
schleichardt <michael.schleichardt@commercetools.de>
2015-05-15 12:41:45 +0200

    Fix ChannelDeleteCommand documentation link.

--- sphere-models/src/main/java/io/sphere/sdk/channels/commands/ChannelDeleteCommand.java
+++ sphere-models/src/main/java/io/sphere/sdk/channels/commands/ChannelDeleteCommand.java
@@ -8,7 +8,7 @@ import io.sphere.sdk.models.Versioned;
 /** Deletes a channel.
 
  <p>Example:</p>
- {@include.example }
+ {@include.example io.sphere.sdk.channels.commands.ChannelDeleteCommandTest#execution()}
  */
 public class ChannelDeleteCommand extends ByIdDeleteCommandImpl<Channel> {
     private ChannelDeleteCommand(final Versioned<Channel> versioned) {
