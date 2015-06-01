7fca35e07b511dc025fa6b5fb1b9d8c781a81f91
Stefan Meissner <stefan.meissner@commercetools.de>
2015-06-01 17:45:08 +0200

    Update CustomObjectDocumentation.java
    fixed wording and phrasing

--- src/main/java/io/sphere/sdk/meta/CustomObjectDocumentation.java
+++ src/main/java/io/sphere/sdk/meta/CustomObjectDocumentation.java
@@ -33,10 +33,10 @@ import io.sphere.sdk.customobjects.queries.CustomObjectQuery;
 
  <h4 id=pojo-update-custom-objects>Update Custom Objects</h4>
 
-In this case you use also {@link io.sphere.sdk.customobjects.commands.CustomObjectUpsertCommand}.
+In this case you also use {@link io.sphere.sdk.customobjects.commands.CustomObjectUpsertCommand}.
 
  For update operations you can use {@link io.sphere.sdk.customobjects.CustomObjectDraft#ofVersionedUpdate(io.sphere.sdk.customobjects.CustomObject, Object, com.fasterxml.jackson.core.type.TypeReference)}
- to use optimistic concurrency control as in here:
+ to realize optimistic concurrency control as in here:
 
  {@include.example io.sphere.sdk.customobjects.commands.CustomObjectUpsertCommandTest#updateWithVersion()}
 
@@ -60,7 +60,7 @@ In this case you use also {@link io.sphere.sdk.customobjects.commands.CustomObje
 
  <h3 id=custom-json-mapper>Custom Objects with a custom JSON mapper</h3>
 
- <p>If you need to create your own models and prefer another JSON tool over Jackson you can use it at your own risk.</p>
+ <p>If you need to create your own models and you prefer another JSON tool over Jackson you can use it at your own risk.</p>
  <h4>The models for the demo</h4>
  {@include.example io.sphere.sdk.customobjects.demo.GsonFooCustomObjectDraft}
  {@include.example io.sphere.sdk.customobjects.demo.GsonFoo}
@@ -80,15 +80,15 @@ In this case you use also {@link io.sphere.sdk.customobjects.commands.CustomObje
  <p>In this example we want to create unique readable IDs (successive numbers)
  for customers since SPHERE.IO returns IDs like 8547b810-9dad-11d1-80b4-44c04fd430c8.</p>
 
- The data model contains the last number and the last SPHERE.IO customer ID (just in there to have more than one field).
+ The data model contains the last number and the last SPHERE.IO customer ID (just to have more than one field).
 
  {@include.example io.sphere.sdk.customobjects.occexample.CustomerNumberCounter}
 
- <p>Imagine the customer number should start at 1000 so we do the following request for the initialization of the counter:</p>
+ <p>Imagine, the customer number should start at 1000, so we do the following request for the initialization of the counter:</p>
 
  {@include.example io.sphere.sdk.customobjects.occexample.FlowTest#setupInitialValue()}
 
- <p>An update then works this way:</p>
+ <p>An update works this way then:</p>
  {@include.example io.sphere.sdk.customobjects.occexample.FlowTest#doAnUpdate()}
 
 <h3 id=migrations>Migrations</h3>
@@ -96,7 +96,7 @@ In this case you use also {@link io.sphere.sdk.customobjects.commands.CustomObje
  <p>Your data model in the custom objects probably will evolve and you should plan for it.</p>
 
  <h4 id=migrations-additional-fields>Adding fields</h4>
- For new fields you can add an {@link java.util.Optional} so it is obvious to callers that this value can be unset.
+ For new fields you can add an {@link java.util.Optional} to make it obvious to callers that this value can be unset.
  <p>Before adding the field (there are already saved custom objects):</p>
 
  {@include.example io.sphere.sdk.customobjects.migrations.version1.Xyz}
@@ -119,25 +119,25 @@ In this case you use also {@link io.sphere.sdk.customobjects.commands.CustomObje
 
  {@include.example io.sphere.sdk.customobjects.migrations.version3.Xyz}
 
- The next time you save this object the field will be gone in the JSON.
+ Next time you'll save this object the field will be no more in the JSON.
 
 
 <h4 id=migrations-moving-data>Moving data</h4>
- Suppose you have this as data model:
+ Suppose you have this data model:
 
  {@include.example io.sphere.sdk.customobjects.migrations.version1.Uvw}
 
- And you want to make foo not a String but an object with members a and b.
+ And you want to make 'foo' not a String, but an object with members 'a' and 'b'.
 
  {@include.example io.sphere.sdk.customobjects.migrations.version2.Uvw}
 
- And let's say for simplicity you can directly migrate the String to the object by splitting the String the right way.
+ And let's say, for simplicity you can directly migrate the String to the object by splitting the String the right way.
 
- A possible solution is to create an interface for what you want:
+ A possible solution is to create an interface for that purpose:
 
  {@include.example io.sphere.sdk.customobjects.migrations.version3.Uvw}
 
- For the two incarnations you create a class for each. The newest class contains all the fields, the schema version and implements the interface:
+ For the two incarnations you create a class for each. The newest class contains all the field plus the schema version and it implements the interface:
 
  {@include.example io.sphere.sdk.customobjects.migrations.version3.UvwSchemaVersion2}
 
@@ -146,15 +146,15 @@ In this case you use also {@link io.sphere.sdk.customobjects.commands.CustomObje
 
  {@include.example io.sphere.sdk.customobjects.migrations.version3.UvwSchemaVersion1}
 
- As a result you only work with the interface and it is transparent if it is an old or new version:
+ As a result you only work with the interface making it transparent if it is an old or new version:
 
  {@include.example io.sphere.sdk.customobjects.migrations.CustomObjectsMigrationsTest#exampleForMigrationCall()}
 
 
  <h3 id="traps">Traps</h3>
 
- <p>Unlike other query models, {@link CustomObjectQuery#model()} takes a type parameter of the result type of the custom object.
- Notice that it is necessary to explicitly declare the type when requesting the query model, as shown in the following examples:</p>
+ <p>Unlike other query models, {@link CustomObjectQuery#model()} takes a type-parameter of the custom object's result type.
+ Please note, that it is necessary to explicitly declare the type when requesting the query model, as shown in the following examples:</p>
  {@include.example io.sphere.sdk.customobjects.queries.CustomObjectQueryTest#demoModelTypeParameter()}
 
  */
