1b9c74029d064851c63f1d1b79e575ee7543bdec
Stefan Meissner <stefan.meissner@commercetools.de>
2015-06-01 17:24:40 +0200

    Update ConstructionDocumentation.java
    fixed wording and typos

--- src/main/java/io/sphere/sdk/meta/ConstructionDocumentation.java
+++ src/main/java/io/sphere/sdk/meta/ConstructionDocumentation.java
@@ -4,15 +4,15 @@ package io.sphere.sdk.meta;
  * This documentation is about creating objects.
  *
  * SPHERE.IO saves e-commerce data in the cloud. The JVM SDK is one way to read and write the data.
- * There can be other tools like the Merchant Center or product sync tools which also can change data.
+ * There can be other tools like the Merchant Center or product sync tools which can also change data.
  * As a result the data loaded with the JVM SDK is a record of the present or the
- * past since directly after loading the data another tool might changed the data.
- * Therefore <strong>data</strong> containers are <strong>immutable</strong> and contain <strong>no setters</strong>.
+ * past since another tool might have changed the data just after loading the data .
+ * Therefore, <strong>data</strong> containers are <strong>immutable</strong> and contain <strong>no setters</strong>.
  *
- *  <h3 id=static-of-method>Static of method</h3>
- *  For flexibility in the implementation the SDK mostly don't let objects be instantiated with the {@code new} operator.
+ *  <h3 id=static-of-method>Static 'of' method</h3>
+ *  To achieve flexibility in the implementation the SDK mostly doesn't support object instantiation by the {@code new} operator.
  *
- *  Simple objects with few optional parameters have typically a static method called {@code of} to create an object.
+ *  Simple objects with few optional parameters typically have a static method called {@code of} to create an object.
  *
  *  <p> For example use {@link io.sphere.sdk.models.LocalizedStrings#of(java.util.Locale, String, java.util.Locale, String)}
  *   to create a translation for different locales:</p>
@@ -25,8 +25,8 @@ package io.sphere.sdk.meta;
  *  <h3 id=builders>Builders</h3>
  *
  *  For objects with a lot of optional parameters the SDK provides builders. The builder is initialized with a
- *  static of method and the mandatory parameters for creating the target object. The builder has methods
- *  to fill each optional field of the target object.
+ *  static {@code of} method and the mandatory parameters for creating the target object. The builder provides methods
+ *  to fill each target object's optional field.
  *  To create the target object a method called {@link io.sphere.sdk.models.Builder#build()} has to be executed.
  *
  *  <p>The {@link io.sphere.sdk.models.Address} object with the {@link io.sphere.sdk.models.AddressBuilder}
@@ -42,21 +42,21 @@ package io.sphere.sdk.meta;
  *
  *  <h3 id=copy-method>Copy methods</h3>
  *
- *  Sometimes you need to clone objects and update one field since most objects are immutable,
+ *  Sometimes you need to clone objects and to update one field since most objects are immutable;
  *  you can create updated copies with a method starting with "with":
  *
  *  {@include.example io.sphere.sdk.meta.ConstructionDocumentationTest#possibilities1()}
  *
- *  The with copy methods are inspired by Scala and the Java date and time API: {@link java.time.OffsetDateTime#withMinute(int)}.
+ *  The 'with copy'-methods are inspired by Scala and the Java date and time API: {@link java.time.OffsetDateTime#withMinute(int)}.
  *
  *  <h3 id=persistent-objects>Persistent objects vs. local test doubles</h3>
  *
- *  You can create local instances of SPHERE.IO resources like
- *  {@link io.sphere.sdk.products.Product} for unit testing with a {@link io.sphere.sdk.products.ProductBuilder}.
+ *  You can create local instances of SPHERE.IO resources, like
+ *  {@link io.sphere.sdk.products.Product}, for unit testing with a {@link io.sphere.sdk.products.ProductBuilder}.
  *  But these resources are not persistent.
  *
  *  <p>For persistent changes you need to use a SPHERE.IO
- *  client and execute a {@link io.sphere.sdk.commands.Command}. Available commands for the SPHERE.IO resources
+ *  client and to execute a {@link io.sphere.sdk.commands.Command}. Available commands for the SPHERE.IO resources
  *  are listed in {@link io.sphere.sdk.meta.SphereResources}</p>
  *
  *  Typically there are three kinds of commands:
