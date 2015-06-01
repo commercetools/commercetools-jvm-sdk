cc4a06d938e19c34e42260f98464330ea3392fb0
schleichardt <michael.schleichardt@commercetools.de>
2015-06-01 17:34:24 +0200

    Add reference expansion for InventoryEntryByIdFetch. #318

--- sphere-models/src/main/java/io/sphere/sdk/inventories/queries/InventoryEntryByIdFetch.java
+++ sphere-models/src/main/java/io/sphere/sdk/inventories/queries/InventoryEntryByIdFetch.java
@@ -1,6 +1,7 @@
 package io.sphere.sdk.inventories.queries;
 
 import io.sphere.sdk.inventories.InventoryEntry;
+import io.sphere.sdk.models.Identifiable;
 import io.sphere.sdk.queries.ByIdFetchImpl;
 
 public class InventoryEntryByIdFetch extends ByIdFetchImpl<InventoryEntry> {
@@ -8,7 +9,15 @@ public class InventoryEntryByIdFetch extends ByIdFetchImpl<InventoryEntry> {
         super(id, Endpoint.ENDPOINT);
     }
 
+    public static InventoryEntryByIdFetch of(final Identifiable<InventoryEntry> inventoryEntry) {
+        return of(inventoryEntry.getId());
+    }
+
     public static InventoryEntryByIdFetch of(final String id) {
         return new InventoryEntryByIdFetch(id);
+    }
+
+    public static InventoryEntryExpansionModel<InventoryEntry> expansionPath() {
+        return new InventoryEntryExpansionModel<>();
     }
 }
