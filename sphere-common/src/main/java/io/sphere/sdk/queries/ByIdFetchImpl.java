5a6d5543c2853fe7255e3ac1f391452350060566
schleichardt <michael.schleichardt@commercetools.de>
2015-06-01 17:04:28 +0200

    Fix compilation bug.

--- sphere-common/src/main/java/io/sphere/sdk/queries/ByIdFetchImpl.java
+++ sphere-common/src/main/java/io/sphere/sdk/queries/ByIdFetchImpl.java
@@ -1,10 +1,17 @@
 package io.sphere.sdk.queries;
 
 import io.sphere.sdk.client.JsonEndpoint;
+import io.sphere.sdk.http.HttpQueryParameter;
+
+import java.util.List;
 
 public abstract class ByIdFetchImpl<T> extends FetchImpl<T> {
 
     protected ByIdFetchImpl(final String id, final JsonEndpoint<T> endpoint) {
         super(endpoint, id);
+    }
+
+    public ByIdFetchImpl(final JsonEndpoint<T> endpoint, final String identifierToSearchFor, final List<HttpQueryParameter> additionalParameters) {
+        super(endpoint, identifierToSearchFor, additionalParameters);
     }
 }
--- sphere-models/src/main/java/io/sphere/sdk/products/queries/ProductProjectionByIdFetch.java
+++ sphere-models/src/main/java/io/sphere/sdk/products/queries/ProductProjectionByIdFetch.java
@@ -1,22 +1,15 @@
 package io.sphere.sdk.products.queries;
 
+import io.sphere.sdk.http.HttpQueryParameter;
 import io.sphere.sdk.products.ProductProjection;
 import io.sphere.sdk.products.ProductProjectionType;
 import io.sphere.sdk.queries.ByIdFetchImpl;
-import io.sphere.sdk.http.UrlQueryBuilder;
+
+import static java.util.Arrays.asList;
 
 public class ProductProjectionByIdFetch extends ByIdFetchImpl<ProductProjection> {
-    private final ProductProjectionType projectionType;
-
     private ProductProjectionByIdFetch(final String id, final ProductProjectionType projectionType) {
-        super(id, ProductProjectionsEndpoint.ENDPOINT);
-        this.projectionType = projectionType;
-    }
-
-    @Override
-    protected UrlQueryBuilder additionalQueryParameters() {
-        final String value = projectionType == ProductProjectionType.STAGED ? "true" : "false";
-        return super.additionalQueryParameters().add("staged", value, false);
+        super(ProductProjectionsEndpoint.ENDPOINT, id, asList(HttpQueryParameter.of("staged", projectionType.isStaged().toString())));
     }
 
     public static ProductProjectionByIdFetch of(final String id, final ProductProjectionType projectionType) {
