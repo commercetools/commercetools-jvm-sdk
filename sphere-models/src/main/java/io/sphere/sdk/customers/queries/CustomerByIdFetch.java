a2f2a2821e414a8a3d7cc5e6b868492445792ae2
schleichardt <michael.schleichardt@commercetools.de>
2015-06-01 16:59:26 +0200

    Add CustomerByIdFetch with reference expansion. #318

--- sphere-models/src/main/java/io/sphere/sdk/customers/queries/CustomerByIdFetch.java
+++ sphere-models/src/main/java/io/sphere/sdk/customers/queries/CustomerByIdFetch.java
@@ -1,6 +1,7 @@
 package io.sphere.sdk.customers.queries;
 
 import io.sphere.sdk.customers.Customer;
+import io.sphere.sdk.models.Identifiable;
 import io.sphere.sdk.queries.ByIdFetchImpl;
 
 public class CustomerByIdFetch extends ByIdFetchImpl<Customer> {
@@ -8,7 +9,15 @@ public class CustomerByIdFetch extends ByIdFetchImpl<Customer> {
         super(id, CustomersEndpoint.ENDPOINT);
     }
 
+    public static CustomerByIdFetch of(final Identifiable<Customer> id) {
+        return of(id.getId());
+    }
+
     public static CustomerByIdFetch of(final String id) {
         return new CustomerByIdFetch(id);
+    }
+
+    public static CustomerExpansionModel<Customer> expansionPath() {
+        return new CustomerExpansionModel<>();
     }
 }
