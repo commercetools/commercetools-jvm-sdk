274d00e3cb12360e47bd964f88e5db61b9f139e4
Michael Schleichardt <michael@schleichardt.net>
2015-06-03 11:47:21 +0200

    Remove redundant type IntegerQuerySortingModel. #296

--- sphere-common/src/main/java/io/sphere/sdk/queries/IntegerQuerySortingModelImpl.java
+++
@@ -1,9 +0,0 @@
-package io.sphere.sdk.queries;
-
-import java.util.Optional;
-
-final class IntegerQuerySortingModelImpl<T> extends IntegerLikeQuerySortingModel<T, Integer> implements IntegerQuerySortingModel<T> {
-    public IntegerQuerySortingModelImpl(final Optional<? extends QueryModel<T>> parent, final String pathSegment) {
-        super(parent, pathSegment);
-    }
-}
