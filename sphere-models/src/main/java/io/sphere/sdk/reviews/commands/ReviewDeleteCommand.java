f9a07c1a25e913405b567d9afb38d32f74fcfe25
Christoph Neijenhuis <christoph.neijenhuis@commercetools.de>
2015-06-03 14:17:03 +0200

    Delete reviews

---
+++ sphere-models/src/main/java/io/sphere/sdk/reviews/commands/ReviewDeleteCommand.java
@@ -0,0 +1,22 @@
+package io.sphere.sdk.reviews.commands;
+
+import io.sphere.sdk.commands.ByIdDeleteCommandImpl;
+import io.sphere.sdk.commands.DeleteCommand;
+import io.sphere.sdk.models.Versioned;
+import io.sphere.sdk.reviews.Review;
+
+/**
+ * Deletes a review in SPHERE.IO.
+ *
+ * {@include.example io.sphere.sdk.reviews.commands.ReviewDeleteCommandTest#execution()}
+ */
+public class ReviewDeleteCommand extends ByIdDeleteCommandImpl<Review> {
+
+    private ReviewDeleteCommand(final Versioned<Review> versioned) {
+        super(versioned, ReviewsEndpoint.ENDPOINT);
+    }
+
+    public static DeleteCommand<Review> of(final Versioned<Review> versioned) {
+        return new ReviewDeleteCommand(versioned);
+    }
+}
