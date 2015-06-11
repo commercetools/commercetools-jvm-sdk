d61ac29a75b592b55ca57693f88e65960c102307
schleichardt <michael.schleichardt@commercetools.de>
2015-06-11 09:30:23 +0200

    Create table of contents for attribute tutorial. #467

---
+++ src/main/java/io/sphere/sdk/meta/ProductAttributeDocumentation.java
@@ -0,0 +1,17 @@
+package io.sphere.sdk.meta;
+
+/**
+ <h3 id="intro">Introduction</h3>
+ <h3 id="product-type-creation">ProductType Creation</h3>
+ <h3 id="product-creation">Product Creation</h3>
+ <h3 id="attribute-access-without-product-type">Reading Attributes</h3>
+ <h3 id="attribute-table-creation">Creating a table of attributes</h3>
+ <h3 id="attribute-update">Update attributes in a product</h3>
+ <h3 id="attribute-stubs-for-tests">Create attribute stubs for unit tests</h3>
+ <h3 id="attributes-in-order-import">Create attributes for importing orders</h3>
+ <h3 id="nested-attributes">Nested attributes (experimental)</h3>
+ */
+public class ProductAttributeDocumentation {
+    private ProductAttributeDocumentation() {
+    }
+}
