1dd717d57fd1c79c33fb57a7c50489e0e366c0db
schleichardt <michael.schleichardt@commercetools.de>
2015-06-16 16:11:37 +0200

    reactivate nested attribute test in attributes-improvements branch #466.

--- sphere-models/src/main/java/io/sphere/sdk/products/AttributeContainerDraft.java
+++ sphere-models/src/main/java/io/sphere/sdk/products/AttributeContainerDraft.java
@@ -1,27 +1,22 @@
 package io.sphere.sdk.products;
 
+import io.sphere.sdk.attributes.Attribute;
 import io.sphere.sdk.attributes.AttributeDraft;
 
 import java.util.List;
 
 import static java.util.Arrays.asList;
+import static java.util.stream.Collectors.toList;
 
 public class AttributeContainerDraft {
-    private final List<AttributeDraft> attributes;
-
-    private AttributeContainerDraft(final List<AttributeDraft> attributes) {
-        this.attributes = attributes;
-    }
-
-    public static AttributeContainerDraft of(final AttributeDraft ... attributeDrafts) {
+    public static AttributeContainer of(final AttributeDraft ... attributeDrafts) {
         return of(asList(attributeDrafts));
     }
 
-    public static AttributeContainerDraft of(final List<AttributeDraft> attributes) {
-        return new AttributeContainerDraft(attributes);
-    }
-
-    public List<AttributeDraft> getAttributes() {
-        return attributes;
+    public static AttributeContainer of(final List<AttributeDraft> attributeDrafts) {
+        final List<Attribute> attributes = attributeDrafts.stream()
+                .map(attributeDraft -> Attribute.of(attributeDraft.getName(), attributeDraft.getValue()))
+                .collect(toList());
+        return AttributeContainer.of(attributes);
     }
 }
