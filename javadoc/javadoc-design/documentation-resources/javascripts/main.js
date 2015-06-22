4b4f6ed65616cee08c3d90663e26ec19970b3407
sphere-oss <automation@commercetools.de>
2015-06-22 13:00:20 +0000

    update gh-pages

---
+++ javadoc/javadoc-design/documentation-resources/javascripts/main.js
@@ -0,0 +1,48 @@
+(function () {
+    var headers = $("h3").not(".toc-ignore");
+
+    var toc = "";
+
+    headers.each(function() {
+        var element = $(this);
+        var title = element.text();
+        var anker = "#" + element.attr("id");
+
+        if(anker.length > 1 && anker != "#undefined" && title.length > 0) {
+            toc += "<li><a href='" + anker + "'>" + title + "</a></li>";
+        }
+    });
+
+    if(toc.length > 0) {
+        $(".contentContainer", 0).prepend(
+            "<nav role='navigation' id='page-toc' class='table-of-contents'><strong>Table of Contents</strong><ul>" +
+            toc + "</ul></nav>");
+    }
+
+    //moves this blocks up before the package listings
+    var pullUpClass = "pull-up";
+    $("." + pullUpClass).appendTo(".subTitle", 0).removeClass(pullUpClass);
+
+    $(".hide").hide();
+
+    var importShowButton = $("button.reveal-imports");
+    importShowButton.show();
+    importShowButton.click(function(eventObject) {
+        $(this).siblings(".code-example-imports").toggle();
+    });
+})();
+
+// ReleaseNotes - toggling hidden div
+
+$( ".show-more" ).click(function() {
+    $( this ).nextAll(".hidden").first().slideToggle( "fast", function() {
+    });
+    $( this ).toggleClass("show-more-rotate");
+});
+
+// Expand all hidden divs
+
+$( ".expand-all" ).click(function () {
+    $(".hidden").slideToggle(".hidden");
+    $(".show-more").toggleClass("show-more-rotate");
+});
