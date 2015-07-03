8756aade96d489ad6cb0c83f32e885b674c66c6b
schleichardt <michael.schleichardt@commercetools.de>
2015-07-03 12:16:26 +0200

    Add InventoryEntry update action "changeQuantity". close #501

--- sphere-models/src/it/java/io/sphere/sdk/inventories/commands/InventoryEntryUpdateCommandTest.java
+++ sphere-models/src/it/java/io/sphere/sdk/inventories/commands/InventoryEntryUpdateCommandTest.java
@@ -2,10 +2,7 @@ package io.sphere.sdk.inventories.commands;
 
 import io.sphere.sdk.commands.UpdateAction;
 import io.sphere.sdk.inventories.InventoryEntry;
-import io.sphere.sdk.inventories.commands.updateactions.AddQuantity;
-import io.sphere.sdk.inventories.commands.updateactions.RemoveQuantity;
-import io.sphere.sdk.inventories.commands.updateactions.SetExpectedDelivery;
-import io.sphere.sdk.inventories.commands.updateactions.SetRestockableInDays;
+import io.sphere.sdk.inventories.commands.updateactions.*;
 import io.sphere.sdk.test.IntegrationTest;
 import io.sphere.sdk.test.SphereTestUtils;
 import org.junit.Test;
@@ -24,6 +21,16 @@ public class InventoryEntryUpdateCommandTest extends IntegrationTest {
             final UpdateAction<InventoryEntry> action = AddQuantity.of(additionalQuantity);
             final InventoryEntry updatedEntry = execute(InventoryEntryUpdateCommand.of(entry, action));
             assertThat(updatedEntry.getQuantityOnStock()).isEqualTo(entry.getQuantityOnStock() + additionalQuantity);
+            return updatedEntry;
+        });
+    }
+
+    @Test
+    public void changeQuantity() throws Exception {
+        withUpdateableInventoryEntry(client(), entry -> {
+            final UpdateAction<InventoryEntry> action = ChangeQuantity.of(5000);
+            final InventoryEntry updatedEntry = execute(InventoryEntryUpdateCommand.of(entry, action));
+            assertThat(updatedEntry.getQuantityOnStock()).isEqualTo(5000);
             return updatedEntry;
         });
     }
