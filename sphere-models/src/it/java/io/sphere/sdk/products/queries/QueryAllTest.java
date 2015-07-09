4f9ae2d1d5d6a87105c187d466a24806c2cfbdbb
Michael Schleichardt <michael@schleichardt.net>
2015-07-09 19:33:12 +0200

    Fix QueryAllTest.

--- sphere-models/src/it/java/io/sphere/sdk/products/queries/QueryAllTest.java
+++ sphere-models/src/it/java/io/sphere/sdk/products/queries/QueryAllTest.java
@@ -8,7 +8,6 @@ import org.junit.Test;
 
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
 
@@ -16,9 +15,7 @@ import static java.util.stream.Collectors.toList;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class QueryAllTest extends QueryAllBase {
-    //yes, this is a small page size, but since this is tested,
-    //we create not million products for test execution speed reasons
-    //in production code it would higher
+    //in production code it would higher #smalltestset
     private static final int PAGE_SIZE = 5;
 
     @Test
@@ -30,9 +27,8 @@ public class QueryAllTest extends QueryAllBase {
                 //important, we sort by id, otherwise id > $lastId would not make sense
                 .withSort(m -> m.id().sort().asc())
                 .withLimit(PAGE_SIZE)
-                .withFetchTotal(false);
-        final CompletionStage<List<Product>> resultStage = sphereClient().execute(seedQuery)
-                .thenCompose(r -> queryStep(seedQuery, new LinkedList<>(), r));
+                .withFetchTotal(false);//saves also resources and time
+        final CompletionStage<List<Product>> resultStage = findNext(seedQuery, seedQuery, new LinkedList<>());
         final List<Product> actualProducts = resultStage.toCompletableFuture().join();
         assertThat(actualProducts).hasSize(createdProducts.size());
         //!!! the underlying database has a different algorithm to sort by ID, it is a UUID, which differs from String sorting
@@ -40,21 +36,20 @@ public class QueryAllTest extends QueryAllBase {
         assertThat(javaSortedActual).isEqualTo(createdProducts);
     }
 
-    public CompletionStage<List<Product>> queryStep(final ProductQuery seedQuery, final List<Product> accumulator,
-                                                    final PagedQueryResult<Product> pagedQueryResult) {
-        accumulator.addAll(pagedQueryResult.getResults());
-
-        final Optional<String> lastProductIdOptional = getIdForNextQuery(pagedQueryResult);
-        return lastProductIdOptional.map(lastId -> {
-            //here is the important part, we don't use offset, but query id > $lastId
-            //since we have a predicate for a product type, we connect them via and
-            //if no predicate is given, use {@code withPredicate(m -> m.id().isGreaterThan(lastId))};
-            final ProductQuery sphereRequest = seedQuery
-                    .withPredicate(m -> seedQuery.predicate().get().and(m.id().isGreaterThan(lastId)));
-            final CompletionStage<PagedQueryResult<Product>> execute = sphereClient().execute(sphereRequest);
-            return execute.thenCompose(r -> queryStep(seedQuery, accumulator, r));
-        })
-        .orElseGet(() -> CompletableFuture.completedFuture(accumulator));
+    private CompletionStage<List<Product>> findNext(final ProductQuery seedQuery, final ProductQuery query, final List<Product> products) {
+        final CompletionStage<PagedQueryResult<Product>> pageResult = sphereClient().execute(query);
+        return pageResult.thenCompose(page -> {
+            final List<Product> results = page.getResults();
+            products.addAll(results);
+            final boolean isLastQueryPage = results.size() < PAGE_SIZE;
+            if (isLastQueryPage) {
+                return CompletableFuture.completedFuture(products);
+            } else {
+                final String lastId = getIdForNextQuery(page);
+                return findNext(seedQuery, seedQuery
+                        .withPredicate(m -> seedQuery.predicate().get().and(m.id().isGreaterThan(lastId))), products);
+            }
+        });
     }
 
     /**
@@ -63,11 +58,9 @@ public class QueryAllTest extends QueryAllBase {
      * @param pagedResult the result to extract the id
      * @return the last ID, if present
      */
-    private <T extends Identifiable<T>> Optional<String> getIdForNextQuery(final PagedResult<T> pagedResult) {
+    private <T extends Identifiable<T>> String getIdForNextQuery(final PagedResult<T> pagedResult) {
         final List<T> results = pagedResult.getResults();
-        return Optional.of(results.size() - 1)
-                .filter(index -> index >= 0 && !pagedResult.isLast())
-                .map(index -> results.get(index))
-                .map(p -> p.getId());
+        final int indexLastElement = results.size() - 1;
+        return results.get(indexLastElement).getId();
     }
 }
