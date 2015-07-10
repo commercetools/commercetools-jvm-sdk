af45b364cdcee4ea3bdfe307776457a4c0202ea6
schleichardt <michael.schleichardt@commercetools.de>
2015-07-10 10:11:28 +0200

    Disable ApacheClientTest for internal TeamCity build.

--- sphere-models/src/it/java/io/sphere/sdk/client/ApacheClientTest.java
+++ sphere-models/src/it/java/io/sphere/sdk/client/ApacheClientTest.java
@@ -12,11 +12,14 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class ApacheClientTest extends IntegrationTest {
     @Test
     public void itWorks() throws Exception {
-        final SphereClientConfig config = getSphereClientConfig();
-        final HttpClient httpClient = ApacheHttpClientAdapter.of();
-        final SphereAccessTokenSupplier supplier = SphereAccessTokenSupplier.ofOneTimeFetchingToken(config, httpClient, false);
-        final SphereClient client = SphereClient.of(config, httpClient, supplier);
-        final Project project = client.execute(ProjectGet.of()).toCompletableFuture().join();
-        assertThat(project.getKey()).isEqualTo(getSphereClientConfig().getProjectKey());
+        //we cannot check this in TeamCity with sphere CI, but with travis
+        if (!"false".equals(System.getenv("JVM_SDK_IT_SSL_VALIDATION"))) {
+            final SphereClientConfig config = getSphereClientConfig();
+            final HttpClient httpClient = ApacheHttpClientAdapter.of();
+            final SphereAccessTokenSupplier supplier = SphereAccessTokenSupplier.ofOneTimeFetchingToken(config, httpClient, false);
+            final SphereClient client = SphereClient.of(config, httpClient, supplier);
+            final Project project = client.execute(ProjectGet.of()).toCompletableFuture().join();
+            assertThat(project.getKey()).isEqualTo(getSphereClientConfig().getProjectKey());
+        }
     }
 }
