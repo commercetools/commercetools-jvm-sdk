f9b04a65f6fb83ca4fb0e55c66d9b16011f6ae38
schleichardt <michael.schleichardt@commercetools.de>
2015-07-15 17:44:14 +0200

    Make ShippingMethodsByLocationGet usable with reference expansion. close #566

--- sphere-models/src/main/java/io/sphere/sdk/shippingmethods/queries/ShippingMethodsByLocationGet.java
+++ sphere-models/src/main/java/io/sphere/sdk/shippingmethods/queries/ShippingMethodsByLocationGet.java
@@ -1,19 +1,12 @@
 package io.sphere.sdk.shippingmethods.queries;
 
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.neovisionaries.i18n.CountryCode;
-import io.sphere.sdk.client.HttpRequestIntent;
-import io.sphere.sdk.client.SphereRequest;
-import io.sphere.sdk.client.SphereRequestBase;
-import io.sphere.sdk.http.HttpResponse;
+import io.sphere.sdk.queries.MetaModelFetchDsl;
 import io.sphere.sdk.shippingmethods.ShippingMethod;
-import io.sphere.sdk.http.UrlQueryBuilder;
+import io.sphere.sdk.shippingmethods.expansion.ShippingMethodExpansionModel;
 
 import javax.money.CurrencyUnit;
 import java.util.List;
-import java.util.Optional;
-
-import static io.sphere.sdk.http.HttpMethod.GET;
 
 /**
  * Retrieves all the shipping methods that can ship to the given location.
@@ -21,50 +14,20 @@ import static io.sphere.sdk.http.HttpMethod.GET;
  *
  * {@include.example io.sphere.sdk.shippingmethods.queries.ShippingMethodsByLocationGetTest#execution()}
  */
-public class ShippingMethodsByLocationGet extends SphereRequestBase implements SphereRequest<List<ShippingMethod>> {
-    private final CountryCode country;
-    private final Optional<String> state;
-    private final Optional<CurrencyUnit> currency;
-
-    private ShippingMethodsByLocationGet(final CountryCode country, final Optional<String> state, final Optional<CurrencyUnit> currency) {
-        this.country = country;
-        this.state = state;
-        this.currency = currency;
+public interface ShippingMethodsByLocationGet extends MetaModelFetchDsl<List<ShippingMethod>, ShippingMethod, ShippingMethodsByLocationGet, ShippingMethodExpansionModel<ShippingMethod>> {
+    static ShippingMethodsByLocationGet of(final CountryCode countryCode) {
+        return of(countryCode, null, null);
     }
 
-    @Override
-    public List<ShippingMethod> deserialize(final HttpResponse httpResponse) {
-        return resultMapperOf(new TypeReference<List<ShippingMethod>>() {
-            @Override
-            public String toString() {
-                return "TypeReference<List<ShippingMethod>>";
-            }
-        }).apply(httpResponse);
+    static ShippingMethodsByLocationGet of(final CountryCode countryCode, final String state) {
+        return of(countryCode, state, null);
     }
 
-    @Override
-    public HttpRequestIntent httpRequestIntent() {
-        return HttpRequestIntent.of(GET, "/shipping-methods" + queryParameters());
+    static ShippingMethodsByLocationGet of(final CountryCode countryCode, final CurrencyUnit currencyUnit) {
+        return of(countryCode, null, currencyUnit);
     }
 
-    public static ShippingMethodsByLocationGet of(final CountryCode countryCode) {
-        return new ShippingMethodsByLocationGet(countryCode, Optional.<String>empty(), Optional.<CurrencyUnit>empty());
-    }
-
-    public ShippingMethodsByLocationGet withState(final String state) {
-        return new ShippingMethodsByLocationGet(country, Optional.of(state), currency);
-    }
-
-    public ShippingMethodsByLocationGet withCurrency(final CurrencyUnit currency) {
-        return new ShippingMethodsByLocationGet(country, state, Optional.of(currency));
-    }
-
-    private String queryParameters() {
-        final UrlQueryBuilder urlQueryBuilder = UrlQueryBuilder.of();
-        urlQueryBuilder.add("country", country.getAlpha2());
-        state.ifPresent(x -> urlQueryBuilder.add("state", x));
-        currency.ifPresent(x -> urlQueryBuilder.add("currency", x.getCurrencyCode()));
-
-        return urlQueryBuilder.toStringWithOptionalQuestionMark();
+    static ShippingMethodsByLocationGet of(final CountryCode countryCode, final String state, final CurrencyUnit currencyUnit) {
+        return new ShippingMethodsByLocationGetImpl(countryCode, state, currencyUnit);
     }
 }
---
+++ sphere-models/src/main/java/io/sphere/sdk/shippingmethods/queries/ShippingMethodsByLocationGetImpl.java
@@ -0,0 +1,46 @@
+package io.sphere.sdk.shippingmethods.queries;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.neovisionaries.i18n.CountryCode;
+import io.sphere.sdk.http.UrlQueryBuilder;
+import io.sphere.sdk.queries.MetaModelFetchDslBuilder;
+import io.sphere.sdk.queries.MetaModelFetchDslImpl;
+import io.sphere.sdk.shippingmethods.ShippingMethod;
+import io.sphere.sdk.shippingmethods.expansion.ShippingMethodExpansionModel;
+
+import javax.annotation.Nullable;
+import javax.money.CurrencyUnit;
+import java.util.List;
+import java.util.Optional;
+
+import static java.util.Objects.requireNonNull;
+
+/**
+ * Retrieves all the shipping methods that can ship to the given location.
+ * If the currency parameter is given, then the shipping methods must also have a rate defined in the specified currency.
+ *
+ * {@include.example io.sphere.sdk.shippingmethods.queries.ShippingMethodsByLocationGetTest#execution()}
+ */
+public class ShippingMethodsByLocationGetImpl extends MetaModelFetchDslImpl<List<ShippingMethod>, ShippingMethod, ShippingMethodsByLocationGet, ShippingMethodExpansionModel<ShippingMethod>> implements ShippingMethodsByLocationGet {
+    ShippingMethodsByLocationGetImpl(final CountryCode country, @Nullable final String state, @Nullable final CurrencyUnit currency) {
+        super(identifierToSearchFor(requireNonNull(country), state, currency), ShippingMethodEndpoint.ENDPOINT.withTypeReference(new TypeReference<List<ShippingMethod>>() {
+            @Override
+            public String toString() {
+                return "TypeReference<List<ShippingMethod>>";
+            }
+        }), ShippingMethodExpansionModel.of(), ShippingMethodsByLocationGetImpl::new);
+    }
+
+    public ShippingMethodsByLocationGetImpl(final MetaModelFetchDslBuilder<List<ShippingMethod>, ShippingMethod, ShippingMethodsByLocationGet, ShippingMethodExpansionModel<ShippingMethod>> builder) {
+        super(builder);
+    }
+
+    private static String identifierToSearchFor(final CountryCode country, @Nullable final String state, @Nullable final CurrencyUnit currency) {
+        final UrlQueryBuilder urlQueryBuilder = UrlQueryBuilder.of();
+        urlQueryBuilder.add("country", country.getAlpha2());
+        Optional.ofNullable(state).ifPresent(x -> urlQueryBuilder.add("state", x));
+        Optional.ofNullable(currency).ifPresent(x -> urlQueryBuilder.add("currency", x.getCurrencyCode()));
+
+        return urlQueryBuilder.toStringWithOptionalQuestionMark();
+    }
+}
