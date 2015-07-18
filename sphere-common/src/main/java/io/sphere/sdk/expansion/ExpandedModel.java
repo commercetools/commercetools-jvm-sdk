44736cb8a990e7a3a89b7649a2c29f5632ca37a4
schleichardt <michael.schleichardt@commercetools.de>
2015-07-18 09:18:43 +0200

    Remove ExpansionPath stereotype from ExpansionModel. #515

--- sphere-common/src/main/java/io/sphere/sdk/expansion/ExpandedModel.java
+++ sphere-common/src/main/java/io/sphere/sdk/expansion/ExpandedModel.java
@@ -1,30 +1,37 @@
 package io.sphere.sdk.expansion;
 
-import io.sphere.sdk.models.Base;
-
 import java.util.Optional;
 
 import static org.apache.commons.lang3.StringUtils.isEmpty;
 
-public class ExpansionModel<T> extends Base {
+public class ExpandedModel<T> extends ExpansionPathBase<T> {
     protected final Optional<String> parentPath;
     protected final Optional<String> path;
 
-    protected ExpansionModel(final Optional<String> parentPath, final Optional<String> path) {
+    protected ExpandedModel(final Optional<String> parentPath, final Optional<String> path) {
         this.parentPath = parentPath;
         this.path = path;
     }
 
-    protected ExpansionModel(final String parentPath, final String path) {
+    protected ExpandedModel(final String parentPath, final String path) {
         this(Optional.of(parentPath), Optional.of(path));
     }
 
-    public ExpansionModel() {
+    public ExpandedModel() {
         this(Optional.empty(), Optional.empty());
     }
 
     protected static String collection(final String segmentName, final int index) {
         return String.format("%s[%d]", segmentName, index);
+    }
+
+    @Override
+    public String toSphereExpand() {
+        return buildPathExpression();
+    }
+
+    protected ExpansionPath<T> expansionPath(final String path) {
+        return new ExpandedModel<>(buildPathExpression(), path);
     }
 
     protected final String buildPathExpression() {
@@ -34,9 +41,5 @@ public class ExpansionModel<T> extends Base {
     protected final Optional<String> pathExpressionOption() {
         final String expression = buildPathExpression();
         return isEmpty(expression) ? Optional.empty() : Optional.of(expression);
-    }
-
-    protected ExpansionPath<T> expansionPath(final String path) {
-        return new ExpandedModel<>(buildPathExpression(), path);
     }
 }
