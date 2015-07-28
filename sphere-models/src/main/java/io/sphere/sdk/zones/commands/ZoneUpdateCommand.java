b29487d28b7400aa1c07a66a53ffb2d76f2269f9
schleichardt <michael.schleichardt@commercetools.de>
2015-07-28 11:46:10 +0200

    Migrate ZoneUpdateCommand to interface. #483

--- sphere-models/src/main/java/io/sphere/sdk/zones/commands/ZoneUpdateCommand.java
+++ sphere-models/src/main/java/io/sphere/sdk/zones/commands/ZoneUpdateCommand.java
@@ -1,7 +1,7 @@
 package io.sphere.sdk.zones.commands;
 
 import io.sphere.sdk.commands.UpdateAction;
-import io.sphere.sdk.commands.UpdateCommandDslImpl;
+import io.sphere.sdk.commands.UpdateCommandDsl;
 import io.sphere.sdk.models.Versioned;
 import io.sphere.sdk.zones.Zone;
 
@@ -12,16 +12,12 @@ import static java.util.Arrays.asList;
 /**
  {@doc.gen list actions}
  */
-public class ZoneUpdateCommand extends UpdateCommandDslImpl<Zone, ZoneUpdateCommand> {
-    private ZoneUpdateCommand(final Versioned<Zone> versioned, final List<? extends UpdateAction<Zone>> updateActions) {
-        super(versioned, updateActions, ZoneEndpoint.ENDPOINT);
-    }
-
-    public static ZoneUpdateCommand of(final Versioned<Zone> versioned, final UpdateAction<Zone> updateAction) {
+public interface ZoneUpdateCommand extends UpdateCommandDsl<Zone, ZoneUpdateCommand> {
+    static ZoneUpdateCommand of(final Versioned<Zone> versioned, final UpdateAction<Zone> updateAction) {
         return of(versioned, asList(updateAction));
     }
 
-    public static ZoneUpdateCommand of(final Versioned<Zone> versioned, final List<? extends UpdateAction<Zone>> updateActions) {
-        return new ZoneUpdateCommand(versioned, updateActions);
+    static ZoneUpdateCommand of(final Versioned<Zone> versioned, final List<? extends UpdateAction<Zone>> updateActions) {
+        return new ZoneUpdateCommandImpl(versioned, updateActions);
     }
 }
---
+++ sphere-models/src/main/java/io/sphere/sdk/zones/commands/ZoneUpdateCommandImpl.java
@@ -0,0 +1,14 @@
+package io.sphere.sdk.zones.commands;
+
+import io.sphere.sdk.commands.UpdateAction;
+import io.sphere.sdk.commands.UpdateCommandDslImpl;
+import io.sphere.sdk.models.Versioned;
+import io.sphere.sdk.zones.Zone;
+
+import java.util.List;
+
+final class ZoneUpdateCommandImpl extends UpdateCommandDslImpl<Zone, ZoneUpdateCommandImpl> implements ZoneUpdateCommand {
+    ZoneUpdateCommandImpl(final Versioned<Zone> versioned, final List<? extends UpdateAction<Zone>> updateActions) {
+        super(versioned, updateActions, ZoneEndpoint.ENDPOINT);
+    }
+}
