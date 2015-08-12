517825b2eb77bb1048de9bed32f76572e478ce00
Laura Luiz <laura.luiz@commercetools.de>
2015-08-12 10:31:55 +0200

    Remove unused field

--- sphere-common/src/main/java/io/sphere/sdk/search/TermFacetExpression.java
+++ sphere-common/src/main/java/io/sphere/sdk/search/TermFacetExpression.java
@@ -5,7 +5,6 @@ import javax.annotation.Nullable;
 import static java.util.Collections.emptyList;
 
 public class TermFacetExpression<T, V> extends TermExpression<T, V> implements FacetExpressionBase<T> {
-    SearchModel<T> model;
 
     TermFacetExpression(final SearchModel<T> searchModel, final TypeSerializer<V> typeSerializer, @Nullable final String alias) {
         super(searchModel, typeSerializer, emptyList(), alias);
