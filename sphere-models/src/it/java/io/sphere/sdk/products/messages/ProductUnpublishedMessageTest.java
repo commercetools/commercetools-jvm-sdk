b7942edd5c18343bafeebc9eaf97c808f4a908a1
schleichardt <michael.schleichardt@commercetools.de>
2015-08-12 15:39:41 +0200

    Add ProductUnpublishedMessage. close #633

--- sphere-models/src/it/java/io/sphere/sdk/products/messages/ProductUnpublishedMessageTest.java
+++ sphere-models/src/it/java/io/sphere/sdk/products/messages/ProductUnpublishedMessageTest.java
@@ -5,28 +5,29 @@ import io.sphere.sdk.products.Product;
 import io.sphere.sdk.products.ProductFixtures;
 import io.sphere.sdk.products.commands.ProductUpdateCommand;
 import io.sphere.sdk.products.commands.updateactions.Publish;
+import io.sphere.sdk.products.commands.updateactions.Unpublish;
 import io.sphere.sdk.queries.PagedQueryResult;
 import io.sphere.sdk.test.IntegrationTest;
 import org.junit.Test;
 
+import static java.util.Arrays.asList;
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class ProductPublishedMessageTest extends IntegrationTest {
+public class ProductUnpublishedMessageTest extends IntegrationTest {
     @Test
     public void message() {
         ProductFixtures.withUpdateableProduct(client(), product -> {
             assertThat(product.getMasterData().isPublished()).isFalse();
-            final Product publishedProduct = execute(ProductUpdateCommand.of(product, Publish.of()));
-            final PagedQueryResult<ProductPublishedMessage> queryResult = execute(MessageQuery.of()
+            final Product unpublishedProduct = execute(ProductUpdateCommand.of(product, asList(Publish.of(), Unpublish.of())));
+            final PagedQueryResult<ProductUnpublishedMessage> queryResult = execute(MessageQuery.of()
                     .withSort(m -> m.createdAt().sort().desc())
                     .withExpansionPaths(m -> m.resource())
                     .withLimit(1)
-                    .forMessageType(ProductPublishedMessage.MESSAGE_HINT));
-            final ProductPublishedMessage message = queryResult.head().get();
+                    .forMessageType(ProductUnpublishedMessage.MESSAGE_HINT));
+            final ProductUnpublishedMessage message = queryResult.head().get();
             assertThat(message.getResource().getId()).isEqualTo(product.getId());
-            assertThat(message.getProductProjection().getMasterVariant()).isEqualTo(publishedProduct.getMasterData().getCurrent().getMasterVariant());
-            assertThat(message.getResource().getObj().getMasterData().getCurrent().getSlug()).isEqualTo(message.getProductProjection().getSlug());
-            return publishedProduct;
+            assertThat(message.getResource().getObj().getMasterData().isPublished()).isFalse();
+            return unpublishedProduct;
         });
     }
 }
