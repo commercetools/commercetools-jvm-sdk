1f2a2efae3ff77ad66854ef5e4d7c595781aa283
schleichardt <michael.schleichardt@commercetools.de>
2015-08-18 10:40:37 +0200

    Remove unused method.

--- sphere-common/src/main/java/io/sphere/sdk/utils/ListUtils.java
+++ sphere-common/src/main/java/io/sphere/sdk/utils/ListUtils.java
@@ -14,26 +14,6 @@ public final class ListUtils {
     private ListUtils() {
     }
 
-    /**
-     * Partitions <code>list</code> in two lists according to <code>predicate</code>.
-     * @param list the list which should be divided
-     * @param predicate returns true if the element of <code>list</code> should belong to the first result list
-     * @param <T> generic type of the list
-     * @return the first list satisfies <code>predicate</code>, the second one not.
-     */
-    public static <T> Pair<List<T>, List<T>> partition(final List<T> list, final Predicate<T> predicate) {
-        final List<T> matchingPredicate = new ArrayList<>();
-        final List<T> notMatchingPredicate = new ArrayList<>();
-        for (final T element : list) {
-            if (predicate.test(element)) {
-                matchingPredicate.add(element);
-            } else {
-                notMatchingPredicate.add(element);
-            }
-        }
-        return Pair.of(matchingPredicate, notMatchingPredicate);
-    }
-
     public static <T> String join(final List<T> list) {
         return list.stream().map(i -> i.toString()).collect(joining(", "));
     }
