da05b97755f2050fd900a31ccd463603cfa09be0
schleichardt <michael.schleichardt@commercetools.de>
2015-09-23 07:07:10 +0200

    Add update action changeName for Types. #593

---
+++ sphere-models/src/it/java/io/sphere/sdk/types/commands/TypeUpdateCommandTest.java
@@ -0,0 +1,23 @@
+package io.sphere.sdk.types.commands;
+
+import io.sphere.sdk.models.LocalizedString;
+import io.sphere.sdk.test.IntegrationTest;
+import io.sphere.sdk.types.Type;
+import io.sphere.sdk.types.commands.updateactions.ChangeName;
+import org.junit.Test;
+
+import static io.sphere.sdk.test.SphereTestUtils.randomSlug;
+import static io.sphere.sdk.types.TypeFixtures.withUpdateableType;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class TypeUpdateCommandTest extends IntegrationTest {
+    @Test
+    public void changeName() {
+        withUpdateableType(client(), type -> {
+            final LocalizedString newName = randomSlug();
+            final Type updatedType = execute(TypeUpdateCommand.of(type, ChangeName.of(newName)));
+            assertThat(updatedType.getName()).isEqualTo(newName);
+            return updatedType;
+        });
+    }
+}
