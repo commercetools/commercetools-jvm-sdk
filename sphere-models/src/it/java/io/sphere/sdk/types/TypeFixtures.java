ab4f1eca868be7cdbd7c0a59c32248f338186b47
Michael Schleichardt <michael@schleichardt.net>
2015-09-25 14:02:44 +0200

    Add helper like "getFieldAsString" to CustomFields. #593

--- sphere-models/src/it/java/io/sphere/sdk/types/TypeFixtures.java
+++ sphere-models/src/it/java/io/sphere/sdk/types/TypeFixtures.java
@@ -23,6 +23,14 @@ import static java.util.stream.Collectors.toList;
 
 public class TypeFixtures {
 
+    public static final String LOC_STRING_FIELD_NAME = "locstring-field-name";
+    public static final String BOOLEAN_FIELD_NAME = "bool-field-name";
+    public static final String DATE_FIELD_NAME = "date-field-name";
+    public static final String TIME_FIELD_NAME = "time-field-name";
+    public static final String DATETIME_FIELD_NAME = "datetime-field-name";
+    public static final String MONEY_FIELD_NAME = "money-field-name";
+    public static final String INT_FIELD_NAME = "int-field-name";
+    public static final String DOUBLE_FIELD_NAME = "double-field-name";
     public static final String ENUM_FIELD_NAME = "enum-field-name";
     public static final String CAT_REFERENCE_FIELD_NAME = "catref";
     public static final String LOCALIZED_ENUM_FIELD_NAME = "localized-enum-field-name";
@@ -30,25 +38,72 @@ public class TypeFixtures {
     public static final String STRING_FIELD_NAME = "string-field-name";
 
     public static void withUpdateableType(final TestClient client, final UnaryOperator<Type> operator) {
-        final FieldDefinition stringFieldDefinition =
-                FieldDefinition.of(StringType.of(), STRING_FIELD_NAME, en("label"), false, TextInputHint.SINGLE_LINE);
-        final List<EnumValue> enumValues = asList(EnumValue.of("key1", "label1"), EnumValue.of("key2", "label2"));
-        final FieldDefinition enumFieldDefinition =
-                FieldDefinition.of(EnumType.of(enumValues), ENUM_FIELD_NAME, en("enum label"), false, TextInputHint.SINGLE_LINE);
-        final List<LocalizedEnumValue> localizedEnumValues = asList("1", "2").stream()
-                .map(s -> LocalizedEnumValue.of("key" + s, en("label " + s)))
-                .collect(toList());
-        final FieldDefinition localizedEnumFieldDefinition =
-                FieldDefinition.of(LocalizedEnumType.of(localizedEnumValues), LOCALIZED_ENUM_FIELD_NAME, en("localized enum label"), false, TextInputHint.SINGLE_LINE);
-        final FieldDefinition catReferenceFieldDefinition = FieldDefinition
-                .of(ReferenceType.of(Category.referenceTypeId()), CAT_REFERENCE_FIELD_NAME, en("category reference"), false, TextInputHint.SINGLE_LINE);
         final String typeKey = randomKey();
         final TypeDraft typeDraft = TypeDraftBuilder.of(typeKey, en("name of the custom type"), TYPE_IDS)
                 .description(en("description"))
-                .fieldDefinitions(asList(stringFieldDefinition, enumFieldDefinition, localizedEnumFieldDefinition, catReferenceFieldDefinition))
+                .fieldDefinitions(asList(stringfieldDefinition(), enumFieldDefinition(), localizedEnumFieldDefinition(), catRefDefinition(),
+                        booleanDefinition(), LocalizedStringDefinition(), intDefinition(), doubleDefinition(), moneyDefinition(),
+                        dateDefinition(), dateTimeDefinition(), timeDefinition()))
                 .build();
         final Type type = client.execute(TypeCreateCommand.of(typeDraft));
         final Type updatedType = operator.apply(type);
         client.execute(TypeDeleteCommand.of(updatedType));
+    }
+
+    private static FieldDefinition dateDefinition() {
+        return fieldDefinition(DateType.of(), DATE_FIELD_NAME);
+    }
+
+    private static FieldDefinition dateTimeDefinition() {
+        return fieldDefinition(DateTimeType.of(), DATETIME_FIELD_NAME);
+    }
+
+    private static FieldDefinition timeDefinition() {
+        return fieldDefinition(TimeType.of(), TIME_FIELD_NAME);
+    }
+
+    private static FieldDefinition booleanDefinition() {
+        return fieldDefinition(BooleanType.of(), BOOLEAN_FIELD_NAME);
+    }
+
+    private static FieldDefinition moneyDefinition() {
+        return fieldDefinition(MoneyType.of(), MONEY_FIELD_NAME);
+    }
+
+    private static FieldDefinition LocalizedStringDefinition() {
+        return fieldDefinition(LocalizedStringType.of(), LOC_STRING_FIELD_NAME);
+    }
+
+    private static FieldDefinition catRefDefinition() {
+        return fieldDefinition(ReferenceType.of(Category.referenceTypeId()), CAT_REFERENCE_FIELD_NAME);
+    }
+
+    private static FieldDefinition intDefinition() {
+        return fieldDefinition(NumberType.of(), INT_FIELD_NAME);
+    }
+
+    private static FieldDefinition doubleDefinition() {
+        return fieldDefinition(NumberType.of(), DOUBLE_FIELD_NAME);
+    }
+
+    private static FieldDefinition localizedEnumFieldDefinition() {
+        final List<LocalizedEnumValue> localizedEnumValues = asList("1", "2").stream()
+                .map(s -> LocalizedEnumValue.of("key" + s, en("label " + s)))
+                .collect(toList());
+        return fieldDefinition(LocalizedEnumType.of(localizedEnumValues), LOCALIZED_ENUM_FIELD_NAME);
+    }
+
+    private static FieldDefinition enumFieldDefinition() {
+        final List<EnumValue> enumValues = asList(EnumValue.of("key1", "label1"), EnumValue.of("key2", "label2"));
+        return fieldDefinition(EnumType.of(enumValues), ENUM_FIELD_NAME);
+    }
+
+    private static FieldDefinition stringfieldDefinition() {
+        return fieldDefinition(StringType.of(), STRING_FIELD_NAME);
+    }
+
+    private static FieldDefinition fieldDefinition(final FieldType fieldType, final String fieldName) {
+        return FieldDefinition
+                .of(fieldType, fieldName, en(fieldName), false, TextInputHint.SINGLE_LINE);
     }
 }
--- sphere-models/src/main/java/io/sphere/sdk/types/CustomFields.java
+++ sphere-models/src/main/java/io/sphere/sdk/types/CustomFields.java
@@ -3,10 +3,14 @@ package io.sphere.sdk.types;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonNode;
 import io.sphere.sdk.json.SphereJsonUtils;
-import io.sphere.sdk.models.Base;
-import io.sphere.sdk.models.Reference;
+import io.sphere.sdk.json.TypeReferences;
+import io.sphere.sdk.models.*;
 
 import javax.annotation.Nullable;
+import javax.money.MonetaryAmount;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.ZonedDateTime;
 import java.util.Map;
 import java.util.Optional;
 
@@ -26,9 +30,69 @@ public class CustomFields extends Base {
 
     @Nullable
     public <T> T getField(final String name, final TypeReference<T> typeReference) {
-        return Optional.ofNullable(fields.get(name))
+        return Optional.ofNullable(getField(name))
                 .map(jsonNode -> SphereJsonUtils.readObject(jsonNode, typeReference))
                 .orElse(null);
+    }
+
+    @Nullable
+    public String getFieldAsString(final String name) {
+        return getField(name, TypeReferences.stringTypeReference());
+    }
+
+    @Nullable
+    public Boolean getFieldAsBoolean(final String name) {
+        return getField(name, TypeReferences.booleanTypeReference());
+    }
+
+    @Nullable
+    public LocalizedString getFieldAsLocalizedString(final String name) {
+        return getField(name, LocalizedString.typeReference());
+    }
+
+    @Nullable
+    public String getFieldAsEnumKey(final String name) {
+        return getFieldAsString(name);
+    }
+
+    @Nullable
+    public String getFieldAsLocalizedEnumKey(final String name) {
+        return getFieldAsString(name);
+    }
+
+    @Nullable
+    public Integer getFieldAsInteger(final String name) {
+        return getField(name, TypeReferences.integerTypeReference());
+    }
+
+    @Nullable
+    public Long getFieldAsLong(final String name) {
+        return getField(name, TypeReferences.longTypeReference());
+    }
+
+    @Nullable
+    public Double getFieldAsDouble(final String name) {
+        return getField(name, TypeReferences.doubleTypeReference());
+    }
+
+    @Nullable
+    public MonetaryAmount getFieldAsMoney(final String name) {
+        return getField(name, TypeReferences.monetaryAmountTypeReference());
+    }
+
+    @Nullable
+    public LocalDate getFieldAsDate(final String name) {
+        return getField(name, TypeReferences.localDateTypeReference());
+    }
+
+    @Nullable
+    public ZonedDateTime getFieldAsDateTime(final String name) {
+        return getField(name, TypeReferences.zonedDateTimeTypeReference());
+    }
+
+    @Nullable
+    public LocalTime getFieldAsTime(final String name) {
+        return getField(name, TypeReferences.localTimeTypeReference());
     }
 
     public Reference<Type> getType() {
