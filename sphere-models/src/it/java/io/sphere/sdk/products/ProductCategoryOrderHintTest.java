0543df3028dc5bef89950cfc0a4a47b5964bf296
schleichardt <michael.schleichardt@commercetools.de>
2015-09-29 16:56:05 +0200

    Increase thread sleep due to eventual consistency. close #737

--- sphere-models/src/it/java/io/sphere/sdk/products/ProductCategoryOrderHintTest.java
+++ sphere-models/src/it/java/io/sphere/sdk/products/ProductCategoryOrderHintTest.java
@@ -123,7 +123,7 @@ public class ProductCategoryOrderHintTest extends IntegrationTest {
 
                     //eventual consistency
                     try {
-                        Thread.sleep(1000);
+                        Thread.sleep(2000);
                     } catch (InterruptedException e) {
                         throw new RuntimeException(e);
                     }
