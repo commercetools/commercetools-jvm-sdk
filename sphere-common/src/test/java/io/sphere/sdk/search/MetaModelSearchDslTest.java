b1169900df6dacfe169f8bfc3a07f2585d128a15
Laura Luiz <laura.luiz@commercetools.de>
2015-10-08 11:26:44 +0200

    Simplify test assertions

--- sphere-common/src/test/java/io/sphere/sdk/search/MetaModelSearchDslTest.java
+++ sphere-common/src/test/java/io/sphere/sdk/search/MetaModelSearchDslTest.java
@@ -38,65 +38,65 @@ public class MetaModelSearchDslTest {
     public void buildsFacets() throws Exception {
         final TestableSearchDsl dsl = new TestableSearchDsl().withFacets(FACET_EXPR_ONE);
         assertThat(dsl.facets())
-                .containsExactlyElementsOf(singletonList(FACET_EXPR_ONE));
+                .isEqualTo(singletonList(FACET_EXPR_ONE));
         assertThat(dsl.plusFacets(FACET_EXPR_TWO).facets())
-                .containsExactlyElementsOf(asList(FACET_EXPR_ONE, FACET_EXPR_TWO));
+                .isEqualTo(asList(FACET_EXPR_ONE, FACET_EXPR_TWO));
     }
 
     @Test
     public void buildsQueryFilters() throws Exception {
         final TestableSearchDsl dsl = new TestableSearchDsl().withQueryFilters(singletonList(FILTER_EXPR_ONE));
         assertThat(dsl.queryFilters())
-                .containsExactlyElementsOf(singletonList(FILTER_EXPR_ONE));
+                .isEqualTo(singletonList(FILTER_EXPR_ONE));
         assertThat(dsl.plusQueryFilters(singletonList(FILTER_EXPR_TWO)).queryFilters())
-                .containsExactlyElementsOf(asList(FILTER_EXPR_ONE, FILTER_EXPR_TWO));
+                .isEqualTo(asList(FILTER_EXPR_ONE, FILTER_EXPR_TWO));
     }
 
     @Test
     public void buildsResultFilters() throws Exception {
         final TestableSearchDsl dsl = new TestableSearchDsl().withResultFilters(singletonList(FILTER_EXPR_ONE));
         assertThat(dsl.resultFilters())
-                .containsExactlyElementsOf(singletonList(FILTER_EXPR_ONE));
+                .isEqualTo(singletonList(FILTER_EXPR_ONE));
         assertThat(dsl.plusResultFilters(singletonList(FILTER_EXPR_TWO)).resultFilters())
-                .containsExactlyElementsOf(asList(FILTER_EXPR_ONE, FILTER_EXPR_TWO));
+                .isEqualTo(asList(FILTER_EXPR_ONE, FILTER_EXPR_TWO));
     }
 
     @Test
     public void buildsFacetFilters() throws Exception {
         final TestableSearchDsl dsl = new TestableSearchDsl().withFacetFilters(singletonList(FILTER_EXPR_ONE));
         assertThat(dsl.facetFilters())
-                .containsExactlyElementsOf(singletonList(FILTER_EXPR_ONE));
+                .isEqualTo(singletonList(FILTER_EXPR_ONE));
         assertThat(dsl.plusFacetFilters(singletonList(FILTER_EXPR_TWO)).facetFilters())
-                .containsExactlyElementsOf(asList(FILTER_EXPR_ONE, FILTER_EXPR_TWO));
+                .isEqualTo(asList(FILTER_EXPR_ONE, FILTER_EXPR_TWO));
     }
 
     @Test
     public void buildsFacetedSearch() throws Exception {
         final TestableSearchDsl dsl = new TestableSearchDsl().plusFacetedSearch(FACETED_SEARCH_EXPR);
         assertThat(dsl.facets())
-                .containsExactlyElementsOf(singletonList(FACETED_SEARCH_EXPR.facetExpression()));
+                .isEqualTo(singletonList(FACETED_SEARCH_EXPR.facetExpression()));
         assertThat(dsl.queryFilters()).isEmpty();
         assertThat(dsl.resultFilters())
-                .containsExactlyElementsOf(dsl.facetFilters())
-                .containsExactlyElementsOf(FACETED_SEARCH_EXPR.filterExpressions());
+                .isEqualTo(dsl.facetFilters())
+                .isEqualTo(FACETED_SEARCH_EXPR.filterExpressions());
     }
 
     @Test
     public void buildsSort() throws Exception {
         final TestableSearchDsl dsl = new TestableSearchDsl().withSort(SORT_EXPR_ONE);
         assertThat(dsl.sort())
-                .containsExactlyElementsOf(singletonList(SORT_EXPR_ONE));
+                .isEqualTo(singletonList(SORT_EXPR_ONE));
         assertThat(dsl.plusSort(singletonList(SORT_EXPR_TWO)).sort())
-                .containsExactlyElementsOf(asList(SORT_EXPR_ONE, SORT_EXPR_TWO));
+                .isEqualTo(asList(SORT_EXPR_ONE, SORT_EXPR_TWO));
     }
 
     @Test
     public void buildsExpansion() throws Exception {
         final TestableSearchDsl dsl = new TestableSearchDsl().withExpansionPaths(EXPANSION_PATH_ONE);
         assertThat(dsl.expansionPaths())
-                .containsExactlyElementsOf(singletonList(EXPANSION_PATH_ONE));
+                .isEqualTo(singletonList(EXPANSION_PATH_ONE));
         assertThat(dsl.plusExpansionPaths(singletonList(EXPANSION_PATH_TWO)).expansionPaths())
-                .containsExactlyElementsOf(asList(EXPANSION_PATH_ONE, EXPANSION_PATH_TWO));
+                .isEqualTo(asList(EXPANSION_PATH_ONE, EXPANSION_PATH_TWO));
     }
 
     static final class TestableSearchDsl extends MetaModelSearchDslImpl<Object, TestableSearchDsl, Object, Object> {
