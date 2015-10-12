d2067c3788da9d3fb0b01f37e61aed2d7c73c750
schleichardt <michael.schleichardt@commercetools.de>
2015-10-12 17:04:29 +0200

    Move to sphere ideomatic transaction type constants. #768

--- sphere-models/src/main/java/io/sphere/sdk/payments/TransactionType.java
+++ sphere-models/src/main/java/io/sphere/sdk/payments/TransactionType.java
@@ -1,5 +1,13 @@
 package io.sphere.sdk.payments;
 
-public enum TransactionType {
+import com.fasterxml.jackson.annotation.JsonCreator;
+import io.sphere.sdk.models.SphereEnumeration;
+
+public enum TransactionType implements SphereEnumeration {
     AUTHORIZATION, CANCEL_AUTHORIZATION, CHARGE, REFUND, CHARGEBACK;
+
+    @JsonCreator
+    public static TransactionType ofSphereValue(final String value) {
+        return SphereEnumeration.findBySphereName(values(), value).get();
+    }
 }
