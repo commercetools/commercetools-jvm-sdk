18710a1edc81e604b9d71748cbc6c83614dfd00b
schleichardt <michael.schleichardt@commercetools.de>
2015-10-12 10:43:24 +0200

    Add payment update action addTransaction. #768

--- sphere-models/src/main/java/io/sphere/sdk/payments/commands/updateactions/AddTransaction.java
+++ sphere-models/src/main/java/io/sphere/sdk/payments/commands/updateactions/AddTransaction.java
@@ -4,6 +4,15 @@ import io.sphere.sdk.commands.UpdateActionImpl;
 import io.sphere.sdk.payments.Payment;
 import io.sphere.sdk.payments.Transaction;
 
+/**
+ * Adds a new financial transaction. Transactions should only be set if a notification from the PSP was received that a transaction was completed successfully. E.g. in the case of a refund, the transaction is not added when the refund-request is being sent to the PSP or the PSP acknowledges the request (the interfaceInteractions may be used for these), but when the PSP sends a notification that the refund has been executed.
+ *
+ * {@doc.gen intro}
+ *
+ * {@include.example io.sphere.sdk.payments.commands.PaymentUpdateCommandTest#transActions()}
+ *
+ *  @see Payment
+ */
 public class AddTransaction extends UpdateActionImpl<Payment> {
     private final Transaction transaction;
 
