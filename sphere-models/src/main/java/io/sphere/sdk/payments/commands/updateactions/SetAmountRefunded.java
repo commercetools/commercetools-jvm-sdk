90e6103bf8376420aa4df5c30d98085b2a3990be
schleichardt <michael.schleichardt@commercetools.de>
2015-10-12 10:00:32 +0200

    Add payment update action setAmountRefunded. #768

--- sphere-models/src/main/java/io/sphere/sdk/payments/commands/updateactions/SetAmountRefunded.java
+++ sphere-models/src/main/java/io/sphere/sdk/payments/commands/updateactions/SetAmountRefunded.java
@@ -6,6 +6,20 @@ import io.sphere.sdk.payments.Payment;
 import javax.annotation.Nullable;
 import javax.money.MonetaryAmount;
 
+/**
+ * Sets the amount of money that has been refunded to the customer. If no amount is provided, the amount is unset.
+ *
+ * {@doc.gen intro}
+ *
+ * {@include.example io.sphere.sdk.payments.commands.PaymentUpdateCommandTest#refunded()}
+ *
+ * <p>Example with multiple refunds:</p>
+ * {@include.example io.sphere.sdk.payments.commands.PaymentUpdateCommandTest#multiRefund()}
+ *
+ * @see Payment
+ *
+ *
+ */
 public class SetAmountRefunded extends UpdateActionImpl<Payment> {
     @Nullable
     private final MonetaryAmount amount;
