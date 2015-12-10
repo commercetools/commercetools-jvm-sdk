97a7aaeef0a3df4a3be448a1ec5b47e1d08e8e64
schleichardt <michael.schleichardt@commercetools.de>
2015-12-10 15:22:03 +0100

    TestClient implements BlockingSphereClient. #875

--- sphere-test-lib/src/main/java/io/sphere/sdk/client/TestClient.java
+++ sphere-test-lib/src/main/java/io/sphere/sdk/client/TestClient.java
@@ -1,32 +1,36 @@
 package io.sphere.sdk.client;
 
-import io.sphere.sdk.models.Base;
-
-import java.util.concurrent.ExecutionException;
+import java.util.concurrent.CompletionStage;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
 
-public final class TestClient extends Base implements AutoCloseable {
-    private final SphereClient underlying;
+public final class TestClient implements BlockingSphereClient {
+    private final BlockingSphereClient delegate;
 
     public TestClient(final SphereClient underlying) {
-        this.underlying = underlying;
-    }
-
-    public <T> T executeBlocking(final SphereRequest<T> sphereRequest) {
-        try {
-            return underlying.execute(sphereRequest).toCompletableFuture().get(20, TimeUnit.SECONDS);
-        } catch (final InterruptedException | ExecutionException | TimeoutException e) {
-            throw (e.getCause() instanceof RuntimeException) ? ((RuntimeException) e.getCause()) : new TestClientException(e);
-        }
+        this.delegate = BlockingSphereClient.of(underlying, 20, TimeUnit.SECONDS);
     }
 
     @Override
     public void close() {
-        underlying.close();
+        delegate.close();
+    }
+
+    @Override
+    public <T> CompletionStage<T> execute(final SphereRequest<T> sphereRequest) {
+        return delegate.execute(sphereRequest);
+    }
+
+    @Override
+    public <T> T executeBlocking(final SphereRequest<T> sphereRequest) {
+        return delegate.executeBlocking(sphereRequest);
+    }
+
+    @Override
+    public <T> T executeBlocking(final SphereRequest<T> sphereRequest, final long timeout, final TimeUnit unit) {
+        return delegate.executeBlocking(sphereRequest, timeout, unit);
     }
 
     public SphereClient getUnderlying() {
-        return underlying;
+        return this;
     }
 }
