2c722a6dc2a48c167ed0180db6a694aca58b6618
Laura Luiz <laura.luiz@commercetools.de>
2015-12-10 18:31:41 +0100

    Implement IT for matching variant flag

--- sphere-models/src/test/java/io/sphere/sdk/products/ProductProjectionTest.java
+++ sphere-models/src/test/java/io/sphere/sdk/products/ProductProjectionTest.java
@@ -25,14 +25,6 @@ public class ProductProjectionTest {
         assertThat(current).overridingErrorMessage("current can be empty").isNull();
     }
 
-    private Product getProduct() throws IOException {
-        return SphereJsonUtils.readObjectFromResource("ProductProjectionTest/product.json", Product.typeReference());
-    }
-
-    private ProductProjection getProductProjectionFromSearch() throws IOException {
-        return SphereJsonUtils.readObjectFromResource("ProductProjectionTest/product-projection-from-search.json", ProductProjection.typeReference());
-    }
-
     @Test
     public void deserialization() throws Exception {
         final String jsonString = "{\"offset\":0,\"count\":1,\"total\":15,\"results\":[{\"masterVariant\":{\"id\":1,\"sku\":\"2\",\"prices\":[{\"value\":{\"currencyCode\":\"EUR\",\"centAmount\":1413}}],\"images\":[{\"url\":\"https://s3.eu-central-1.amazonaws.com/commercetools-angry-bird-demo/Red+Skywalker+Plush+Toy.jpg\",\"dimensions\":{\"w\":0,\"h\":0}}],\"attributes\":[{\"name\":\"size\",\"value\":{\"key\":\"onesize\",\"label\":\"one size\"}},{\"name\":\"color\",\"value\":{\"key\":\"multicolor\",\"label\":\"multi color\"}}]},\"id\":\"184aaadc-63f6-4e6a-95fe-22b4f001612f\",\"version\":2,\"productType\":{\"typeId\":\"product-type\",\"id\":\"371e1d3a-e553-4b76-a834-dd3745c9afa0\"},\"name\":{\"en\":\"Red Skywalker Plush Toy\"},\"description\":{\"en\":\"Red Skywalker is a committed Rebel and is learning to be a Jedi Bird warrior.\"},\"categories\":[{\"typeId\":\"category\",\"id\":\"bc67f617-79ca-4f48-bf1b-18d5dbc7b552\"}],\"slug\":{\"en\":\"Red-Skywalker-Plush-Toy\"},\"metaTitle\":{\"en\":\"Red Skywalker Plush Toy\"},\"metaKeywords\":{\"en\":\"star wars, toy, angry birds\"},\"metaDescription\":{\"en\":\"Red Skywalker is a committed Rebel and is learning to be a Jedi Bird warrior.\"},\"variants\":[],\"searchKeywords\":{},\"hasStagedChanges\":false,\"published\":true,\"taxCategory\":{\"typeId\":\"tax-category\",\"id\":\"ab3939bd-0e2d-4fbb-a640-ae8a33c4e2c9\"},\"createdAt\":\"2015-02-25T11:20:11.466Z\",\"lastModifiedAt\":\"2015-02-25T11:20:11.687Z\"}],\"facets\":{}}";
@@ -50,20 +42,44 @@ public class ProductProjectionTest {
     }
 
     @Test
-    public void findMatchingVariants() throws Exception {
+    public void findMatchingVariantsOnSearch() throws Exception {
         final ProductProjection product = getProductProjectionFromSearch();
         assertThat(product.findMatchingVariants().collect(toList())).extracting(v -> v.getId()).containsExactly(2, 4);
     }
 
     @Test
-    public void findFirstMatchingVariant() throws Exception {
+    public void findFirstMatchingVariantOnSearch() throws Exception {
         final ProductProjection product = getProductProjectionFromSearch();
         assertThat(product.findFirstMatchingVariant().get().getId()).isEqualTo(2);
+    }
+
+    @Test
+    public void findMatchingVariantsOnQuery() throws Exception {
+        final ProductProjection product = getProductProjectionFromQuery();
+        assertThat(product.findMatchingVariants().collect(toList())).extracting(v -> v.getId()).isEmpty();
+    }
+
+    @Test
+    public void findFirstMatchingVariantOnQuery() throws Exception {
+        final ProductProjection product = getProductProjectionFromQuery();
+        assertThat(product.findFirstMatchingVariant()).isEmpty();
     }
 
     @Test
     public void productProjectionIsAVersionedProduct() throws IOException {
         final ProductProjection productProjection = getProduct().toProjection(ProductProjectionType.STAGED);
         final Versioned<Product> versioned = productProjection;
+    }
+
+    private Product getProduct() throws IOException {
+        return SphereJsonUtils.readObjectFromResource("ProductProjectionTest/product.json", Product.typeReference());
+    }
+
+    private ProductProjection getProductProjectionFromSearch() throws IOException {
+        return SphereJsonUtils.readObjectFromResource("ProductProjectionTest/product-projection-from-search.json", ProductProjection.typeReference());
+    }
+
+    private ProductProjection getProductProjectionFromQuery() throws IOException {
+        return SphereJsonUtils.readObjectFromResource("ProductProjectionTest/product-projection-from-query.json", ProductProjection.typeReference());
     }
 }
