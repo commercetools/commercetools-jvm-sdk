e62c2d144414d1192110089ac3fb3d1281adcd15
schleichardt <michael.schleichardt@commercetools.de>
2016-01-08 17:03:02 +0100

    Allow JSON creation of taxCategories. #785

---
+++ sphere-models/src/it/java/io/sphere/sdk/taxcategories/commands/TaxCategoryCreateCommandTest.java
@@ -0,0 +1,26 @@
+package io.sphere.sdk.taxcategories.commands;
+
+import com.neovisionaries.i18n.CountryCode;
+import io.sphere.sdk.json.SphereJsonUtils;
+import io.sphere.sdk.taxcategories.TaxCategoryDraft;
+import io.sphere.sdk.taxcategories.TaxRate;
+import io.sphere.sdk.taxcategories.TaxRateBuilder;
+import io.sphere.sdk.test.IntegrationTest;
+import org.junit.Test;
+
+import static io.sphere.sdk.taxcategories.TaxCategoryFixtures.withTaxCategory;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class TaxCategoryCreateCommandTest extends IntegrationTest {
+    @Test
+    public void createByJson() {
+        final TaxCategoryDraft draft = SphereJsonUtils.readObjectFromResource("drafts-tests/taxCategory.json", TaxCategoryDraft.class);
+        withTaxCategory(client(), draft, taxCategory -> {
+            assertThat(taxCategory.getName()).isEqualTo("a tax category");
+            final TaxRate taxRate = taxCategory.getTaxRates().get(0);
+            final TaxRate expectedTaxRate = TaxRateBuilder.of("default-tax", 0.19, true, CountryCode.DE).id(taxRate.getId()).build();
+            assertThat(taxRate).isEqualTo(expectedTaxRate);
+        });
+
+    }
+}
