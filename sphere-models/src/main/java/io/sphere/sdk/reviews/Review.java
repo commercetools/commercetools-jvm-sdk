ace5eea304b4ca83555c31c077119ad5138c9f7d
schleichardt <michael.schleichardt@commercetools.de>
2016-01-12 10:53:19 +0100

    Add Review model. #925

---
+++ sphere-models/src/main/java/io/sphere/sdk/reviews/Review.java
@@ -0,0 +1,68 @@
+package io.sphere.sdk.reviews;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import io.sphere.sdk.customers.Customer;
+import io.sphere.sdk.models.Reference;
+import io.sphere.sdk.models.Resource;
+import io.sphere.sdk.models.ResourceIdentifier;
+import io.sphere.sdk.states.State;
+import io.sphere.sdk.types.CustomFieldsDraft;
+
+import javax.annotation.Nullable;
+import java.util.Locale;
+
+public interface Review extends Resource<Review> {
+    @Nullable
+    String getAuthorName();
+
+    @Nullable
+    CustomFieldsDraft getCustom();
+
+    @Nullable
+    ResourceIdentifier<Customer> getCustomer();
+
+    @Nullable
+    String getKey();
+
+    @Nullable
+    Locale getLocale();
+
+    @Nullable
+    Integer getRating();
+
+    @Nullable
+    ResourceIdentifier<State> getState();
+
+    @Nullable
+    ResourceIdentifier<?> getTarget();
+
+    @Nullable
+    String getText();
+
+    @Nullable
+    String getTitle();
+
+    @Nullable
+    String getUniquenessValue();
+
+    static String referenceTypeId() {
+        return "review";
+    }
+
+    static TypeReference<Review> typeReference(){
+        return new TypeReference<Review>() {
+            @Override
+            public String toString() {
+                return "TypeReference<Review>";
+            }
+        };
+    }
+
+    static Reference<Review> referenceOfId(final String id) {
+        return Reference.of(referenceTypeId(), id);
+    }
+
+    default Reference<Review> toReference() {
+        return Reference.of(referenceTypeId(), getId(), this);
+    }
+}
