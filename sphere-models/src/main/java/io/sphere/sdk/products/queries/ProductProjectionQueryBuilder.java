5f361e861615cb2056f30cc56678d624e86f7007
schleichardt <michael.schleichardt@commercetools.de>
2016-01-18 12:30:26 +0100

    Add missing query builders. close #892

---
+++ sphere-models/src/main/java/io/sphere/sdk/products/queries/ProductProjectionQueryBuilder.java
@@ -0,0 +1,141 @@
+package io.sphere.sdk.products.queries;
+
+import io.sphere.sdk.expansion.ExpansionPathContainer;
+import io.sphere.sdk.products.ProductProjection;
+import io.sphere.sdk.products.ProductProjectionType;
+import io.sphere.sdk.products.expansion.ProductProjectionExpansionModel;
+import io.sphere.sdk.queries.QueryPredicate;
+import io.sphere.sdk.queries.QuerySort;
+import io.sphere.sdk.queries.ResourceMetaModelQueryDslBuilderImpl;
+
+import java.util.List;
+import java.util.function.Function;
+
+public class ProductProjectionQueryBuilder extends ResourceMetaModelQueryDslBuilderImpl<ProductProjectionQueryBuilder, ProductProjection, ProductProjectionQuery, ProductProjectionQueryModel, ProductProjectionExpansionModel<ProductProjection>> {
+
+    private ProductProjectionQueryBuilder(final ProductProjectionQuery template) {
+        super(template);
+    }
+
+    static ProductProjectionQueryBuilder ofStaged() {
+        return of(ProductProjectionType.STAGED);
+    }
+
+    static ProductProjectionQueryBuilder ofCurrent() {
+        return of(ProductProjectionType.CURRENT);
+    }
+
+    static ProductProjectionQueryBuilder of(final ProductProjectionType productProjectionType) {
+        return new ProductProjectionQueryBuilder(ProductProjectionQuery.of(productProjectionType));
+    }
+
+    @Override
+    protected ProductProjectionQueryBuilder getThis() {
+        return this;
+    }
+
+    @Override
+    public ProductProjectionQuery build() {
+        return super.build();
+    }
+
+    @Override
+    public ProductProjectionQueryBuilder fetchTotal(final boolean fetchTotal) {
+        return super.fetchTotal(fetchTotal);
+    }
+
+    @Override
+    public ProductProjectionQueryBuilder limit(final Long limit) {
+        return super.limit(limit);
+    }
+
+    @Override
+    public ProductProjectionQueryBuilder limit(final long limit) {
+        return super.limit(limit);
+    }
+
+    @Override
+    public ProductProjectionQueryBuilder offset(final Long offset) {
+        return super.offset(offset);
+    }
+
+    @Override
+    public ProductProjectionQueryBuilder offset(final long offset) {
+        return super.offset(offset);
+    }
+
+    @Override
+    public ProductProjectionQueryBuilder plusExpansionPaths(final Function<ProductProjectionExpansionModel<ProductProjection>, ExpansionPathContainer<ProductProjection>> m) {
+        return super.plusExpansionPaths(m);
+    }
+
+    @Override
+    public ProductProjectionQueryBuilder plusPredicates(final Function<ProductProjectionQueryModel, QueryPredicate<ProductProjection>> m) {
+        return super.plusPredicates(m);
+    }
+
+    @Override
+    public ProductProjectionQueryBuilder plusPredicates(final QueryPredicate<ProductProjection> queryPredicate) {
+        return super.plusPredicates(queryPredicate);
+    }
+
+    @Override
+    public ProductProjectionQueryBuilder plusPredicates(final List<QueryPredicate<ProductProjection>> queryPredicates) {
+        return super.plusPredicates(queryPredicates);
+    }
+
+    @Override
+    public ProductProjectionQueryBuilder plusSort(final Function<ProductProjectionQueryModel, QuerySort<ProductProjection>> m) {
+        return super.plusSort(m);
+    }
+
+    @Override
+    public ProductProjectionQueryBuilder plusSort(final List<QuerySort<ProductProjection>> sort) {
+        return super.plusSort(sort);
+    }
+
+    @Override
+    public ProductProjectionQueryBuilder plusSort(final QuerySort<ProductProjection> sort) {
+        return super.plusSort(sort);
+    }
+
+    @Override
+    public ProductProjectionQueryBuilder predicates(final Function<ProductProjectionQueryModel, QueryPredicate<ProductProjection>> m) {
+        return super.predicates(m);
+    }
+
+    @Override
+    public ProductProjectionQueryBuilder predicates(final QueryPredicate<ProductProjection> queryPredicate) {
+        return super.predicates(queryPredicate);
+    }
+
+    @Override
+    public ProductProjectionQueryBuilder predicates(final List<QueryPredicate<ProductProjection>> queryPredicates) {
+        return super.predicates(queryPredicates);
+    }
+
+    @Override
+    public ProductProjectionQueryBuilder sort(final Function<ProductProjectionQueryModel, QuerySort<ProductProjection>> m) {
+        return super.sort(m);
+    }
+
+    @Override
+    public ProductProjectionQueryBuilder sort(final List<QuerySort<ProductProjection>> sort) {
+        return super.sort(sort);
+    }
+
+    @Override
+    public ProductProjectionQueryBuilder sort(final QuerySort<ProductProjection> sort) {
+        return super.sort(sort);
+    }
+
+    @Override
+    public ProductProjectionQueryBuilder sortMulti(final Function<ProductProjectionQueryModel, List<QuerySort<ProductProjection>>> m) {
+        return super.sortMulti(m);
+    }
+
+    @Override
+    public ProductProjectionQueryBuilder expansionPaths(final Function<ProductProjectionExpansionModel<ProductProjection>, ExpansionPathContainer<ProductProjection>> m) {
+        return super.expansionPaths(m);
+    }
+}
---
+++ sphere-models/src/main/java/io/sphere/sdk/products/queries/ProductQueryBuilder.java
@@ -0,0 +1,132 @@
+package io.sphere.sdk.products.queries;
+
+import io.sphere.sdk.expansion.ExpansionPathContainer;
+import io.sphere.sdk.products.Product;
+import io.sphere.sdk.products.expansion.ProductExpansionModel;
+import io.sphere.sdk.queries.QueryPredicate;
+import io.sphere.sdk.queries.QuerySort;
+import io.sphere.sdk.queries.ResourceMetaModelQueryDslBuilderImpl;
+
+import java.util.List;
+import java.util.function.Function;
+
+public class ProductQueryBuilder extends ResourceMetaModelQueryDslBuilderImpl<ProductQueryBuilder, Product, ProductQuery, ProductQueryModel, ProductExpansionModel<Product>> {
+
+    private ProductQueryBuilder(final ProductQuery template) {
+        super(template);
+    }
+
+    public static ProductQueryBuilder of() {
+        return new ProductQueryBuilder(ProductQuery.of());
+    }
+
+    @Override
+    protected ProductQueryBuilder getThis() {
+        return this;
+    }
+
+    @Override
+    public ProductQuery build() {
+        return super.build();
+    }
+
+    @Override
+    public ProductQueryBuilder fetchTotal(final boolean fetchTotal) {
+        return super.fetchTotal(fetchTotal);
+    }
+
+    @Override
+    public ProductQueryBuilder limit(final Long limit) {
+        return super.limit(limit);
+    }
+
+    @Override
+    public ProductQueryBuilder limit(final long limit) {
+        return super.limit(limit);
+    }
+
+    @Override
+    public ProductQueryBuilder offset(final Long offset) {
+        return super.offset(offset);
+    }
+
+    @Override
+    public ProductQueryBuilder offset(final long offset) {
+        return super.offset(offset);
+    }
+
+    @Override
+    public ProductQueryBuilder plusExpansionPaths(final Function<ProductExpansionModel<Product>, ExpansionPathContainer<Product>> m) {
+        return super.plusExpansionPaths(m);
+    }
+
+    @Override
+    public ProductQueryBuilder plusPredicates(final Function<ProductQueryModel, QueryPredicate<Product>> m) {
+        return super.plusPredicates(m);
+    }
+
+    @Override
+    public ProductQueryBuilder plusPredicates(final QueryPredicate<Product> queryPredicate) {
+        return super.plusPredicates(queryPredicate);
+    }
+
+    @Override
+    public ProductQueryBuilder plusPredicates(final List<QueryPredicate<Product>> queryPredicates) {
+        return super.plusPredicates(queryPredicates);
+    }
+
+    @Override
+    public ProductQueryBuilder plusSort(final Function<ProductQueryModel, QuerySort<Product>> m) {
+        return super.plusSort(m);
+    }
+
+    @Override
+    public ProductQueryBuilder plusSort(final List<QuerySort<Product>> sort) {
+        return super.plusSort(sort);
+    }
+
+    @Override
+    public ProductQueryBuilder plusSort(final QuerySort<Product> sort) {
+        return super.plusSort(sort);
+    }
+
+    @Override
+    public ProductQueryBuilder predicates(final Function<ProductQueryModel, QueryPredicate<Product>> m) {
+        return super.predicates(m);
+    }
+
+    @Override
+    public ProductQueryBuilder predicates(final QueryPredicate<Product> queryPredicate) {
+        return super.predicates(queryPredicate);
+    }
+
+    @Override
+    public ProductQueryBuilder predicates(final List<QueryPredicate<Product>> queryPredicates) {
+        return super.predicates(queryPredicates);
+    }
+
+    @Override
+    public ProductQueryBuilder sort(final Function<ProductQueryModel, QuerySort<Product>> m) {
+        return super.sort(m);
+    }
+
+    @Override
+    public ProductQueryBuilder sort(final List<QuerySort<Product>> sort) {
+        return super.sort(sort);
+    }
+
+    @Override
+    public ProductQueryBuilder sort(final QuerySort<Product> sort) {
+        return super.sort(sort);
+    }
+
+    @Override
+    public ProductQueryBuilder sortMulti(final Function<ProductQueryModel, List<QuerySort<Product>>> m) {
+        return super.sortMulti(m);
+    }
+
+    @Override
+    public ProductQueryBuilder expansionPaths(final Function<ProductExpansionModel<Product>, ExpansionPathContainer<Product>> m) {
+        return super.expansionPaths(m);
+    }
+}
