eaad72b4b52a8f2a130f541b93b7b856f968d9e5
schleichardt <michael.schleichardt@commercetools.de>
2016-01-22 15:01:49 +0100

    PartialProductCatalogDataQueryModel to interface.

--- sphere-models/src/main/java/io/sphere/sdk/products/queries/PartialProductCatalogDataQueryModel.java
+++ sphere-models/src/main/java/io/sphere/sdk/products/queries/PartialProductCatalogDataQueryModel.java
@@ -1,15 +1,7 @@
 package io.sphere.sdk.products.queries;
 
-import io.sphere.sdk.queries.QueryModel;
-
-import javax.annotation.Nullable;
-
-public class PartialProductCatalogDataQueryModel extends ProductCatalogDataQueryModelImpl<PartialProductCatalogDataQueryModel> {
-    private PartialProductCatalogDataQueryModel(@Nullable final QueryModel<PartialProductCatalogDataQueryModel> parent, @Nullable final String pathSegment) {
-        super(parent, pathSegment);
-    }
-
-    public static PartialProductCatalogDataQueryModel of() {
-        return new PartialProductCatalogDataQueryModel(null, null);
+public interface PartialProductCatalogDataQueryModel extends ProductCatalogDataQueryModel<PartialProductCatalogDataQueryModel> {
+    static PartialProductCatalogDataQueryModel of() {
+        return new PartialProductCatalogDataQueryModelImpl(null, null);
     }
 }
