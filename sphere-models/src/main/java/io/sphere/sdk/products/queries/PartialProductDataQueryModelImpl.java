44c51f08b26a3403c0fd0f95fb4aebce6ede9713
schleichardt <michael.schleichardt@commercetools.de>
2016-01-22 13:58:04 +0100

    PartialProductDataQueryModel to interface.

---
+++ sphere-models/src/main/java/io/sphere/sdk/products/queries/PartialProductDataQueryModelImpl.java
@@ -0,0 +1,68 @@
+package io.sphere.sdk.products.queries;
+
+import io.sphere.sdk.categories.Category;
+import io.sphere.sdk.queries.*;
+
+import javax.annotation.Nullable;
+import java.util.function.Function;
+
+final class PartialProductDataQueryModelImpl extends ProductDataQueryModel<PartialProductDataQueryModel> implements PartialProductDataQueryModel {
+    PartialProductDataQueryModelImpl(@Nullable final QueryModel<PartialProductDataQueryModel> parent, @Nullable final String pathSegment) {
+        super(parent, pathSegment);
+    }
+
+    @Override
+    public ReferenceCollectionQueryModel<PartialProductDataQueryModel, Category> categories() {
+        return super.categories();
+    }
+
+    @Override
+    public LocalizedStringQueryModel<PartialProductDataQueryModel> description() {
+        return super.description();
+    }
+
+    @Override
+    public ProductVariantQueryModel<PartialProductDataQueryModel> masterVariant() {
+        return super.masterVariant();
+    }
+
+    @Override
+    public LocalizedStringQuerySortingModel<PartialProductDataQueryModel> metaDescription() {
+        return super.metaDescription();
+    }
+
+    @Override
+    public LocalizedStringQuerySortingModel<PartialProductDataQueryModel> metaKeywords() {
+        return super.metaKeywords();
+    }
+
+    @Override
+    public LocalizedStringQuerySortingModel<PartialProductDataQueryModel> metaTitle() {
+        return super.metaTitle();
+    }
+
+    @Override
+    public LocalizedStringQuerySortingModel<PartialProductDataQueryModel> name() {
+        return super.name();
+    }
+
+    @Override
+    public LocalizedStringQuerySortingModel<PartialProductDataQueryModel> slug() {
+        return super.slug();
+    }
+
+    @Override
+    public ProductVariantQueryModel<PartialProductDataQueryModel> variants() {
+        return super.variants();
+    }
+
+    @Override
+    public QueryPredicate<PartialProductDataQueryModel> where(final Function<PartialProductDataQueryModel, QueryPredicate<PartialProductDataQueryModel>> embeddedPredicate) {
+        return super.where(embeddedPredicate);
+    }
+
+    @Override
+    public QueryPredicate<PartialProductDataQueryModel> where(final QueryPredicate<PartialProductDataQueryModel> embeddedPredicate) {
+        return super.where(embeddedPredicate);
+    }
+}
