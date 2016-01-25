7df743a99bf73f22624599547cd4f1b353f735ed
schleichardt <michael.schleichardt@commercetools.de>
2016-01-25 14:15:57 +0100

    Fix tests. close #942

--- sphere-models/src/main/java/io/sphere/sdk/inventory/InventoryEntryDraft.java
+++ sphere-models/src/main/java/io/sphere/sdk/inventory/InventoryEntryDraft.java
@@ -1,11 +1,13 @@
 package io.sphere.sdk.inventory;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import io.sphere.sdk.channels.Channel;
 import io.sphere.sdk.models.Reference;
 
 import javax.annotation.Nullable;
 import java.time.ZonedDateTime;
 
+@JsonDeserialize(as = InventoryEntryDraftDsl.class)
 public interface InventoryEntryDraft {
     @Nullable
     ZonedDateTime getExpectedDelivery();
