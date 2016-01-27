dc9f32003c41f3bcd585b1c13e5d45d5e83e8d68
schleichardt <michael.schleichardt@commercetools.de>
2016-01-27 15:19:48 +0100

    Improve ApacheHttpClientAdapter. #666

--- sdk-http-apache-async/src/main/java/io/sphere/sdk/http/ApacheHttpClientAdapter.java
+++ sdk-http-apache-async/src/main/java/io/sphere/sdk/http/ApacheHttpClientAdapter.java
@@ -2,6 +2,11 @@ package io.sphere.sdk.http;
 
 import org.apache.http.impl.nio.client.CloseableHttpAsyncClient;
 
+/**
+ * Adapter to use a {@link CloseableHttpAsyncClient} as {@link HttpClient}.
+ *
+ * For more information about <a href="https://hc.apache.org" target="_blank">HttpComponents AsyncClient use their webpage</a>.
+ */
 public interface ApacheHttpClientAdapter extends HttpClient {
 
     static HttpClient of(final CloseableHttpAsyncClient client) {
