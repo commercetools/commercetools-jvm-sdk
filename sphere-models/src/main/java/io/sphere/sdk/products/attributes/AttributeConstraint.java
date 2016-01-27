b9981ccd408b2c2db23571f10302b52c5291de83
schleichardt <michael.schleichardt@commercetools.de>
2016-01-27 15:43:12 +0100

    Fix AttributeConstraint. #666

--- sphere-models/src/main/java/io/sphere/sdk/products/attributes/AttributeConstraint.java
+++ sphere-models/src/main/java/io/sphere/sdk/products/attributes/AttributeConstraint.java
@@ -3,6 +3,9 @@ package io.sphere.sdk.products.attributes;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import io.sphere.sdk.models.SphereEnumeration;
 
+/**
+ * AttributeConstraint enum tells how an attribute or a set of attributes should be validated across all variants of a product.
+ */
 public enum AttributeConstraint implements SphereEnumeration {
     /**
      * No constraints are applied to the attribute.
