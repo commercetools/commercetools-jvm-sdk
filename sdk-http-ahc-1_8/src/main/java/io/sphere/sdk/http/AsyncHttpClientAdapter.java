7c428349468cc01eefdcc2362f6b0778f789326a
schleichardt <michael.schleichardt@commercetools.de>
2016-01-27 15:23:00 +0100

    Improve AsyncHttpClientAdapter. #666

--- sdk-http-ahc-1_8/src/main/java/io/sphere/sdk/http/AsyncHttpClientAdapter.java
+++ sdk-http-ahc-1_8/src/main/java/io/sphere/sdk/http/AsyncHttpClientAdapter.java
@@ -2,6 +2,9 @@ package io.sphere.sdk.http;
 
 import com.ning.http.client.AsyncHttpClient;
 
+/**
+ * Adapter to use {@link AsyncHttpClient} (version 1.8.x) as {@link HttpClient}.
+ */
 public interface AsyncHttpClientAdapter extends HttpClient {
 
     static HttpClient of(final AsyncHttpClient asyncHttpClient) {
--- sdk-http-ahc-1_9/src/main/java/io/sphere/sdk/http/AsyncHttpClientAdapter.java
+++ sdk-http-ahc-1_9/src/main/java/io/sphere/sdk/http/AsyncHttpClientAdapter.java
@@ -2,6 +2,9 @@ package io.sphere.sdk.http;
 
 import com.ning.http.client.*;
 
+/**
+ * Adapter to use {@link AsyncHttpClient} version 1.9.x as {@link HttpClient}.
+ */
 public interface AsyncHttpClientAdapter extends HttpClient {
 
     static HttpClient of(final AsyncHttpClient asyncHttpClient) {
