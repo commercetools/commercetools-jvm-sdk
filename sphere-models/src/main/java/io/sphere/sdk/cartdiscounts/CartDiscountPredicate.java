75d4865f27dd26d870b9141cf74b2aa48cfda811
schleichardt <michael.schleichardt@commercetools.de>
2016-01-28 15:30:13 +0100

    Improve CartDiscountPredicate. #666

--- sphere-models/src/main/java/io/sphere/sdk/cartdiscounts/CartDiscountPredicate.java
+++ sphere-models/src/main/java/io/sphere/sdk/cartdiscounts/CartDiscountPredicate.java
@@ -1,5 +1,8 @@
 package io.sphere.sdk.cartdiscounts;
 
+/**
+ * The predicate offers a flexible way to define whether the discount can be applied to a cart.
+ */
 public interface CartDiscountPredicate {
     String toSphereCartPredicate();
 
