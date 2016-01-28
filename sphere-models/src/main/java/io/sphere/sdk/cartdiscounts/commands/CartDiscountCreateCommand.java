77654883ed148097cda415366db68836eb903268
schleichardt <michael.schleichardt@commercetools.de>
2016-01-28 15:09:58 +0100

    Improve CartDiscountCreateCommand. #666

--- sphere-models/src/main/java/io/sphere/sdk/cartdiscounts/commands/CartDiscountCreateCommand.java
+++ sphere-models/src/main/java/io/sphere/sdk/cartdiscounts/commands/CartDiscountCreateCommand.java
@@ -7,6 +7,8 @@ import io.sphere.sdk.commands.DraftBasedCreateCommand;
 import io.sphere.sdk.expansion.MetaModelReferenceExpansionDsl;
 
 /**
+ * Creates a {@link CartDiscount}.
+ *
  * {@include.example io.sphere.sdk.cartdiscounts.commands.CartDiscountCreateCommandTest#execution()}
  */
 public interface CartDiscountCreateCommand extends DraftBasedCreateCommand<CartDiscount, CartDiscountDraft>, MetaModelReferenceExpansionDsl<CartDiscount, CartDiscountCreateCommand, CartDiscountExpansionModel<CartDiscount>> {
