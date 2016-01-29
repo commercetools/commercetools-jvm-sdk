a11429ba9a804e735dce026c109486cf6b6e4e8f
schleichardt <michael.schleichardt@commercetools.de>
2016-01-29 18:54:51 +0100

    Improve CustomResourceQueryModelImpl. #666

--- sphere-models/src/main/java/io/sphere/sdk/types/queries/CustomResourceQueryModelImpl.java
+++ sphere-models/src/main/java/io/sphere/sdk/types/queries/CustomResourceQueryModelImpl.java
@@ -3,6 +3,10 @@ package io.sphere.sdk.types.queries;
 import io.sphere.sdk.queries.QueryModel;
 import io.sphere.sdk.queries.ResourceQueryModelImpl;
 
+/**
+ * Internal base class
+ * @param <T> resource type
+ */
 public abstract class CustomResourceQueryModelImpl<T> extends ResourceQueryModelImpl<T> implements WithCustomQueryModel<T> {
     public CustomResourceQueryModelImpl(final QueryModel<T> parent, final String pathSegment) {
         super(parent, pathSegment);
