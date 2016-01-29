046ae204becf8a729b53f74f452990589a38bcba
schleichardt <michael.schleichardt@commercetools.de>
2016-01-29 09:57:05 +0100

    Improve. #666

--- sphere-models/src/main/java/io/sphere/sdk/producttypes/commands/updateactions/ChangeAttributeDefinitionLabel.java
+++ sphere-models/src/main/java/io/sphere/sdk/producttypes/commands/updateactions/ChangeAttributeDefinitionLabel.java
@@ -2,6 +2,7 @@ package io.sphere.sdk.producttypes.commands.updateactions;
 
 import io.sphere.sdk.commands.UpdateActionImpl;
 import io.sphere.sdk.models.LocalizedString;
+import io.sphere.sdk.products.attributes.AttributeDefinition;
 import io.sphere.sdk.producttypes.ProductType;
 
 /**
@@ -10,6 +11,8 @@ import io.sphere.sdk.producttypes.ProductType;
  * {@doc.gen intro}
  *
  * {@include.example io.sphere.sdk.producttypes.commands.ProductTypeUpdateCommandTest#changeLabel()}
+ *
+ * @see AttributeDefinition#getLabel()
  */
 public class ChangeAttributeDefinitionLabel extends UpdateActionImpl<ProductType> {
     private final String attributeName;
