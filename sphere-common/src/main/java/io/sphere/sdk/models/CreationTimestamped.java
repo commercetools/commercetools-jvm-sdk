143d57c08b2a5371fb53ddb43db26a352e4bbab4
schleichardt <michael.schleichardt@commercetools.de>
2016-01-29 15:27:35 +0100

    Some improvements. #666

--- sphere-common/src/main/java/io/sphere/sdk/models/CreationTimestamped.java
+++ sphere-common/src/main/java/io/sphere/sdk/models/CreationTimestamped.java
@@ -1,8 +1,10 @@
 package io.sphere.sdk.models;
 
-
 import java.time.ZonedDateTime;
 
+/**
+ * Interface for objects which have a timestamp for their creation time.
+ */
 public interface CreationTimestamped {
     ZonedDateTime getCreatedAt();
 }
--- sphere-models/src/main/java/io/sphere/sdk/types/CustomDraft.java
+++ sphere-models/src/main/java/io/sphere/sdk/types/CustomDraft.java
@@ -2,6 +2,9 @@ package io.sphere.sdk.types;
 
 import javax.annotation.Nullable;
 
+/**
+ * Interface for draft objects which include custom fields.
+ */
 public interface CustomDraft {
     @Nullable
     CustomFieldsDraft getCustom();
