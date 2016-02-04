df9f414970ee9b47de4ad3f752a8f9adde243a56
schleichardt <michael.schleichardt@commercetools.de>
2016-02-04 14:25:19 +0100

    Improve InventoryEntryExpansionModel. #666

--- sphere-models/src/main/java/io/sphere/sdk/inventory/expansion/InventoryEntryExpansionModel.java
+++ sphere-models/src/main/java/io/sphere/sdk/inventory/expansion/InventoryEntryExpansionModel.java
@@ -1,7 +1,7 @@
 package io.sphere.sdk.inventory.expansion;
 
+import io.sphere.sdk.channels.expansion.ChannelExpansionModel;
 import io.sphere.sdk.expansion.ExpansionModel;
-import io.sphere.sdk.expansion.ExpansionPathContainer;
 import io.sphere.sdk.inventory.InventoryEntry;
 
 public final class InventoryEntryExpansionModel<T> extends ExpansionModel<T> {
@@ -9,8 +9,8 @@ public final class InventoryEntryExpansionModel<T> extends ExpansionModel<T> {
     InventoryEntryExpansionModel() {
     }
 
-    public ExpansionPathContainer<T> supplyChannel() {
-        return expansionPath("supplyChannel");
+    public ChannelExpansionModel<T> supplyChannel() {
+        return ChannelExpansionModel.of(buildPathExpression(), "supplyChannel");
     }
 
     public static InventoryEntryExpansionModel<InventoryEntry> of() {
