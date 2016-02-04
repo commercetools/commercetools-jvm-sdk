18a987022f7ce2392c22e9056c7dfb2816e25ad5
schleichardt <michael.schleichardt@commercetools.de>
2016-02-04 11:57:10 +0100

    CustomObjectExpansionModel as interface. #666

--- sphere-models/src/main/java/io/sphere/sdk/customobjects/expansion/CustomObjectExpansionModel.java
+++ sphere-models/src/main/java/io/sphere/sdk/customobjects/expansion/CustomObjectExpansionModel.java
@@ -1,19 +1,14 @@
 package io.sphere.sdk.customobjects.expansion;
 
 import io.sphere.sdk.customobjects.CustomObject;
-import io.sphere.sdk.expansion.ExpansionModel;
 
 /**
  * Placeholder for custom object reference expansion. Does not provide a functionality yet.
  *
  * @param <T> The type of the value of this custom object.
  */
-public final class CustomObjectExpansionModel<T> extends ExpansionModel<T> {
-    private CustomObjectExpansionModel() {
-        super();
-    }
-
-    public static <T> CustomObjectExpansionModel<CustomObject<T>> of(){
-        return new CustomObjectExpansionModel<>();
+public interface CustomObjectExpansionModel<T> {
+    static <T> CustomObjectExpansionModel<CustomObject<T>> of(){
+        return new CustomObjectExpansionModelImpl<>();
     }
 }
---
+++ sphere-models/src/main/java/io/sphere/sdk/customobjects/expansion/CustomObjectExpansionModelImpl.java
@@ -0,0 +1,9 @@
+package io.sphere.sdk.customobjects.expansion;
+
+import io.sphere.sdk.expansion.ExpansionModel;
+
+final class CustomObjectExpansionModelImpl<T> extends ExpansionModel<T> implements CustomObjectExpansionModel<T> {
+    CustomObjectExpansionModelImpl() {
+        super();
+    }
+}
