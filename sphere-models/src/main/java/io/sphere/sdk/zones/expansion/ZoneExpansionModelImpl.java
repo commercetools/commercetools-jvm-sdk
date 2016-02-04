c67678f5fdfea471f91a99e3d875782db990478b
schleichardt <michael.schleichardt@commercetools.de>
2016-02-04 10:33:54 +0100

    ZoneExpansionModel as interface. #666

---
+++ sphere-models/src/main/java/io/sphere/sdk/zones/expansion/ZoneExpansionModelImpl.java
@@ -0,0 +1,22 @@
+package io.sphere.sdk.zones.expansion;
+
+import io.sphere.sdk.expansion.ExpansionModel;
+import io.sphere.sdk.expansion.ExpansionPath;
+
+import java.util.List;
+
+public final class ZoneExpansionModelImpl<T> extends ExpansionModel<T> implements ZoneExpansionModel<T> {
+    ZoneExpansionModelImpl(final List<String> parentPath, final String path) {
+        super(parentPath, path);
+    }
+
+    ZoneExpansionModelImpl() {
+        super();
+    }
+
+
+    @Override
+    public List<ExpansionPath<T>> expansionPaths() {
+        return buildExpansionPaths();
+    }
+}
