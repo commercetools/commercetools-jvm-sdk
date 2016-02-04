bb285f232496e9977f55f30141fc0bdb9e0d8d6d
schleichardt <michael.schleichardt@commercetools.de>
2016-02-04 13:19:24 +0100

    ReviewExpansionModel as interface. #666

--- sphere-models/src/main/java/io/sphere/sdk/reviews/expansion/ReviewExpansionModel.java
+++ sphere-models/src/main/java/io/sphere/sdk/reviews/expansion/ReviewExpansionModel.java
@@ -1,38 +1,23 @@
 package io.sphere.sdk.reviews.expansion;
 
 import io.sphere.sdk.customers.expansion.CustomerExpansionModel;
-import io.sphere.sdk.expansion.ExpansionModel;
 import io.sphere.sdk.expansion.ExpansionPathContainer;
 import io.sphere.sdk.reviews.Review;
 import io.sphere.sdk.states.expansion.StateExpansionModel;
 
 /**
-  DSL class to create expansion path expressions.
+ DSL class to create expansion path expressions.
 
  @param <T> the type for which the expansion path is
  */
-public final class ReviewExpansionModel<T> extends ExpansionModel<T> {
-    public ReviewExpansionModel(final String parentPath, final String path) {
-        super(parentPath, path);
-    }
+public interface ReviewExpansionModel<T> {
+    CustomerExpansionModel<T> customer();
 
-    ReviewExpansionModel() {
-        super();
-    }
+    StateExpansionModel<T> state();
 
-    public static ReviewExpansionModel<Review> of() {
-        return new ReviewExpansionModel<>();
-    }
+    ExpansionPathContainer<T> target();
 
-    public CustomerExpansionModel<T> customer() {
-        return CustomerExpansionModel.of(buildPathExpression(), "customer");
-    }
-
-    public StateExpansionModel<T> state() {
-        return StateExpansionModel.of(buildPathExpression(), "state");
-    }
-
-    public ExpansionPathContainer<T> target() {
-        return expansionPath("target");
+    static ReviewExpansionModel<Review> of() {
+        return new ReviewExpansionModelImpl<>();
     }
 }
---
+++ sphere-models/src/main/java/io/sphere/sdk/reviews/expansion/ReviewExpansionModelImpl.java
@@ -0,0 +1,31 @@
+package io.sphere.sdk.reviews.expansion;
+
+import io.sphere.sdk.customers.expansion.CustomerExpansionModel;
+import io.sphere.sdk.expansion.ExpansionModel;
+import io.sphere.sdk.expansion.ExpansionPathContainer;
+import io.sphere.sdk.states.expansion.StateExpansionModel;
+
+final class ReviewExpansionModelImpl<T> extends ExpansionModel<T> implements ReviewExpansionModel<T> {
+    ReviewExpansionModelImpl(final String parentPath, final String path) {
+        super(parentPath, path);
+    }
+
+    ReviewExpansionModelImpl() {
+        super();
+    }
+
+    @Override
+    public CustomerExpansionModel<T> customer() {
+        return CustomerExpansionModel.of(buildPathExpression(), "customer");
+    }
+
+    @Override
+    public StateExpansionModel<T> state() {
+        return StateExpansionModel.of(buildPathExpression(), "state");
+    }
+
+    @Override
+    public ExpansionPathContainer<T> target() {
+        return expansionPath("target");
+    }
+}
