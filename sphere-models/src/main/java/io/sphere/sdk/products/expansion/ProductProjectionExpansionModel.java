68c208f92494592469fe46e0dc151f5fc17009cb
schleichardt <michael.schleichardt@commercetools.de>
2016-02-04 10:52:12 +0100

    ProductProjectionExpansionModel as interface. #666

--- sphere-models/src/main/java/io/sphere/sdk/products/expansion/ProductProjectionExpansionModel.java
+++ sphere-models/src/main/java/io/sphere/sdk/products/expansion/ProductProjectionExpansionModel.java
@@ -1,63 +1,26 @@
 package io.sphere.sdk.products.expansion;
 
 import io.sphere.sdk.categories.expansion.CategoryExpansionModel;
-import io.sphere.sdk.expansion.ExpansionModel;
 import io.sphere.sdk.products.ProductProjection;
 import io.sphere.sdk.producttypes.expansion.ProductTypeExpansionModel;
 import io.sphere.sdk.taxcategories.expansion.TaxCategoryExpansionModel;
 
-import java.util.List;
-import java.util.stream.Collectors;
+public interface ProductProjectionExpansionModel<T> {
+    ProductTypeExpansionModel<T> productType();
 
-import static java.util.Arrays.asList;
-import static org.apache.commons.lang3.StringUtils.isEmpty;
+    TaxCategoryExpansionModel<T> taxCategory();
 
-public final class ProductProjectionExpansionModel<T> extends ExpansionModel<T> {
+    CategoryExpansionModel<ProductProjection> categories(int index);
 
-    private static final String VARIANTS = "variants[*]";
-    private static final String MASTER_VARIANT = "masterVariant";
+    CategoryExpansionModel<ProductProjection> categories();
 
-    private ProductProjectionExpansionModel() {
-    }
+    ProductVariantExpansionModel<T> masterVariant();
 
-    public ProductTypeExpansionModel<T> productType() {
-        return ProductTypeExpansionModel.of(buildPathExpression(), "productType");
-    }
+    ProductVariantExpansionModel<T> variants();
 
-    public TaxCategoryExpansionModel<T> taxCategory() {
-        return TaxCategoryExpansionModel.of(buildPathExpression(), "taxCategory");
-    }
+    ProductVariantExpansionModel<ProductProjection> allVariants();
 
-    public CategoryExpansionModel<ProductProjection> categories(final int index) {
-        return new CategoryExpansionModel<>(pathExpression(), "categories[" + index + "]");
-    }
-
-    public CategoryExpansionModel<ProductProjection> categories() {
-        return new CategoryExpansionModel<>(pathExpression(), "categories[*]");
-    }
-
-    public ProductVariantExpansionModel<T> masterVariant() {
-        return new ProductVariantExpansionModel<>(pathExpression(), MASTER_VARIANT);
-    }
-
-    public ProductVariantExpansionModel<T> variants() {
-        return new ProductVariantExpansionModel<>(pathExpression(), VARIANTS);
-    }
-
-    public ProductVariantExpansionModel<ProductProjection> allVariants() {
-        final List<String> parents = pathExpression();
-        final List<String> currentPaths = asList(MASTER_VARIANT, VARIANTS);
-        final List<String> paths;
-        if (parents.isEmpty()) {
-            paths = currentPaths;
-        } else {
-            paths = parents.stream().flatMap(p -> currentPaths.stream().map(c -> (isEmpty(p) ? "" : p + ".") + c)).collect(Collectors.toList());
-        }
-
-        return new ProductVariantExpansionModel<>(paths, null);
-    }
-
-    public static ProductProjectionExpansionModel<ProductProjection> of() {
-        return new ProductProjectionExpansionModel<>();
+    static ProductProjectionExpansionModel<ProductProjection> of() {
+        return new ProductProjectionExpansionModelImpl<>();
     }
 }
---
+++ sphere-models/src/main/java/io/sphere/sdk/products/expansion/ProductProjectionExpansionModelImpl.java
@@ -0,0 +1,66 @@
+package io.sphere.sdk.products.expansion;
+
+import io.sphere.sdk.categories.expansion.CategoryExpansionModel;
+import io.sphere.sdk.expansion.ExpansionModel;
+import io.sphere.sdk.products.ProductProjection;
+import io.sphere.sdk.producttypes.expansion.ProductTypeExpansionModel;
+import io.sphere.sdk.taxcategories.expansion.TaxCategoryExpansionModel;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.util.Arrays.asList;
+import static org.apache.commons.lang3.StringUtils.isEmpty;
+
+final class ProductProjectionExpansionModelImpl<T> extends ExpansionModel<T> implements ProductProjectionExpansionModel<T> {
+
+    private static final String VARIANTS = "variants[*]";
+    private static final String MASTER_VARIANT = "masterVariant";
+
+    ProductProjectionExpansionModelImpl() {
+    }
+
+    @Override
+    public ProductTypeExpansionModel<T> productType() {
+        return ProductTypeExpansionModel.of(buildPathExpression(), "productType");
+    }
+
+    @Override
+    public TaxCategoryExpansionModel<T> taxCategory() {
+        return TaxCategoryExpansionModel.of(buildPathExpression(), "taxCategory");
+    }
+
+    @Override
+    public CategoryExpansionModel<ProductProjection> categories(final int index) {
+        return new CategoryExpansionModel<>(pathExpression(), "categories[" + index + "]");
+    }
+
+    @Override
+    public CategoryExpansionModel<ProductProjection> categories() {
+        return new CategoryExpansionModel<>(pathExpression(), "categories[*]");
+    }
+
+    @Override
+    public ProductVariantExpansionModel<T> masterVariant() {
+        return new ProductVariantExpansionModel<>(pathExpression(), MASTER_VARIANT);
+    }
+
+    @Override
+    public ProductVariantExpansionModel<T> variants() {
+        return new ProductVariantExpansionModel<>(pathExpression(), VARIANTS);
+    }
+
+    @Override
+    public ProductVariantExpansionModel<ProductProjection> allVariants() {
+        final List<String> parents = pathExpression();
+        final List<String> currentPaths = asList(MASTER_VARIANT, VARIANTS);
+        final List<String> paths;
+        if (parents.isEmpty()) {
+            paths = currentPaths;
+        } else {
+            paths = parents.stream().flatMap(p -> currentPaths.stream().map(c -> (isEmpty(p) ? "" : p + ".") + c)).collect(Collectors.toList());
+        }
+
+        return new ProductVariantExpansionModel<>(paths, null);
+    }
+}
