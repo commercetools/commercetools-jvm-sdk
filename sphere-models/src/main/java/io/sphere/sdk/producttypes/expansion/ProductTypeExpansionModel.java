f91b0bb1aef370635aa7aef7722b2e61e01a5159
schleichardt <michael.schleichardt@commercetools.de>
2016-02-04 10:41:36 +0100

    ProductTypeExpansionModel as interface. #666

--- sphere-models/src/main/java/io/sphere/sdk/producttypes/expansion/ProductTypeExpansionModel.java
+++ sphere-models/src/main/java/io/sphere/sdk/producttypes/expansion/ProductTypeExpansionModel.java
@@ -1,18 +1,14 @@
 package io.sphere.sdk.producttypes.expansion;
 
-import io.sphere.sdk.expansion.ExpansionModel;
 import io.sphere.sdk.producttypes.ProductType;
 
 /**
-  DSL class to create expansion path expressions.
+ DSL class to create expansion path expressions.
 
  @param <T> the type for which the expansion path is
  */
-public final class ProductTypeExpansionModel<T> extends ExpansionModel<T> {
-    private ProductTypeExpansionModel() {
-    }
-
-    public static ProductTypeExpansionModel<ProductType> of() {
-        return new ProductTypeExpansionModel<>();
+public interface ProductTypeExpansionModel<T> {
+    static ProductTypeExpansionModel<ProductType> of() {
+        return new ProductTypeExpansionModelImpl<>();
     }
 }
---
+++ sphere-models/src/main/java/io/sphere/sdk/producttypes/expansion/ProductTypeExpansionModelImpl.java
@@ -0,0 +1,8 @@
+package io.sphere.sdk.producttypes.expansion;
+
+import io.sphere.sdk.expansion.ExpansionModel;
+
+final class ProductTypeExpansionModelImpl<T> extends ExpansionModel<T> implements ProductTypeExpansionModel<T> {
+    ProductTypeExpansionModelImpl() {
+    }
+}
