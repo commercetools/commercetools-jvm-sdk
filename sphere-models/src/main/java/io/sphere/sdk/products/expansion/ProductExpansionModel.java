2c50f449d244f6386336ddc7dd031a66bc241e17
schleichardt <michael.schleichardt@commercetools.de>
2016-02-04 14:22:13 +0100

    Improve ProductExpansionModel. #666

--- sphere-models/src/main/java/io/sphere/sdk/products/expansion/ProductExpansionModel.java
+++ sphere-models/src/main/java/io/sphere/sdk/products/expansion/ProductExpansionModel.java
@@ -1,19 +1,20 @@
 package io.sphere.sdk.products.expansion;
 
 import io.sphere.sdk.expansion.ExpansionModel;
-import io.sphere.sdk.expansion.ExpansionPathContainer;
 import io.sphere.sdk.products.Product;
+import io.sphere.sdk.producttypes.expansion.ProductTypeExpansionModel;
+import io.sphere.sdk.taxcategories.expansion.TaxCategoryExpansionModel;
 
 public final class ProductExpansionModel<T> extends ExpansionModel<T> {
     ProductExpansionModel() {
     }
 
-    public ExpansionPathContainer<T> productType() {
-        return expansionPath("productType");
+    public ProductTypeExpansionModel<T> productType() {
+        return ProductTypeExpansionModel.of(buildPathExpression(), "productType");
     }
 
-    public ExpansionPathContainer<T> taxCategory() {
-        return expansionPath("taxCategory");
+    public TaxCategoryExpansionModel<T> taxCategory() {
+        return TaxCategoryExpansionModel.of(buildPathExpression(), "taxCategory");
     }
 
     public ProductCatalogExpansionModel<T> masterData() {
