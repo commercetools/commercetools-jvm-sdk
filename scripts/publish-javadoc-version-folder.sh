a5623e249ea04efed2f348f5e3715b02ee0ded13
schleichardt <michael.schleichardt@commercetools.de>
2016-02-08 15:05:00 +0100

    fix Javadoc commands #626

--- scripts/publish-javadoc-version-folder.sh
+++ scripts/publish-javadoc-version-folder.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 if [ "$TRAVIS_BRANCH"  = "master" -a "$TRAVIS_PULL_REQUEST" = "false" ]; then
-    ./sbt "sphere-jvm-sdk/gitPublish target/javaunidoc https://$GH_TOKEN:x-oauth-basic@github.com/$TRAVIS_REPO_SLUG.git \"javadoc/<version>\" sphere-oss automation@commercetools.de"
+    ./sbt "commercetools-jvm-sdk/gitPublish target/javaunidoc https://$GH_TOKEN:x-oauth-basic@github.com/$TRAVIS_REPO_SLUG.git \"javadoc/<version>\" sphere-oss automation@commercetools.de"
 fi
