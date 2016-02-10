35bece83feb9a5d8e6c57a4780f6459a927f5556
schleichardt <michael.schleichardt@commercetools.de>
2016-02-10 10:21:50 +0100

    Review io.sphere.sdk.commands package, close #757

--- commercetools-sdk-base/src/main/java/io/sphere/sdk/commands/UpdateCommand.java
+++ commercetools-sdk-base/src/main/java/io/sphere/sdk/commands/UpdateCommand.java
@@ -2,6 +2,14 @@ package io.sphere.sdk.commands;
 
 import java.util.List;
 
+/**
+ * Command which updates a commercetools resource based on a list of update actions.
+ * @param <T> type of the updated object
+ */
 public interface UpdateCommand<T> extends Command<T> {
+    /**
+     * Gets the list of updates which should be applied to the object
+     * @return update actions
+     */
     List<? extends UpdateAction<T>> getUpdateActions();
 }
--- commercetools-sdk-base/src/main/java/io/sphere/sdk/commands/UpdateCommandDsl.java
+++ commercetools-sdk-base/src/main/java/io/sphere/sdk/commands/UpdateCommandDsl.java
@@ -3,9 +3,20 @@ package io.sphere.sdk.commands;
 import io.sphere.sdk.models.ResourceView;
 import io.sphere.sdk.models.Versioned;
 
+/**
+ * Subinterface for update commands which supports updating the version for retries on {@link io.sphere.sdk.client.ConcurrentModificationException}s
+ * @param <T> the type of the resource to be updated
+ * @param <C> the type of the update command
+ */
 public interface UpdateCommandDsl<T extends ResourceView<T, T>, C extends UpdateCommandDsl<T, C>> extends UpdateCommand<T> {
     //!do not add a getter for the version for this class hierarchy
 
-    //note if an endpoint supports also update by key then add new method and deletegate to this method with Versioned.of("key=" + key, version)
+    //note if an endpoint supports also update by key then add new method and delegate to this method with Versioned.of("key=" + key, version)
+
+    /**
+     * Creates a copy of this update command with a different value for version.
+     * @param versioned the new versioned parameter which is expected to contain the most recent version of the resource, do not change the ID
+     * @return the new update command
+     */
     C withVersion(final Versioned<T> versioned);
 }
