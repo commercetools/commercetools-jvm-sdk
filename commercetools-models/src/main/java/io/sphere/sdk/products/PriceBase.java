d63bfa99aef4de702e5fdd2c8e9a9405e7a364e5
schleichardt <michael.schleichardt@commercetools.de>
2016-03-10 11:56:53 +0100

    Price to interface, add ScopedPrice interface #1042

---
+++ commercetools-models/src/main/java/io/sphere/sdk/products/PriceBase.java
@@ -0,0 +1,149 @@
+package io.sphere.sdk.products;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.neovisionaries.i18n.CountryCode;
+import io.sphere.sdk.channels.Channel;
+import io.sphere.sdk.customergroups.CustomerGroup;
+import io.sphere.sdk.models.Base;
+import io.sphere.sdk.models.Reference;
+import io.sphere.sdk.productdiscounts.DiscountedPrice;
+import io.sphere.sdk.types.CustomFields;
+import io.sphere.sdk.types.TypeDraft;
+
+import javax.annotation.Nullable;
+import javax.money.MonetaryAmount;
+import java.time.ZonedDateTime;
+
+/**
+ * Amount that must be paid when buying goods.
+ *
+ * <p>A Price can have {@link io.sphere.sdk.types.Custom custom fields}.</p>
+ *
+ * For construction use a {@link PriceBuilder}.
+ *
+ * @see io.sphere.sdk.products.commands.updateactions.AddPrice
+ * @see io.sphere.sdk.products.commands.updateactions.ChangePrice
+ * @see io.sphere.sdk.products.commands.updateactions.RemovePrice
+ * @see ProductVariant#getPrices()
+ */
+public final class PriceBase extends Base implements Price, ScopedPrice {
+    @Nullable
+    private final MonetaryAmount currentValue;
+    private final MonetaryAmount value;
+    @Nullable
+    private final CountryCode country;
+    @Nullable
+    private final Reference<CustomerGroup> customerGroup;
+    @Nullable
+    private final Reference<Channel> channel;
+    @Nullable
+    private final DiscountedPrice discounted;
+    @Nullable
+    private final ZonedDateTime validFrom;
+    @Nullable
+    private final ZonedDateTime validUntil;
+    @Nullable
+    private final String id;
+    @Nullable
+    private final CustomFields custom;
+
+    @JsonCreator
+    PriceBase(final MonetaryAmount value, @Nullable final CountryCode country,
+              @Nullable final Reference<CustomerGroup> customerGroup, @Nullable final Reference<Channel> channel,
+              @Nullable final DiscountedPrice discounted,
+              @Nullable final ZonedDateTime validFrom, @Nullable final ZonedDateTime validUntil, @Nullable final String id,
+              @Nullable final CustomFields custom, @Nullable final MonetaryAmount currentValue) {
+        this.value = value;
+        this.country = country;
+        this.customerGroup = customerGroup;
+        this.channel = channel;
+        this.discounted = discounted;
+        this.validFrom = validFrom;
+        this.validUntil = validUntil;
+        this.id = id;
+        this.custom = custom;
+        this.currentValue = currentValue;
+    }
+
+    @Override
+    public MonetaryAmount getValue() {
+        return value;
+    }
+
+    @Override
+    @Nullable
+    public CountryCode getCountry() {
+        return country;
+    }
+
+    @Override
+    @Nullable
+    public Reference<CustomerGroup> getCustomerGroup() {
+        return customerGroup;
+    }
+
+    @Override
+    @Nullable
+    public Reference<Channel> getChannel() {
+        return channel;
+    }
+
+    /**
+     * Supplies a discount if there is any.
+     * Beware that another discount can win and in here is another discount than you expect.
+     * @return discount data
+     */
+    @Override
+    @Nullable
+    public DiscountedPrice getDiscounted() {
+        return discounted;
+    }
+
+    @Override
+    @Nullable
+    public ZonedDateTime getValidFrom() {
+        return validFrom;
+    }
+
+    @Override
+    @Nullable
+    public ZonedDateTime getValidUntil() {
+        return validUntil;
+    }
+
+    /**
+     * The unique ID of this price. Only read only.
+     * @return price id
+     */
+    @Override
+    @Nullable
+    public String getId() {
+        return id;
+    }
+
+    @Override
+    @Nullable
+    public CustomFields getCustom() {
+        return custom;
+    }
+
+    @Override
+    @Nullable
+    public MonetaryAmount getCurrentValue() {
+        return currentValue;
+    }
+
+    public boolean equalsIgnoreId(final PriceBase price) {
+        return price != null && withId(null).equals(price.withId(null));
+    }
+
+    /**
+     * An identifier for this resource which supports {@link CustomFields}.
+     * @see TypeDraft#getResourceTypeIds()
+     * @see io.sphere.sdk.types.Custom
+     * @return ID of this resource type
+     */
+    public static String resourceTypeId() {
+        return "product-price";
+    }
+}
--- commercetools-models/src/main/java/io/sphere/sdk/products/PriceBuilder.java
+++ commercetools-models/src/main/java/io/sphere/sdk/products/PriceBuilder.java
@@ -93,6 +93,6 @@ public final class PriceBuilder extends Base implements Builder<Price> {
 
     @Override
     public Price build() {
-        return new Price(value, country, customerGroup, channel, discounted, validFrom, validUntil, id, null);
+        return new PriceBase(value, country, customerGroup, channel, discounted, validFrom, validUntil, id, null, null);
     }
 }
