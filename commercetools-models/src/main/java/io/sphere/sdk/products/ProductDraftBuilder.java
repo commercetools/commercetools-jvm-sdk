3108999475d956a502d31a09cd13d84f8a7cf6ea
schleichardt <michael.schleichardt@commercetools.de>
2016-04-04 10:37:26 +0200

    inline ProductDataProductDraftBuilderBase

--- commercetools-models/src/main/java/io/sphere/sdk/products/ProductDraftBuilder.java
+++ commercetools-models/src/main/java/io/sphere/sdk/products/ProductDraftBuilder.java
@@ -1,25 +1,39 @@
 package io.sphere.sdk.products;
 
-import io.sphere.sdk.models.Builder;
-import io.sphere.sdk.models.LocalizedString;
-import io.sphere.sdk.models.ResourceIdentifiable;
-import io.sphere.sdk.models.ResourceIdentifier;
+import io.sphere.sdk.categories.Category;
+import io.sphere.sdk.models.*;
 import io.sphere.sdk.producttypes.ProductType;
+import io.sphere.sdk.search.SearchKeywords;
+import io.sphere.sdk.states.State;
+import io.sphere.sdk.taxcategories.TaxCategory;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import javax.annotation.Nullable;
+import java.util.*;
 
 import static io.sphere.sdk.utils.SphereInternalUtils.listOf;
 
-public final class ProductDraftBuilder extends ProductDataProductDraftBuilderBase<ProductDraftBuilder> implements Builder<ProductDraft> {
+public final class ProductDraftBuilder extends Base implements Builder<ProductDraft>,WithLocalizedSlug, MetaAttributes {
 
     private final ResourceIdentifier<ProductType> productType;
     private ProductVariantDraft masterVariant;
     private List<ProductVariantDraft> variants = Collections.emptyList();
+    private LocalizedString name;
+    private LocalizedString slug;
+    private LocalizedString description;
+    private LocalizedString metaTitle;
+    private LocalizedString metaDescription;
+    private LocalizedString metaKeywords;
+    private Set<Reference<Category>> categories = Collections.emptySet();
+    private SearchKeywords searchKeywords = SearchKeywords.of();
+    private Reference<TaxCategory> taxCategory;
+    @Nullable
+    private Reference<State> state;
+    @Nullable
+    private CategoryOrderHints categoryOrderHints;
 
     private ProductDraftBuilder(final ResourceIdentifier<ProductType> productType, final LocalizedString name, final LocalizedString slug, final ProductVariantDraft masterVariant) {
-        super(name, slug);
+        this.name = name;
+        this.slug = slug;
         this.productType = productType;
         this.masterVariant = masterVariant;
     }
@@ -38,7 +52,6 @@ public final class ProductDraftBuilder extends ProductDataProductDraftBuilderBas
         return new ProductDraftImpl(productType, getName(), getSlug(), getDescription(), getCategories(), getMetaTitle(), getMetaDescription(), getMetaKeywords(), masterVariant, variants, getTaxCategory(), getSearchKeywords(), getState(), getCategoryOrderHints());
     }
 
-    @Override
     protected ProductDraftBuilder getThis() {
         return this;
     }
@@ -73,5 +86,114 @@ public final class ProductDraftBuilder extends ProductDataProductDraftBuilderBas
 
     public List<ProductVariantDraft> getVariants() {
         return variants;
+    }
+
+    public ProductDraftBuilder slug(final LocalizedString slug) {
+        this.slug = slug;
+        return getThis();
+    }
+
+    public ProductDraftBuilder name(final LocalizedString name) {
+        this.name = name;
+        return getThis();
+    }
+
+    public ProductDraftBuilder description(@Nullable final LocalizedString description) {
+        this.description = description;
+        return getThis();
+    }
+
+    public ProductDraftBuilder metaTitle(@Nullable final LocalizedString metaTitle) {
+        this.metaTitle = metaTitle;
+        return getThis();
+    }
+
+    public ProductDraftBuilder metaDescription(@Nullable final LocalizedString metaDescription) {
+        this.metaDescription = metaDescription;
+        return getThis();
+    }
+
+    public ProductDraftBuilder metaKeywords(@Nullable final LocalizedString metaKeywords) {
+        this.metaKeywords = metaKeywords;
+        return getThis();
+    }
+
+    public ProductDraftBuilder categories(final Set<Reference<Category>> categories) {
+        this.categories = categories;
+        return getThis();
+    }
+
+    public ProductDraftBuilder categories(final List<Reference<Category>> categories) {
+        return categories(new LinkedHashSet<>(categories));
+    }
+
+    public ProductDraftBuilder searchKeywords(final SearchKeywords searchKeywords) {
+        this.searchKeywords = searchKeywords;
+        return getThis();
+    }
+
+    public ProductDraftBuilder categoryOrderHints(@Nullable final CategoryOrderHints categoryOrderHints) {
+        this.categoryOrderHints = categoryOrderHints;
+        return getThis();
+    }
+
+    public LocalizedString getName() {
+        return name;
+    }
+
+    public LocalizedString getSlug() {
+        return slug;
+    }
+
+    @Nullable
+    public LocalizedString getDescription() {
+        return description;
+    }
+
+    @Nullable
+    public LocalizedString getMetaTitle() {
+        return metaTitle;
+    }
+
+    @Nullable
+    public LocalizedString getMetaDescription() {
+        return metaDescription;
+    }
+
+    @Nullable
+    public LocalizedString getMetaKeywords() {
+        return metaKeywords;
+    }
+
+    public Set<Reference<Category>> getCategories() {
+        return categories;
+    }
+
+    public SearchKeywords getSearchKeywords() {
+        return searchKeywords;
+    }
+
+    public Reference<TaxCategory> getTaxCategory() {
+        return taxCategory;
+    }
+
+    public ProductDraftBuilder taxCategory(final Referenceable<TaxCategory> taxCategory) {
+        this.taxCategory = taxCategory != null ? taxCategory.toReference() : null;
+        return getThis();
+    }
+
+    public ProductDraftBuilder state(@Nullable final Referenceable<State> state) {
+        this.state = Optional.ofNullable(state).map(Referenceable::toReference).orElse(null);
+        return getThis();
+    }
+
+    @Nullable
+    public Reference<State> getState() {
+        return state;
+    }
+
+    @Nullable
+    public CategoryOrderHints getCategoryOrderHints() {
+        return categoryOrderHints;
     }
 }
