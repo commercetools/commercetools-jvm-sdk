9234f6d18ddd75b578ff776e255f2ee39f9f8ec2
schleichardt <michael.schleichardt@commercetools.de>
2016-05-02 12:02:54 +0200

    add missing queryable fields for channels, close #1117

--- commercetools-models/src/main/java/io/sphere/sdk/channels/queries/ChannelQueryModel.java
+++ commercetools-models/src/main/java/io/sphere/sdk/channels/queries/ChannelQueryModel.java
@@ -1,8 +1,8 @@
 package io.sphere.sdk.channels.queries;
 
 import io.sphere.sdk.channels.Channel;
-import io.sphere.sdk.queries.ResourceQueryModel;
-import io.sphere.sdk.queries.StringQuerySortingModel;
+import io.sphere.sdk.channels.ChannelRole;
+import io.sphere.sdk.queries.*;
 import io.sphere.sdk.reviews.queries.ReviewRatingStatisticsQueryModel;
 import io.sphere.sdk.types.queries.WithCustomQueryModel;
 
@@ -10,6 +10,12 @@ public interface ChannelQueryModel extends ResourceQueryModel<Channel>, WithCust
     StringQuerySortingModel<Channel> key();
 
     ReviewRatingStatisticsQueryModel<Channel> reviewRatingStatistics();
+
+    SphereEnumerationCollectionQueryModel<Channel, ChannelRole> roles();
+
+    LocalizedStringOptionalQueryModel<Channel> name();
+
+    LocalizedStringOptionalQueryModel<Channel> description();
 
     static ChannelQueryModel of() {
         return new ChannelQueryModelImpl(null, null);
