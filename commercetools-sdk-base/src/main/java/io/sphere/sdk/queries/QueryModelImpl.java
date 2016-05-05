56c4ce0d543625e37b1db27d4bfa3fa73e08aaed
schleichardt <michael.schleichardt@commercetools.de>
2016-05-05 13:35:49 +0200

    fix merge errors

--- commercetools-sdk-base/src/main/java/io/sphere/sdk/queries/QueryModelImpl.java
+++ commercetools-sdk-base/src/main/java/io/sphere/sdk/queries/QueryModelImpl.java
@@ -5,6 +5,7 @@ import io.sphere.sdk.models.SphereEnumeration;
 
 import javax.annotation.Nullable;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static io.sphere.sdk.queries.StringQuerySortingModel.normalize;
 import static io.sphere.sdk.utils.SphereInternalUtils.toStream;
@@ -57,7 +58,7 @@ public class QueryModelImpl<T> extends Base implements QueryModel<T> {
         return new ReferenceCollectionQueryModelImpl<>(this, pathSegment);
     }
 
-    protected <E extends SphereEnumeration> SphereEnumerationOptionalQueryModel<T, E> enumerationQueryModel(final String pathSegment) {
+    protected <E extends SphereEnumeration> SphereEnumerationQueryModelImpl<T, E> enumerationQueryModel(final String pathSegment) {
         return new SphereEnumerationQueryModelImpl<>(this, pathSegment);
     }
 
