919998abe4358f135e751dcdcc6ae11949479098
schleichardt <michael.schleichardt@commercetools.de>
2016-05-17 10:07:24 +0200

    remove ProductTypeDraft.of without key parameter #1129

--- commercetools-models/src/main/java/io/sphere/sdk/producttypes/ProductTypeDraft.java
+++ commercetools-models/src/main/java/io/sphere/sdk/producttypes/ProductTypeDraft.java
@@ -24,20 +24,6 @@ public interface ProductTypeDraft extends WithKey {
     /**
      * Creates a input object to create a {@link ProductType}.
      *
-     * @param name the name of the product type, SPHERE.IO does not check that the name will be unique so it is best practice to check if a product type of this name already exists
-     * @param description description of the product type
-     * @param attributes definitions of attributes for the product type
-     * @return draft for a product type
-     * @deprecated use {@link #of(String, String, String, List)} since new product types should include a key
-     */
-    @Deprecated
-    static ProductTypeDraft of(final String name, final String description, final List<AttributeDefinition> attributes) {
-        return of(null, name, description, attributes);
-    }
-
-    /**
-     * Creates a input object to create a {@link ProductType}.
-     *
      * @param key unique key to identify the created {@link ProductType}
      * @param name the name of the product type, SPHERE.IO does not check that the name will be unique so it is best practice to check if a product type of this name already exists
      * @param description description of the product type
