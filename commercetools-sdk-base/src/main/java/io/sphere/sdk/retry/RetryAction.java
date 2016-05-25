3fc3181204f71fb125c8acf7e04c7023609878d0
schleichardt <michael.schleichardt@commercetools.de>
2016-05-25 23:47:08 +0200

    WIP retry strategies #818

--- commercetools-sdk-base/src/main/java/io/sphere/sdk/retry/RetryAction.java
+++ commercetools-sdk-base/src/main/java/io/sphere/sdk/retry/RetryAction.java
@@ -1,9 +1,114 @@
 package io.sphere.sdk.retry;
 
 import javax.annotation.Nullable;
+import java.time.Duration;
+import java.util.function.Function;
+
+import static io.sphere.sdk.retry.RetryActions.convert;
+import static io.sphere.sdk.retry.RetryActions.validateMaxAttempts;
 
 @FunctionalInterface
 public interface RetryAction {
     @Nullable
     <P> RetryResult<P> apply(final RetryOperationContext<P> retryOperationContext);
+
+    //TODO param??? maybe RetryRuleContext? rename parameter?
+    static RetryAction scheduledRetry(final long maxAttempts, final Function<RetryContext, Duration> f) {
+        validateMaxAttempts(maxAttempts);
+
+        return new RetryActionImpl() {
+            @Override
+            public <P> RetryResult<P> apply(final RetryOperationContext<P> retryOperationContext) {
+                if (retryOperationContext.getAttempt() > maxAttempts) {
+                    return RetryResult.resume(retryOperationContext.getLatest().getError());
+                } else {
+                    final Duration duration = f.apply(convert(retryOperationContext));
+                    final P parameterObject = retryOperationContext.getLatest().getParameter();
+                    return RetryResult.retryScheduled(parameterObject, duration);
+                }
+            }
+
+            @Override
+            protected String getDescription() {
+                return "schedule retry retry up to " + maxAttempts + " times";
+            }
+        };
+    }
+
+    static RetryAction shutdownServiceAndSendFirstException() {
+        return new RetryActionImpl() {
+            @Override
+            public <P> RetryResult<P> apply(final RetryOperationContext<P> retryOperationContext) {
+                return RetryResult.stop(retryOperationContext.getFirst().getError());
+            }
+
+            @Override
+            protected String getDescription() {
+                return "shut down and send first exception";
+            }
+        };
+    }
+
+    static RetryAction shutdownServiceAndSendLatestException() {
+        return new RetryActionImpl() {
+            @Override
+            public <P> RetryResult<P> apply(final RetryOperationContext<P> retryOperationContext) {
+                return RetryResult.stop(retryOperationContext.getLatest().getError());
+            }
+
+            @Override
+            protected String getDescription() {
+                return "shut down and send latest exception";
+            }
+        };
+    }
+
+    static RetryAction giveUpAndSendFirstException() {
+        return new RetryActionImpl() {
+            @Override
+            public <P> RetryResult<P> apply(final RetryOperationContext<P> retryOperationContext) {
+                return RetryResult.resume(retryOperationContext.getFirst().getError());
+            }
+
+            @Override
+            protected String getDescription() {
+                return "shut down and send first exception";
+            }
+        };
+    }
+
+    static RetryAction giveUpAndSendLatestException() {
+        return new RetryActionImpl() {
+            @Override
+            public <P> RetryResult<P> apply(final RetryOperationContext<P> retryOperationContext) {
+                return RetryResult.resume(retryOperationContext.getLatest().getError());
+            }
+
+            @Override
+            protected String getDescription() {
+                return "shut down and send latest exception";
+            }
+        };
+    }
+
+    static RetryAction immediateRetries(final long maxAttempts) {
+        validateMaxAttempts(maxAttempts);
+
+        return new RetryActionImpl() {
+            @Override
+            public <P> RetryResult<P> apply(final RetryOperationContext<P> retryOperationContext) {
+                if (retryOperationContext.getAttempt() > maxAttempts) {
+                    return RetryResult.resume(retryOperationContext.getLatest().getError());
+                } else {
+                    final P parameterObject = retryOperationContext.getLatest().getParameter();
+                    return RetryResult.retryImmediately(parameterObject);
+                }
+            }
+
+            @Override
+            protected String getDescription() {
+                return "immediate retry up to " + maxAttempts + " times";
+            }
+        };
+    }
 }
--- commercetools-sdk-base/src/main/java/io/sphere/sdk/retry/RetryActions.java
+++ commercetools-sdk-base/src/main/java/io/sphere/sdk/retry/RetryActions.java
@@ -1,121 +1,17 @@
 package io.sphere.sdk.retry;
 
-import java.time.Duration;
-import java.util.function.Function;
-
-public final class RetryActions {
+final class RetryActions {
 
     private RetryActions() {
     }
 
-    private static RetryContext convert(final RetryOperationContext<?> c) {
+    static RetryContext convert(final RetryOperationContext<?> c) {
         return new RetryContextImpl<>(c.getAttempt(), c.getLatest().getError(), c.getLatest().getParameter());
     }
 
-
-    //TODO param??? maybe RetryRuleContext? rename parameter?
-    public static RetryAction scheduledRetry(final long maxAttempts, final Function<RetryContext, Duration> f) {
-        validateMaxAttempts(maxAttempts);
-
-        return new RetryActionImpl() {
-            @Override
-            public <P> RetryResult<P> apply(final RetryOperationContext<P> retryOperationContext) {
-                if (retryOperationContext.getAttempt() > maxAttempts) {
-                    return RetryResult.resume(retryOperationContext.getLatest().getError());
-                } else {
-                    final Duration duration = f.apply(convert(retryOperationContext));
-                    final P parameterObject = retryOperationContext.getLatest().getParameter();
-                    return RetryResult.retryScheduled(parameterObject, duration);
-                }
-            }
-
-            @Override
-            protected String getDescription() {
-                return "schedule retry retry up to " + maxAttempts + " times";
-            }
-        };
-    }
-
-    private static void validateMaxAttempts(final long maxAttempts) {
+    static void validateMaxAttempts(final long maxAttempts) {
         if (maxAttempts < 1) {
             throw new IllegalArgumentException("Max attempts must be greater than 0.");
         }
-    }
-
-    public static RetryAction shutdownServiceAndSendFirstException() {
-        return new RetryActionImpl() {
-            @Override
-            public <P> RetryResult<P> apply(final RetryOperationContext<P> retryOperationContext) {
-                return RetryResult.stop(retryOperationContext.getFirst().getError());
-            }
-
-            @Override
-            protected String getDescription() {
-                return "shut down and send first exception";
-            }
-        };
-    }
-
-    public static RetryAction shutdownServiceAndSendLatestException() {
-        return new RetryActionImpl() {
-            @Override
-            public <P> RetryResult<P> apply(final RetryOperationContext<P> retryOperationContext) {
-                return RetryResult.stop(retryOperationContext.getLatest().getError());
-            }
-
-            @Override
-            protected String getDescription() {
-                return "shut down and send latest exception";
-            }
-        };
-    }
-
-    public static RetryAction giveUpAndSendFirstException() {
-        return new RetryActionImpl() {
-            @Override
-            public <P> RetryResult<P> apply(final RetryOperationContext<P> retryOperationContext) {
-                return RetryResult.resume(retryOperationContext.getFirst().getError());
-            }
-
-            @Override
-            protected String getDescription() {
-                return "shut down and send first exception";
-            }
-        };
-    }
-
-    public static RetryAction giveUpAndSendLatestException() {
-        return new RetryActionImpl() {
-            @Override
-            public <P> RetryResult<P> apply(final RetryOperationContext<P> retryOperationContext) {
-                return RetryResult.resume(retryOperationContext.getLatest().getError());
-            }
-
-            @Override
-            protected String getDescription() {
-                return "shut down and send latest exception";
-            }
-        };
-    }
-
-    public static RetryAction immediateRetries(final long maxAttempts) {
-        validateMaxAttempts(maxAttempts);
-
-        return new RetryActionImpl() {
-            @Override
-            public <P> RetryResult<P> apply(final RetryOperationContext<P> retryOperationContext) {
-                if (retryOperationContext.getAttempt() > maxAttempts) {
-                    return RetryResult.resume(retryOperationContext.getLatest().getError());
-                } else {
-                    final P parameterObject = retryOperationContext.getLatest().getParameter();
-                    return RetryResult.retryImmediately(parameterObject);
-                }
-            }
-
-            @Override
-            protected String getDescription() {
-                return "immediate retry up to " + maxAttempts + " times";
-            }
-        };
     }
 }
