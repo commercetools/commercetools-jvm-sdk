7e26906883571b3f095b9ee6ff4d41d4ca3461cd
schleichardt <michael.schleichardt@commercetools.de>
2016-07-07 10:41:17 +0200

    WIP add messages field to the Project class #1160

---
+++ commercetools-models/src/main/java/io/sphere/sdk/projects/MessagesConfiguration.java
@@ -0,0 +1,8 @@
+package io.sphere.sdk.projects;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+
+@JsonDeserialize(as = MessagesConfigurationImpl.class)
+public interface MessagesConfiguration {
+    Boolean isEnabled();
+}
