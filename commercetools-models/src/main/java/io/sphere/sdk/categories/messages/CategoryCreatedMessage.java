643687b25aa5a560f57490d45e4e23788ab81d30
mht-dotsource <mht@dotsource.de>
2016-07-11 13:52:48 +0200

    added Messages for category created and slug change. close #1025

---
+++ commercetools-models/src/main/java/io/sphere/sdk/categories/messages/CategoryCreatedMessage.java
@@ -0,0 +1,36 @@
+package io.sphere.sdk.categories.messages;
+
+import java.time.ZonedDateTime;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+
+import io.sphere.sdk.categories.Category;
+import io.sphere.sdk.messages.GenericMessageImpl;
+import io.sphere.sdk.messages.MessageDerivateHint;
+
+@JsonDeserialize(as = CategoryCreatedMessage.class)//important to override annotation in Message class
+public final class CategoryCreatedMessage extends GenericMessageImpl<Category> {
+    public static final String MESSAGE_TYPE = "CategoryCreated";
+    public static final MessageDerivateHint<CategoryCreatedMessage> MESSAGE_HINT =
+            MessageDerivateHint.ofSingleMessageType(MESSAGE_TYPE, CategoryCreatedMessage.class, Category.referenceTypeId());
+
+    private final Category category;
+
+    @JsonCreator
+    private CategoryCreatedMessage(final String id, final Long version, final ZonedDateTime createdAt, final ZonedDateTime lastModifiedAt, final JsonNode resource, final Long sequenceNumber, final Long resourceVersion, final String type, final Category category) {
+        super(id, version, createdAt, lastModifiedAt, resource, sequenceNumber, resourceVersion, type, Category.class);
+        this.category = category;
+    }
+
+    /**
+     * Gets the category object at creation time.
+     *
+     * @return the category at creation time
+     */
+    public Category getCategory(){
+        return category;
+    }
+
+}
---
+++ commercetools-models/src/main/java/io/sphere/sdk/categories/messages/CategorySlugChangedMessage.java
@@ -0,0 +1,37 @@
+package io.sphere.sdk.categories.messages;
+
+import java.time.ZonedDateTime;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+
+import io.sphere.sdk.categories.Category;
+import io.sphere.sdk.messages.GenericMessageImpl;
+import io.sphere.sdk.messages.MessageDerivateHint;
+import io.sphere.sdk.models.LocalizedString;
+
+@JsonDeserialize(as = CategorySlugChangedMessage.class)//important to override annotation in Message class
+public final class CategorySlugChangedMessage extends GenericMessageImpl<Category> {
+    public static final String MESSAGE_TYPE = "CategorySlugChanged";
+    public static final MessageDerivateHint<CategorySlugChangedMessage> MESSAGE_HINT =
+            MessageDerivateHint.ofSingleMessageType(MESSAGE_TYPE, CategorySlugChangedMessage.class, Category.referenceTypeId());
+
+    private final LocalizedString slug;
+
+    @JsonCreator
+    private CategorySlugChangedMessage(final String id, final Long version, final ZonedDateTime createdAt, final ZonedDateTime lastModifiedAt, final JsonNode resource, final Long sequenceNumber, final Long resourceVersion, final String type, final LocalizedString slug) {
+        super(id, version, createdAt, lastModifiedAt, resource, sequenceNumber, resourceVersion, type, Category.class);
+        this.slug = slug;
+    }
+
+    /**
+     * Gets the new Slug
+     *
+     * @return the new slug
+     */
+    public LocalizedString getSlug(){
+        return slug;
+    }
+
+}
