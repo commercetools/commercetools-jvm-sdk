817a7df3af3a9329f2412beebd54e794f0e67344
schleichardt <michael.schleichardt@commercetools.de>
2016-09-09 14:48:20 +0200

    fix JsonNodeQuery does not accept URL query parameters, close #1208

--- commercetools-models/src/test/java/io/sphere/sdk/jsonnodes/queries/JsonNodeQueryIntegrationTest.java
+++ commercetools-models/src/test/java/io/sphere/sdk/jsonnodes/queries/JsonNodeQueryIntegrationTest.java
@@ -2,9 +2,13 @@ package io.sphere.sdk.jsonnodes.queries;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import io.sphere.sdk.categories.CategoryFixtures;
+import io.sphere.sdk.products.Product;
+import io.sphere.sdk.products.ProductFixtures;
 import io.sphere.sdk.queries.PagedQueryResult;
 import io.sphere.sdk.test.IntegrationTest;
 import org.junit.Test;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -23,6 +27,19 @@ public class JsonNodeQueryIntegrationTest extends IntegrationTest {
             assertThat(jsonNode.get("id").asText()).isEqualTo(cat.getId());
             assertThat(jsonNode.get("parent").get("obj").get("id").asText()).isEqualTo(parent.getId());
             assertThat(jsonNode.get("ancestors").get(0).get("obj").get("id").asText()).isEqualTo(parent.getId());
+        });
+    }
+
+    @Test
+    public void allowToUseHttpQueryParameters() {
+        ProductFixtures.withProduct(client(), (Product product) -> {
+            final JsonNodeQuery query = JsonNodeQuery.of("/product-projections?staged=true")
+                    .plusPredicates(m -> m.id().is(product.getId()));
+            final String path = query.httpRequestIntent().getPath();
+            assertThat(path).startsWith("/product-projections?staged=true&where=id%3D%22");
+            final List<JsonNode> results = client().executeBlocking(query).getResults();
+            assertThat(results).hasSize(1);
+            assertThat(results.get(0).get("id").asText()).isEqualTo(product.getId());
         });
     }
 }
