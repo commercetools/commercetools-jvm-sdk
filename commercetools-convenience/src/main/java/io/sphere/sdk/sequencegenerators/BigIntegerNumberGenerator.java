6247ebaa3ba7a902e7c8438a12644c66ad448741
ccsalazarr-commercetools <cristian.salazar@commercetools.de>
2016-09-12 10:40:49 +0200

    Complete documentation for javadoc

--- commercetools-convenience/src/main/java/io/sphere/sdk/sequencegenerators/BigIntegerNumberGenerator.java
+++ commercetools-convenience/src/main/java/io/sphere/sdk/sequencegenerators/BigIntegerNumberGenerator.java
@@ -3,6 +3,11 @@ package io.sphere.sdk.sequencegenerators;
 import java.math.BigInteger;
 import java.util.concurrent.CompletionStage;
 
+/**
+ Defines how to implement an incremental number generator
+
+ */
+
 public interface BigIntegerNumberGenerator {
     CompletionStage<BigInteger> getNextNumber();
 }
