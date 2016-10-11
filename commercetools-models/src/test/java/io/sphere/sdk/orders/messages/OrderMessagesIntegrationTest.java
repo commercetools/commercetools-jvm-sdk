78a70d4089cd370ab63a227c8f573d6ab6d5704b
ccsalazarr-commercetools <cristian.salazar@commercetools.de>
2016-10-11 13:11:15 +0200

    Add OrderImported Message, issue #962

--- commercetools-models/src/test/java/io/sphere/sdk/orders/messages/OrderMessagesIntegrationTest.java
+++ commercetools-models/src/test/java/io/sphere/sdk/orders/messages/OrderMessagesIntegrationTest.java
@@ -1,19 +1,24 @@
 package io.sphere.sdk.orders.messages;
 
 import io.sphere.sdk.messages.queries.MessageQuery;
-import io.sphere.sdk.orders.Order;
-import io.sphere.sdk.orders.OrderState;
+import io.sphere.sdk.models.LocalizedString;
+import io.sphere.sdk.models.Reference;
+import io.sphere.sdk.orders.*;
+import io.sphere.sdk.orders.commands.OrderImportCommand;
 import io.sphere.sdk.orders.commands.OrderUpdateCommand;
 import io.sphere.sdk.orders.commands.updateactions.ChangeOrderState;
+import io.sphere.sdk.products.Price;
 import io.sphere.sdk.queries.PagedQueryResult;
 import io.sphere.sdk.queries.Query;
 import io.sphere.sdk.test.IntegrationTest;
 import org.junit.Test;
 
+import javax.money.MonetaryAmount;
 import java.util.Optional;
 
 import static io.sphere.sdk.orders.OrderFixtures.withOrderAndReturnInfo;
-import static io.sphere.sdk.test.SphereTestUtils.assertEventually;
+import static io.sphere.sdk.products.ProductFixtures.withProduct;
+import static io.sphere.sdk.test.SphereTestUtils.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class OrderMessagesIntegrationTest extends IntegrationTest {
@@ -64,5 +69,45 @@ public class OrderMessagesIntegrationTest extends IntegrationTest {
 
             return order;
         }));
+    }
+
+    @Test
+    public void orderImportedMessage() throws Exception {
+        withProduct(client(), product -> {
+            final String productId = product.getId();
+            final int variantId = 1;
+            final LocalizedString name = en("a name");
+            final long quantity = 1;
+            final Price price = Price.of(EURO_10);
+            final OrderState orderState = OrderState.OPEN;
+            final MonetaryAmount amount = EURO_10;
+            final ProductVariantImportDraft variant = ProductVariantImportDraftBuilder.of(productId, variantId).build();
+
+            final LineItemImportDraft lineItemImportDraft = LineItemImportDraftBuilder.of(variant, quantity, price, name).build();
+            final OrderImportDraft orderImportDraft = OrderImportDraftBuilder.ofLineItems(amount, orderState, asList(lineItemImportDraft))
+                    .country(DE).build();
+            final OrderImportCommand cmd = OrderImportCommand.of(orderImportDraft);
+
+            final Order order = client().executeBlocking(cmd);
+
+            //you can observe a message
+            final Query<OrderImportedMessage> messageQuery = MessageQuery.of()
+                    .withExpansionPaths(m -> m.resource())
+                    .withLimit(1L)
+                    .withPredicates(m -> m.resource().is(order))
+                    .forMessageType(OrderImportedMessage.MESSAGE_HINT);
+            assertEventually(() -> {
+                final Optional<OrderImportedMessage> orderImportedMessageOptional = client().executeBlocking(messageQuery).head();
+                assertThat(orderImportedMessageOptional).isPresent();
+                final OrderImportedMessage orderImportedMessage = orderImportedMessageOptional.get();
+                final Order orderFromMessage = orderImportedMessage.getOrder();
+                assertThat(orderFromMessage.getId()).isEqualTo(order.getId());
+                final Reference<Order> resource = orderImportedMessage.getResource();
+                assertThat(resource.getObj()).isNotNull();
+                assertThat(resource.getId()).isEqualTo(order.getId());
+
+            });
+
+        });
     }
 }
