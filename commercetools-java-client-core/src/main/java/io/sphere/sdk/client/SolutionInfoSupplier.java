cea454d07b99b816231bc101addad5179dbcd94c
schleichardt <michael.schleichardt@commercetools.de>
2016-10-13 14:41:19 +0200

    document user agent solution setting #1231

--- commercetools-java-client-core/src/main/java/io/sphere/sdk/client/SolutionInfoSupplier.java
+++ commercetools-java-client-core/src/main/java/io/sphere/sdk/client/SolutionInfoSupplier.java
@@ -6,25 +6,31 @@ import java.util.function.Supplier;
  *
  * Marker interface to generate a solution info for the JVM SDK user agent.
  *
- * example:
- * {@code commercetools-jvm-sdk/1.4.1 (AHC/2.0) Java/1.8.0_92-b14 (Mac OS X; x86_64) SOLUTION_NAME/SOLUTION_VERSION (+https://website.tld; +info@SOLUTION.com)}
+ * <p>A User-Agent header with a solution information looks like this:
+ * {@code commercetools-jvm-sdk/1.4.1 (AHC/2.0) Java/1.8.0_92-b14 (Mac OS X; x86_64) SOLUTION_NAME/SOLUTION_VERSION (+https://website.tld; +info@SOLUTION.com)}</p>
  *
- * Create a resource file "src/main/resources/META-INF/services/io.sphere.sdk.client.SolutionInfoSupplier" which contains for the line
- * tld.SOLUTION.client.SOLUTIONSolutionInfoSupplier
+ * <p>To add a solution information to the JVM SDK create a resource file {@codesrc/main/resources/META-INF/services/io.sphere.sdk.client.SolutionInfoSupplier}
+ * which contains a fully qualified class name like (replace at least SOLUTION with your solution name)
+ * {@code tld.SOLUTION.client.SOLUTIONSolutionInfoSupplier}</p>
  *
- * Then create a class "tld.SOLUTION.client.SOLUTIONSolutionInfoSupplier"
+ * Then create a class {@code tld.SOLUTION.client.SOLUTIONSolutionInfoSupplier}:
  *
- * {@code public class TestSolutionInfoSupplier extends Base implements SolutionInfoSupplier {
-@Override
-public SolutionInfo get() {
-    final SolutionInfo solutionInfo = new SolutionInfo();
-    solutionInfo.setName("SOLUTION_NAME");//no whitespaces!
-    solutionInfo.setVersion("SOLUTION_VERSION");
-    solutionInfo.setWebsite("https://website.tld; +info@SOLUTION.com");
-    solutionInfo.setEmergencyContact("info@SOLUTION.com");
-    return solutionInfo;
+<pre>
+{@code
+    public class TestSolutionInfoSupplier extends Base implements SolutionInfoSupplier {
+        public SolutionInfo get() {
+            final SolutionInfo solutionInfo = new SolutionInfo();
+            solutionInfo.setName("SOLUTION_NAME");//no whitespaces!
+            solutionInfo.setVersion("SOLUTION_VERSION");//no whitespaces!
+            solutionInfo.setWebsite("https://website.tld");
+            solutionInfo.setEmergencyContact("info@SOLUTION.com");
+            return solutionInfo;
+        }
+    }
 }
-}}
+</pre>
+
+ This class will be loaded via reflection.
  *
  */
 @FunctionalInterface
