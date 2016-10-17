a2ba4b6322e16618cab44594bcd09cae1e57b278
schleichardt <michael.schleichardt@commercetools.de>
2016-10-17 10:36:33 +0200

    MessagesConfigurationImpl extend Base

--- commercetools-models/src/main/java/io/sphere/sdk/projects/MessagesConfigurationImpl.java
+++ commercetools-models/src/main/java/io/sphere/sdk/projects/MessagesConfigurationImpl.java
@@ -1,8 +1,9 @@
 package io.sphere.sdk.projects;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
+import io.sphere.sdk.models.Base;
 
-final class MessagesConfigurationImpl implements MessagesConfiguration {
+final class MessagesConfigurationImpl extends Base implements MessagesConfiguration {
     private Boolean enabled;
 
     @JsonCreator
