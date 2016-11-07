489206d0b54fdd08bc35e618da90ce325284ffac
schleichardt <michael.schleichardt@commercetools.de>
2016-11-07 14:00:54 +0100

    generate CustomerGroup impl classes

--- commercetools-models/src/main/java/io/sphere/sdk/customergroups/CustomerGroupDraft.java
+++ commercetools-models/src/main/java/io/sphere/sdk/customergroups/CustomerGroupDraft.java
@@ -1,6 +1,8 @@
 package io.sphere.sdk.customergroups;
 
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import io.sphere.sdk.annotations.FactoryMethod;
+import io.sphere.sdk.annotations.ResourceDraftValue;
 import io.sphere.sdk.types.CustomDraft;
 
 /**
@@ -9,11 +11,12 @@ import io.sphere.sdk.types.CustomDraft;
  * @see io.sphere.sdk.customergroups.commands.CustomerGroupCreateCommand
  * @see CustomerGroup
  */
-@JsonDeserialize(as = CustomerGroupDraftImpl.class)
+@JsonDeserialize(as = CustomerGroupDraftDsl.class)
+@ResourceDraftValue(factoryMethods = @FactoryMethod(parameterNames = "groupName"))
 public interface CustomerGroupDraft {
     String getGroupName();
 
     static CustomerGroupDraft of(final String groupName) {
-        return new CustomerGroupDraftImpl(groupName);
+        return CustomerGroupDraftDsl.of(groupName);
     }
 }
