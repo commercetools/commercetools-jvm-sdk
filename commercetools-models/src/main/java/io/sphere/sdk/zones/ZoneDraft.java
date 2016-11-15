c1f9f6209bc62ed9dea3808dd3c992d749e14c87
schleichardt <michael.schleichardt@commercetools.de>
2016-11-15 12:26:34 +0100

    generate zone implementation classes

--- commercetools-models/src/main/java/io/sphere/sdk/zones/ZoneDraft.java
+++ commercetools-models/src/main/java/io/sphere/sdk/zones/ZoneDraft.java
@@ -2,13 +2,19 @@ package io.sphere.sdk.zones;
 
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.neovisionaries.i18n.CountryCode;
+import io.sphere.sdk.annotations.FactoryMethod;
+import io.sphere.sdk.annotations.ResourceDraftValue;
 
 import javax.annotation.Nullable;
 import java.util.Set;
 
 import static java.util.stream.Collectors.toSet;
 
-@JsonDeserialize(as = ZoneDraftImpl.class)
+@JsonDeserialize(as = ZoneDraftDsl.class)
+@ResourceDraftValue(factoryMethods = {
+        @FactoryMethod(parameterNames = {"name", "locations", "description"}),
+        @FactoryMethod(parameterNames = {"name", "locations"})
+})
 public interface ZoneDraft {
     @Nullable
     String getDescription();
@@ -20,15 +26,15 @@ public interface ZoneDraft {
 
     static ZoneDraft ofCountries(final String name, final Set<CountryCode> countries, @Nullable final String description) {
         final Set<Location> locations = countries.stream().map(country -> Location.of(country)).collect(toSet());
-        return new ZoneDraftImpl(name, locations, description);
+        return of(name, locations, description);
     }
 
     static ZoneDraft of(final String name, final Set<Location> locations, @Nullable final String description) {
-        return new ZoneDraftImpl(name, locations, description);
+        return ZoneDraftDsl.of(name, locations, description);
     }
 
     static ZoneDraft of(final String name, final Set<Location> locations) {
-        return of(name, locations, null);
+        return ZoneDraftDsl.of(name, locations);
     }
 
 }
