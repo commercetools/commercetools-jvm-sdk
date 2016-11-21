5437a8dd675d381dfb2eca2c4cff54a7ee864ebe
schleichardt <michael.schleichardt@commercetools.de>
2016-11-21 16:48:12 +0100

    generate payment by field getters

--- commercetools-models/src/main/java/io/sphere/sdk/payments/Payment.java
+++ commercetools-models/src/main/java/io/sphere/sdk/payments/Payment.java
@@ -2,6 +2,7 @@ package io.sphere.sdk.payments;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import io.sphere.sdk.annotations.HasByIdGetEndpoint;
 import io.sphere.sdk.annotations.HasQueryEndpoint;
 import io.sphere.sdk.annotations.ResourceInfo;
 import io.sphere.sdk.annotations.ResourceValue;
@@ -34,6 +35,7 @@ import java.util.List;
 @ResourceValue
 @HasQueryEndpoint()
 @ResourceInfo(pluralName = "payments", pathElement = "payments")
+@HasByIdGetEndpoint(javadocSummary = "Retrieves a payment by a known ID.", includeExamples = "io.sphere.sdk.payments.queries.PaymentByIdGetIntegrationTest#execution()")
 public interface Payment extends Resource<Payment>, Custom {
     @Nullable
     Reference<Customer> getCustomer();
--- commercetools-models/src/main/java/io/sphere/sdk/payments/queries/PaymentEndpoint.java
+++
@@ -1,8 +0,0 @@
-package io.sphere.sdk.payments.queries;
-
-import io.sphere.sdk.client.JsonEndpoint;
-import io.sphere.sdk.payments.Payment;
-
-final class PaymentEndpoint {
-    public static final JsonEndpoint<Payment> ENDPOINT = JsonEndpoint.of(Payment.typeReference(), "/payments");
-}
