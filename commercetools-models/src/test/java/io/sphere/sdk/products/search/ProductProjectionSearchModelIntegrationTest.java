4030c2d6f28286e1c42c73791cd8ece43e326ea1
schleichardt <michael.schleichardt@commercetools.de>
2016-11-21 14:46:47 +0100

    activate aggregations

--- commercetools-models/src/test/java/io/sphere/sdk/products/search/ProductProjectionSearchModelIntegrationTest.java
+++ commercetools-models/src/test/java/io/sphere/sdk/products/search/ProductProjectionSearchModelIntegrationTest.java
@@ -32,7 +32,7 @@ public abstract class ProductProjectionSearchModelIntegrationTest extends Integr
     protected static ProductType productType;
 
     private static final boolean expectAggregations = Optional.ofNullable(System.getenv("EXPECT_AGGREGATIONS"))
-            .map("true"::equals).orElse(false);
+            .map("false"::equals).orElse(true);
 
     @Rule
     public RetryIntegrationTest retry = new RetryIntegrationTest(10, 10000, LoggerFactory.getLogger(this.getClass()));
