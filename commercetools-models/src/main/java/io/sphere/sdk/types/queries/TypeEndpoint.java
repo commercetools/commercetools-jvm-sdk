e9c4aeba71de9518fbe10e512789d6f2ddb1acdf
schleichardt <michael.schleichardt@commercetools.de>
2016-11-21 16:09:35 +0100

    delete TypeEndpoint in queries

--- commercetools-models/src/main/java/io/sphere/sdk/types/queries/TypeEndpoint.java
+++
@@ -1,8 +0,0 @@
-package io.sphere.sdk.types.queries;
-
-import io.sphere.sdk.client.JsonEndpoint;
-import io.sphere.sdk.types.Type;
-
-final class TypeEndpoint {
-    static final JsonEndpoint<Type> ENDPOINT = JsonEndpoint.of(Type.typeReference(), "/types");
-}
