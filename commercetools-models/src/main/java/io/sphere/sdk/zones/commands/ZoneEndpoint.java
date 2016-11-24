9e4ce05298d16ab26b3651b737fdb845e750a2de
schleichardt <michael.schleichardt@commercetools.de>
2016-11-24 11:16:08 +0100

    generate Zone command classes

--- commercetools-models/src/main/java/io/sphere/sdk/zones/commands/ZoneEndpoint.java
+++
@@ -1,8 +0,0 @@
-package io.sphere.sdk.zones.commands;
-
-import io.sphere.sdk.client.JsonEndpoint;
-import io.sphere.sdk.zones.Zone;
-
-final class ZoneEndpoint {
-    static final JsonEndpoint<Zone> ENDPOINT = JsonEndpoint.of(Zone.typeReference(), "/zones");
-}
