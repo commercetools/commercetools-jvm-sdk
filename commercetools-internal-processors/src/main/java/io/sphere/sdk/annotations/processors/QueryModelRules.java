ba0099516c0719f6fcfdd735ff1ce7e7f5f63351
schleichardt <michael.schleichardt@commercetools.de>
2016-11-30 15:55:53 +0100

    wip add rules for query model generation

--- commercetools-internal-processors/src/main/java/io/sphere/sdk/annotations/processors/QueryModelRules.java
+++ commercetools-internal-processors/src/main/java/io/sphere/sdk/annotations/processors/QueryModelRules.java
@@ -1,33 +1,43 @@
 package io.sphere.sdk.annotations.processors;
 
+import io.sphere.sdk.models.LocalizedString;
 import io.sphere.sdk.models.Resource;
+import io.sphere.sdk.models.SphereEnumeration;
 import io.sphere.sdk.queries.ResourceQueryModel;
 
-import javax.lang.model.element.Element;
+import javax.annotation.Nullable;
 import javax.lang.model.element.ExecutableElement;
 import javax.lang.model.element.TypeElement;
+import javax.lang.model.type.DeclaredType;
 import javax.lang.model.type.ReferenceType;
+import javax.lang.model.type.TypeMirror;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import static io.sphere.sdk.annotations.processors.ClassConfigurer.createBeanGetterStream;
 import static io.sphere.sdk.annotations.processors.ClassConfigurer.fieldNameFromGetter;
 import static io.sphere.sdk.annotations.processors.ClassConfigurer.getType;
+import static java.util.Arrays.asList;
 
 final class QueryModelRules {
     private final TypeElement typeElement;
     private final ClassModelBuilder builder;
     private final LinkedList<InterfaceRule> interfaceRules = new LinkedList<>();
     private final LinkedList<BeanMethodRule> beanMethodRules = new LinkedList<>();
+    private final LinkedList<QueryModelSelectionRule> queryModelSelectionRules = new LinkedList<>();
 
     QueryModelRules(final TypeElement typeElement, final ClassModelBuilder builder) {
         this.typeElement = typeElement;
         this.builder = builder;
         interfaceRules.add(new ResourceRule());
         beanMethodRules.add(new GenerateMethodRule());
+        queryModelSelectionRules.add(new LocalizedStringQueryModelSelectionRule());
+        queryModelSelectionRules.add(new SetOfSphereEnumerationQueryModelSelectionRule());
+        queryModelSelectionRules.add(new SimpleQueryModelSelectionRule("java.lang.String", "StringQuerySortingModel"));
+        queryModelSelectionRules.add(new SimpleQueryModelSelectionRule("io.sphere.sdk.reviews.ReviewRatingStatistics", "ReviewRatingStatisticsQueryModel"));
     }
 
     void execute() {
@@ -37,7 +47,7 @@ final class QueryModelRules {
                 .filter(r -> r.accept((ReferenceType)i))
                 .findFirst());
         createBeanGetterStream(typeElement).forEach(beanGetter -> beanMethodRules.stream()
-                .filter(r -> r.accept(beanGetter))
+                .filter(r -> r.accept((ExecutableElement)beanGetter))
                 .findFirst());
     }
 
@@ -58,22 +68,23 @@ final class QueryModelRules {
     }
 
     private abstract class BeanMethodRule {
-        public abstract boolean accept(final Element beanGetter);
+        public abstract boolean accept(final ExecutableElement beanGetter);
     }
 
     public class GenerateMethodRule extends BeanMethodRule {
         @Override
-        public boolean accept(final Element beanGetter) {
+        public boolean accept(final ExecutableElement beanGetter) {
             final String fieldName = fieldNameFromGetter(beanGetter);
             final String type = getType(beanGetter);
             final MethodModel methodModel = new MethodModel();
             methodModel.setName(fieldName);
-            if ("java.lang.String".equals(type)) {
-                methodModel.setReturnType("String");
-            } else {
-                methodModel.setReturnType("void " + type);
-                methodModel.setName(fieldName + "TODO");
-            }
+
+            final String contextType = typeElement.getSimpleName().toString();
+
+            queryModelSelectionRules.stream()
+                    .filter(rule -> rule.x(beanGetter, methodModel, contextType))
+                    .findFirst()
+            .orElseThrow(() -> new RuntimeException("no query model type for " + typeElement + " " + beanGetter));
             builder.addMethod(methodModel);
             return true;
         }
@@ -90,8 +101,75 @@ final class QueryModelRules {
         }
 
         @Override
-        public boolean accept(final Element beanGetter) {
+        public boolean accept(final ExecutableElement beanGetter) {
             return methodNames.contains(beanGetter.getSimpleName().toString());
+        }
+    }
+
+    private abstract class QueryModelSelectionRule {
+        public abstract boolean x(final ExecutableElement beanGetter, final MethodModel methodModel, final String contextType);
+    }
+
+    public class SetOfSphereEnumerationQueryModelSelectionRule extends QueryModelSelectionRule {
+        @Override
+        public boolean x(final ExecutableElement beanGetter, final MethodModel methodModel, final String contextType) {
+            final TypeMirror returnType = beanGetter.getReturnType();
+            if (returnType instanceof DeclaredType) {
+                final DeclaredType x = (DeclaredType) returnType;
+                final String uncheckedType = x.asElement().getSimpleName().toString();
+                if (asList("Set", "List").contains(uncheckedType) && x.getTypeArguments().size() == 1) {
+                    final DeclaredType declaredType = (DeclaredType) x.getTypeArguments().get(0);
+                    final TypeElement classInList = (TypeElement) declaredType.asElement();
+                    final Optional<TypeElement> typeParameterOptional = classInList.getInterfaces().stream()
+                            .map(i -> (DeclaredType) i)
+                            .map(i -> (TypeElement) i.asElement())
+                            .filter(i -> i.getQualifiedName().toString().equals(SphereEnumeration.class.getCanonicalName())).findFirst();
+                    final boolean isSphereEnumeration = typeParameterOptional.map(xs -> true).orElse(false);
+                    if (isSphereEnumeration) {
+                        final String typeParameterName = typeParameterOptional.get().getQualifiedName().toString();
+                        methodModel.setReturnType("SphereEnumerationCollectionQueryModel<" + contextType + ", " + typeParameterName + ">");
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+
+    public class SimpleQueryModelSelectionRule extends QueryModelSelectionRule {
+
+        private final String expectedType;
+        private final String resultGenericType;
+
+        public SimpleQueryModelSelectionRule(final String expectedType, final String resultGenericType) {
+            this.expectedType = expectedType;
+            this.resultGenericType = resultGenericType;
+        }
+
+        @Override
+        public boolean x(final ExecutableElement beanGetter, final MethodModel methodModel, final String contextType) {
+            return ds(beanGetter, methodModel, contextType, expectedType, resultGenericType);
+        }
+
+        private boolean ds(final ExecutableElement beanGetter, final MethodModel methodModel, final String contextType, final String expectedType, final String resultGenericType) {
+            if (beanGetter.getReturnType().toString().equals(expectedType)) {
+                methodModel.setReturnType(resultGenericType + "<" + contextType + ">");
+                return true;
+            }
+            return false;
+        }
+    }
+
+    private class LocalizedStringQueryModelSelectionRule extends QueryModelSelectionRule {
+        @Override
+        public boolean x(final ExecutableElement beanGetter, final MethodModel methodModel, final String contextType) {
+            if (beanGetter.getReturnType().toString().equals(LocalizedString.class.getCanonicalName())) {
+                final boolean optional = beanGetter.getAnnotation(Nullable.class) == null;
+                final String type = optional ? "LocalizedStringOptionalQueryModel" : "LocalizedStringQueryModel";
+                methodModel.setReturnType(type);
+                return true;
+            }
+            return false;
         }
     }
 }
