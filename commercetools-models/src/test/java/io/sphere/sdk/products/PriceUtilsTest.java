de95c5eb6978a5469bea31d4541dda7386016aba
Laura Luiz <laura.luiz@commercetools.de>
2016-12-28 10:07:16 +0100

    Add gross/net test cases where to test rounding

--- commercetools-models/src/test/java/io/sphere/sdk/products/PriceUtilsTest.java
+++ commercetools-models/src/test/java/io/sphere/sdk/products/PriceUtilsTest.java
@@ -34,10 +34,24 @@ public class PriceUtilsTest {
     }
 
     @Test
+    public void convertsToGrossWithoutRounding() throws Exception {
+        final MonetaryAmount netAmount = monetaryAmountOf(3.5);
+        final double taxRate = 0.19;
+        assertThat(convertNetToGrossPrice(netAmount, taxRate)).isEqualTo(monetaryAmountOf(4.165));
+    }
+
+    @Test
     public void convertsToNet() throws Exception {
         final MonetaryAmount grossAmount = monetaryAmountOf(119);
         final double taxRate = 0.19;
         assertThat(convertGrossToNetPrice(grossAmount, taxRate)).isEqualTo(monetaryAmountOf(100));
+    }
+
+    @Test
+    public void convertsToNetWithoutRounding() throws Exception {
+        final MonetaryAmount grossAmount = monetaryAmountOf(3.5);
+        final double taxRate = 0.19;
+        assertThat(convertGrossToNetPrice(grossAmount, taxRate)).isEqualTo(monetaryAmountOf(2.94118));
     }
 
     @Test
