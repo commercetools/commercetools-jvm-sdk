88315cb0e764f25059b1995c4ff28ac1e7b59a4a
Laura Luiz <laura.luiz@commercetools.de>
2016-12-28 09:20:59 +0100

    Add more cases to cart tests and make them more readable

--- commercetools-models/src/test/java/io/sphere/sdk/carts/CartLikeTest.java
+++ commercetools-models/src/test/java/io/sphere/sdk/carts/CartLikeTest.java
@@ -5,6 +5,8 @@ import io.sphere.sdk.utils.MoneyImpl;
 import org.junit.Before;
 import org.junit.Test;
 
+import javax.money.MonetaryAmount;
+
 import static io.sphere.sdk.models.DefaultCurrencyUnits.EUR;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -12,21 +14,27 @@ public class CartLikeTest {
 
     private Cart cartWithTaxes;
     private Cart cartWithoutTaxes;
+    private Cart emptyCart;
 
     @Before
     public void setUp() throws Exception {
-        cartWithTaxes = SphereJsonUtils.readObjectFromResource("cart-with-taxes.json", Cart.class);
-        cartWithoutTaxes = SphereJsonUtils.readObjectFromResource("cart-without-taxes.json", Cart.class);
+        cartWithTaxes = SphereJsonUtils.readObjectFromResource("carts/cart-with-taxes.json", Cart.class);
+        cartWithoutTaxes = SphereJsonUtils.readObjectFromResource("carts/cart-without-taxes.json", Cart.class);
+        emptyCart = SphereJsonUtils.readObjectFromResource("carts/cart-empty.json", Cart.class);
     }
 
     @Test
     public void calculatesTotalAppliedTaxes() throws Exception {
+        assertThat(emptyCart.calculateTotalAppliedTaxes()).isEmpty();
         assertThat(cartWithoutTaxes.calculateTotalAppliedTaxes()).isEmpty();
-        assertThat(cartWithTaxes.calculateTotalAppliedTaxes()).contains(MoneyImpl.ofCents(41918, EUR));
+        assertThat(cartWithTaxes.calculateTotalAppliedTaxes()).contains(MoneyImpl.ofCents(262540 - 220622, EUR));
     }
 
     @Test
     public void calculatesSubTotalPrice() throws Exception {
-        assertThat(cartWithTaxes.calculateSubTotalPrice()).isEqualTo(MoneyImpl.ofCents(262150, EUR));
+        assertThat(emptyCart.calculateSubTotalPrice()).isEqualTo(MoneyImpl.ofCents(0, EUR));
+        final MonetaryAmount sumLineItems = MoneyImpl.ofCents(188750 + 65000 + 8400, EUR);
+        assertThat(cartWithoutTaxes.calculateSubTotalPrice()).isEqualTo(sumLineItems);
+        assertThat(cartWithTaxes.calculateSubTotalPrice()).isEqualTo(sumLineItems);
     }
 }
