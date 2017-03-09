da291a44e6ba8c9705a46d5d8d2962d6f474a4de
mkoester <matthias.koester@commercetools.de>
2017-03-09 17:27:58 +0100

    Fixed misspelled @JsonProperty which should fix our failing integration test.

--- commercetools-models/src/main/java/io/sphere/sdk/products/ProductVariant.java
+++ commercetools-models/src/main/java/io/sphere/sdk/products/ProductVariant.java
@@ -60,7 +60,7 @@ public interface ProductVariant extends AttributeContainer {
      *
      * @return
      */
-    @JsonProperty("isScopedPriceDiscounted")
+    @JsonProperty("scopedPriceDiscounted")
     @Nullable
     Boolean isScopedPriceDiscounted();
 
