7bfb28f7b902a8468340c22ac6768cbd5ded24a3
Matthias Köster <katmatt@users.noreply.github.com>
2017-03-27 13:48:42 +0200

    Implement a generic code generator for builders. (#1349)
    * Created new annotation @HasBuilder, created a code generator for it and used it to replace the manually written price builder.
    
    * Replaced hand written class with generated one and switched Scopedprice to also use a generated implementation class.

---
+++ commercetools-internal-processors/src/test/java/io/sphere/sdk/annotations/processors/generators/HasBuilderGeneratorTest.java
@@ -0,0 +1,24 @@
+package io.sphere.sdk.annotations.processors.generators;
+
+import io.sphere.sdk.annotations.processors.generators.examples.ExampleResource;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+/**
+ * Unit tests for {@link HasBuilderGenerator}.
+ */
+public class HasBuilderGeneratorTest extends AbstractGeneratorTest {
+    @Before
+    public void setup() {
+        generator = new HasBuilderGenerator(compilationRule.getElements());
+    }
+
+    @Test
+    public void generateExampleResourceBuilder() throws Exception {
+        final String content = generateAsString(ExampleResource.class);
+
+        assertThat(content).isEqualTo(expectedContent(ExampleResource.class));
+    }
+}
