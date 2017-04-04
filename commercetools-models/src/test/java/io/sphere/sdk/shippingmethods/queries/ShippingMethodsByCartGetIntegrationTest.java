f20fd0d27f03d3b0c78c841aaceafbec064337d4
mkoester <matthias.koester@commercetools.de>
2017-04-04 13:31:27 +0200

    Add property isMatching to shipping rate. Switch ZoneRate to use code generator, which requires switching from a class to an interface. Add asserts for integration tests.

--- commercetools-models/src/test/java/io/sphere/sdk/shippingmethods/queries/ShippingMethodsByCartGetIntegrationTest.java
+++ commercetools-models/src/test/java/io/sphere/sdk/shippingmethods/queries/ShippingMethodsByCartGetIntegrationTest.java
@@ -2,12 +2,17 @@ package io.sphere.sdk.shippingmethods.queries;
 
 import io.sphere.sdk.carts.Cart;
 import io.sphere.sdk.shippingmethods.ShippingMethod;
+import io.sphere.sdk.shippingmethods.ShippingRate;
+import io.sphere.sdk.shippingmethods.ZoneRate;
 import io.sphere.sdk.test.IntegrationTest;
+import org.assertj.core.api.Condition;
 import org.junit.Test;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static io.sphere.sdk.carts.CartFixtures.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 
 public class ShippingMethodsByCartGetIntegrationTest extends IntegrationTest {
@@ -15,7 +20,16 @@ public class ShippingMethodsByCartGetIntegrationTest extends IntegrationTest {
     public void execution() throws Exception {
         final Cart cart = createCartWithShippingAddress(client());
 
-        final List<ShippingMethod> result =
+        final List<ShippingMethod> shippingMethods =
                 client().executeBlocking(ShippingMethodsByCartGet.of(cart));
+        assertThat(shippingMethods).isNotEmpty();
+
+        for (final ShippingMethod shippingMethod : shippingMethods) {
+            final List<ShippingRate> shippingRates = shippingMethod.getZoneRates().stream()
+                    .flatMap(zoneRate -> zoneRate.getShippingRates().stream())
+                    .collect(Collectors.toList());
+
+            assertThat(shippingRates).areExactly(1, new Condition<>(ShippingRate::isMatching, "Shipping rate is matching"));
+        }
     }
 }
--- commercetools-models/src/test/java/io/sphere/sdk/shippingmethods/queries/ShippingMethodsByLocationGetIntegrationTest.java
+++ commercetools-models/src/test/java/io/sphere/sdk/shippingmethods/queries/ShippingMethodsByLocationGetIntegrationTest.java
@@ -2,17 +2,30 @@ package io.sphere.sdk.shippingmethods.queries;
 
 import com.neovisionaries.i18n.CountryCode;
 import io.sphere.sdk.shippingmethods.ShippingMethod;
+import io.sphere.sdk.shippingmethods.ShippingRate;
 import io.sphere.sdk.test.IntegrationTest;
+import org.assertj.core.api.Condition;
 import org.junit.Test;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static io.sphere.sdk.models.DefaultCurrencyUnits.USD;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class ShippingMethodsByLocationGetIntegrationTest extends IntegrationTest {
     @Test
     public void execution() throws Exception {
-        final List<ShippingMethod> result =
+        final List<ShippingMethod> shippingMethods =
                 client().executeBlocking(ShippingMethodsByLocationGet.of(CountryCode.US, "Kansas", USD));
+        assertThat(shippingMethods).isNotEmpty();
+
+        for (final ShippingMethod shippingMethod : shippingMethods) {
+            final List<ShippingRate> shippingRates = shippingMethod.getZoneRates().stream()
+                    .flatMap(zoneRate -> zoneRate.getShippingRates().stream())
+                    .collect(Collectors.toList());
+
+            assertThat(shippingRates).areAtLeastOne(new Condition<>(ShippingRate::isMatching, "Shipping rate is matching"));
+        }
     }
 }
