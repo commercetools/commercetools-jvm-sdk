1b307ae40269c78b5e717544e0873371e9cc0f67
Matthias Köster <katmatt@users.noreply.github.com>
2017-04-11 13:47:52 +0200

    Shipping rate is matching (#1366)
    * Add property isMatching to shipping rate.
    Switch ZoneRate to use code generator, which requires switching from a class to an interface.
    Add asserts for integration tests.
    
    * Improve javadoc.
    
    * Fix integration test failing on travis.

--- commercetools-models/src/main/java/io/sphere/sdk/shippingmethods/ShippingRate.java
+++ commercetools-models/src/main/java/io/sphere/sdk/shippingmethods/ShippingRate.java
@@ -1,39 +1,47 @@
 package io.sphere.sdk.shippingmethods;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonInclude;
-import io.sphere.sdk.models.Base;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import io.sphere.sdk.annotations.ResourceValue;
+import io.sphere.sdk.shippingmethods.queries.ShippingMethodsByCartGet;
+import io.sphere.sdk.shippingmethods.queries.ShippingMethodsByLocationGet;
 
 import javax.annotation.Nullable;
 import javax.money.MonetaryAmount;
 
-public final class ShippingRate extends Base {
-    private final MonetaryAmount price;
+@JsonDeserialize(as = ShippingRateImpl.class)
+@ResourceValue
+public interface ShippingRate {
 
-    @JsonInclude(JsonInclude.Include.NON_EMPTY)
+    MonetaryAmount getPrice();
+
+    /**
+     * The shipping is free if the order total (the sum of line item prices) exceeds the free above value.
+     *
+     * @return the free aboce property
+     */
     @Nullable
-    private final MonetaryAmount freeAbove;
+    MonetaryAmount getFreeAbove();
 
-    @JsonCreator
-    private ShippingRate(final MonetaryAmount price, @Nullable final MonetaryAmount freeAbove) {
-        this.price = price;
-        this.freeAbove = freeAbove;
+    /**
+     * Only appears in response to requests for shipping methods by  {@link ShippingMethodsByCartGet}
+     * or {@link ShippingMethodsByLocationGet} to mark this shipping rate as one that matches the cart or location.
+     *
+     * @return true iff. this shipping rate matches the cart or location
+     */
+    @JsonProperty("isMatching")
+    @Nullable
+    Boolean isMatching();
+
+    static ShippingRate of(final MonetaryAmount price, @Nullable final MonetaryAmount freeAbove) {
+        return of(price, freeAbove, null);
     }
 
-    public static ShippingRate of(final MonetaryAmount price, @Nullable final MonetaryAmount freeAbove) {
-        return new ShippingRate(price, freeAbove);
+    static ShippingRate of(final MonetaryAmount price, @Nullable final MonetaryAmount freeAbove, @Nullable final Boolean isMatching) {
+        return new ShippingRateImpl(freeAbove, isMatching, price);
     }
 
-    public static ShippingRate of(final MonetaryAmount price) {
+    static ShippingRate of(final MonetaryAmount price) {
         return of(price, null);
-    }
-
-    public MonetaryAmount getPrice() {
-        return price;
-    }
-
-    @Nullable
-    public MonetaryAmount getFreeAbove() {
-        return freeAbove;
     }
 }
