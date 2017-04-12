7474378a887d9fceec72a981620a02c8a6345e58
mkoester <matthias.koester@commercetools.de>
2017-04-12 09:53:17 +0200

    Add all sub types to message interface so that they can be deserialized correctly.

--- commercetools-models/src/main/java/io/sphere/sdk/messages/Message.java
+++ commercetools-models/src/main/java/io/sphere/sdk/messages/Message.java
@@ -8,8 +8,17 @@ import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import io.sphere.sdk.annotations.HasByIdGetEndpoint;
 import io.sphere.sdk.annotations.ResourceInfo;
 import io.sphere.sdk.categories.messages.CategoryCreatedMessage;
+import io.sphere.sdk.categories.messages.CategorySlugChangedMessage;
+import io.sphere.sdk.customers.messages.CustomerCreatedMessage;
+import io.sphere.sdk.inventory.messages.InventoryEntryDeletedMessage;
 import io.sphere.sdk.models.Reference;
 import io.sphere.sdk.models.Resource;
+import io.sphere.sdk.orders.messages.*;
+import io.sphere.sdk.payments.messages.*;
+import io.sphere.sdk.products.messages.*;
+import io.sphere.sdk.reviews.messages.ReviewCreatedMessage;
+import io.sphere.sdk.reviews.messages.ReviewRatingSetMessage;
+import io.sphere.sdk.reviews.messages.ReviewStateTransitionMessage;
 
 /**
  * A message represents a change or an action performed on a resource (like an Order or a Product). Messages can be seen as a subset of the change history for a resource inside a project. It is a subset because not all changes on resources result in messages.
@@ -23,7 +32,37 @@ import io.sphere.sdk.models.Resource;
         "If you need to receive one specific message class, like {@link io.sphere.sdk.orders.messages.DeliveryAddedMessage},\n" +
         "use {@link MessageQuery} with a predicate by id.")
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "type", visible = true)
-@JsonSubTypes(@JsonSubTypes.Type(value = CategoryCreatedMessage.class, name = CategoryCreatedMessage.MESSAGE_TYPE))
+@JsonSubTypes({
+        @JsonSubTypes.Type(value = CategoryCreatedMessage.class, name = CategoryCreatedMessage.MESSAGE_TYPE),
+        @JsonSubTypes.Type(value = CategorySlugChangedMessage.class, name = CategorySlugChangedMessage.MESSAGE_TYPE),
+        @JsonSubTypes.Type(value = CustomerCreatedMessage.class, name = CustomerCreatedMessage.MESSAGE_TYPE),
+        @JsonSubTypes.Type(value = CustomLineItemStateTransitionMessage.class, name = CustomLineItemStateTransitionMessage.MESSAGE_TYPE),
+        @JsonSubTypes.Type(value = DeliveryAddedMessage.class, name = DeliveryAddedMessage.MESSAGE_TYPE),
+        @JsonSubTypes.Type(value = InventoryEntryDeletedMessage.class, name = InventoryEntryDeletedMessage.MESSAGE_TYPE),
+        @JsonSubTypes.Type(value = LineItemStateTransitionMessage.class, name = LineItemStateTransitionMessage.MESSAGE_TYPE),
+        @JsonSubTypes.Type(value = OrderBillingAddressSetMessage.class, name = ProductStateTransitionMessage.MESSAGE_TYPE),
+        @JsonSubTypes.Type(value = OrderCreatedMessage.class, name = OrderCreatedMessage.MESSAGE_TYPE),
+        @JsonSubTypes.Type(value = OrderImportedMessage.class, name = OrderImportedMessage.MESSAGE_TYPE),
+        @JsonSubTypes.Type(value = OrderShippingAddressSetMessage.class, name = OrderShippingAddressSetMessage.MESSAGE_TYPE),
+        @JsonSubTypes.Type(value = OrderStateChangedMessage.class, name = OrderStateChangedMessage.MESSAGE_TYPE),
+        @JsonSubTypes.Type(value = OrderStateTransitionMessage.class, name = OrderStateTransitionMessage.MESSAGE_TYPE),
+        @JsonSubTypes.Type(value = ParcelAddedToDeliveryMessage.class, name = ParcelAddedToDeliveryMessage.MESSAGE_TYPE),
+        @JsonSubTypes.Type(value = PaymentCreatedMessage.class, name = PaymentCreatedMessage.MESSAGE_TYPE),
+        @JsonSubTypes.Type(value = PaymentInteractionAddedMessage.class, name = PaymentInteractionAddedMessage.MESSAGE_TYPE),
+        @JsonSubTypes.Type(value = PaymentStatusStateTransitionMessage.class, name = PaymentStatusStateTransitionMessage.MESSAGE_TYPE),
+        @JsonSubTypes.Type(value = PaymentTransactionAddedMessage.class, name = PaymentTransactionAddedMessage.MESSAGE_TYPE),
+        @JsonSubTypes.Type(value = PaymentTransactionStateChangedMessage.class, name = PaymentTransactionStateChangedMessage.MESSAGE_TYPE),
+        @JsonSubTypes.Type(value = ProductCreatedMessage.class, name = ProductCreatedMessage.MESSAGE_TYPE),
+        @JsonSubTypes.Type(value = ProductPublishedMessage.class, name = ProductPublishedMessage.MESSAGE_TYPE),
+        @JsonSubTypes.Type(value = ProductSlugChangedMessage.class, name = ProductSlugChangedMessage.MESSAGE_TYPE),
+        @JsonSubTypes.Type(value = ProductStateTransitionMessage.class, name = ProductStateTransitionMessage.MESSAGE_TYPE),
+        @JsonSubTypes.Type(value = ProductUnpublishedMessage.class, name = ProductUnpublishedMessage.MESSAGE_TYPE),
+        @JsonSubTypes.Type(value = ReturnInfoAddedMessage.class, name = ReturnInfoAddedMessage.MESSAGE_TYPE),
+        @JsonSubTypes.Type(value = ReviewCreatedMessage.class, name = ReviewCreatedMessage.MESSAGE_TYPE),
+        @JsonSubTypes.Type(value = ReviewRatingSetMessage.class, name = ReviewRatingSetMessage.MESSAGE_TYPE),
+        @JsonSubTypes.Type(value = ReviewStateTransitionMessage.class, name = ReviewStateTransitionMessage.MESSAGE_TYPE),
+        @JsonSubTypes.Type(value = ReviewStateTransitionMessage.class, name = ReviewStateTransitionMessage.MESSAGE_TYPE)
+})
 public interface Message extends Resource<Message> {
     /**
      * A type hint for references which resource type is linked in a reference.
