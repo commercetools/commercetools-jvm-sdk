e797271bd5eca30d567e58c204a4a7a1a6e43757
mkoester <matthias.koester@commercetools.de>
2017-04-12 10:41:55 +0200

    Fix broken integration test for ShippingRate#ismatching.

--- commercetools-models/src/test/java/io/sphere/sdk/shippingmethods/queries/ShippingMethodsByCartGetIntegrationTest.java
+++ commercetools-models/src/test/java/io/sphere/sdk/shippingmethods/queries/ShippingMethodsByCartGetIntegrationTest.java
@@ -1,9 +1,10 @@
 package io.sphere.sdk.shippingmethods.queries;
 
 import io.sphere.sdk.carts.Cart;
+import io.sphere.sdk.carts.commands.CartUpdateCommand;
+import io.sphere.sdk.carts.commands.updateactions.SetShippingAddress;
 import io.sphere.sdk.shippingmethods.ShippingMethod;
 import io.sphere.sdk.shippingmethods.ShippingRate;
-import io.sphere.sdk.shippingmethods.ZoneRate;
 import io.sphere.sdk.test.IntegrationTest;
 import org.assertj.core.api.Condition;
 import org.junit.Test;
@@ -11,25 +12,32 @@ import org.junit.Test;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import static io.sphere.sdk.carts.CartFixtures.*;
+import static io.sphere.sdk.carts.CartFixtures.GERMAN_ADDRESS;
+import static io.sphere.sdk.carts.CartFixtures.withCart;
+import static io.sphere.sdk.shippingmethods.ShippingMethodFixtures.withShippingMethodForGermany;
 import static org.assertj.core.api.Assertions.assertThat;
 
 
 public class ShippingMethodsByCartGetIntegrationTest extends IntegrationTest {
     @Test
     public void execution() throws Exception {
-        final Cart cart = createCartWithShippingAddress(client());
+        withShippingMethodForGermany(client(), shippingMethod -> {
+            withCart(client(), cart -> {
+                final Cart cartWithShippingAddress = client().executeBlocking(CartUpdateCommand.of(cart, SetShippingAddress.of(GERMAN_ADDRESS)));
 
-        final List<ShippingMethod> shippingMethods =
-                client().executeBlocking(ShippingMethodsByCartGet.of(cart));
-        assertThat(shippingMethods).isNotEmpty();
+                final List<ShippingMethod> shippingMethods =
+                        client().executeBlocking(ShippingMethodsByCartGet.of(cartWithShippingAddress));
+                assertThat(shippingMethods).isNotEmpty();
 
-        for (final ShippingMethod shippingMethod : shippingMethods) {
-            final List<ShippingRate> shippingRates = shippingMethod.getZoneRates().stream()
-                    .flatMap(zoneRate -> zoneRate.getShippingRates().stream())
-                    .collect(Collectors.toList());
+                for (final ShippingMethod cartShippingMethod : shippingMethods) {
+                    final List<ShippingRate> shippingRates = cartShippingMethod.getZoneRates().stream()
+                            .flatMap(zoneRate -> zoneRate.getShippingRates().stream())
+                            .collect(Collectors.toList());
 
-            assertThat(shippingRates).areExactly(1, new Condition<>(ShippingRate::isMatching, "Shipping rate is matching"));
-        }
+                    assertThat(shippingRates).areExactly(1, new Condition<>(ShippingRate::isMatching, "Shipping rate is matching"));
+                }
+                return cartWithShippingAddress;
+            });
+        });
     }
 }
