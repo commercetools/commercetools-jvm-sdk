8563503ad4d71559b7ee3944dd2bacbb8769f636
abeniasaad <achraf.beniasaad@commercetools.de>
2017-05-11 14:59:06 +0200

    Enabling auto generation for LineItem.

--- commercetools-models/src/main/java/io/sphere/sdk/carts/LineItemImpl.java
+++ commercetools-models/src/main/java/io/sphere/sdk/carts/LineItemImpl.java
@@ -22,29 +22,11 @@ import java.util.Set;
 
 import static java.util.Collections.emptyList;
 
-final class LineItemImpl extends LineItemLikeImpl implements LineItem {
+final class LineItemImpl extends LineItemImplBase {
 
-    private final String productId;
-    private final LocalizedString name;
-    @Nullable
-    private final LocalizedString productSlug;
+
     private final ProductVariant variant;
-    private final Price price;
-    @Nullable
-    private final TaxRate taxRate;
-    @Nullable
-    private final Reference<Channel> supplyChannel;
-    @Nullable
-    private final Reference<Channel> distributionChannel;
-    @Nullable
-    private final CustomFields custom;
-    private final MonetaryAmount totalPrice;
-    private final List<DiscountedLineItemPriceForQuantity> discountedPricePerQuantity;
-    @Nullable
-    private final TaxedItemPrice taxedPrice;
-    private final LineItemPriceMode priceMode;
-    @Nullable
-    private final Reference<ProductType> productType;
+
 
     @JsonCreator
     LineItemImpl(final String id, final String productId, final LocalizedString name,
@@ -56,100 +38,22 @@ final class LineItemImpl extends LineItemLikeImpl implements LineItem {
                  final List<DiscountedLineItemPriceForQuantity> discountedPricePerQuantity,
                  @Nullable final TaxedItemPrice taxedPrice, final LineItemPriceMode priceMode,
                  @Nullable final Reference<ProductType> productType) {
-        super(id, state, quantity, discountedPrice);
-        this.productId = productId;
-        this.name = name;
-        this.taxedPrice = taxedPrice;
-        this.productType = productType;
+        super(custom, discountedPricePerQuantity, distributionChannel, id, name, price, priceMode, productId, productSlug,
+                productType, quantity, state, supplyChannel, taxRate, taxedPrice, totalPrice, null);
+
         if (variant instanceof ObjectNode) {
             ((ObjectNode) variant).put("productId", productId);
         }
+
         this.variant = SphereJsonUtils.readObject(variant, ProductVariant.class);
-        this.price = price;
-        this.taxRate = taxRate;
-        this.supplyChannel = supplyChannel;
-        this.productSlug = productSlug;
-        this.distributionChannel = distributionChannel;
-        this.custom = custom;
-        this.totalPrice = totalPrice;
-        this.discountedPricePerQuantity = discountedPricePerQuantity != null ? discountedPricePerQuantity : emptyList();
-        this.priceMode = priceMode;
+
     }
 
-    @Override
-    public String getProductId() {
-        return productId;
-    }
-
-    @Override
-    public LocalizedString getName() {
-        return name;
-    }
 
     @Override
     public ProductVariant getVariant() {
         return variant;
     }
 
-    @Override
-    public Price getPrice() {
-        return price;
-    }
 
-    @Override
-    @Nullable
-    public TaxRate getTaxRate() {
-        return taxRate;
-    }
-
-    @Nullable
-    @Override
-    public Reference<Channel> getSupplyChannel() {
-        return supplyChannel;
-    }
-
-    @Nullable
-    @Override
-    public Reference<Channel> getDistributionChannel() {
-        return distributionChannel;
-    }
-
-    @Override
-    @Nullable
-    public LocalizedString getProductSlug() {
-        return productSlug;
-    }
-
-    @Override
-    @Nullable
-    public CustomFields getCustom() {
-        return custom;
-    }
-
-    @Override
-    public MonetaryAmount getTotalPrice() {
-        return totalPrice;
-    }
-
-    @Override
-    public List<DiscountedLineItemPriceForQuantity> getDiscountedPricePerQuantity() {
-        return discountedPricePerQuantity;
-    }
-
-    @Override
-    @Nullable
-    public TaxedItemPrice getTaxedPrice() {
-        return taxedPrice;
-    }
-
-    @Override
-    public LineItemPriceMode getPriceMode() {
-        return priceMode;
-    }
-
-    @Override
-    @Nullable
-    public Reference<ProductType> getProductType() {
-        return productType;
-    }
 }
