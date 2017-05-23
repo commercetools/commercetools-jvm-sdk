31b1e32a64a41eb98e81118997d333a1e79aa0b2
abeniasaad <achraf.beniasaad@commercetools.de>
2017-05-23 15:52:09 +0200

    add 'ProductImageUploadCommand' to our SDK

---
+++ commercetools-models/src/main/java/io/sphere/sdk/products/commands/ProductImageUploadCommand.java
@@ -0,0 +1,104 @@
+package io.sphere.sdk.products.commands;
+
+import com.fasterxml.jackson.databind.JavaType;
+import io.sphere.sdk.client.HttpRequestIntent;
+import io.sphere.sdk.commands.CommandImpl;
+import io.sphere.sdk.http.HttpMethod;
+import io.sphere.sdk.http.UrlQueryBuilder;
+import io.sphere.sdk.json.SphereJsonUtils;
+import io.sphere.sdk.products.ByIdVariantIdentifier;
+import io.sphere.sdk.products.Product;
+
+import javax.annotation.Nullable;
+import java.io.File;
+
+import static org.apache.commons.lang3.StringUtils.isEmpty;
+
+public final class ProductImageUploadCommand extends CommandImpl<Product> {
+    private final String productId;
+    @Nullable
+    private final Integer variant;
+    @Nullable
+    private final String sku;
+    @Nullable
+    private final String filename;
+    @Nullable
+    private final Boolean staged;
+    private final File body;
+    @Nullable
+    private String givenContentType;
+
+    private ProductImageUploadCommand(final File body, final String productId, final Integer variant, final String sku, final String filename, final Boolean staged) {
+        this.productId = productId;
+        this.variant = variant;
+        this.sku = sku;
+        this.filename = filename;
+        this.staged = staged;
+        this.body = body;
+    }
+
+    public static ProductImageUploadCommand ofMasterVariant(final File body, final String productId) {
+        return new ProductImageUploadCommand(body, productId, null, null, null, null);
+    }
+
+    public static ProductImageUploadCommand ofProductIdAndSku(final File body, final String productId, final String sku) {
+        return new ProductImageUploadCommand(body, productId, null, sku, null, null);
+    }
+
+    public static ProductImageUploadCommand ofVariantId(final File body, final ByIdVariantIdentifier variantIdentifier) {
+        return new ProductImageUploadCommand(body, variantIdentifier.getProductId(), variantIdentifier.getVariantId(), null, null, null);
+    }
+
+    public ProductImageUploadCommand withFilename(final String newFilename) {
+        return new ProductImageUploadCommand(body, productId, variant, sku, newFilename, staged);
+    }
+
+    public ProductImageUploadCommand withStaged(final Boolean newStaged) {
+        return new ProductImageUploadCommand(body, productId, variant, sku, filename, newStaged);
+    }
+
+    public ProductImageUploadCommand withContentType(final String contentType) {
+        final ProductImageUploadCommand productImageUploadCommand = new ProductImageUploadCommand(body, productId, variant, sku, filename, staged);
+        productImageUploadCommand.givenContentType = contentType;
+        return productImageUploadCommand;
+    }
+
+    @Override
+    protected JavaType jacksonJavaType() {
+        return SphereJsonUtils.convertToJavaType(Product.typeReference());
+    }
+
+    @Override
+    public HttpRequestIntent httpRequestIntent() {
+        final String contentType;
+        if (givenContentType != null) {
+            contentType = givenContentType;
+        } else if (body.getName().toLowerCase().endsWith(".jpeg") || body.getName().toLowerCase().endsWith(".jpg")) {
+            contentType = "image/jpeg";
+        } else if (body.getName().toLowerCase().endsWith(".gif")) {
+            contentType = "image/gif";
+        } else if (body.getName().toLowerCase().endsWith(".png")) {
+            contentType = "image/png";
+        } else {
+            contentType = "unknown";
+            new IllegalStateException("cannot determine content type for " + body.getName());
+        }
+        final UrlQueryBuilder builder = UrlQueryBuilder.of();
+        if (!isEmpty(sku)) {
+            builder.add("sku", sku);
+        }
+        if (!isEmpty(filename)) {
+            builder.add("filename", filename);
+        }
+        if (null != variant) {
+            builder.add("variant", "" + variant);
+        }
+        if (null != staged) {
+            builder.add("staged", "" + staged);
+        }
+        final String path = String.format("/products/%s/images%s", productId, builder.toStringWithOptionalQuestionMark());
+        return HttpRequestIntent.of(HttpMethod.POST, path, body, contentType);
+    }
+
+
+}
