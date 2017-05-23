049e631d085319f521fb455e73d692cbc8bdd499
abeniasaad <achraf.beniasaad@commercetools.de>
2017-05-23 16:23:19 +0200

    correcting test

--- commercetools-models/src/test/java/io/sphere/sdk/products/commands/ProductImageUploadCommandIntegrationTest.java
+++ commercetools-models/src/test/java/io/sphere/sdk/products/commands/ProductImageUploadCommandIntegrationTest.java
@@ -1,15 +1,14 @@
 package io.sphere.sdk.products.commands;
 
+import io.sphere.sdk.products.ByIdVariantIdentifier;
 import io.sphere.sdk.products.Image;
 import io.sphere.sdk.products.Product;
-import io.sphere.sdk.products.commands.updateactions.AddExternalImage;
 import io.sphere.sdk.test.IntegrationTest;
-import io.sphere.sdk.test.SphereTestUtils;
 import org.junit.Test;
 
-import static io.sphere.sdk.products.ProductFixtures.createExternalImage;
+import java.io.File;
+
 import static io.sphere.sdk.products.ProductFixtures.withUpdateableProduct;
-import static io.sphere.sdk.test.SphereTestUtils.MASTER_VARIANT_ID;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class ProductImageUploadCommandIntegrationTest extends IntegrationTest {
@@ -18,13 +17,17 @@ public class ProductImageUploadCommandIntegrationTest extends IntegrationTest {
     @Test
     public void upload() {
         withUpdateableProduct(client(), (Product product) -> {
-            assertThat(product.getMasterData().getStaged().getMasterVariant().getImages()).hasSize(0);
-
-            final Image image = createExternalImage();
-            final Product updatedProduct = client().executeBlocking(ProductUpdateCommand.of(product, AddExternalImage.of(image, MASTER_VARIANT_ID)));
-
-            assertThat(updatedProduct.getMasterData().getStaged().getMasterVariant().getImages()).isEqualTo(SphereTestUtils.asList(image));
-            return updatedProduct;
+            final ByIdVariantIdentifier identifier = product.getMasterData().getStaged().getMasterVariant().getIdentifier();
+            final ProductImageUploadCommand cmd = ProductImageUploadCommand
+                    .ofVariantId(new File("src/test/resources/ct_logo_farbe.gif"), identifier)
+                    .withFilename("logo.gif")
+                    .withStaged(true);
+            final Product productRes = client().executeBlocking(cmd);
+            final Image image = productRes.getMasterData().getStaged().getMasterVariant().getImages().get(0);
+            assertThat(image.getDimensions().getHeight()).isEqualTo(102);
+            assertThat(image.getDimensions().getWidth()).isEqualTo(460);
+            assertThat(image.getUrl()).contains("logo");
+            return productRes;
         });
     }
 }
