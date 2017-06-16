86507592892c35effc161b7daef5d12eb1363985
abeniasaad <achraf.beniasaad@commercetools.de>
2017-06-16 10:02:35 +0200

    Adding tests for azure Queue

--- commercetools-models/src/test/java/io/sphere/sdk/subscriptions/commands/SubscriptionUpdateCommandIntegrationTest.java
+++ commercetools-models/src/test/java/io/sphere/sdk/subscriptions/commands/SubscriptionUpdateCommandIntegrationTest.java
@@ -1,9 +1,7 @@
 package io.sphere.sdk.subscriptions.commands;
 
 import io.sphere.sdk.payments.Payment;
-import io.sphere.sdk.subscriptions.ChangeSubscription;
-import io.sphere.sdk.subscriptions.MessageSubscription;
-import io.sphere.sdk.subscriptions.Subscription;
+import io.sphere.sdk.subscriptions.*;
 import io.sphere.sdk.subscriptions.commands.updateactions.SetChanges;
 import io.sphere.sdk.subscriptions.commands.updateactions.SetKey;
 import io.sphere.sdk.subscriptions.commands.updateactions.SetMessages;
@@ -12,6 +10,7 @@ import org.junit.Test;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.function.Supplier;
 
 import static io.sphere.sdk.subscriptions.SubscriptionFixtures.*;
 import static io.sphere.sdk.test.SphereTestUtils.randomKey;
@@ -22,11 +21,49 @@ import static org.assertj.core.api.Assertions.assertThat;
  */
 public class SubscriptionUpdateCommandIntegrationTest extends IntegrationTest {
 
-    @Test
-    public void setKeyIronMq() {
-        assumeHasIronMqEnv();
 
-        withSubscription(client(), withCategoryChanges(ironMqSubscriptionDraftBuilder()), subscription -> {
+    @Test
+    public void setKeyAzureSBQueue(){
+        assumeHasAzureSBEnv();
+        setKeyQueue(SubscriptionFixtures::azureServiceBusSubscriptionDraftBuilder);
+    }
+
+    @Test
+    public void setChangeAzureSBQueue(){
+        assumeHasAzureSBEnv();
+        setChangesQueue(SubscriptionFixtures::azureServiceBusSubscriptionDraftBuilder);
+    }
+
+    @Test
+    public void setMessageAzureSBQueue(){
+        assumeHasAzureSBEnv();
+        setMessagesQueue(SubscriptionFixtures::azureServiceBusSubscriptionDraftBuilder);
+    }
+
+    @Test
+    public void setKeyIronMQ(){
+        assumeHasIronMqEnv();
+        setKeyQueue(SubscriptionFixtures::ironMqSubscriptionDraftBuilder);
+    }
+
+    @Test
+    public void setChangeIronMQ(){
+        assumeHasIronMqEnv();
+        setChangesQueue(SubscriptionFixtures::ironMqSubscriptionDraftBuilder);
+    }
+
+    @Test
+    public void setMessageIronMQ(){
+        assumeHasIronMqEnv();
+        setMessagesQueue(SubscriptionFixtures::ironMqSubscriptionDraftBuilder);
+    }
+
+
+
+
+    public void setKeyQueue(Supplier<SubscriptionDraftBuilder> subscriptionDraftBuilderSupplier) {
+
+        withSubscription(client(), withCategoryChanges(subscriptionDraftBuilderSupplier.get()), subscription -> {
             final String newKey = randomKey();
             final SubscriptionUpdateCommand setKeyCommand = SubscriptionUpdateCommand.of(subscription, SetKey.of(newKey));
             final Subscription updatedSubscription = client().executeBlocking(setKeyCommand);
@@ -37,11 +74,10 @@ public class SubscriptionUpdateCommandIntegrationTest extends IntegrationTest {
         });
     }
 
-    @Test
-    public void setChangesIronMq() {
-        assumeHasIronMqEnv();
+    public void setChangesQueue(Supplier<SubscriptionDraftBuilder> subscriptionDraftBuilderSupplier) {
 
-        withSubscription(client(), withCategoryChanges(ironMqSubscriptionDraftBuilder()), subscription -> {
+
+        withSubscription(client(), withCategoryChanges(subscriptionDraftBuilderSupplier.get()), subscription -> {
             final List<ChangeSubscription> newChanges = Collections.singletonList(ChangeSubscription.of(Payment.resourceTypeId()));
 
             final SubscriptionUpdateCommand setChangesCommand = SubscriptionUpdateCommand.of(subscription, SetChanges.of(newChanges));
@@ -53,17 +89,16 @@ public class SubscriptionUpdateCommandIntegrationTest extends IntegrationTest {
         });
     }
 
-    @Test
-    public void setMessagesIronMq() {
-        assumeHasIronMqEnv();
+    public void setMessagesQueue(Supplier<SubscriptionDraftBuilder> subscriptionDraftBuilderSupplier) {
 
-        withSubscription(client(), withCategoryCreatedMessage(ironMqSubscriptionDraftBuilder()), subscription -> {
-            final List<MessageSubscription> newMessages = Collections.singletonList(MessageSubscription.of(Payment.resourceTypeId()));
+        withSubscription(client(), withCategoryCreatedMessage(subscriptionDraftBuilderSupplier.get()), subscription -> {
+            final List<MessageSubscription> newMessages = Collections.singletonList(MessageSubscription.of(Payment.resourceTypeId(),Collections.emptyList()));
 
             final SubscriptionUpdateCommand setMessagesCommand = SubscriptionUpdateCommand.of(subscription, SetMessages.of(newMessages));
             final Subscription updatedSubscription = client().executeBlocking(setMessagesCommand);
 
             assertThat(updatedSubscription.getMessages()).isEqualTo(newMessages);
+
 
             return updatedSubscription;
         });
