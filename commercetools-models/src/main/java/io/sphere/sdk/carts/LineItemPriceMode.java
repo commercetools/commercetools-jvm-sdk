683d8f55bf5e87310b7fec2fb6966849b1eaf7e0
ccsalazarr-commercetools <cristian.salazar@commercetools.de>
2017-06-19 16:24:37 +0200

    Add external line item prices, close #1421 (#1430)
    * Add external price mode to line items
    
    * Add external total price mode to line items
    
    * Add lineItem with external price using addLineItem update action.
    
    * Testing for remove line item and change line item quantity, with external price and external total price
    
    * Add test case when it is not given an external total price in an update action
    
    * Add SetLineItemPrice update action
    
    * Add test for SetLineItemPrice update action, when it is not set the external price
    
    * Replace Money class, for MonetaryAmount
    
    * Use of ResourceDraftValueGenerator for LineItemDraft
    
    * Change Money for MonetaryAmount in SetLineItemPrice
    
    * Change Money for MonetaryAmount in SetLineItemPrice test

--- commercetools-models/src/main/java/io/sphere/sdk/carts/LineItemPriceMode.java
+++ commercetools-models/src/main/java/io/sphere/sdk/carts/LineItemPriceMode.java
@@ -10,10 +10,21 @@ public enum LineItemPriceMode implements SphereEnumeration {
     PLATFORM,
 
     /**
-     * The price was set externally. Cart discounts are disabled for the line items with this price mode. Any changes to the line item quantity will reset it back to the {@code PLATFORM} price mode.
+     * The line item price with the total was set externally. Cart discounts are disabled for the line items with this price mode.
+     * Although a line item with this price mode has both {@code price} and {@code totalPrice} externally set, only {@code totalPrice}
+     * will be used to calculate the total price of a cart. All update actions that change the quantity of a line item
+     * with this price mode can set the new price with the {@code externalTotal} field. If the {@code externalTotal} field is not given
+     * in an update action that changes line item quantity then the external price is unset and the price mode is set back to {@code PLATFORM}.
+     *
      * @see io.sphere.sdk.carts.commands.updateactions.SetLineItemTotalPrice
      */
-    EXTERNAL_TOTAL;
+    EXTERNAL_TOTAL,
+
+    /**
+     * The line item price was set externally. Cart discounts can apply to line items with this price mode. All update
+     * actions that change the quantity of a line item with this price mode require the {@code EXTERNAL_PRICE} field to be given.
+     */
+    EXTERNAL_PRICE;
 
     @JsonCreator
     public static LineItemPriceMode ofSphereValue(final String value) {
--- commercetools-models/src/main/java/io/sphere/sdk/carts/commands/updateactions/ChangeLineItemQuantity.java
+++ commercetools-models/src/main/java/io/sphere/sdk/carts/commands/updateactions/ChangeLineItemQuantity.java
@@ -1,9 +1,13 @@
 package io.sphere.sdk.carts.commands.updateactions;
 
 import io.sphere.sdk.carts.Cart;
+import io.sphere.sdk.carts.ExternalLineItemTotalPrice;
 import io.sphere.sdk.carts.LineItem;
 import io.sphere.sdk.commands.UpdateAction;
 import io.sphere.sdk.commands.UpdateActionImpl;
+
+import javax.annotation.Nullable;
+import javax.money.MonetaryAmount;
 
 /**
  Sets the quantity of the given line item. If quantity is 0, line item is removed from the cart.
@@ -15,11 +19,45 @@ import io.sphere.sdk.commands.UpdateActionImpl;
 public final class ChangeLineItemQuantity extends UpdateActionImpl<Cart> {
     private final String lineItemId;
     private final Long quantity;
+    @Nullable
+    private final MonetaryAmount externalPrice;
+    @Nullable
+    private final ExternalLineItemTotalPrice externalTotalPrice;
 
-    private ChangeLineItemQuantity(final String lineItemId, final Long quantity) {
+    private ChangeLineItemQuantity(final String lineItemId, final Long quantity, @Nullable final MonetaryAmount externalPrice, @Nullable final ExternalLineItemTotalPrice externalTotalPrice) {
         super("changeLineItemQuantity");
         this.lineItemId = lineItemId;
         this.quantity = quantity;
+        this.externalPrice = externalPrice;
+        this.externalTotalPrice = externalTotalPrice;
+    }
+
+    private static ChangeLineItemQuantity of(final String lineItemId, final Long quantity, @Nullable final MonetaryAmount externalPrice, @Nullable final ExternalLineItemTotalPrice externalTotalPrice) {
+        return new ChangeLineItemQuantity(lineItemId, quantity, externalPrice, externalTotalPrice);
+    }
+
+    public static UpdateAction<Cart> of(final LineItem lineItem, final long quantity) {
+        return of(lineItem.getId(), quantity);
+    }
+
+    public static ChangeLineItemQuantity of(final String lineItemId, final long quantity) {
+        return of(lineItemId, quantity, null, null);
+    }
+
+    public static ChangeLineItemQuantity ofLineItemAndExternalPrice(final LineItem lineItem, final long quantity, @Nullable final MonetaryAmount externalPrice) {
+        return ofLineItemAndExternalPrice(lineItem.getId(), quantity, externalPrice);
+    }
+
+    public static ChangeLineItemQuantity ofLineItemAndExternalPrice(final String lineItemId, final long quantity, @Nullable final MonetaryAmount externalPrice) {
+        return of(lineItemId, quantity, externalPrice, null);
+    }
+
+    public static ChangeLineItemQuantity ofLineItemAndExternalTotalPrice(final LineItem lineItem, final long quantity, @Nullable final ExternalLineItemTotalPrice externalTotalPrice) {
+        return ofLineItemAndExternalTotalPrice(lineItem.getId(), quantity, externalTotalPrice);
+    }
+
+    public static ChangeLineItemQuantity ofLineItemAndExternalTotalPrice(final String lineItemId, final long quantity, @Nullable final ExternalLineItemTotalPrice externalTotalPrice) {
+        return of(lineItemId, quantity, null, externalTotalPrice);
     }
 
     public String getLineItemId() {
@@ -30,11 +68,13 @@ public final class ChangeLineItemQuantity extends UpdateActionImpl<Cart> {
         return quantity;
     }
 
-    public static ChangeLineItemQuantity of(final String lineItemId, final long quantity) {
-        return new ChangeLineItemQuantity(lineItemId, quantity);
+    @Nullable
+    public MonetaryAmount getExternalPrice() {
+        return externalPrice;
     }
 
-    public static UpdateAction<Cart> of(final LineItem lineItem, final long quantity) {
-        return of(lineItem.getId(), quantity);
+    @Nullable
+    public ExternalLineItemTotalPrice getExternalTotalPrice() {
+        return externalTotalPrice;
     }
 }
