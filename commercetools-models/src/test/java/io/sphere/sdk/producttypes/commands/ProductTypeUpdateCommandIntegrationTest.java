ed95b7bfa8d8b8f99a6cbab950d16b4bf27a032d
ccsalazarr-commercetools <cristian.salazar@commercetools.de>
2017-06-19 16:00:48 +0200

    Change input hint for attribute definition, close #1424 (#1439)
    * Extension of updateActions code generator, in order to allow 'Enum' types
    
    * Add manually changeInputHint update action, and remove generation of enum types from update action generator
    
    * Remove files related with the update action generator
    
    * Restore MyResource class (for update generator tests) from master branch
    
    * Add final modifier to ChangeInputHint class

--- commercetools-models/src/test/java/io/sphere/sdk/producttypes/commands/ProductTypeUpdateCommandIntegrationTest.java
+++ commercetools-models/src/test/java/io/sphere/sdk/producttypes/commands/ProductTypeUpdateCommandIntegrationTest.java
@@ -3,6 +3,7 @@ package io.sphere.sdk.producttypes.commands;
 import io.sphere.sdk.models.EnumValue;
 import io.sphere.sdk.models.LocalizedEnumValue;
 import io.sphere.sdk.models.LocalizedString;
+import io.sphere.sdk.models.TextInputHint;
 import io.sphere.sdk.products.attributes.*;
 import io.sphere.sdk.producttypes.ProductType;
 import io.sphere.sdk.producttypes.ProductTypeDraft;
@@ -99,7 +100,7 @@ public class ProductTypeUpdateCommandIntegrationTest extends IntegrationTest {
 
             assertThat(updatedProductType.getAttribute(attributeName).getAttributeType())
                     .isInstanceOf(EnumAttributeType.class)
-                    .matches(type -> ((EnumAttributeType)type).getValues().contains(value));
+                    .matches(type -> ((EnumAttributeType) type).getValues().contains(value));
 
             return updatedProductType;
         });
@@ -119,7 +120,7 @@ public class ProductTypeUpdateCommandIntegrationTest extends IntegrationTest {
 
             assertThat(updatedProductType.getAttribute(attributeName).getAttributeType())
                     .isInstanceOf(LocalizedEnumAttributeType.class)
-                    .matches(type -> ((LocalizedEnumAttributeType)type).getValues().contains(value));
+                    .matches(type -> ((LocalizedEnumAttributeType) type).getValues().contains(value));
 
             return updatedProductType;
         });
@@ -193,6 +194,28 @@ public class ProductTypeUpdateCommandIntegrationTest extends IntegrationTest {
             final ProductType updatedProductType = client().executeBlocking(cmd);
 
             assertThat(updatedProductType.getAttribute(attributeName).isSearchable()).isTrue();
+
+            return updatedProductType;
+        });
+    }
+
+    @Test
+    public void changeInputHint() throws Exception {
+        final String key = randomKey();
+        final String attributeName = "stringAttribute";
+        final AttributeDefinition attributeDefinition = AttributeDefinitionBuilder
+                .of(attributeName, randomSlug(), StringAttributeType.of())
+                .inputHint(TextInputHint.SINGLE_LINE)
+                .build();
+        final List<AttributeDefinition> attributes = singletonList(attributeDefinition);
+        withUpdateableProductType(client(), () -> ProductTypeDraft.of(key, key, key, attributes), productType -> {
+            assertThat(productType.getAttribute(attributeName).getInputHint()).isEqualTo(TextInputHint.SINGLE_LINE);
+
+            final ProductTypeUpdateCommand cmd =
+                    ProductTypeUpdateCommand.of(productType, ChangeInputHint.of(attributeName, TextInputHint.MULTI_LINE));
+            final ProductType updatedProductType = client().executeBlocking(cmd);
+
+            assertThat(updatedProductType.getAttribute(attributeName).getInputHint()).isEqualTo(TextInputHint.MULTI_LINE);
 
             return updatedProductType;
         });
