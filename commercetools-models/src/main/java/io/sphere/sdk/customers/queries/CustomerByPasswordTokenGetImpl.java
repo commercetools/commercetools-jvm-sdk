57eb401e8d0c18f7ee7ebc2647839cef7d0028db
mkoester <matthias.koester@commercetools.de>
2017-06-26 16:17:35 +0200

    Add customer by email token endpoint. Change customer by password token endpoint. New uri template and url builder classes.

--- commercetools-models/src/main/java/io/sphere/sdk/customers/queries/CustomerByPasswordTokenGetImpl.java
+++ commercetools-models/src/main/java/io/sphere/sdk/customers/queries/CustomerByPasswordTokenGetImpl.java
@@ -1,17 +1,25 @@
 package io.sphere.sdk.customers.queries;
 
+import io.sphere.sdk.client.JsonEndpoint;
 import io.sphere.sdk.customers.Customer;
 import io.sphere.sdk.customers.expansion.CustomerExpansionModel;
-import io.sphere.sdk.http.NameValuePair;
+import io.sphere.sdk.http.UriTemplate;
+import io.sphere.sdk.http.UrlBuilder;
 import io.sphere.sdk.queries.MetaModelGetDslBuilder;
 import io.sphere.sdk.queries.MetaModelGetDslImpl;
 
-import java.util.Collections;
-
 final class CustomerByPasswordTokenGetImpl extends MetaModelGetDslImpl<Customer, Customer, CustomerByPasswordTokenGet, CustomerExpansionModel<Customer>> implements CustomerByPasswordTokenGet {
+    static final UriTemplate GET_BY_PASSWORD_TOKEN = UriTemplate.of("/customers/password-token={token}");
 
     CustomerByPasswordTokenGetImpl(final String token) {
-        super(CustomerEndpoint.ENDPOINT, "", CustomerExpansionModel.of(), CustomerByPasswordTokenGetImpl::new, Collections.singletonList(NameValuePair.of("token", token)));
+        super("", buildEndpoint(token), CustomerExpansionModel.of(), CustomerByPasswordTokenGetImpl::new);
+    }
+
+    private static JsonEndpoint<Customer> buildEndpoint(final String token) {
+        final String endpoint = UrlBuilder.of(GET_BY_PASSWORD_TOKEN)
+                .addUriParameter(token)
+                .build();
+        return JsonEndpoint.of(Customer.typeReference(), endpoint);
     }
 
     public CustomerByPasswordTokenGetImpl(final MetaModelGetDslBuilder<Customer, Customer, CustomerByPasswordTokenGet, CustomerExpansionModel<Customer>> builder) {
