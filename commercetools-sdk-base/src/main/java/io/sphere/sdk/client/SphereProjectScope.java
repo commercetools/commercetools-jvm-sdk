410e0615bece37ad97779599967e50a3e84303c6
cristian.salazar <cristian.salazar@commercetools.de>
2017-07-11 12:28:03 +0200

    Add missing scopes into the class

--- commercetools-sdk-base/src/main/java/io/sphere/sdk/client/SphereProjectScope.java
+++ commercetools-sdk-base/src/main/java/io/sphere/sdk/client/SphereProjectScope.java
@@ -7,57 +7,68 @@ public final class SphereProjectScope extends Base implements SphereScope {
 
     private final String scope;
 
-    @Deprecated
     /**Grants full access to the APIs for the specified project.*/
     public final static SphereProjectScope MANAGE_PROJECT = SphereProjectScope.of("manage_project");
 
-    @Deprecated
     /** Grants access to the APIs for creating, modifying and viewing anything related to products in a project. Implies view_products for the same project.*/
     public final static SphereProjectScope MANAGE_PRODUCTS = SphereProjectScope.of("manage_products");
 
-    @Deprecated
     /** Grants access to the APIs for viewing anything related to products in a project.*/
     public final static SphereProjectScope VIEW_PRODUCTS = SphereProjectScope.of("view_products");
 
-    @Deprecated
     /** Grants access to the APIs for creating, modifying and viewing anything related to orders in a project. Implies view_orders for the same project.*/
     public final static SphereProjectScope MANAGE_ORDERS = SphereProjectScope.of("manage_orders");
 
-    @Deprecated
     /** Grants access to the APIs for viewing anything related to orders in a project.*/
     public final static SphereProjectScope VIEW_ORDERS = SphereProjectScope.of("view_orders");
 
-    @Deprecated
     /** If used with the password flow, grants access to the APIs for creating, modifying and viewing orders and carts of the customer to whom the access token was issued.*/
+    public final static SphereProjectScope MANAGE_SHOPPING_LISTS = SphereProjectScope.of("manage_shopping_lists");
+
+    /** Grants access to the APIs for viewing anything related to shopping lists in a project.*/
+    public final static SphereProjectScope VIEW_SHOPPING_LISTS = SphereProjectScope.of("view_shopping_lists");
+
+    /** Grants access to the APIs for creating, modifying and viewing anything related to shopping lists in a project. Implies view_shopping_lists for the same project.*/
     public final static SphereProjectScope MANAGE_MY_ORDERS = SphereProjectScope.of("manage_my_orders");
 
-    @Deprecated
     /** Grants access to the APIs for creating, modifying and viewing anything related to customers in a project. Implies view_customers for the same project.*/
     public final static SphereProjectScope MANAGE_CUSTOMERS = SphereProjectScope.of("manage_customers");
 
-    @Deprecated
     /** Grants access to the APIs for viewing anything related to customers in a project.*/
     public final static SphereProjectScope VIEW_CUSTOMERS = SphereProjectScope.of("view_customers");
 
-    @Deprecated
     /** If used with the password flow, grants access to the APIs for creating, modifying and viewing the profile of the customer to whom the access token was issued.*/
     public final static SphereProjectScope MANAGE_MY_PROFILE = SphereProjectScope.of("manage_my_profile");
 
-    @Deprecated
     /** Grants access to the APIs for creating, modifying and viewing anything related to types in a project.*/
     public final static SphereProjectScope MANAGE_TYPES = SphereProjectScope.of("manage_types");
 
-    @Deprecated
     /** Grants access to the APIs for viewing anything related to types in a project.*/
     public final static SphereProjectScope VIEW_TYPES = SphereProjectScope.of("view_types");
 
-    @Deprecated
     /** Grants access to the APIs for creating, modifying and viewing anything related to payments in a project.*/
     public final static SphereProjectScope MANAGE_PAYMENTS = SphereProjectScope.of("manage_payments");
 
-    @Deprecated
     /** Grants access to the APIs for viewing anything related to payments in a project.*/
     public final static SphereProjectScope VIEW_PAYMENTS = SphereProjectScope.of("view_payments");
+
+    /** Grants access to the access tokens for Anonymous Sessions.*/
+    public final static SphereProjectScope CREATE_ANONYMOUS_TOKEN = SphereProjectScope.of("create_anonymous_token");
+
+    /** Grants access to the APIs for creating, modifying and viewing anything related to subscriptions in a project.*/
+    public final static SphereProjectScope MANAGE_SUBSCRIPTIONS = SphereProjectScope.of("manage_subscriptions");
+
+    /** Grants access to the API for viewing the project settings.*/
+    public final static SphereProjectScope VIEW_PROJECT_SETTINGS = SphereProjectScope.of("view_project_settings");
+
+    /** Grants access to the APIs for creating, modifying and viewing anything related to states in a project.*/
+    public final static SphereProjectScope MANAGE_STATES = SphereProjectScope.of("manage_states");
+
+    /** Grants access to the APIs for viewing anything related to states in a project.*/
+    public final static SphereProjectScope VIEW_STATES = SphereProjectScope.of("view_states");
+
+    /** Grants access to the APIs for viewing anything related to messages in a project.*/
+    public final static SphereProjectScope VIEW_MESSAGES = SphereProjectScope.of("view_messages");
 
     private SphereProjectScope(final String scope) {
         this.scope = scope;
--- commercetools-sdk-base/src/test/java/io/sphere/sdk/client/SphereProjectScopeTest.java
+++ commercetools-sdk-base/src/test/java/io/sphere/sdk/client/SphereProjectScopeTest.java
@@ -3,7 +3,7 @@ package io.sphere.sdk.client;
 import io.sphere.sdk.json.SphereJsonUtils;
 import org.junit.Test;
 
-import static io.sphere.sdk.client.SphereProjectScope.MANAGE_PROJECT;
+import static io.sphere.sdk.client.SphereProjectScope.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class SphereProjectScopeTest {
@@ -11,6 +11,26 @@ public class SphereProjectScopeTest {
     @Test
     public void toSphereScope() {
         assertThat(MANAGE_PROJECT.toScopeString()).isEqualTo("manage_project");
+        assertThat(MANAGE_PRODUCTS.toScopeString()).isEqualTo("manage_products");
+        assertThat(VIEW_PRODUCTS.toScopeString()).isEqualTo("view_products");
+        assertThat(MANAGE_ORDERS.toScopeString()).isEqualTo("manage_orders");
+        assertThat(VIEW_ORDERS.toScopeString()).isEqualTo("view_orders");
+        assertThat(MANAGE_MY_ORDERS.toScopeString()).isEqualTo("manage_my_orders");
+        assertThat(MANAGE_SHOPPING_LISTS.toScopeString()).isEqualTo("manage_shopping_lists");
+        assertThat(VIEW_SHOPPING_LISTS.toScopeString()).isEqualTo("view_shopping_lists");
+        assertThat(MANAGE_CUSTOMERS.toScopeString()).isEqualTo("manage_customers");
+        assertThat(VIEW_CUSTOMERS.toScopeString()).isEqualTo("view_customers");
+        assertThat(MANAGE_MY_PROFILE.toScopeString()).isEqualTo("manage_my_profile");
+        assertThat(MANAGE_TYPES.toScopeString()).isEqualTo("manage_types");
+        assertThat(VIEW_TYPES.toScopeString()).isEqualTo("view_types");
+        assertThat(MANAGE_PAYMENTS.toScopeString()).isEqualTo("manage_payments");
+        assertThat(VIEW_PAYMENTS.toScopeString()).isEqualTo("view_payments");
+        assertThat(CREATE_ANONYMOUS_TOKEN.toScopeString()).isEqualTo("create_anonymous_token");
+        assertThat(MANAGE_SUBSCRIPTIONS.toScopeString()).isEqualTo("manage_subscriptions");
+        assertThat(VIEW_PROJECT_SETTINGS.toScopeString()).isEqualTo("view_project_settings");
+        assertThat(MANAGE_STATES.toScopeString()).isEqualTo("manage_states");
+        assertThat(VIEW_STATES.toScopeString()).isEqualTo("view_states");
+        assertThat(VIEW_MESSAGES.toScopeString()).isEqualTo("view_messages");
     }
 
     @Test
