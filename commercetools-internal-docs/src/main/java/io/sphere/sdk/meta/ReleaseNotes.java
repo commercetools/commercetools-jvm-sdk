88ccc858bd5220a70611dd9f984f2a95ed06a545
Matthias Köster <katmatt@users.noreply.github.com>
2017-07-26 12:54:27 +0200

    V1.21.0 fix release notes (#1486)
    * Make release notes more precise.
    
    * Replace invalid links with test.
    
    * Update release notes.
    
    * Fix typos in release notes.
    
    * Fix travis build

--- commercetools-internal-docs/src/main/java/io/sphere/sdk/meta/ReleaseNotes.java
+++ commercetools-internal-docs/src/main/java/io/sphere/sdk/meta/ReleaseNotes.java
@@ -141,8 +141,11 @@ import java.util.function.Function;
  <li class=new-in-release>Added {@link io.sphere.sdk.customers.queries.CustomerByEmailTokenGet} to retrieve a customer by email token.</li>
  <li class=new-in-release>Added {@link io.sphere.sdk.producttypes.commands.updateactions.SetInputTip} update action.</li>
  <li class=fixed-in-release>{@link io.sphere.sdk.customers.queries.CustomerByPasswordTokenGet} now uses the new endpoint.</li>
- <li class=change-in-release>Changed type of {@link ProductDraft#getCategories()} to {@link ResourceIdentifier<Category>}. This change doesn't require changes to your source code,
- but requires a recompilation of all projects that depend on this class. </li>
+ <li class=change-in-release>Changed type of {@link ProductDraft#getCategories()} from Set&lt;Reference&lt;Category&gt;&gt; to Set&lt;ResourceIdentifier&lt;Category&gt;&gt;.
+ This breaking change may require an update of your source code, depending on how you use the {@link ProductDraft} type.
+ The previously returned {@link Reference} instances provided a {@link Reference#getObj()} method
+ that is not available from {@link ResourceIdentifier}. If you relied on the {@link Reference#getObj()} method to retrieve the id or key of the referenced object, you now have
+ to use the {@link ResourceIdentifier#getId()} and {@link ResourceIdentifier#getKey()} as exposed by the {@link ResourceIdentifier} interface.
  <li class=change-in-release>Updated <a href="http://javamoney.github.io/ri.html#welcome-to-moneta---the-jsr-354-reference-implementation">moneta</a> to version 1.1</li>
  <li class=change-in-release>Changed {@link SphereProjectScope} from an enum to a class and added missing scopes. This change doesn't require changes to your source code,
  but requires a recompilation of all projects that depend on this class.</li>
