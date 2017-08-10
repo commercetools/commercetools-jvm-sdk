a5bcf79d5228f303f5c06cb45d0843b989a8715e
ccsalazarr-commercetools <cristian.salazar@commercetools.de>
2017-08-10 14:08:53 +0200

    Publish only price, close #1480 (#1489)
    * Creation of scope to publish only prices
    
    * Add test for publish product scope
    
    * Remove public constructor from PublishScope
    
    * Replace PublishScope class by Enum
    
    * Refactor capitalize function
    
    * Clean code
    
    * Change enums to capital letter
    
    * Replace in setKey method, randomString() by randomKey()
    
    * Implements SphereEnumeration in PublishScope
    
    * Implements from SphereEnum, and add method to findByName

--- commercetools-models/src/main/java/io/sphere/sdk/products/commands/updateactions/Publish.java
+++ commercetools-models/src/main/java/io/sphere/sdk/products/commands/updateactions/Publish.java
@@ -2,22 +2,39 @@ package io.sphere.sdk.products.commands.updateactions;
 
 import io.sphere.sdk.commands.UpdateActionImpl;
 import io.sphere.sdk.products.Product;
+import io.sphere.sdk.products.PublishScope;
+
+import javax.annotation.Nullable;
 
 /**
  * Publishes a product, which causes the staged projection of the product to override the current projection. If the product is published for the first time, the current projection is created.
- *
+ * <p>
  * {@doc.gen intro}
- *
+ * <p>
  * {@include.example io.sphere.sdk.products.commands.ProductUpdateCommandIntegrationTest#publish()}
  *
  * @see io.sphere.sdk.products.messages.ProductPublishedMessage
  */
 public final class Publish extends UpdateActionImpl<Product> {
-    private Publish() {
+
+    @Nullable
+    final private PublishScope scope;
+
+    private Publish(@Nullable final PublishScope scope) {
         super("publish");
+        this.scope = scope;
     }
 
     public static Publish of() {
-        return new Publish();
+        return ofScope(null);
+    }
+
+    public static Publish ofScope(@Nullable final PublishScope scope) {
+        return new Publish(scope);
+    }
+
+    @Nullable
+    public PublishScope getScope() {
+        return scope;
     }
 }
