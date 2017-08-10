90ded1f287e162bc0ce7935ddbfb1083ddc25144
cristian.salazar <cristian.salazar@commercetools.de>
2017-08-10 10:50:33 +0200

    Implements SphereEnumeration in PublishScope

--- commercetools-models/src/main/java/io/sphere/sdk/products/PublishScope.java
+++ commercetools-models/src/main/java/io/sphere/sdk/products/PublishScope.java
@@ -1,23 +1,16 @@
 package io.sphere.sdk.products;
 
-import com.fasterxml.jackson.annotation.JsonValue;
-
-import static org.apache.commons.lang3.StringUtils.capitalize;
+import io.sphere.sdk.models.SphereEnumeration;
 
 /**
  * PublishScope.
  */
-public enum PublishScope {
+public enum PublishScope implements SphereEnumeration {
 
     /* ALL is the scope by default to allow to publish all changes */
     ALL,
 
     /* PRICES allow to publish only the prices */
     PRICES;
-
-    @JsonValue
-    public String toScopeString() {
-        return capitalize(name().toLowerCase());
-    }
 
 }
