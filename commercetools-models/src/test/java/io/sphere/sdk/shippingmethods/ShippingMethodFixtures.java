47763c77a9521b1d13665677b6fff21eeca7a9ff
mkoester <matthias.koester@commercetools.de>
2017-08-31 11:32:02 +0200

    Add integration tests for dynamic shipping methods.

--- commercetools-models/src/test/java/io/sphere/sdk/shippingmethods/ShippingMethodFixtures.java
+++ commercetools-models/src/test/java/io/sphere/sdk/shippingmethods/ShippingMethodFixtures.java
@@ -1,5 +1,6 @@
 package io.sphere.sdk.shippingmethods;
 
+import io.sphere.sdk.cartdiscounts.CartPredicate;
 import io.sphere.sdk.client.BlockingSphereClient;
 import io.sphere.sdk.shippingmethods.commands.ShippingMethodCreateCommand;
 import io.sphere.sdk.shippingmethods.commands.ShippingMethodDeleteCommand;
@@ -56,6 +57,32 @@ public class ShippingMethodFixtures {
     public static void withUpdateableShippingMethod(final BlockingSphereClient client, final Function<ShippingMethod, ShippingMethod> f) {
         withTaxCategory(client, taxCategory -> {
             final ShippingMethodDraft draft = ShippingMethodDraft.of(randomString(), "test shipping method", taxCategory, asList());
+            final ShippingMethod shippingMethod = client.executeBlocking(ShippingMethodCreateCommand.of(draft));
+            final ShippingMethod possiblyUpdatedShippingMethod = f.apply(shippingMethod);
+            client.executeBlocking(ShippingMethodDeleteCommand.of(possiblyUpdatedShippingMethod));
+        });
+    }
+
+    public static void withUpdateableDynamicShippingMethodForGermany(final BlockingSphereClient client, final CartPredicate cartPredicate, final UnaryOperator<ShippingMethod> consumer) {
+        final Optional<Zone> zoneOptional = client.executeBlocking(ZoneQuery.of().byCountry(DE)).head();
+        final Zone zone;
+        if (zoneOptional.isPresent()) {
+            zone = zoneOptional.get();
+        } else {
+            zone = client.executeBlocking(ZoneCreateCommand.of(ZoneDraft.of("de", singleton(Location.of(DE)))));
+        }
+        withUpdateableDynamicShippingMethod(client, cartPredicate, shippingMethodWithOutZone -> {
+            final ShippingMethod updated = client.executeBlocking(ShippingMethodUpdateCommand.of(shippingMethodWithOutZone, asList(AddZone.of(zone), AddShippingRate.of(ShippingRate.of(EURO_1), zone))));
+            return consumer.apply(updated);
+        });
+    }
+
+    public static void withUpdateableDynamicShippingMethod(final BlockingSphereClient client, final CartPredicate cartPredicate, final Function<ShippingMethod, ShippingMethod> f) {
+        withTaxCategory(client, taxCategory -> {
+            final ShippingMethodDraft draft = ShippingMethodDraftBuilder
+                    .of(randomString(), "test shipping method", taxCategory.toReference(), asList(), false)
+                    .predicate(cartPredicate)
+                    .build();
             final ShippingMethod shippingMethod = client.executeBlocking(ShippingMethodCreateCommand.of(draft));
             final ShippingMethod possiblyUpdatedShippingMethod = f.apply(shippingMethod);
             client.executeBlocking(ShippingMethodDeleteCommand.of(possiblyUpdatedShippingMethod));
