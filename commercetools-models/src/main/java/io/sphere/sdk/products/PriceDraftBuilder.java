5b4271c9e0011187801f0a9da0b4e3e28f143d40
mkoester <matthias.koester@commercetools.de>
2017-09-25 18:19:55 +0200

    Improve with feedback from code review.

--- commercetools-models/src/main/java/io/sphere/sdk/products/PriceDraftBuilder.java
+++ commercetools-models/src/main/java/io/sphere/sdk/products/PriceDraftBuilder.java
@@ -146,16 +146,19 @@ public final class PriceDraftBuilder extends Base implements Builder<PriceDraftD
                 .custom(template.getCustom());
     }
 
-    public static PriceDraftBuilder of(final Price template) {
-        final CustomFieldsDraft customFieldsDraft = Optional.ofNullable(template.getCustom())
+    private static CustomFieldsDraft copyCustom(Price template) {
+        return Optional.ofNullable(template.getCustom())
                 .map(customFields -> CustomFieldsDraftBuilder.of(customFields).build())
                 .orElse(null);
+    }
+
+    public static PriceDraftBuilder of(final Price template) {
         return of(template.getValue())
                 .country(template.getCountry())
                 .customerGroup(template.getCustomerGroup())
                 .channel(template.getChannel())
                 .validFrom(template.getValidFrom())
                 .validUntil(template.getValidUntil())
-                .custom(customFieldsDraft);
+                .custom(copyCustom(template));
     }
 }
