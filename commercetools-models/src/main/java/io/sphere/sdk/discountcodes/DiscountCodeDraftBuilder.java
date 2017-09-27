4d513e79bedc9316fa690b675720ec9404e308ea
Matthias Köster <katmatt@users.noreply.github.com>
2017-09-27 15:40:16 +0200

    Add custom fields to DiscountCode. (#1528)
    * Add custom fields to DiscountCode.
    
    * Improve stability of tests by using a random code.

--- commercetools-models/src/main/java/io/sphere/sdk/discountcodes/DiscountCodeDraftBuilder.java
+++ commercetools-models/src/main/java/io/sphere/sdk/discountcodes/DiscountCodeDraftBuilder.java
@@ -5,6 +5,7 @@ import io.sphere.sdk.cartdiscounts.CartPredicate;
 import io.sphere.sdk.models.LocalizedString;
 import io.sphere.sdk.models.Reference;
 import io.sphere.sdk.models.Referenceable;
+import io.sphere.sdk.types.CustomFieldsDraft;
 
 import javax.annotation.Nullable;
 import java.util.Collections;
@@ -12,8 +13,10 @@ import java.util.List;
 
 public final class DiscountCodeDraftBuilder extends DiscountCodeDraftBuilderBase<DiscountCodeDraftBuilder> {
 
-    DiscountCodeDraftBuilder(final Boolean active, final List<Reference<CartDiscount>> cartDiscounts, @Nullable final String cartPredicate, final String code, @Nullable final LocalizedString description, @Nullable final Long maxApplications, @Nullable final Long maxApplicationsPerCustomer, @Nullable final LocalizedString name) {
-        super(active, cartDiscounts, cartPredicate, code, description, maxApplications, maxApplicationsPerCustomer, name);
+    DiscountCodeDraftBuilder(final Boolean active, final List<Reference<CartDiscount>> cartDiscounts, final @Nullable String cartPredicate,
+                             final String code, final @Nullable CustomFieldsDraft custom, final @Nullable LocalizedString description,
+                             final @Nullable Long maxApplications, final @Nullable Long maxApplicationsPerCustomer, final @Nullable LocalizedString name) {
+        super(active, cartDiscounts, cartPredicate, code, custom, description, maxApplications, maxApplicationsPerCustomer, name);
     }
 
     public DiscountCodeDraftBuilder cartPredicate(@Nullable final CartPredicate cartPredicate) {
