f182ea4766087d53c0fb2fe1b13f86315b69d723
abeniasaad <achraf.beniasaad@commercetools.de>
2017-10-16 18:07:02 +0200

    fixing compilation issues

--- commercetools-models/src/main/java/io/sphere/sdk/orders/Parcel.java
+++ commercetools-models/src/main/java/io/sphere/sdk/orders/Parcel.java
@@ -1,6 +1,7 @@
 package io.sphere.sdk.orders;
 
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import io.sphere.sdk.annotations.ResourceValue;
 import io.sphere.sdk.models.CreationTimestamped;
 
 import javax.annotation.Nullable;
@@ -8,9 +9,15 @@ import java.time.ZonedDateTime;
 import java.util.List;
 
 @JsonDeserialize(as = ParcelImpl.class)
+@ResourceValue
 public interface Parcel extends CreationTimestamped {
+    @Deprecated
     static Parcel of(final String id, final ZonedDateTime createdAt, @Nullable final ParcelMeasurements measurements, @Nullable final TrackingData trackingData) {
-        return new ParcelImpl(id, createdAt, measurements, trackingData);
+        return of(createdAt,id,null, measurements,trackingData);
+    }
+
+    static Parcel of(final ZonedDateTime createdAt, final String id,@Nullable final List<DeliveryItem> items, @Nullable final ParcelMeasurements measurements,@Nullable final TrackingData trackingData){
+        return new ParcelImpl(createdAt,id,items, measurements,trackingData);
     }
 
     String getId();
--- commercetools-models/src/main/java/io/sphere/sdk/orders/ParcelMeasurements.java
+++ commercetools-models/src/main/java/io/sphere/sdk/orders/ParcelMeasurements.java
@@ -1,11 +1,13 @@
 package io.sphere.sdk.orders;
 
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import io.sphere.sdk.annotations.ResourceValue;
 
 import javax.annotation.Nullable;
 import java.util.List;
 
 @JsonDeserialize(as = ParcelMeasurementsImpl.class)
+@ResourceValue
 public interface ParcelMeasurements {
     Integer getHeightInMillimeter();
 
@@ -18,7 +20,14 @@ public interface ParcelMeasurements {
     @Nullable
     List<DeliveryItem> getItems();
 
+    @Deprecated
     static ParcelMeasurements of(final Integer lengthInMillimeter, final Integer widthInMillimeter, final Integer heightInMillimeter, final Integer weightInGram) {
-        return new ParcelMeasurementsImpl(lengthInMillimeter, widthInMillimeter, heightInMillimeter, weightInGram);
+        return new ParcelMeasurementsImpl(heightInMillimeter, null, lengthInMillimeter, weightInGram,widthInMillimeter);
     }
+
+    static ParcelMeasurements of(final Integer heightInMillimeter, @Nullable final List<DeliveryItem> items, final Integer lengthInMillimeter, final Integer weightInGram, final Integer widthInMillimeter){
+        return new ParcelMeasurementsImpl(heightInMillimeter, items, lengthInMillimeter, weightInGram,widthInMillimeter);
+    }
+
+
 }
