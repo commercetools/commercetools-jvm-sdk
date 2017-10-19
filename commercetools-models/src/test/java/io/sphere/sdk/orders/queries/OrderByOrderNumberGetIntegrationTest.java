63cfe8e812ec909c364678d87a35805e22bad0be
mkoester <matthias.koester@commercetools.de>
2017-10-19 14:02:35 +0200

    Fix tests.

--- commercetools-models/src/test/java/io/sphere/sdk/orders/queries/OrderByOrderNumberGetIntegrationTest.java
+++ commercetools-models/src/test/java/io/sphere/sdk/orders/queries/OrderByOrderNumberGetIntegrationTest.java
@@ -1,10 +1,13 @@
 package io.sphere.sdk.orders.queries;
 
 import io.sphere.sdk.orders.Order;
+import io.sphere.sdk.orders.commands.OrderUpdateCommand;
+import io.sphere.sdk.orders.commands.updateactions.SetOrderNumber;
 import io.sphere.sdk.test.IntegrationTest;
 import org.junit.Test;
 
 import static io.sphere.sdk.orders.OrderFixtures.withOrder;
+import static io.sphere.sdk.test.SphereTestUtils.randomString;
 import static org.assertj.core.api.Assertions.assertThat;
 
 
@@ -12,9 +15,13 @@ public class OrderByOrderNumberGetIntegrationTest extends IntegrationTest {
     @Test
     public void execution() throws Exception {
         withOrder(client(), order -> {
-            final Order loadedOrder = client().executeBlocking(OrderByOrderNumberGet.of(order.getOrderNumber()));
-            assertThat(loadedOrder.getOrderNumber()).isEqualTo(order.getOrderNumber());
-            return order;
+            final String orderNumber = randomString();
+            final Order orderWithOrderNumber = client().executeBlocking(OrderUpdateCommand.of(order,
+                    SetOrderNumber.of(orderNumber)));
+            final Order loadedOrder = client().executeBlocking(OrderByOrderNumberGet.of(orderNumber));
+
+            assertThat(loadedOrder.getOrderNumber()).isEqualTo(orderNumber);
+            return orderWithOrderNumber;
         });
     }
 }
