c28150f63fa3fb557a3f4fc7c4803d81dd87d10f
abeniasaad <achraf.beniasaad@commercetools.de>
2018-02-19 15:18:14 +0100

    # Add new customer messages
    Add new backend messages for customer update to our sdk
    
    closes #1703

---
+++ commercetools-models/src/main/java/io/sphere/sdk/customers/messages/CustomerEmailVerifiedMessage.java
@@ -0,0 +1,25 @@
+package io.sphere.sdk.customers.messages;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import io.sphere.sdk.customers.Customer;
+import io.sphere.sdk.messages.GenericMessageImpl;
+import io.sphere.sdk.messages.MessageDerivateHint;
+
+import java.time.ZonedDateTime;
+/**
+ * This message is the result of the {@link io.sphere.sdk.customers.commands.CustomerVerifyEmailCommand} command.
+ */
+@JsonDeserialize(as = CustomerEmailVerifiedMessage.class)//important to override annotation in Message class
+public final class CustomerEmailVerifiedMessage extends GenericMessageImpl<Customer> {
+    public static final String MESSAGE_TYPE = "CustomerEmailVerified";
+    public static final MessageDerivateHint<CustomerEmailVerifiedMessage> MESSAGE_HINT =
+            MessageDerivateHint.ofSingleMessageType(MESSAGE_TYPE, CustomerEmailVerifiedMessage.class, Customer.referenceTypeId());
+
+    @JsonCreator
+    private CustomerEmailVerifiedMessage(final String id, final Long version, final ZonedDateTime createdAt, final ZonedDateTime lastModifiedAt, final JsonNode resource, final Long sequenceNumber, final Long resourceVersion, final String type) {
+        super(id, version, createdAt, lastModifiedAt, resource, sequenceNumber, resourceVersion, type, Customer.class);
+    }
+
+}
---
+++ commercetools-models/src/main/java/io/sphere/sdk/customers/messages/CustomerGroupSetMessage.java
@@ -0,0 +1,34 @@
+package io.sphere.sdk.customers.messages;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import io.sphere.sdk.customergroups.CustomerGroup;
+import io.sphere.sdk.customers.Customer;
+import io.sphere.sdk.messages.GenericMessageImpl;
+import io.sphere.sdk.messages.MessageDerivateHint;
+import io.sphere.sdk.models.Reference;
+
+import java.time.ZonedDateTime;
+
+/**
+ * This message is the result of the {@link io.sphere.sdk.customers.commands.updateactions.SetCustomerGroup} update action.
+ */
+@JsonDeserialize(as = CustomerGroupSetMessage.class)//important to override annotation in Message class
+public final class CustomerGroupSetMessage extends GenericMessageImpl<Customer> {
+    public static final String MESSAGE_TYPE = "CustomerGroupSet";
+    public static final MessageDerivateHint<CustomerGroupSetMessage> MESSAGE_HINT =
+            MessageDerivateHint.ofSingleMessageType(MESSAGE_TYPE, CustomerGroupSetMessage.class, Customer.referenceTypeId());
+
+    private final Reference<CustomerGroup> customerGroup;
+
+    @JsonCreator
+    private CustomerGroupSetMessage(final String id, final Long version, final ZonedDateTime createdAt, final ZonedDateTime lastModifiedAt, final JsonNode resource, final Long sequenceNumber, final Long resourceVersion, final String type, final Reference<CustomerGroup> customerGroup) {
+        super(id, version, createdAt, lastModifiedAt, resource, sequenceNumber, resourceVersion, type, Customer.class);
+        this.customerGroup = customerGroup;
+    }
+
+    public Reference<CustomerGroup> getCustomerGroup() {
+        return customerGroup;
+    }
+}
