e60759be477c845c8cdf73d1e43479b873ef356a
abeniasaad <achraf.beniasaad@commercetools.de>
2018-02-21 15:18:55 +0100

    Update release notes.

--- commercetools-internal-docs/src/main/java/io/sphere/sdk/meta/ReleaseNotes.java
+++ commercetools-internal-docs/src/main/java/io/sphere/sdk/meta/ReleaseNotes.java
@@ -136,6 +136,11 @@ import java.util.function.Function;
  <li class=fixed-in-release></li>
  </ul>
  -->
+ <h3 class=released-version id="v1_29_0">1.29.1 (21.02.2018)</h3>
+ <li class=fixed-in-release>Javamoney issue with class loader.</li>
+ <ul>
+ </ul>
+
  <h3 class=released-version id="v1_29_0">1.29.0 (08.02.2018)</h3>
  <ul>
  <li class=change-in-release>added {@link CustomFields} to {@link CustomerGroup}</li>
