d8a56c3ff6e0cda795605ca5c29b28e8304e1351
abeniasaad <achraf.beniasaad@commercetools.de>
2018-02-21 11:49:40 +0100

    Cleaning up code

--- commercetools-sdk-base/src/main/java/javax/money/spi/Bootstrap.java
+++ commercetools-sdk-base/src/main/java/javax/money/spi/Bootstrap.java
@@ -6,7 +6,10 @@ import java.util.Objects;
 import java.util.ServiceLoader;
 import java.util.logging.Logger;
 
-
+/**
+ * This class was overriding from the javamoney library in order to alter the classloader behavior, though it might not be signaled as used by the ide,
+ * it will replace the one provided from the library after the compilation, as long as the canonical class names are kept identical.
+ */
 public final class Bootstrap {
 
     private static volatile ServiceProvider serviceProviderDelegate;
