47051e156fd74f0ce094c59f3cb89c7a90dea7a9
abeniasaad <achraf.beniasaad@commercetools.de>
2018-03-01 09:41:10 +0100

    Code review.

--- commercetools-models/src/main/java/io/sphere/sdk/carts/CartEndpoint.java
+++ commercetools-models/src/main/java/io/sphere/sdk/carts/CartEndpoint.java
@@ -3,5 +3,9 @@ package io.sphere.sdk.carts;
 import io.sphere.sdk.client.JsonEndpoint;
 
 public final class CartEndpoint {
+
     public static final JsonEndpoint<Cart> ENDPOINT = JsonEndpoint.of(Cart.typeReference(), "/carts");
+
+    private CartEndpoint(){}
+    
 }
