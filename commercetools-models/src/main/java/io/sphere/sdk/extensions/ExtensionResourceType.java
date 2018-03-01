b262f4e31d5cab6a5fd2bea3403deae049e1b921
Beniasaad Achraf <beni.asaad.achraf@gmail.com>
2018-03-01 10:08:58 +0100

    Customer added to API Extensions PR (#1721)
    * Customer added to API Extensions PR
    
    * Updating doc.
    
    * Code review.

---
+++ commercetools-models/src/main/java/io/sphere/sdk/extensions/ExtensionResourceType.java
@@ -0,0 +1,28 @@
+package io.sphere.sdk.extensions;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+/**
+ * This is a representation of the resource to which the extension is related, it i not a {@link io.sphere.sdk.models.SphereEnumeration}
+ * it was added to add tye safety while the developer points to a resource, while extending it.
+ *
+ */
+public enum ExtensionResourceType {
+
+    CART,
+    PAYMENT,
+    CUSTOMER;
+
+    @JsonCreator
+    public static ExtensionResourceType forValue(String value) {
+        return valueOf(value.toUpperCase());
+    }
+
+    @JsonValue
+    public String toValue() {
+        return toString().toLowerCase();
+    }
+
+
+}
