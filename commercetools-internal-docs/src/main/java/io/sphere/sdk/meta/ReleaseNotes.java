896018aec5465d183f570dcc0c56f1833d364482
abeniasaad <achraf.beniasaad@commercetools.de>
2018-03-06 09:39:14 +0100

    Adding closing tag release notes

--- commercetools-internal-docs/src/main/java/io/sphere/sdk/meta/ReleaseNotes.java
+++ commercetools-internal-docs/src/main/java/io/sphere/sdk/meta/ReleaseNotes.java
@@ -149,7 +149,7 @@ import java.util.function.Function;
  {@link io.sphere.sdk.customers.messages.CustomerGroupSetMessage}
  </li>
  <li class=new-in-release>Cart replication feature with {@link io.sphere.sdk.carts.commands.CartReplicationCommand} and {@link io.sphere.sdk.carts.commands.CartReplicationDraft}</li>
- <li class=change-in-release>Changed visibility of {@link ProductLike} interface to public.
+ <li class=change-in-release>Changed visibility of {@link ProductLike} interface to public.</li>
  <li class=change-in-release> Added {@link ExtensionResourceType} in order to add type check while creating {@link Trigger#getResourceTypeId()} field, instead of using string laterals.</li>
  <li class=change-in-release> Update jackson from 2.9.3 to 2.9.4 </li>
  <li class=change-in-release>changed return type of {@link ProductTypeDraft#getAttributes()} from {@link List<AttributeDefinition>} for {@link List<io.sphere.sdk.products.attributes.AttributeDefinitionDraft>}</li>
