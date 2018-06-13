f79a0bfbfafc7a1fa9d679981d0848bb9b6b1ef4
abeniasaad <achraf.beniasaad@commercetools.de>
2018-06-13 10:48:08 +0200

    Backend followup

--- commercetools-models/src/main/java/io/sphere/sdk/categories/commands/updateactions/ChangeParent.java
+++ commercetools-models/src/main/java/io/sphere/sdk/categories/commands/updateactions/ChangeParent.java
@@ -4,6 +4,7 @@ import io.sphere.sdk.categories.Category;
 import io.sphere.sdk.commands.UpdateActionImpl;
 import io.sphere.sdk.models.Reference;
 import io.sphere.sdk.models.Referenceable;
+import io.sphere.sdk.models.ResourceIdentifier;
 
 /**
  Sets a concrete parent for the category. Currently it is not possible int the API to remove the parent.
@@ -15,18 +16,22 @@ import io.sphere.sdk.models.Referenceable;
  <p>Consult the documentation for <a href="{@docRoot}/io/sphere/sdk/meta/CategoryDocumentation.html#category-tree-changes">categories</a> for more information.</p>
  */
 public final class ChangeParent extends UpdateActionImpl<Category> {
-    private final Reference<Category> parent;
+    private final ResourceIdentifier<Category> parent;
 
-    private ChangeParent(final Referenceable<Category> parent) {
+    private ChangeParent(final ResourceIdentifier<Category> parent) {
         super("changeParent");
-        this.parent = parent.toReference().filled(null);
+        this.parent = parent.toResourceIdentifier();
     }
 
     public static ChangeParent of(final Referenceable<Category> parent) {
+        return new ChangeParent(parent.toResourceIdentifier());
+    }
+
+    public static ChangeParent of(final ResourceIdentifier<Category> parent) {
         return new ChangeParent(parent);
     }
 
-    public Reference<Category> getParent() {
+    public ResourceIdentifier<Category> getParent() {
         return parent;
     }
 }
