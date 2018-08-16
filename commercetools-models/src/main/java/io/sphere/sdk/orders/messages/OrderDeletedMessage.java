4a29cb8d7124b65ce5cafa18e5f97ea9c2aa6f3f
Matthias Köster <katmatt@users.noreply.github.com>
2018-08-16 11:54:49 +0200

    Add new OrderDeletedMessage. (#1799)
    * Add new OrderDeletedMessage.

---
+++ commercetools-models/src/main/java/io/sphere/sdk/orders/messages/OrderDeletedMessage.java
@@ -0,0 +1,33 @@
+package io.sphere.sdk.orders.messages;
+
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import io.sphere.sdk.messages.GenericMessageImpl;
+import io.sphere.sdk.messages.MessageDerivateHint;
+import io.sphere.sdk.orders.Order;
+
+import java.time.ZonedDateTime;
+
+@JsonDeserialize(as = OrderDeletedMessage.class)//important to override annotation in Message class
+public final class OrderDeletedMessage extends GenericMessageImpl<Order> {
+
+    public static final String MESSAGE_TYPE = "OrderDeleted";
+    public static final MessageDerivateHint<OrderDeletedMessage> MESSAGE_HINT =
+            MessageDerivateHint.ofSingleMessageType(MESSAGE_TYPE, OrderDeletedMessage.class, Order.referenceTypeId());
+
+
+    private final Order order;
+
+    @JsonCreator
+    private OrderDeletedMessage(final String id, final Long version, final ZonedDateTime createdAt, final ZonedDateTime lastModifiedAt,
+                                final JsonNode resource, final Long sequenceNumber, final Long resourceVersion, final String type, final Order order) {
+        super(id, version, createdAt, lastModifiedAt, resource, sequenceNumber, resourceVersion, type, Order.class);
+        this.order = order;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+}
