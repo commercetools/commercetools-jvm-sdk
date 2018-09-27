7555073d9007cadf1904702d9a176904c0929b11
abeniasaad <achraf.beniasaad@commercetools.de>
2018-09-27 17:50:27 +0200

    Fixing tests

--- commercetools-models/src/main/java/io/sphere/sdk/orders/messages/OrderCustomerGroupSetMessage.java
+++ commercetools-models/src/main/java/io/sphere/sdk/orders/messages/OrderCustomerGroupSetMessage.java
@@ -9,22 +9,21 @@ import io.sphere.sdk.messages.MessageDerivateHint;
 import io.sphere.sdk.messages.UserProvidedIdentifiers;
 import io.sphere.sdk.models.Reference;
 import io.sphere.sdk.orders.Order;
-import io.sphere.sdk.orders.OrderState;
 
 import java.time.ZonedDateTime;
 
-@JsonDeserialize(as = OrderCustomerGroupSetMessge.class)//important to override annotation in Message class
-public final class OrderCustomerGroupSetMessge extends GenericMessageImpl<Order> {
+@JsonDeserialize(as = OrderCustomerGroupSetMessage.class)//important to override annotation in Message class
+public final class OrderCustomerGroupSetMessage extends GenericMessageImpl<Order> {
     public static final String MESSAGE_TYPE = "OrderCustomerGroupSet";
-    public static final MessageDerivateHint<OrderCustomerGroupSetMessge> MESSAGE_HINT =
-            MessageDerivateHint.ofSingleMessageType(MESSAGE_TYPE, OrderCustomerGroupSetMessge.class, Order.referenceTypeId());
+    public static final MessageDerivateHint<OrderCustomerGroupSetMessage> MESSAGE_HINT =
+            MessageDerivateHint.ofSingleMessageType(MESSAGE_TYPE, OrderCustomerGroupSetMessage.class, Order.referenceTypeId());
 
     private final Reference<CustomerGroup> customerGroup;
 
     private final Reference<CustomerGroup> oldCustomerGroup;
 
     @JsonCreator
-    private OrderCustomerGroupSetMessge(final String id, final Long version, final ZonedDateTime createdAt, final ZonedDateTime lastModifiedAt, final JsonNode resource, final Long sequenceNumber, final Long resourceVersion, final String type, final UserProvidedIdentifiers resourceUserProvidedIdentifiers, final Reference<CustomerGroup> customerGroup, final Reference<CustomerGroup> oldCustomerGroup) {
+    private OrderCustomerGroupSetMessage(final String id, final Long version, final ZonedDateTime createdAt, final ZonedDateTime lastModifiedAt, final JsonNode resource, final Long sequenceNumber, final Long resourceVersion, final String type, final UserProvidedIdentifiers resourceUserProvidedIdentifiers, final Reference<CustomerGroup> customerGroup, final Reference<CustomerGroup> oldCustomerGroup) {
         super(id, version, createdAt, lastModifiedAt, resource, sequenceNumber, resourceVersion, type,resourceUserProvidedIdentifiers, Order.class);
         this.customerGroup = customerGroup;
         this.oldCustomerGroup = oldCustomerGroup;
