0482a29832f2fcfb1995774a63b3a75224a10d03
Beniasaad Achraf <beni.asaad.achraf@gmail.com>
2018-10-17 11:02:01 +0200

    Update version badge (#1834)

--- commercetools-models/src/main/java/io/sphere/sdk/shippingmethods/commands/updateactions/ChangeTaxCategory.java
+++ commercetools-models/src/main/java/io/sphere/sdk/shippingmethods/commands/updateactions/ChangeTaxCategory.java
@@ -1,8 +1,8 @@
 package io.sphere.sdk.shippingmethods.commands.updateactions;
 
 import io.sphere.sdk.commands.UpdateActionImpl;
-import io.sphere.sdk.models.Reference;
 import io.sphere.sdk.models.Referenceable;
+import io.sphere.sdk.models.ResourceIdentifier;
 import io.sphere.sdk.shippingmethods.ShippingMethod;
 import io.sphere.sdk.taxcategories.TaxCategory;
 
@@ -15,18 +15,22 @@ import io.sphere.sdk.taxcategories.TaxCategory;
  * {@include.example io.sphere.sdk.shippingmethods.commands.ShippingMethodUpdateCommandIntegrationTest#changeTaxCategory()}
  */
 public final class ChangeTaxCategory extends UpdateActionImpl<ShippingMethod> {
-    private final Reference<TaxCategory> taxCategory;
+    private final ResourceIdentifier<TaxCategory> taxCategory;
 
-    private ChangeTaxCategory(final Reference<TaxCategory> taxCategory) {
+    private ChangeTaxCategory(final ResourceIdentifier<TaxCategory> taxCategory) {
         super("changeTaxCategory");
         this.taxCategory = taxCategory;
     }
 
-    public Reference<TaxCategory> getTaxCategory() {
+    public ResourceIdentifier<TaxCategory> getTaxCategory() {
         return taxCategory;
     }
 
     public static ChangeTaxCategory of(final Referenceable<TaxCategory> taxCategory) {
         return new ChangeTaxCategory(taxCategory.toReference());
+    }
+
+    public static ChangeTaxCategory of(final ResourceIdentifier<TaxCategory> taxCategory) {
+        return new ChangeTaxCategory(taxCategory);
     }
 }
