6e87bedee41df372e21bd4dd4f01ca16b4b23fe6
abeniasaad <achraf.beniasaad@commercetools.de>
2018-10-24 11:01:16 +0200

    Fixing versions

--- sdk-http-ahc-2_5/src/main/java/io/sphere/sdk/http/AsyncHttpClientAdapter.java
+++ sdk-http-ahc-2_5/src/main/java/io/sphere/sdk/http/AsyncHttpClientAdapter.java
@@ -3,7 +3,7 @@ package io.sphere.sdk.http;
 import org.asynchttpclient.AsyncHttpClient;
 
 /**
- * Adapter to use {@code AsyncHttpClient} (version 2.0.x) as {@code HttpClient}.
+ * Adapter to use {@code AsyncHttpClient} (version 2.5.x) as {@code HttpClient}.
  */
 public interface AsyncHttpClientAdapter extends HttpClient {
 
