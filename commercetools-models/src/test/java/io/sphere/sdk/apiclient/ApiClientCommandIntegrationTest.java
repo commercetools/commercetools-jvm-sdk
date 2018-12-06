cfb14cfd56cfc5c18f22ac6594bacbf0751e6eaf
abeniasaad <achraf.beniasaad@commercetools.de>
2018-12-06 15:56:32 +0100

    Update timeout

--- commercetools-models/src/test/java/io/sphere/sdk/apiclient/ApiClientCommandIntegrationTest.java
+++ commercetools-models/src/test/java/io/sphere/sdk/apiclient/ApiClientCommandIntegrationTest.java
@@ -1,7 +1,6 @@
 package io.sphere.sdk.apiclient;
 
 import io.sphere.sdk.apiclient.commands.ApiClientCreateCommand;
-
 import io.sphere.sdk.apiclient.commands.ApiClientDeleteCommand;
 import io.sphere.sdk.apiclient.queries.ApiClientQuery;
 import io.sphere.sdk.client.SphereScope;
@@ -9,14 +8,13 @@ import io.sphere.sdk.queries.PagedQueryResult;
 import io.sphere.sdk.test.IntegrationTest;
 import org.junit.Test;
 
-import java.time.ZonedDateTime;
-
 import java.util.stream.Collectors;
 
-import static org.assertj.core.api.Assertions.*;
-import static io.sphere.sdk.client.SphereProjectScope.*;
+import static io.sphere.sdk.client.SphereProjectScope.MANAGE_API_CLIENTS;
+import static io.sphere.sdk.client.SphereProjectScope.MANAGE_MY_ORDERS;
+import static io.sphere.sdk.test.SphereTestUtils.assertEventually;
+import static org.assertj.core.api.Assertions.assertThat;
 import static java.util.Arrays.*;
-import static io.sphere.sdk.apiclient.ApiClientFixtures.*;
 
 public class ApiClientCommandIntegrationTest extends IntegrationTest {
 
@@ -41,7 +39,10 @@ public class ApiClientCommandIntegrationTest extends IntegrationTest {
         final PagedQueryResult<ApiClient> result = client().executeBlocking(ApiClientQuery.of()
                 .plusPredicates(m -> m.id().is(res.getId()))
         );
-        assertThat(result.getResults()).hasSize(1);
+
+        assertEventually(() ->{
+            assertThat(result.getResults()).hasSize(1);
+        });
 
         assertThat(result.getResults().get(0)).isEqualToIgnoringGivenFields(res,"secret");
         final ApiClient deletedRes =client().executeBlocking(ApiClientDeleteCommand.of(res));
