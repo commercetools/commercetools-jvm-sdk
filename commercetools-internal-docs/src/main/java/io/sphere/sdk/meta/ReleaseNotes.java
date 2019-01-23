29a77c3c3782d8405cb8c539cfa3a278fb02a174
abeniasaad <achraf.beniasaad@commercetools.de>
2019-01-23 13:59:08 +0100

    Update release notes

--- commercetools-internal-docs/src/main/java/io/sphere/sdk/meta/ReleaseNotes.java
+++ commercetools-internal-docs/src/main/java/io/sphere/sdk/meta/ReleaseNotes.java
@@ -154,6 +154,7 @@ import java.util.function.Function;
 
  <h3 class=released-version id="v1_40_0">1.40.0</h3>
  <ul>
+    <li class=new-in-release>{@link java.time.ZonedDateTime} formatter now always include milliseconds even if they are equal to 0</li>
     <li class=new-in-release>{@link io.sphere.sdk.customobjects.queries.CustomObjectByIdGet} and {@link CustomObjectByKeyGet} now support expansion parameter</li>
     <li class=change-in-release>{@link io.sphere.sdk.producttypes.commands.updateactions.ChangeAttributeOrder} is now deprecated</li>
     <li class=new-in-release>{@link io.sphere.sdk.producttypes.commands.updateactions.ChangeAttributeOrderByName} added</li>
