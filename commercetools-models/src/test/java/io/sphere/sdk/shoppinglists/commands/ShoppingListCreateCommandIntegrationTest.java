1c1893e136e1823f847b76116a29aadf10d15551
alex-ct <aleksandar.petrovic@commercetools.de>
2019-02-21 17:09:55 +0100

    upgraded more dependencies, removed osgi from main module until project builds successfully with java 11

--- commercetools-models/src/test/java/io/sphere/sdk/shoppinglists/commands/ShoppingListCreateCommandIntegrationTest.java
+++ commercetools-models/src/test/java/io/sphere/sdk/shoppinglists/commands/ShoppingListCreateCommandIntegrationTest.java
@@ -7,12 +7,11 @@ import io.sphere.sdk.products.commands.updateactions.SetSku;
 import io.sphere.sdk.shoppinglists.*;
 import io.sphere.sdk.shoppinglists.queries.ShoppingListQuery;
 import io.sphere.sdk.test.IntegrationTest;
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.UUID;
 
 import static io.sphere.sdk.products.ProductFixtures.withUpdateableProduct;
 import static io.sphere.sdk.test.SphereTestUtils.en;
@@ -20,22 +19,13 @@ import static io.sphere.sdk.test.SphereTestUtils.randomKey;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class ShoppingListCreateCommandIntegrationTest extends IntegrationTest {
-    private static final String DEMO_SHOPPING_LIST_KEY = "demo-shopping-list-key";
-
-    @Before
-    @After
-    public void clean() {
-        List<ShoppingList> results = client().executeBlocking(ShoppingListQuery.of()
-                .withPredicates(l -> l.key().is(DEMO_SHOPPING_LIST_KEY)))
-                .getResults();
-        results.forEach(shoppingList -> client().executeBlocking(ShoppingListDeleteCommand.of(shoppingList)));
-    }
 
     @Test
     public void execution() {
+        final String shoppingCartKey = "demo-shopping-list-key" + UUID.randomUUID().toString();
         final int deleteDaysAfterLastModification = 1;
         final ShoppingListDraftDsl draft = ShoppingListFixtures.newShoppingListDraftBuilder()
-                .key(DEMO_SHOPPING_LIST_KEY)
+                .key(shoppingCartKey)
                 .description(en("Demo shopping list description."))
                 .slug(en("demo-shopping-list-slug"))
                 .deleteDaysAfterLastModification(deleteDaysAfterLastModification)
@@ -44,18 +34,20 @@ public class ShoppingListCreateCommandIntegrationTest extends IntegrationTest {
 
         assertThat(shoppingList).isNotNull();
         assertThat(shoppingList.getId()).isNotNull();
-        assertThat(shoppingList.getKey()).isEqualTo(DEMO_SHOPPING_LIST_KEY);
+        assertThat(shoppingList.getKey()).isEqualTo(shoppingCartKey);
         assertThat(shoppingList.getDeleteDaysAfterLastModification()).isEqualTo(deleteDaysAfterLastModification);
+        clean(shoppingCartKey);
     }
 
     @Test
     public void withLineItemBySku() {
+        final String shoppingCartKey = "demo-shopping-list-key" + UUID.randomUUID().toString();
         withUpdateableProduct(client(), product -> {
            final String sku = randomKey();
             final Product productWithSku = client().executeBlocking(ProductUpdateCommand.of(product, Arrays.asList(SetSku.of(1, sku), Publish.of())));
             final LineItemDraftDsl lineItemBySku = LineItemDraftBuilder.ofSku(sku, 1L).build();
             final ShoppingListDraft shoppingListDraft = ShoppingListFixtures.newShoppingListDraftBuilder()
-                    .key(DEMO_SHOPPING_LIST_KEY)
+                    .key(shoppingCartKey)
                     .plusLineItems(lineItemBySku)
                     .build();
             final ShoppingList shoppingList = client().executeBlocking(ShoppingListCreateCommand.of(shoppingListDraft).withExpansionPaths(m -> m.lineItems()));
@@ -66,5 +58,13 @@ public class ShoppingListCreateCommandIntegrationTest extends IntegrationTest {
 
             return productWithSku;
         });
+        clean(shoppingCartKey);
+    }
+
+    private void clean(String key) {
+        List<ShoppingList> results = client().executeBlocking(ShoppingListQuery.of()
+                .withPredicates(l -> l.key().is(key)))
+                .getResults();
+        results.forEach(shoppingList -> client().executeBlocking(ShoppingListDeleteCommand.of(shoppingList)));
     }
 }
