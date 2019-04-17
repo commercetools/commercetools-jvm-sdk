a654498fb14406cb32083ed5314bc692714a3c05
alex-ct <aleksandar.petrovic@commercetools.de>
2019-04-17 08:43:55 +0200

    removed update with key from store update command to test if it causes problem on travis

--- commercetools-models/src/main/java/io/sphere/sdk/stores/Store.java
+++ commercetools-models/src/main/java/io/sphere/sdk/stores/Store.java
@@ -20,7 +20,7 @@ import javax.annotation.Nullable;
 @HasByIdGetEndpoint(javadocSummary = "Gets a store by ID.", includeExamples = "io.sphere.sdk.stores.queries.StoreGetIntegrationTest#getById()")
 @HasByKeyGetEndpoint(javadocSummary = "Get store by Key", includeExamples = "io.sphere.sdk.stores.queries.StoreGetIntegrationTest#getByKey()")
 @HasCreateCommand(includeExamples = "io.sphere.sdk.stores.commands.StoreCreateCommandIntegrationTest#execute()")
-@HasUpdateCommand(updateWith = "key")
+@HasUpdateCommand()
 @HasDeleteCommand(deleteWith = "key", includeExamples = "io.sphere.sdk.stores.commands.StoreDeleteCommandIntegrationTest#deleteByKey()")
 @HasQueryModel()
 public interface Store extends Resource<Store> {
