b366dfa096a6d0141c417698783157fad1f631a4
alex-ct <aleksandar.petrovic@commercetools.de>
2019-05-23 08:53:18 +0200

    updated release notes

--- commercetools-internal-docs/src/main/java/io/sphere/sdk/meta/ReleaseNotes.java
+++ commercetools-internal-docs/src/main/java/io/sphere/sdk/meta/ReleaseNotes.java
@@ -157,6 +157,7 @@ import java.util.function.Function;
     <li class=fixed-in-release>Added support for {@link io.sphere.sdk.extensions.AuthorizationHeaderAuthentication} for api extensions.</li>
     <li class=fixed-in-release>Fixed {@link io.sphere.sdk.orderedits.commands.stagedactions.AddLineItem} staged update action structure</li>
     <li class=new-in-release>Added support for {@link io.sphere.sdk.stores.Store}</li>
+    <li class=new-in-release>Added support commands that allow you to access carts and orders belonging to a specific store.</li>
  </ul>
 
  <h3 class=released-version id="v1_41_0">1.41.0 (10.04.2019)</h3>
