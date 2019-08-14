bc24c6e30de445d9722ed55ba42c0ac0d4264565
alex-ct <aleksandar.petrovic@commercetools.de>
2019-08-14 09:03:17 +0200

    fixed failing class rules validation

--- commercetools-models/src/main/java/io/sphere/sdk/customers/queries/CustomerInStoreByKeyGetImpl.java
+++ commercetools-models/src/main/java/io/sphere/sdk/customers/queries/CustomerInStoreByKeyGetImpl.java
@@ -6,7 +6,7 @@ import io.sphere.sdk.customers.expansion.CustomerExpansionModel;
 import io.sphere.sdk.queries.MetaModelGetDslBuilder;
 import io.sphere.sdk.queries.MetaModelGetDslImpl;
 
-public class CustomerInStoreByKeyGetImpl extends MetaModelGetDslImpl<Customer, Customer, CustomerInStoreByKeyGet, CustomerExpansionModel<Customer>> implements CustomerInStoreByKeyGet {
+final class CustomerInStoreByKeyGetImpl extends MetaModelGetDslImpl<Customer, Customer, CustomerInStoreByKeyGet, CustomerExpansionModel<Customer>> implements CustomerInStoreByKeyGet {
 
     CustomerInStoreByKeyGetImpl(final String storeKey, final String key) {
         super("key=" + key, JsonEndpoint.of(Customer.typeReference(), "/in-store/key=" + storeKey + "/customers"), CustomerExpansionModel.of(), CustomerInStoreByKeyGetImpl::new);
