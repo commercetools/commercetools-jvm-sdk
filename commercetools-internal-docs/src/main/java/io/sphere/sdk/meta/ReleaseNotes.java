e9f14c857dd5771d512250902e175509637e2384
alex-ct <aleksandar.petrovic@commercetools.de>
2019-08-21 11:29:58 +0200

    updated release notes

--- commercetools-internal-docs/src/main/java/io/sphere/sdk/meta/ReleaseNotes.java
+++ commercetools-internal-docs/src/main/java/io/sphere/sdk/meta/ReleaseNotes.java
@@ -156,7 +156,8 @@ import java.util.function.Function;
 
  <h3 class=released-version id="v1_45_0">1.45.0 (02.08.2019)</h3>
  <ul>
- <li class=fixed-in-release>{@link Category}, {@link CategoryDraft}, {@link ProductLike} and {@link ProductDraft} now extend the {@link WithKey} interface.</li>
+     <li class=fixed-in-release>{@link Category}, {@link CategoryDraft}, {@link ProductLike} and {@link ProductDraft} now extend the {@link WithKey} interface.</li>
+     <li class=fixed-in-release>Update actions for setting custom fields now accept empty array values</li>
  </ul>
 
  <h3 class=released-version id="v1_44_0">1.44.0 (07.06.2019)</h3>
