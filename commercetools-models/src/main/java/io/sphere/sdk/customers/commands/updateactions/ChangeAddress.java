8023cb01d752c5bcb262ac04ce3a7437d26c0c49
Barbara Palumbo <barbara.palumbo@commercetools.de>
2020-11-13 13:29:17 +0100

    WIP: update of method now accept nullable params and fix exception and return

--- commercetools-models/src/main/java/io/sphere/sdk/customers/commands/updateactions/ChangeAddress.java
+++ commercetools-models/src/main/java/io/sphere/sdk/customers/commands/updateactions/ChangeAddress.java
@@ -3,6 +3,13 @@ package io.sphere.sdk.customers.commands.updateactions;
 import io.sphere.sdk.commands.UpdateActionImpl;
 import io.sphere.sdk.customers.Customer;
 import io.sphere.sdk.models.Address;
+import io.sphere.sdk.orders.Delivery;
+import io.sphere.sdk.orders.DeliveryItem;
+import io.sphere.sdk.orders.ParcelDraft;
+import io.sphere.sdk.orders.commands.updateactions.AddParcelToDelivery;
+
+import javax.annotation.Nullable;
+import java.util.List;
 
 import static java.lang.String.format;
 
@@ -17,10 +24,12 @@ import static java.lang.String.format;
  */
 public final class ChangeAddress extends UpdateActionImpl<Customer> {
     private final Address address;
+    @Nullable
     private final String addressId;
+    @Nullable
     private final String addressKey;
 
-    private ChangeAddress(final String addressId, final Address address, String addressKey) {
+    private ChangeAddress(@Nullable final String addressId, final Address address, @Nullable String addressKey) {
         super("changeAddress");
         this.address = address;
         this.addressId = addressId;
@@ -28,18 +37,22 @@ public final class ChangeAddress extends UpdateActionImpl<Customer> {
     }
 
     public static ChangeAddress of(final String addressId, final Address address) {
-        return new ChangeAddress(addressId, address, address.getKey());
+        return new ChangeAddress(addressId, address, null);
     }
 
-    public static ChangeAddress of(final String addressId, final String addressKey, final Address address) {
-        return new ChangeAddress(addressId, address, addressKey);
+    public static ChangeAddress of(final Address address, final String addressKey) {
+        return new ChangeAddress(null, address, addressKey);
     }
 
     public static ChangeAddress ofOldAddressToNewAddress(final Address oldAddress, final Address newAddress) {
-        if (oldAddress.getId() == null) {
-            throw new IllegalArgumentException(format("The address %s should have an id.", oldAddress));
+        if (oldAddress.getId() == null & oldAddress.getKey() == null) {
+            throw new IllegalArgumentException(format("The address %s should have an id or a key.", oldAddress));
         }
-        return of(oldAddress.getId(), oldAddress.getKey(), newAddress);
+        if (oldAddress.getId() != null) {
+            return of(oldAddress.getId(), newAddress);
+        } else {
+            return of(newAddress, oldAddress.getKey());
+        }
     }
 
     public Address getAddress() {
