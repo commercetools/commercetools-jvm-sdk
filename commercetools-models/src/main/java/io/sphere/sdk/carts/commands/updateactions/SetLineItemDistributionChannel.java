2577e65441ca155b8544333a227b87866f192789
Barbara Palumbo <barbara.palumbo@commercetools.de>
2021-06-07 16:43:35 +0200

    remove of methods and left one

--- commercetools-models/src/main/java/io/sphere/sdk/carts/commands/updateactions/SetLineItemDistributionChannel.java
+++ commercetools-models/src/main/java/io/sphere/sdk/carts/commands/updateactions/SetLineItemDistributionChannel.java
@@ -1,6 +1,7 @@
 package io.sphere.sdk.carts.commands.updateactions;
 
 import io.sphere.sdk.carts.Cart;
+import io.sphere.sdk.carts.ExternalLineItemTotalPrice;
 import io.sphere.sdk.carts.LineItem;
 import io.sphere.sdk.channels.Channel;
 import io.sphere.sdk.commands.UpdateActionImpl;
@@ -26,18 +27,6 @@ public final class SetLineItemDistributionChannel extends UpdateActionImpl<Cart>
         super("setLineItemDistributionChannel");
         this.lineItemId = lineItemId;
         this.distributionChannel = distributionChannel;
-    }
-
-    public static SetLineItemDistributionChannel of(final LineItem lineItem) {
-        return of(lineItem.getId());
-    }
-
-    public static SetLineItemDistributionChannel of(final String lineItemId) {
-        return of(lineItemId, null);
-    }
-
-    public static SetLineItemDistributionChannel of(final LineItem lineItem, @Nullable final Reference<Channel> distributionChannel) {
-        return of(lineItem.getId(), distributionChannel);
     }
 
     public static SetLineItemDistributionChannel of(final String lineItemId, @Nullable final Reference<Channel> distributionChannel) {
