0b28d244f6c40b912e085ec74353f6b8aa2a808b
Barbara Palumbo <barbara.palumbo@commercetools.de>
2021-06-08 13:36:07 +0200

    add assertEventually to assertions

--- commercetools-models/src/test/java/io/sphere/sdk/products/attributes/DefaultProductAttributeFormatterIntegrationTest.java
+++ commercetools-models/src/test/java/io/sphere/sdk/products/attributes/DefaultProductAttributeFormatterIntegrationTest.java
@@ -14,6 +14,7 @@ import java.util.Collection;
 import java.util.Locale;
 import java.util.regex.Pattern;
 
+import static io.sphere.sdk.test.SphereTestUtils.assertEventually;
 import static java.util.Arrays.asList;
 import static java.util.Collections.singleton;
 import static java.util.Collections.singletonList;
@@ -82,82 +83,114 @@ public class DefaultProductAttributeFormatterIntegrationTest extends Integration
 
     @Test
     public void convertProductReferenceSet() {
-        assertThat(converting(ATTR_NAME_REF_SET)).matches(Pattern.compile("\\S+, \\S+"));
+        assertEventually(() -> {
+            assertThat(converting(ATTR_NAME_REF_SET)).matches(Pattern.compile("\\S+, \\S+"));
+        });
     }
 
     @Test
     public void convertProductReference() {
-        assertThat(converting(ATTR_NAME_REF)).matches(Pattern.compile("\\S+"));
+        assertEventually(() -> {
+            assertThat(converting(ATTR_NAME_REF)).matches(Pattern.compile("\\S+"));
+        });
     }
 
     @Test
     public void convertMoneySet() {
-        assertThat(converting(ATTR_NAME_MONEY_SET)).isEqualTo("500,00 EUR, 1.000,00 USD");
+        assertEventually(() -> {
+            assertThat(converting(ATTR_NAME_MONEY_SET)).isEqualTo("500,00 EUR, 1.000,00 USD");
+        });
     }
 
     @Test
     public void convertLongSet() {
-        assertThat(converting(ATTR_NAME_NUMBER_SET)).isEqualTo("5, 10");
+        assertEventually(() -> {
+            assertThat(converting(ATTR_NAME_NUMBER_SET)).isEqualTo("5, 10");
+        });
     }
 
     @Test
     public void convertLong() {
-        assertThat(converting(ATTR_NAME_NUMBER)).isEqualTo("5");
+        assertEventually(() -> {
+            assertThat(converting(ATTR_NAME_NUMBER)).isEqualTo("5");
+        });
     }
 
     @Test
     public void convertTimeSet() {
-        assertThat(converting(ATTR_NAME_TIME_SET)).isEqualTo("22:05, 23:06");
+        assertEventually(() -> {
+            assertThat(converting(ATTR_NAME_TIME_SET)).isEqualTo("22:05, 23:06");
+        });
     }
 
     @Test
     public void convertTime() {
-        assertThat(converting(ATTR_NAME_TIME)).isEqualTo("22:05");
+        assertEventually(() -> {
+            assertThat(converting(ATTR_NAME_TIME)).isEqualTo("22:05");
+        });
     }
 
     @Test
     public void convertLocalizedStringSet() {
-        assertThat(converting(ATTR_NAME_LOC_TEXT_SET)).isEqualTo("localized bar, localized foo");
+        assertEventually(() -> {
+            assertThat(converting(ATTR_NAME_LOC_TEXT_SET)).isEqualTo("localized bar, localized foo");
+        });
     }
 
     @Test
     public void convertLocalizedEnumValueSet() {
-        assertThat(converting(ATTR_NAME_LOC_ENUM_SET)).isEqualTo("drei, zwei");
+        assertEventually(() -> {
+            assertThat(converting(ATTR_NAME_LOC_ENUM_SET)).isEqualTo("drei, zwei");
+        });
     }
 
     @Test
     public void convertIntegerSet() {
-        assertThat(converting(ATTR_NAME_NUMBER_SET, numberAsIntegerMapper)).isEqualTo("5, 10");
+        assertEventually(() -> {
+            assertThat(converting(ATTR_NAME_NUMBER_SET, numberAsIntegerMapper)).isEqualTo("5, 10");
+        });
     }
 
     @Test
     public void convertInteger() {
-        assertThat(converting(ATTR_NAME_NUMBER, numberAsIntegerMapper)).isEqualTo("5");
+        assertEventually(() -> {
+            assertThat(converting(ATTR_NAME_NUMBER, numberAsIntegerMapper)).isEqualTo("5");
+        });
     }
 
     @Test
     public void convertEnumValueSet() {
-        assertThat(converting(ATTR_NAME_ENUM_SET)).isEqualTo("three, two");
+        assertEventually(() -> {
+            assertThat(converting(ATTR_NAME_ENUM_SET)).isEqualTo("three, two");
+        });
     }
 
     @Test
     public void convertDoubleSet() {
-        assertThat(converting(ATTR_NAME_NUMBER_SET, numberAsDoubleMapper)).isEqualTo("5.0, 10.0");
+        assertEventually(() -> {
+            assertThat(converting(ATTR_NAME_NUMBER_SET, numberAsDoubleMapper)).isEqualTo("5.0, 10.0");
+        });
     }
 
     @Test
     public void convertDouble() {
-        assertThat(converting(ATTR_NAME_NUMBER, numberAsDoubleMapper)).isEqualTo("5.0");
+        assertEventually(() -> {
+            assertThat(converting(ATTR_NAME_NUMBER, numberAsDoubleMapper)).isEqualTo("5.0");
+        });
     }
 
     @Test
     public void convertDateTimeSet() {
-        assertThat(converting(ATTR_NAME_DATE_TIME_SET)).isEqualTo("11.09.01 22:05, 12.10.02 23:06");
+        assertEventually(() -> {
+            assertThat(converting(ATTR_NAME_DATE_TIME_SET)).isEqualTo("11.09.01 22:05, 12.10.02 23:06");
+        });
     }
 
     @Test
     public void convertDateSet() {
-        assertThat(converting(ATTR_NAME_DATE_SET)).isEqualTo("11.09.2001, 12.10.2002");
+        assertEventually(() -> {
+            assertThat(converting(ATTR_NAME_DATE_SET)).isEqualTo("11.09.2001, 12.10.2002");
+        });
     }
 
     @Ignore
@@ -182,47 +215,65 @@ public class DefaultProductAttributeFormatterIntegrationTest extends Integration
 
     @Test
     public void convertBooleanSet() {
-        assertThat(converting(ATTR_NAME_BOOLEAN_SET)).isEqualTo("false, true");
+        assertEventually(() -> {
+            assertThat(converting(ATTR_NAME_BOOLEAN_SET)).isEqualTo("false, true");
+        });
     }
 
     @Test
     public void convertString() {
-        assertThat(converting(ATTR_NAME_TEXT)).isEqualTo("foo");
+        assertEventually(() -> {
+            assertThat(converting(ATTR_NAME_TEXT)).isEqualTo("foo");
+        });
     }
 
     @Test
     public void convertMoney() {
-        assertThat(converting(ATTR_NAME_MONEY)).isEqualTo("500,00 EUR");
+        assertEventually(() -> {
+            assertThat(converting(ATTR_NAME_MONEY)).isEqualTo("500,00 EUR");
+        });
     }
 
     @Test
     public void convertLocalizedString() {
-        assertThat(converting(ATTR_NAME_LOC_TEXT2)).isEqualTo("German foo");
+        assertEventually(() -> {
+            assertThat(converting(ATTR_NAME_LOC_TEXT2)).isEqualTo("German foo");
+        });
     }
 
     @Test
     public void convertLocalizedEnumValue() {
-        assertThat(converting(ATTR_NAME_LOC_ENUM)).isEqualTo("zwei");
+        assertEventually(() -> {
+            assertThat(converting(ATTR_NAME_LOC_ENUM)).isEqualTo("zwei");
+        });
     }
 
     @Test
     public void convertEnumValue() {
-        assertThat(converting(ATTR_NAME_ENUM)).isEqualTo("two");
+        assertEventually(() -> {
+            assertThat(converting(ATTR_NAME_ENUM)).isEqualTo("two");
+        });
     }
 
     @Test
     public void convertDateTime() {
-        assertThat(converting(ATTR_NAME_DATE_TIME)).isEqualTo("11.09.01 22:05");
+        assertEventually(() -> {
+            assertThat(converting(ATTR_NAME_DATE_TIME)).isEqualTo("11.09.01 22:05");
+        });
     }
 
     @Test
     public void convertDate() {
-        assertThat(converting(ATTR_NAME_DATE)).isEqualTo("11.09.2001");
+        assertEventually(() -> {
+            assertThat(converting(ATTR_NAME_DATE)).isEqualTo("11.09.2001");
+        });
     }
 
     @Test
     public void convertBoolean() {
-        assertThat(converting(ATTR_NAME_BOOLEAN)).isEqualTo("true");
+        assertEventually(() -> {
+            assertThat(converting(ATTR_NAME_BOOLEAN)).isEqualTo("true");
+        });
     }
 
 
