4ea48c0e32b90061588e2b84e902101a605329fd
Barbara Palumbo <barbara.palumbo@commercetools.de>
2022-03-29 16:06:23 +0200

    modified a test

--- commercetools-models/src/test/java/io/sphere/sdk/productselections/queries/ProductSelectionByKeyGetIntegrationTest.java
+++ commercetools-models/src/test/java/io/sphere/sdk/productselections/queries/ProductSelectionByKeyGetIntegrationTest.java
@@ -1,10 +1,13 @@
 package io.sphere.sdk.productselections.queries;
 
 import io.sphere.sdk.productselections.ProductSelection;
+import io.sphere.sdk.productselections.commands.ProductSelectionUpdateCommand;
+import io.sphere.sdk.productselections.commands.updateactions.SetKey;
 import io.sphere.sdk.test.IntegrationTest;
 import org.junit.Test;
 
 import static io.sphere.sdk.productselections.ProductSelectionFixtures.withProductSelection;
+import static io.sphere.sdk.productselections.ProductSelectionFixtures.withUpdateableProductSelection;
 import static io.sphere.sdk.test.SphereTestUtils.randomKey;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -12,12 +15,17 @@ public class ProductSelectionByKeyGetIntegrationTest extends IntegrationTest {
 
     @Test
     public void fetchByKeyWithUpdateAction() {
-        withProductSelection(client(), productSelection -> {
-            final String key = randomKey();
+        withUpdateableProductSelection(client(), productSelection -> {
+            final String newKey = randomKey();
+            final ProductSelection updatedProductSelection =
+                    client().executeBlocking(ProductSelectionUpdateCommand.of(productSelection, SetKey.of(newKey)));
+            assertThat(updatedProductSelection).isNotNull();
 
-            final ProductSelection loadedProductSelection = client().executeBlocking(ProductSelectionByKeyGet.of(key));
+            final ProductSelection loadedProductSelection = client().executeBlocking(ProductSelectionByKeyGet.of(newKey));
 
             assertThat(loadedProductSelection).isEqualTo(productSelection);
+
+            return updatedProductSelection;
         });
 
     }
