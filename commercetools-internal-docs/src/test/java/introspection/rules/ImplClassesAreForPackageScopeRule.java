33fbe0c53c600c6da5acc5dee4ee584fd161ca12
Barbara Palumbo <barbara.palumbo@commercetools.de>
2022-03-29 11:29:16 +0200

    missing comma

--- commercetools-internal-docs/src/test/java/introspection/rules/ImplClassesAreForPackageScopeRule.java
+++ commercetools-internal-docs/src/test/java/introspection/rules/ImplClassesAreForPackageScopeRule.java
@@ -9,7 +9,7 @@ public class ImplClassesAreForPackageScopeRule extends ClassStrategyRule {
             asList(("ByIdDeleteCommandImpl,CommandImpl,CreateCommandImpl," +
                     "CustomObjectQueryImpl,ResourceImpl,ResourceQueryModelImpl," +
                     "ResourceViewImpl,GenericMessageImpl,MessageImpl," +
-                    "MetaModelGetDslImpl,MetaModelQueryDslImpl,MetaModelHeadDslImplMetaModelQueryDslImpl,MoneyImpl," +
+                    "MetaModelGetDslImpl,MetaModelQueryDslImpl,MetaModelHeadDslImpl,MetaModelQueryDslImpl,MoneyImpl," +
                     "QueryModelImpl,SearchDslImpl,UpdateCommandDslImpl,SearchModelImpl," +
                     "UpdateActionImpl,MetaModelSearchDslImpl,MetaModelByIdDeleteCommandImpl," +
                     "CustomResourceQueryModelImpl," +
