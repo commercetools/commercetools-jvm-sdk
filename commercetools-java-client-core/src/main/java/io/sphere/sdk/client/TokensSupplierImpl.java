7b9c12c22809381fc39c93d85d044f373252fb7a
Jeremiah Olufayo <32069279+jherey@users.noreply.github.com>
2022-06-09 09:50:52 +0200

    Update commercetools-java-client-core/src/main/java/io/sphere/sdk/client/TokensSupplierImpl.java
    Co-authored-by: Melinda Fekete <melinda.fekete@commercetools.com>

--- commercetools-java-client-core/src/main/java/io/sphere/sdk/client/TokensSupplierImpl.java
+++ commercetools-java-client-core/src/main/java/io/sphere/sdk/client/TokensSupplierImpl.java
@@ -17,7 +17,7 @@ import static java.lang.String.format;
 import static java.util.stream.Collectors.joining;
 
 /**
- * Component that can fetch Composable Commerce access tokens.
+ * Component that can fetch access tokens from Composable Commerce.
  * Does not refresh them,
  */
 final class TokensSupplierImpl extends AutoCloseableService implements TokensSupplier {
