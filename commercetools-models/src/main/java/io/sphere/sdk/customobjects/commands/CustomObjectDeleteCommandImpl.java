9d360860da22ad4ffebf0212b18e9ff059636fcc
Jeremiah Olufayo <32069279+jherey@users.noreply.github.com>
2022-06-09 09:53:07 +0200

    Update commercetools-models/src/main/java/io/sphere/sdk/customobjects/commands/CustomObjectDeleteCommandImpl.java
    Co-authored-by: Melinda Fekete <melinda.fekete@commercetools.com>

--- commercetools-models/src/main/java/io/sphere/sdk/customobjects/commands/CustomObjectDeleteCommandImpl.java
+++ commercetools-models/src/main/java/io/sphere/sdk/customobjects/commands/CustomObjectDeleteCommandImpl.java
@@ -12,7 +12,7 @@ import io.sphere.sdk.json.SphereJsonUtils;
 import static java.lang.String.format;
 
 /**
- * Deletes a custom object in the system.
+ * Deletes a custom object in Composable Commerce.
  *
  *
  */
