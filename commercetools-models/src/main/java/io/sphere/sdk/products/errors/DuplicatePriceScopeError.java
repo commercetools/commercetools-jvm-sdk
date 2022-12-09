7f7d4e9d71676b3658a9d8be9037b7acf935e83a
Jens Schulze <jens.schulze@commercetools.com>
2022-12-09 13:48:25 +0100

    fix conflictingPrice error

--- commercetools-models/src/main/java/io/sphere/sdk/products/errors/DuplicatePriceScopeError.java
+++ commercetools-models/src/main/java/io/sphere/sdk/products/errors/DuplicatePriceScopeError.java
@@ -1,6 +1,7 @@
 package io.sphere.sdk.products.errors;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
+import com.google.common.collect.Lists;
 import io.sphere.sdk.models.errors.SphereError;
 import io.sphere.sdk.products.Price;
 
@@ -13,13 +14,13 @@ public final class DuplicatePriceScopeError extends SphereError {
     private final List<Price> conflictingPrices;
 
     @JsonCreator
-    private DuplicatePriceScopeError(final String message, final List<Price> conflictingPrices) {
+    private DuplicatePriceScopeError(final String message, final List<Price> conflictingPrices, final Price conflictingPrice) {
         super(CODE, message);
-        this.conflictingPrices = conflictingPrices;
+        this.conflictingPrices = conflictingPrices != null ? conflictingPrices : Lists.newArrayList(conflictingPrice);
     }
 
     public static DuplicatePriceScopeError of(final String message, final List<Price> conflictingPrices) {
-        return new DuplicatePriceScopeError(message, conflictingPrices);
+        return new DuplicatePriceScopeError(message, conflictingPrices, null);
     }
 
     public List<Price> getConflictingPrices() {
