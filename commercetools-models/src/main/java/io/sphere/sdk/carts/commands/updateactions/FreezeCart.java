a72d6786be9ed39825b7d02535d7b1436ee6c56c
Jens Schulze <jens.schulze@commercetools.com>
2024-03-01 10:26:11 +0100

    make update actions freeze & unfreeze final

--- commercetools-models/src/main/java/io/sphere/sdk/carts/commands/updateactions/FreezeCart.java
+++ commercetools-models/src/main/java/io/sphere/sdk/carts/commands/updateactions/FreezeCart.java
@@ -3,7 +3,7 @@ package io.sphere.sdk.carts.commands.updateactions;
 import io.sphere.sdk.carts.Cart;
 import io.sphere.sdk.commands.UpdateActionImpl;
 
-public class FreezeCart extends UpdateActionImpl<Cart> {
+public final class FreezeCart extends UpdateActionImpl<Cart> {
     private FreezeCart() {
         super("freezeCart");
     }
--- commercetools-models/src/main/java/io/sphere/sdk/carts/commands/updateactions/UnfreezeCart.java
+++ commercetools-models/src/main/java/io/sphere/sdk/carts/commands/updateactions/UnfreezeCart.java
@@ -3,7 +3,7 @@ package io.sphere.sdk.carts.commands.updateactions;
 import io.sphere.sdk.carts.Cart;
 import io.sphere.sdk.commands.UpdateActionImpl;
 
-public class UnfreezeCart extends UpdateActionImpl<Cart> {
+public final class UnfreezeCart extends UpdateActionImpl<Cart> {
     private UnfreezeCart() {
         super("unfreezeCart");
     }
